9c5181b70b0537e98990774a49715d3dbcd13c0f
Philipe Mulet <pmulet>
2001-09-27 18:36:25 +0000

    Fix for 1GJYFUO

--- org.eclipse.jdt.core/eval/org/eclipse/jdt/internal/eval/CodeSnippetMessageSend.java
+++ org.eclipse.jdt.core/eval/org/eclipse/jdt/internal/eval/CodeSnippetMessageSend.java
@@ -163,8 +163,10 @@ public TypeBinding resolveType(BlockScope scope) {
 		if (argHasError)
 			return null;
 	}
-	if (receiverType == null)
+	if (receiverType == null) {
+		scope.problemReporter().codeSnippetMissingMethod(new String(scope.referenceType().name), new String(selector), parametersAsString(argumentTypes), this.sourceStart, this.sourceEnd);
 		return null;
+	}
 
 	// base type cannot receive any message
 	if (receiverType.isBaseType()) {
@@ -252,5 +254,14 @@ public TypeBinding resolveType(BlockScope scope) {
 		&& !binding.declaringClass.canBeSeenBy(scope))
 		binding = new MethodBinding(binding, (ReferenceBinding) receiverType);
 	return binding.returnType;
+}
+private String parametersAsString(TypeBinding[] params) {
+	StringBuffer buffer = new StringBuffer();
+	for (int i = 0, length = params.length; i < length; i++) {
+		if (i != 0)
+			buffer.append(", "); //$NON-NLS-1$
+		buffer.append(new String(params[i].readableName()));
+	}
+	return buffer.toString();
 }
 }
