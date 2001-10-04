f8065ae134834f9481154dc34f90c12640c5c175
Philipe Mulet <pmulet>
2001-10-04 11:44:10 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/util/ObjectVector.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/util/ObjectVector.java
@@ -6,64 +6,96 @@ package org.eclipse.jdt.internal.compiler.util;
 import org.eclipse.jdt.internal.compiler.*;
 
 public final class ObjectVector {
+	
 	static int INITIAL_SIZE = 10;
-    
+
 	public int size;
 	int maxSize;
 	Object[] elements;
-public ObjectVector() {
-	maxSize = INITIAL_SIZE;
-	size = 0;
-	elements = new Object[maxSize];
-}
-public void add(Object newElement) {
-	if (size == maxSize)    // knows that size starts <= maxSize
-		System.arraycopy(elements, 0, (elements = new Object[maxSize *= 2]), 0, size);
-	elements[size++] = newElement;
-}
-public void addAll(Object[] newElements) {
-	if (size + newElements.length >= maxSize) {
-		maxSize = size + newElements.length;    // assume no more elements will be added
-		System.arraycopy(elements, 0, (elements = new Object[maxSize]), 0, size);
+	
+	public ObjectVector() {
+
+		this.maxSize = INITIAL_SIZE;
+		this.size = 0;
+		this.elements = new Object[this.maxSize];
 	}
-	System.arraycopy(newElements, 0, elements, size, newElements.length);
-	size += newElements.length;
-}
-public boolean contains(Object element) {
-	for (int i = size; --i >= 0;)
-		if (element == elements[i])
-			return true;
-	return false;
-}
-public Object elementAt(int index) {
-	return elements[index];
-}
-public Object find(Object element) {
-	for (int i = size; --i >= 0;)
-		if (element == elements[i])
-			return elements[i];
-	return null;
-}
-public Object remove(Object element) {
-	// assumes only one occurrence of the element exists
-	for (int i = size; --i >= 0;)
-		if (element == elements[i]) {
-			// shift the remaining elements down one spot
-			System.arraycopy(elements, i + 1, elements, i, --size - i);
-			elements[size] = null;
-			return element;
+
+	public void add(Object newElement) {
+
+		if (this.size == this.maxSize) // knows that size starts <= maxSize
+			System.arraycopy(this.elements, 0, (this.elements = new Object[this.maxSize *= 2]), 0, this.size);
+		this.elements[this.size++] = newElement;
+	}
+
+	public void addAll(Object[] newElements) {
+
+		if (this.size + newElements.length >= this.maxSize) {
+			maxSize = this.size + newElements.length; // assume no more elements will be added
+			System.arraycopy(this.elements, 0, (this.elements = new Object[this.maxSize]), 0, this.size);
 		}
-	return null;
-}
-public void removeAll() {
-	for (int i = size; --i >= 0;)
-		elements[i] = null;
-	size = 0;
-}
-public String toString() {
-	String s = ""; //$NON-NLS-1$
-	for (int i = 0; i < size; i++)
-		s += elements[i].toString() + "\n"; //$NON-NLS-1$
-	return s;
-}
+		System.arraycopy(newElements, 0, this.elements, size, newElements.length);
+		this.size += newElements.length;
+	}
+
+	/**
+	 * Identity check
+	 */
+	public boolean contains(Object element) {
+
+		for (int i = this.size; --i >= 0;)
+			if (element == this.elements[i])
+				return true;
+		return false;
+	}
+
+	public void copyInto(Object[] targetArray){
+		
+		System.arraycopy(this.elements, 0, targetArray, 0, this.size);
+	}
+	
+	public Object elementAt(int index) {
+
+		return this.elements[index];
+	}
+
+	public Object find(Object element) {
+
+		for (int i = this.size; --i >= 0;)
+			if (element == this.elements[i])
+				return element;
+		return null;
+	}
+
+	public Object remove(Object element) {
+
+		// assumes only one occurrence of the element exists
+		for (int i = this.size; --i >= 0;)
+			if (element == this.elements[i]) {
+				// shift the remaining elements down one spot
+				System.arraycopy(this.elements, i + 1, this.elements, i, --this.size - i);
+				this.elements[this.size] = null;
+				return element;
+			}
+		return null;
+	}
+
+	public void removeAll() {
+		
+		for (int i = this.size; --i >= 0;)
+			this.elements[i] = null;
+		this.size = 0;
+	}
+
+	public int size(){
+		
+		return this.size;
+	}
+	
+	public String toString() {
+		
+		String s = ""; //$NON-NLS-1$
+		for (int i = 0; i < this.size; i++)
+			s += this.elements[i].toString() + "\n"; //$NON-NLS-1$
+		return s;
+	}
 }
--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/util/SimpleNameVector.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/util/SimpleNameVector.java
@@ -6,59 +6,84 @@ package org.eclipse.jdt.internal.compiler.util;
 import org.eclipse.jdt.internal.compiler.*;
 
 public final class SimpleNameVector {
+	
 	static int INITIAL_SIZE = 10;
-    
+
 	public int size;
 	int maxSize;
 	char[][] elements;
-public SimpleNameVector() {
-	maxSize = INITIAL_SIZE;
-	size = 0;
-	elements = new char[maxSize][];
-}
-public void add(char[] newElement) {
-	if (size == maxSize)    // knows that size starts <= maxSize
-		System.arraycopy(elements, 0, (elements = new char[maxSize *= 2][]), 0, size);
-	elements[size++] = newElement;
-}
-public void addAll(char[][] newElements) {
-	if (size + newElements.length >= maxSize) {
-		maxSize = size + newElements.length;    // assume no more elements will be added
-		System.arraycopy(elements, 0, (elements = new char[maxSize][]), 0, size);
+	
+	public SimpleNameVector() {
+		
+		this.maxSize = INITIAL_SIZE;
+		this.size = 0;
+		this.elements = new char[this.maxSize][];
 	}
-	System.arraycopy(newElements, 0, elements, size, newElements.length);
-	size += newElements.length;
-}
-public boolean contains(char[] element) {
-	for (int i = size; --i >= 0;)
-		if (CharOperation.equals(element, elements[i]))
-			return true;
-	return false;
-}
-public char[] elementAt(int index) {
-	return elements[index];
-}
-public char[] remove(char[] element) {
-	// assumes only one occurrence of the element exists
-	for (int i = size; --i >= 0;)
-		if (element == elements[i]) {
-			// shift the remaining elements down one spot
-			System.arraycopy(elements, i + 1, elements, i, --size - i);
-			elements[size] = null;
-			return element;
+	
+	public void add(char[] newElement) {
+		
+		if (this.size == this.maxSize) // knows that size starts <= maxSize
+			System.arraycopy(this.elements, 0, (this.elements = new char[this.maxSize *= 2][]), 0, this.size);
+		this.elements[size++] = newElement;
+	}
+	
+	public void addAll(char[][] newElements) {
+		
+		if (this.size + newElements.length >= this.maxSize) {
+			this.maxSize = this.size + newElements.length; // assume no more elements will be added
+			System.arraycopy(this.elements, 0, (this.elements = new char[this.maxSize][]), 0, this.size);
 		}
-	return null;
-}
-public void removeAll() {
-	for (int i = size; --i >= 0;)
-		elements[i] = null;
-	size = 0;
-}
-public String toString() {
-	StringBuffer buffer = new StringBuffer();
-	for (int i = 0; i < size; i++) {
-		buffer.append(elements[i]).append("\n"); //$NON-NLS-1$
+		System.arraycopy(newElements, 0, this.elements, this.size, newElements.length);
+		this.size += newElements.length;
 	}
-	return buffer.toString();
-}
+	
+	public void copyInto(Object[] targetArray){
+		
+		System.arraycopy(this.elements, 0, targetArray, 0, this.size);
+	}
+	
+	public boolean contains(char[] element) {
+		
+		for (int i = this.size; --i >= 0;)
+			if (CharOperation.equals(element, this.elements[i]))
+				return true;
+		return false;
+	}
+	
+	public char[] elementAt(int index) {
+		return this.elements[index];
+	}
+	
+	public char[] remove(char[] element) {
+		
+		// assumes only one occurrence of the element exists
+		for (int i = this.size; --i >= 0;)
+			if (element == this.elements[i]) {
+				// shift the remaining elements down one spot
+				System.arraycopy(this.elements, i + 1, this.elements, i, --this.size - i);
+				this.elements[this.size] = null;
+				return element;
+			}
+		return null;
+	}
+	
+	public void removeAll() {
+		
+		for (int i = this.size; --i >= 0;)
+			this.elements[i] = null;
+		this.size = 0;
+	}
+	
+	public int size(){
+		
+		return this.size;
+	}
+	
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < this.size; i++) {
+			buffer.append(this.elements[i]).append("\n"); //$NON-NLS-1$
+		}
+		return buffer.toString();
+	}
 }
