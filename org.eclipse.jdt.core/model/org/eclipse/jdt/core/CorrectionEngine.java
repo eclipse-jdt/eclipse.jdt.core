6129c96fd350dcde0d7f903174774b91492d2736
Philipe Mulet <pmulet>
2002-01-29 09:50:01 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/CorrectionEngine.java
@@ -0,0 +1,322 @@
+package org.eclipse.jdt.core;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.internal.compiler.lookup.ProblemReasons;
+import org.eclipse.jdt.internal.compiler.parser.*;
+import org.eclipse.jdt.internal.compiler.problem.ProblemIrritants;
+import org.eclipse.jdt.internal.compiler.util.CharOperation;
+import org.eclipse.jdt.internal.core.*;
+
+/**
+ * This class is the entry point for source corrections.
+ */
+public class CorrectionEngine implements ProblemIrritants, ProblemReasons {
+	protected int correctionStart;
+	protected int correctionEnd;
+	protected int prefixLength;
+	protected ICompilationUnit unit;
+	protected ICorrectionRequestor requestor;
+	
+	protected static final int CLASSES = 0x00000001;
+	protected static final int INTERFACES = 0x00000002;
+	protected static final int IMPORT = 0x00000004;
+	protected static final int METHOD = 0x00000008;
+	protected static final int FIELD = 0x00000010;
+	protected static final int LOCAL = 0x00000020;
+	
+	protected int filter;
+		
+	/**
+	 * The CorrectionEngine is responsible for computing problem corrections.
+	 *
+	 *  @param setting java.util.Map
+	 *		set of options used to configure the code correction engine.
+	 * 		CURRENTLY THERE IS NO CORRECTION SPECIFIC SETTINGS.
+	 */
+	public CorrectionEngine(Map setting) {
+		
+	}
+	
+	/**
+	 * Performs code correction for the given marker,
+	 * reporting results to the given correction requestor.
+	 * 
+	 * @return void
+	 *      correction results are answered through a requestor.
+	 *
+	 * @exception IllegalArgumentException if <code>requestor</code> is <code>null</code>
+	 * @since 2.0 
+	 */
+	public void computeCorrections(IMarker marker, ICorrectionRequestor requestor) throws JavaModelException {
+		
+		IJavaElement element = JavaCore.create(marker.getResource());
+		
+		if(!(element instanceof ICompilationUnit))
+			return;
+			
+		ICompilationUnit unit = (ICompilationUnit) element;
+		
+		int id = marker.getAttribute(IJavaModelMarker.ID, -1);
+		String[] args = Util.getProblemArgumentsFromMarker(marker.getAttribute(IJavaModelMarker.ARGUMENTS, ""));
+		int start = marker.getAttribute(IMarker.CHAR_START, -1);
+		int end = marker.getAttribute(IMarker.CHAR_END, -1);
+		
+		if(id == -1 || args == null || start == -1 || end == -1)
+			return;
+		
+		computeCorrections(unit, id, start, end, args, requestor);
+	}
+	
+	/**
+	 * Ask the engine to compute a correction for the specified problem
+	 * of the given compilation unit.
+	 *
+	 *  @return void
+	 *      correction results are answered through a requestor.
+	 *
+	 *  @param unit org.eclipse.jdt.internal.core.ICompilationUnit
+	 *      the compilation unit.
+	 *  
+	 * 	@param id int
+	 * 		the id of the problem.
+	 * 
+	 * 	@param start int
+	 * 		a position in the source where the error begin.
+	 *
+	 *  @param end int
+	 *      a position in the source where the error finish. 
+	 * 
+	 * 	@param arguments String[]
+	 * 		arguments of the problem.
+	 * 
+	 * @exception IllegalArgumentException if <code>requestor</code> is <code>null</code>
+	 * @since 2.0
+	 */
+	public void computeCorrections(ICompilationUnit unit, int id, int start, int end, String[] arguments, ICorrectionRequestor requestor) throws JavaModelException{
+		if (requestor == null) {
+			throw new IllegalArgumentException(Util.bind("correction.nullRequestor")); //$NON-NLS-1$
+		}
+		
+		this.requestor = requestor;
+		this.correctionStart = start;
+		this.correctionEnd = end;
+		this.unit = unit;
+		
+		String argument = null;
+		try {
+			switch (id) {
+				// Type correction
+				case FieldTypeProblemBase + NotFound :
+				case ArgumentProblemBase + NotFound :
+					filter = CLASSES | INTERFACES;
+					argument = arguments[2];
+					break;
+				case InvalidSuperclassBase + NotFound :
+					filter = CLASSES;
+					argument = arguments[0];
+					break;
+				case InvalidInterfaceBase + NotFound :
+					filter = INTERFACES;
+					argument = arguments[0];
+					break;
+				case ExceptionTypeProblemBase + NotFound :
+					filter = CLASSES;
+					argument = arguments[1];
+					break;
+				case ReturnTypeProblemBase + NotFound :
+					filter = CLASSES | INTERFACES;
+					argument = arguments[1];
+					break;
+				case ImportProblemBase + NotFound :
+					filter = IMPORT;
+					argument = arguments[0];
+					break;
+				case UndefinedType :
+					filter = CLASSES | INTERFACES;
+					argument = arguments[0];
+					break;
+					
+				// Method correction
+				case UndefinedMethod :
+					filter = METHOD;
+					argument = arguments[1];
+					break;
+					
+				// Field and local variable correction
+				case UndefinedField :
+					filter = FIELD;
+					argument = arguments[0];
+					break;
+				case UndefinedName :
+					filter = FIELD | LOCAL;
+					argument = arguments[0];
+					break;
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return;
+		}
+		if(argument != null) {
+			correct(argument.toCharArray());
+		}
+	}
+
+	private void correct(char[] argument) throws JavaModelException {
+		try {
+			String source = unit.getSource();
+			Scanner scanner = new Scanner();
+			scanner.setSourceBuffer(source.toCharArray());
+			
+			scanner.resetTo(correctionStart, correctionEnd);
+			int token = 0;
+			char[] argumentSource = new char[0];
+			
+			// search last segment position
+			while(true) {
+				token = scanner.getNextToken();
+				if (token == TerminalSymbols.TokenNameEOF) return;
+				
+				char[] tokenSource = scanner.getCurrentTokenSource();
+				
+				argumentSource = CharOperation.concat(argumentSource, tokenSource);
+				if(!CharOperation.startsWith(argument, argumentSource))
+					return;
+				
+				if(CharOperation.equals(argument, argumentSource)) {
+					correctionStart = scanner.startPosition;
+					correctionEnd = scanner.currentPosition;
+					prefixLength = CharOperation.lastIndexOf('.', argument) + 1;
+					break;
+				}
+				
+			}
+		
+			// search completion position
+			int completionPosition = correctionStart;
+			scanner.resetTo(completionPosition, correctionEnd);
+			int position = completionPosition;
+			
+			for (int i = 0; i < 4; i++) {
+				if(scanner.getNextCharAsJavaIdentifierPart()) {
+					completionPosition = position;
+					position = scanner.currentPosition;
+				} else {
+					break;
+				}
+			}
+			
+			unit.codeComplete(
+				completionPosition,
+				completionRequestor
+			);
+		} catch (JavaModelException e) {
+			return;
+		} catch (InvalidInputException e) {
+			return;
+		}
+	}
+
+	protected ICompletionRequestor completionRequestor = new ICompletionRequestor() {
+		public void acceptAnonymousType(char[] superTypePackageName,char[] superTypeName,char[][] parameterPackageNames,char[][] parameterTypeNames,char[][] parameterNames,char[] completionName,int modifiers,int completionStart,int completionEnd) {}
+		public void acceptClass(char[] packageName,char[] className,char[] completionName,int modifiers,int completionStart,int completionEnd) {
+			if((filter & (CLASSES | INTERFACES)) != 0) {
+				requestor.acceptClass(
+					packageName,
+					className,
+					CharOperation.subarray(completionName, prefixLength, completionName.length),
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			} else if((filter & IMPORT) != 0) {
+				char[] fullName = CharOperation.concat(packageName, className, '.');
+				requestor.acceptClass(
+					packageName,
+					className,
+					CharOperation.subarray(fullName, prefixLength, fullName.length),
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			}
+		}
+		public void acceptError(IMarker marker) {}
+		public void acceptField(char[] declaringTypePackageName,char[] declaringTypeName,char[] name,char[] typePackageName,char[] typeName,char[] completionName,int modifiers,int completionStart,int completionEnd) {
+			if((filter & FIELD) != 0) {
+				requestor.acceptField(
+					declaringTypePackageName,
+					declaringTypeName,
+					name,
+					typePackageName,
+					typeName,
+					name,
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			}
+		}
+		public void acceptInterface(char[] packageName,char[] interfaceName,char[] completionName,int modifiers,int completionStart,int completionEnd) {
+			if((filter & (CLASSES | INTERFACES)) != 0) {
+				requestor.acceptInterface(
+					packageName,
+					interfaceName,
+					CharOperation.subarray(completionName, prefixLength, completionName.length),
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			} else if((filter & IMPORT) != 0) {
+				char[] fullName = CharOperation.concat(packageName, interfaceName, '.');
+				requestor.acceptInterface(
+					packageName,
+					interfaceName,
+					CharOperation.subarray(fullName, prefixLength, fullName.length),
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			}
+		}
+		public void acceptKeyword(char[] keywordName,int completionStart,int completionEnd) {}
+		public void acceptLabel(char[] labelName,int completionStart,int completionEnd) {}
+		public void acceptLocalVariable(char[] name,char[] typePackageName,char[] typeName,int modifiers,int completionStart,int completionEnd) {
+			if((filter & LOCAL) != 0) {
+				requestor.acceptLocalVariable(
+					name,
+					typePackageName,
+					typeName,
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			}
+		}
+		public void acceptMethod(char[] declaringTypePackageName,char[] declaringTypeName,char[] selector,char[][] parameterPackageNames,char[][] parameterTypeNames,char[][] parameterNames,char[] returnTypePackageName,char[] returnTypeName,char[] completionName,int modifiers,int completionStart,int completionEnd) {
+			if((filter & METHOD) != 0) {
+				requestor.acceptMethod(
+					declaringTypePackageName,
+					declaringTypeName,
+					selector,
+					parameterPackageNames,
+					parameterTypeNames,
+					parameterNames,
+					returnTypePackageName,
+					returnTypeName,
+					selector,
+					modifiers,
+					correctionStart,
+					correctionEnd);
+			}
+		}
+		public void acceptMethodDeclaration(char[] declaringTypePackageName,char[] declaringTypeName,char[] selector,char[][] parameterPackageNames,char[][] parameterTypeNames,char[][] parameterNames,char[] returnTypePackageName,char[] returnTypeName,char[] completionName,int modifiers,int completionStart,int completionEnd) {}
+		public void acceptModifier(char[] modifierName,int completionStart,int completionEnd) {}
+		public void acceptPackage(char[] packageName,char[] completionName,int completionStart,int completionEnd) {
+			if((filter & (CLASSES | INTERFACES | IMPORT)) != 0) {
+				requestor.acceptPackage(
+					packageName,
+					CharOperation.subarray(packageName, prefixLength, packageName.length),
+					correctionStart,
+					correctionEnd);
+			}
+		}
+		public void acceptType(char[] packageName,char[] typeName,char[] completionName,int completionStart,int completionEnd) {}
+		public void acceptVariableName(char[] typePackageName,char[] typeName,char[] name,char[] completionName,int completionStart,int completionEnd) {}
+	};
+}
---
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/ICorrectionRequestor.java
@@ -0,0 +1,176 @@
+package org.eclipse.jdt.core;
+
+/**
+ * A callback interface for receiving java problem correction.
+ */
+public interface ICorrectionRequestor {
+/**
+ * Notification of a class correction.
+ * 
+ * @return void - Nothing is answered back to correction engine
+ *
+ * @param packageName char[] - Declaring package name of the class.
+ * @param className char[] - Name of the class.
+ * @param correctionName char[] - The correction for the class.
+ * @param modifiers int - The modifiers of the class.
+ * @param correctionStart int - The start position of insertion of the correction of the class.
+ * @param correctionEnd int - The end position of insertion of the correction of the class.
+ *
+ * NOTE - All package and type names are presented in their readable form:
+ *    Package names are in the form "a.b.c".
+ *    Nested type names are in the qualified form "A.M".
+ *    The default package is represented by an empty array.
+ */
+void acceptClass(
+	char[] packageName,
+	char[] className,
+	char[] correctionName,
+	int modifiers,
+	int correctionStart,
+	int correctionEnd);
+/**
+ * Notification of a field correction.
+ *
+ * @return void - Nothing is answered back to correction engine
+ *
+ * @param declaringTypePackageName char[] - Name of the package in which the type that contains this field is declared.
+ * @param declaringTypeName char[] - Name of the type declaring this field.
+ * @param name char[] - Name of the field.
+ * @param typePackageName char[] - Name of the package in which the type of this field is declared.
+ * @param typeName char[] - Name of the type of this field.
+ * @param correctionName char[] - The correction for the field.
+ * @param modifiers int - The modifiers of this field.
+ * @param correctionStart int - The start position of insertion of the correction of this field.
+ * @param correctionEnd int - The end position of insertion of the correction of this field.
+ *
+ * NOTE - All package and type names are presented in their readable form:
+ *    Package names are in the form "a.b.c".
+ *    Base types are in the form "int" or "boolean".
+ *    Array types are in the qualified form "M[]" or "int[]".
+ *    Nested type names are in the qualified form "A.M".
+ *    The default package is represented by an empty array.
+ */
+void acceptField(
+	char[] declaringTypePackageName,
+	char[] declaringTypeName,
+	char[] name,
+	char[] typePackageName,
+	char[] typeName,
+	char[] correctionName,
+	int modifiers,
+	int correctionStart,
+	int correctionEnd);
+/**
+ * Notification of an interface correction.
+ *
+ * @return void - Nothing is answered back to correction engine
+ *
+ * @param packageName char[] - Declaring package name of the interface.
+ * @param className char[] - Name of the interface.
+ * @param correctionName char[] - The correction for the interface.
+ *   Can include ';' for imported interfaces.
+ * @param modifiers int - The modifiers of the interface.
+ * @param correctionStart int - The start position of insertion of the correction of the interface.
+ * @param correctionEnd int - The end position of insertion of the correction of the interface.
+ *
+ * NOTE - All package and type names are presented in their readable form:
+ *    Package names are in the form "a.b.c".
+ *    Nested type names are in the qualified form "A.M".
+ *    The default package is represented by an empty array.
+ */
+void acceptInterface(
+	char[] packageName,
+	char[] interfaceName,
+	char[] correctionName,
+	int modifiers,
+	int correctionStart,
+	int correctionEnd);
+/**
+ * Notification of a local variable correction.
+ *
+ * @return void - Nothing is answered back to correction engine
+ *
+ * @param name char[] - Name of the local variable.
+ * @param typePackageName char[] - Name of the package in which the type of this local variable is declared.
+ * @param typeName char[] - Name of the type of this local variable.
+ * @param modifiers int - The modifiers of this local variable.
+ * @param correctionStart int - The start position of insertion of the correction of this local variable.
+ * @param correctionEnd int - The end position of insertion of the correction of this local variable.
+ *
+ * NOTE - All package and type names are presented in their readable form:
+ *    Package names are in the form "a.b.c".
+ *    Base types are in the form "int" or "boolean".
+ *    Array types are in the qualified form "M[]" or "int[]".
+ *    Nested type names are in the qualified form "A.M".
+ *    The default package is represented by an empty array.
+ */
+void acceptLocalVariable(
+	char[] name,
+	char[] typePackageName,
+	char[] typeName,
+	int modifiers,
+	int correctionStart,
+	int correctionEnd);
+/**
+ * Notification of a method correction.
+ *
+ * @return void - Nothing is answered back to correction engine
+ *
+ * @param declaringTypePackageName char[] - Name of the package in which the type that contains this method is declared.
+ * @param declaringTypeName char[] - Name of the type declaring this method.
+ * @param selector char[] - Name of the method.
+ * @param parameterPackageNames char[][] -  Names of the packages in which the parameter types are declared.
+ *    Should contain as many elements as parameterTypeNames.
+ * @param parameterTypeNames char[][] - Names of the parameters types.
+ *    Should contain as many elements as parameterPackageNames.
+ * @param returnTypePackageName char[] - Name of the package in which the return type is declared.
+ * @param returnTypeName char[] - Name of the return type of this method, should be <code>null</code> for a constructor.
+ * @param correctionName char[] - The correction for the method.
+ *   Can include zero, one or two brackets. If the closing bracket is included, then the cursor should be placed before it.
+ * @param modifiers int - The modifiers of this method.
+ * @param correctionStart int - The start position of insertion of the correction of this method.
+ * @param correctionEnd int - The end position of insertion of the correction of this method.
+ *
+ * NOTE - All package and type names are presented in their readable form:
+ *    Package names are in the form "a.b.c".
+ *    Base types are in the form "int" or "boolean".
+ *    Array types are in the qualified form "M[]" or "int[]".
+ *    Nested type names are in the qualified form "A.M".
+ *    The default package is represented by an empty array.
+ *
+ * NOTE: parameter names can be retrieved from the source model after the user selects a specific method.
+ */
+void acceptMethod(
+	char[] declaringTypePackageName,
+	char[] declaringTypeName,
+	char[] selector,
+	char[][] parameterPackageNames,
+	char[][] parameterTypeNames,
+	char[][] parameterNames,
+	char[] returnTypePackageName,
+	char[] returnTypeName,
+	char[] correctionName,
+	int modifiers,
+	int correctionStart,
+	int correctionEnd);
+/**
+ * Notification of a package correction.
+ *
+ * @return void - Nothing is answered back to correction engine
+ *
+ * @param packageName char[] - The package name.
+ * @param correctionName char[] - The correction for the package.
+ *   Can include '.*;' for imports.
+ * @param correctionStart int - The start position of insertion of the correction of this package.
+ * @param correctionEnd int - The end position of insertion of the correction of this package.
+ *
+ * NOTE - All package names are presented in their readable form:
+ *    Package names are in the form "a.b.c".
+ *    The default package is represented by an empty array.
+ */
+void acceptPackage(
+	char[] packageName,
+	char[] correctionName,
+	int correctionStart,
+	int correctionEnd);
+}
