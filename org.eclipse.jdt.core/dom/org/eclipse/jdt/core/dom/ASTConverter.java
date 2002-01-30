81386e0f544c99151266d6f5759fbfdbbf143f1d
Jim Desrivieres <jdesrivieres>
2002-01-30 16:28:49 +0000

    Fix up copyright notices

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2001 IBM Corporation and others.
+ * Copyright (c) 2001 International Business Machines Corp. and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v0.5 
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v05.html
  * 
  * Contributors:
- *     IBM Corporation - initial test suite for AST API
+ *     IBM Corporation - initial API and implementation
  ******************************************************************************/
 
 package org.eclipse.jdt.core.dom;
--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Type.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Type.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001 IBM Corporation and others.
+ * Copyright (c) 2001 International Business Machines Corp. and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v0.5 
  * which accompanies this distribution, and is available at
