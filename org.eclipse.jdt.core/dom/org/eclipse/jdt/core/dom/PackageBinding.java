66b7e8e91fa4046b559362ff39b853f7da795c35
Jim Desrivieres <jdesrivieres>
2002-01-30 15:53:11 +0000

    Add missing copyright notices

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/PackageBinding.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/PackageBinding.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2002 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v0.5 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.jdt.core.dom;
 
 import java.util.ArrayList;
