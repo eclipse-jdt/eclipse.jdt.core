f9dd349e81c38b4d7aa04d14d028c8527117ce48
Philipe Mulet <pmulet>
2002-02-13 12:39:29 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/JavaModelOperation.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/JavaModelOperation.java
@@ -126,7 +126,11 @@ protected void addDelta(IJavaElementDelta delta) {
  * @see IProgressMonitor
  */
 public void beginTask(String name, int totalWork) {
-	if (fMonitor != null) {
+	if (fMonitor == null) return;
+	if (fMonitor instanceof SubProgressMonitor){
+		fMonitor.beginTask("", totalWork); //$NON-NLS-1$
+		fMonitor.subTask(name);
+	} else {
 		fMonitor.beginTask(name, totalWork);
 	}
 }
