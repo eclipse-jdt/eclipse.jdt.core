1688f525154d78475b57ab205626c955a0060844
Philipe Mulet <pmulet>
2002-04-24 11:40:35 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/IClasspathEntry.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/IClasspathEntry.java
@@ -6,13 +6,14 @@ package org.eclipse.jdt.core;
  */
 import org.eclipse.core.runtime.IPath;
 
-/** TOFIX
+/** 
  * An entry on a Java project classpath identifying one or more package fragment
  * roots. A classpath entry has a content kind (either source, 
  * <code>K_SOURCE</code>, or binary, <code>K_BINARY</code>) which is inherited
  * by each package fragment root and package fragment associated with the entry.
  * <p>
  * A classpath entry can refer to any of the following:<ul>
+ * 
  *	<li>Source code in the current project. In this case, the entry identifies a
  *		root folder in the current project containing package fragments and
  *		<code>.java</code> source files. The root folder itself represents a default
@@ -21,12 +22,14 @@ import org.eclipse.core.runtime.IPath;
  * 		the project is built. The classpath entry must specify the
  *		absolute path to the root folder. Entries of this kind are 
  *		associated with the <code>CPE_SOURCE</code> constant.</li>
+ * 
  *	<li>A binary library in the current project, in another project, or in the external
  *		file system. In this case the entry identifies a JAR (or root folder) containing
  *		package fragments and <code>.class</code> files.  The classpath entry
  *		must specify the absolute path to the JAR (or root folder), and in case it refers
  *		to an external JAR, then there is no associated resource in the workbench. Entries 
  *		of this kind are associated with the <code>CPE_LIBRARY</code> constant.</li>
+ * 
  *	<li>A required project. In this case the entry identifies another project in
  *		the workspace. The required project is used as a binary library when compiling
  *		(that is, the builder looks in the output location of the required project
@@ -42,16 +45,35 @@ import org.eclipse.core.runtime.IPath;
  *     code or associated <code>.class</code> files located in its output location. 
  *     It will also automatically include any other libraries or projects that the required project's classpath 
  *     refers to, iff the corresponding classpath entries are tagged as being exported 
- *     (<code>IClasspathEntry.isExported()</code>). 
+ *     (<code>IClasspathEntry#isExported</code>). 
  *    Unless exporting some classpath entries, classpaths are not chained by default - 
  *    each project must specify its own classpath in its entirety.</li>
+ * 
  *  <li> A path beginning in a classpath variable defined globally to the workspace.
  *		Entries of this kind are  associated with the <code>CPE_VARIABLE</code> constant.  
- *      These entries are created using <code>JavaCore.setClasspathVariable</code>,
+ *      Classpath variables are created using <code>JavaCore#setClasspathVariable</code>,
  * 		and gets resolved, to either a project or library entry, using
- *      <code>JavaCore.getResolvedClasspathVariable</code>.</li>
+ *      <code>JavaCore#getResolvedClasspathVariable</code>.
+ *		It is also possible to register an automatic initializer (<code>ClasspathVariableInitializer</code>)
+ * 	which will be invoked through the extension point "org.eclipse.jdt.core.classpathVariableInitializer".
+ * 	After resolution, a classpath variable entry may either correspond to a project or a library entry. </li>
+ * 
+ *  <li> A named classpath container, which can be resolved into a set of classpath entries. 
+ *		Entries of this kind are  associated with the <code>CPE_CONTAINER</code> constant.  
+ * 	The path associated with this entry is used to identify a container which will exclusively be
+ * 	resolved by a <code>ClasspathContainerResolver</code> registered as an extension to the
+ * 	extension point "org.eclipse.jdt.core.ClasspathContainerResolver").
+ * 	A classpath container entry can be resolved explicitly using <code>JavaCore#getResolvedClasspathContainer</code>
+ * 	and the resulting entries can contain any non-container entry. In particular, it may contain variable entries which
+ * 	in turn needs to be resolved before being directly used. 
+ * 	<br> Also note that the container resolution APIs include an IJavaProject argument, so as to allow the same
+ * 	container path to be interpreted in different ways for different projects. </li>
  * </ul>
  * </p>
+ * The result of <code>IJavaProject#getResolvedClasspath</code> will have all entries of type
+ * <code>CPE_VARIABLE</code> and <code>CPE_CONTAINER</code> resolved to a set of 
+ * <code>CPE_SOURCE</code>, <code>CPE_LIBRARY</code> or <code>CPE_PROJECT</code>
+ * classpath entries.
  * <p>
  * Any classpath entry other than a source folder (kind <code>CPE_SOURCE</code>) can
  * be marked as being exported. Exported entries are automatically contributed to
@@ -68,6 +90,8 @@ import org.eclipse.core.runtime.IPath;
  * @see JavaCore#newSourceEntry
  * @see JavaCore#newVariableEntry
  * @see JavaCore#newContainerEntry
+ * @see org.eclipse.jdt.core.ClasspathVariableInitializer
+ * @see org.eclipse.jdt.core.ClasspathContainerResolver
  */
 public interface IClasspathEntry {
 
@@ -152,6 +176,9 @@ public interface IClasspathEntry {
 	 *		is bound to the path <it>P</it>, the path of the corresponding classpath entry
 	 *		is computed by appending to <it>P</it> the segments of the returned
 	 *		path without the variable.</li>
+	 *  <li> A container entry (<code>CPE_CONTAINER</code>) - the path of the entry
+	 * 	is the name of the classpath container, which can be bound to a set of classpath entries
+	 * 	after resolution. </li>
 	 * </ul>
 	 *
 	 * @return the path of this classpath entry
@@ -212,7 +239,8 @@ public interface IClasspathEntry {
 	 * <p>
 	 * @return the resolved library or project classpath entry, or <code>null</code>
 	 *   if the given path could not be resolved to a classpath entry
-	 *
+	 *	<p> 
+	 * Note that this deprecated API doesn't handle CPE_CONTAINER entries.
 	 * @deprecated - use JavaCore.getResolvedClasspathEntry(...)
 	 */
 	IClasspathEntry getResolvedEntry();	
