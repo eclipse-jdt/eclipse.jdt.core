1f830f83543e72e48c068d4c78355528eb9c7c75
David Audel <daudel>
2002-04-29 11:36:51 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/complete/CompletionOnInterfaceReference.java
+++ org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/complete/CompletionOnInterfaceReference.java
@@ -1,11 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2001 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v0.5 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.jdt.internal.codeassist.complete;
 
-/**
- * @author davida
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- */
 public class CompletionOnInterfaceReference extends CompletionOnSingleTypeReference {
 	public CompletionOnInterfaceReference(char[] source, long pos) {
 		super(source, pos);
