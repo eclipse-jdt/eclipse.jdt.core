56e0fc7aba6871cd475a06affdeeb59f64adf83b
David Audel <daudel>
2002-04-29 11:56:49 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/complete/CompletionOnClassReference.java
+++ org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/complete/CompletionOnClassReference.java
@@ -1,11 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2001 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v0.5 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ * 
+ ******************************************************************************/
+
 package org.eclipse.jdt.internal.codeassist.complete;
 
-/**
- * @author davida
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- */
 public class CompletionOnClassReference extends CompletionOnSingleTypeReference {
 	public CompletionOnClassReference(char[] source, long pos) {
 		super(source, pos);
