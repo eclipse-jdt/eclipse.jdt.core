576bc951250fb31c065cc929ea46367bf85adc79
Philipe Mulet <pmulet>
2002-05-03 09:21:13 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/IInitializer.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/IInitializer.java
@@ -1,10 +1,15 @@
+/**********************************************************************
+Copyright (c) 2000, 2001, 2002 IBM Corp. and others.
+All rights reserved.   This program and the accompanying materials
+are made available under the terms of the Common Public License v0.5
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v05.html
+ 
+Contributors:
+     IBM Corporation - initial API and implementation
+**********************************************************************/
 package org.eclipse.jdt.core;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
- 
 /**
  * Represents a stand-alone instance or class (static) initializer in a type.
  * <p>
