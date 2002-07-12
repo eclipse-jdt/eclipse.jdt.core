f5467ccbf9d5f2241ae993b8f25a994e438951ed
Olivier Thomann <othomann>
2002-07-12 19:51:43 +0000

    Change static fields to be constant fields

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Modifier.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Modifier.java
@@ -42,73 +42,73 @@ public final class Modifier {
 	/**
 	 * Modifier constant (bit mask, value 0) indicating no modifiers.
 	 */
-	public static int NONE = 0x0000;
+	public static final int NONE = 0x0000;
 
 	/**
 	 * "public" modifier constant (bit mask).
 	 * Applicable to types, methods, constructors, and fields.
 	 */
-	public static int PUBLIC = 0x0001;
+	public static final int PUBLIC = 0x0001;
 
 	/**
 	 * "private" modifier constant (bit mask).
 	 * Applicable to types, methods, constructors, and fields.
 	 */
-	public static int PRIVATE = 0x0002;
+	public static final int PRIVATE = 0x0002;
 
 	/**
 	 * "protected" modifier constant (bit mask).
 	 * Applicable to types, methods, constructors, and fields.
 	 */
-	public static int PROTECTED = 0x0004;
+	public static final int PROTECTED = 0x0004;
 
 	/**
 	 * "static" modifier constant (bit mask).
 	 * Applicable to types, methods, fields, and initializers.
 	 */
-	public static int STATIC = 0x0008;
+	public static final int STATIC = 0x0008;
 
 	/**
 	 * "final" modifier constant (bit mask).
 	 * Applicable to types, methods, fields, and variables.
 	 */
-	public static int FINAL = 0x0010;
+	public static final int FINAL = 0x0010;
 
 	/**
 	 * "synchronized" modifier constant (bit mask).
 	 * Applicable only to methods.
 	 */
-	public static int SYNCHRONIZED = 0x0020;
+	public static final int SYNCHRONIZED = 0x0020;
 
 	/**
 	 * "volatile" modifier constant (bit mask).
 	 * Applicable only to fields.
 	 */
-	public static int VOLATILE = 0x0040;
+	public static final int VOLATILE = 0x0040;
 
 	/**
 	 * "transient" modifier constant (bit mask).
 	 * Applicable only to fields.
 	 */
-	public static int TRANSIENT = 0x0080;
+	public static final int TRANSIENT = 0x0080;
 
 	/**
 	 * "native" modifier constant (bit mask).
 	 * Applicable only to methods.
 	 */
-	public static int NATIVE = 0x0100;
+	public static final int NATIVE = 0x0100;
 
 	/**
 	 * "abstract" modifier constant (bit mask).
 	 * Applicable to types and methods.
 	 */
-	public static int ABSTRACT = 0x0400;
+	public static final int ABSTRACT = 0x0400;
 
 	/**
 	 * "strictfp" modifier constant (bit mask).
 	 * Applicable to types and methods.
 	 */
-	public static int STRICTFP = 0x0800;
+	public static final int STRICTFP = 0x0800;
 
 	/**
 	 * Returns whether the given flags includes the "public" modifier.
