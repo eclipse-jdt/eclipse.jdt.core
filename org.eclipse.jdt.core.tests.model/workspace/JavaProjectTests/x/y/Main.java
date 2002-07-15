4d9121ed2a04922a42c094ba9b1c4c45752bb46c
Philipe Mulet <pmulet>
2002-07-15 15:12:37 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.core.tests.model/workspace/JavaProjectTests/x/y/Main.java
@@ -0,0 +1,11 @@
+package x.y;
+
+import java.io.Serializable;
+/**
+ * A class that uses the minimal library.
+ */
+public class Main {
+	public static void main(String[] args) {
+		new Main().toString();
+	}
+}
