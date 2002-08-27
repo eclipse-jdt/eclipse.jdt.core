aeb77fd627d833f0418f83403fad03a9bbc6965f
Jerome lanneluc <jlanneluc>
2002-08-27 16:58:43 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/parser/TestAll.java
@@ -0,0 +1,71 @@
+package org.eclipse.jdt.core.tests.compiler.parser;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.jdt.core.tests.junit.extension.ExcludedTestSuite;
+
+/**
+ * Run all parser regression tests
+ */
+public class TestAll extends TestCase {
+	public static boolean EXPERT; // an expert doesn't exclude any tests
+	public static String[] EXCLUDED_TESTS = new String[] {
+		"CompletionParserTest", "testVB_2", // completion on field access on anonymous inner class with syntax error
+		"CompletionParserTest", "testVB_4", // completion on field access on anonymous inner class with syntax error
+		"CompletionParserTest", "testVB_5", // completion on field access on anonymous inner class with syntax error
+		"LabelStatementCompletionTest", "testInLabeledInnerClassWithErrorBefore", // cannot get labels in recovery mode yet
+	};
+/**
+ * TestAll constructor comment.
+ * @param testName java.lang.String
+ */
+public TestAll(String testName) {
+	super(testName);
+}
+/**
+ * Adds all the tests in the given class to the suite except
+ * the ones that are excluded.
+ */
+public static void addTest(TestSuite suite, Class testClass) {
+	TestSuite innerSuite = new TestSuite(testClass);
+	suite.addTest(innerSuite);
+}
+public static Test suite() {
+	TestSuite suite = new TestSuite(TestAll.class.getName());
+	
+	/* completion tests */
+	addTest(suite, AllocationExpressionCompletionTest.class);
+	addTest(suite, ClassLiteralAccessCompletionTest.class);
+	addTest(suite, CompletionParserTest.class);
+	addTest(suite, CompletionRecoveryTest.class);
+	addTest(suite, DietCompletionTest.class);
+	addTest(suite, ExplicitConstructorInvocationCompletionTest.class);
+	addTest(suite, FieldAccessCompletionTest.class);
+	addTest(suite, InnerTypeCompletionTest.class);
+	addTest(suite, LabelStatementCompletionTest.class);
+	addTest(suite, MethodInvocationCompletionTest.class);
+	addTest(suite, NameReferenceCompletionTest.class);
+	addTest(suite, ReferenceTypeCompletionTest.class);
+
+	/* selection tests */
+	addTest(suite, ExplicitConstructorInvocationSelectionTest.class);
+	addTest(suite, SelectionTest.class);
+
+	/* recovery tests */
+	addTest(suite, DietRecoveryTest.class);
+
+	/* source element parser tests */
+	addTest(suite, SourceElementParserTest.class);
+
+	/* syntax error diagnosis tests */
+	addTest(suite, SyntaxErrorTest.class);
+
+	if (EXPERT) {
+		return suite;
+	} else {
+		return new ExcludedTestSuite(suite, EXCLUDED_TESTS);
+	}
+}
+}
---
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/junit/extension/ExcludedTestSuite.java
@@ -0,0 +1,46 @@
+package org.eclipse.jdt.core.tests.junit.extension;
+
+import junit.framework.*;
+public class ExcludedTestSuite extends TestSuite {
+	String[] excludedTests = null;
+/**
+ * Creates a new test suite by excluding the given tests
+ * from the given test suite.
+ */
+public ExcludedTestSuite(Test suite, String[] excludedTests) {
+	this.excludedTests = excludedTests;
+	this.addTest(suite);
+}
+/**
+ * Adds a test to the suite.
+ */
+public void addTest(Test test) {
+	if (test instanceof TestCase) {
+		if (!isExcluded((TestCase)test)) {
+			super.addTest(test);
+		}
+	} else if (test instanceof TestSuite) {
+		java.util.Enumeration enum = ((TestSuite)test).tests();
+		while (enum.hasMoreElements()) {
+			this.addTest((Test)enum.nextElement());
+		}
+	}
+}
+/**
+ * Returns whether the test is excluded.
+ */
+public boolean isExcluded(TestCase test) {
+	if (this.excludedTests == null) {
+		return false;
+	}
+	for (int i = 0; i < this.excludedTests.length; i += 2) {
+		String className = this.excludedTests[i];
+		String methodName = this.excludedTests[i+1];
+		if (test.getName().equals(methodName) &&
+			test.getClass().getName().endsWith(className)) {
+				return true;
+		}
+	}
+	return false;
+}
+}
