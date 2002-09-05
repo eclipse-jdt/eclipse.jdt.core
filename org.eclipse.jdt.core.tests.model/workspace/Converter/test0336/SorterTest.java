9a76fb8f9434ae4b71dc267e686b050759ff3536
Olivier Thomann <othomann>
2002-09-05 15:29:10 +0000

    Updates tests

--- org.eclipse.jdt.core.tests.model/workspace/Converter/test0336/SorterTest.java
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/test0336/SorterTest.java
@@ -4,5 +4,5 @@ import junit.runner.Sorter;
 
 public class SorterTest  {
 	static class Swapper implements Sorter.Swapper {
-        }
+    }
 }
---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/test0385/A.java
@@ -0,0 +1,8 @@
+package test0385;
+
+public class A {
+
+	int getClass() {
+		return 0;
+	}
+}
