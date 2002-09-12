fcc2548fbc73bcb0b67a679db4f7cd599859707d
Jim Desrivieres <jdesrivieres>
2002-09-12 16:08:40 +0000

    Correct doc - these are statements not expressions

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ConstructorInvocation.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ConstructorInvocation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001 International Business Machines Corp. and others.
+ * Copyright (c) 2001, 2002 International Business Machines Corp. and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v0.5 
  * which accompanies this distribution, and is available at
@@ -14,11 +14,11 @@ package org.eclipse.jdt.core.dom;
 import java.util.List;
 
 /**
- * Alternate constructor invocation expression AST node type.
+ * Alternate constructor invocation statement AST node type.
  *
  * <pre>
  * ConstructorInvocation:
- *		<b>this</b> <b>(</b> [ Expression { <b>,</b> Expression } ] <b>)</b>
+ *		<b>this</b> <b>(</b> [ Expression { <b>,</b> Expression } ] <b>)</b> <b>;</b>
  * </pre>
  * 
  * @since 2.0
--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/SuperConstructorInvocation.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/SuperConstructorInvocation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001 International Business Machines Corp. and others.
+ * Copyright (c) 2001, 2002 International Business Machines Corp. and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v0.5 
  * which accompanies this distribution, and is available at
@@ -14,12 +14,12 @@ package org.eclipse.jdt.core.dom;
 import java.util.List;
 
 /**
- * Super constructor invocation expression AST node type.
+ * Super constructor invocation statement AST node type.
  *
  * <pre>
  * SuperConstructorInvocation:
  *		[ Expression <b>.</b> ] <b>super</b>
- * 				<b>(</b> [ Expression { <b>,</b> Expression } ] <b>)</b>
+ * 				<b>(</b> [ Expression { <b>,</b> Expression } ] <b>)</b> <b>;</b>
  * </pre>
  * 
  * @since 2.0
