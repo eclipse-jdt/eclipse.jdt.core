52798549d1160e53aae5ff98d41f70747e6d5052
David Audel <daudel>
2002-09-23 13:11:23 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.builder/src/org/eclipse/jdt/core/tests/builder/BasicBuildTests.java
+++ org.eclipse.jdt.core.tests.builder/src/org/eclipse/jdt/core/tests/builder/BasicBuildTests.java
@@ -10,8 +10,11 @@
  ******************************************************************************/
 package org.eclipse.jdt.core.tests.builder;
 
+import java.util.Hashtable;
+
 import junit.framework.*;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.tests.util.Util;
 
 /**
@@ -47,5 +50,36 @@ public class BasicBuildTests extends Tests {
 			);
 			
 		incrementalBuild(projectPath);
+	}
+	
+	/*
+	 * http://bugs.eclipse.org/bugs/show_bug.cgi?id=23894
+	 */
+	public void testToDoMarker() {
+		Hashtable options = JavaCore.getOptions();
+		Hashtable newOptions = JavaCore.getOptions();
+		newOptions.put(JavaCore.COMPILER_TASK_TAGS, "todo");
+		
+		JavaCore.setOptions(newOptions);
+		
+		IPath projectPath = env.addProject("Project");
+		env.addExternalJar(projectPath, Util.getJavaClassLib());
+
+		// remove old package fragment root so that names don't collide
+		env.removePackageFragmentRoot(projectPath, "");
+
+		IPath root = env.addPackageFragmentRoot(projectPath, "src");
+		env.setOutputFolder(projectPath, "bin");
+
+		IPath pathToA = env.addClass(root, "p", "A",
+			"package p; \n"+
+			"//todo nothing\n"+
+			"public class A {\n"+
+			"}");
+
+		fullBuild(projectPath);
+		expectingOnlySpecificProblemFor(pathToA, new Problem("A", "todo nothing", pathToA));
+		
+		JavaCore.setOptions(options);
 	}
 }
