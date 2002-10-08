e8743e058a713296ad6f810f3473ad0820265b39
Olivier Thomann <othomann>
2002-10-08 16:34:26 +0000

    Fix for 24500,  24501, 24502 and 24499

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/DefaultBindingResolver.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/DefaultBindingResolver.java
@@ -239,7 +239,36 @@ class DefaultBindingResolver extends BindingResolver {
 				return this.getTypeBinding((ReferenceBinding)singleNameReference.binding);
 			} else {
 				// this is a variable or a field
-				return this.getVariableBinding((org.eclipse.jdt.internal.compiler.lookup.VariableBinding) singleNameReference.binding);				
+				Binding binding = singleNameReference.binding;
+				if (binding == null) {
+					return null;
+				} else if (binding.isValidBinding()) {
+					return this.getVariableBinding((org.eclipse.jdt.internal.compiler.lookup.VariableBinding) binding);				
+				} else {
+					/*
+					 * http://dev.eclipse.org/bugs/show_bug.cgi?id=24449
+					 */
+					if (binding instanceof ProblemFieldBinding) {
+						ProblemFieldBinding problemFieldBinding = (ProblemFieldBinding) binding;
+						switch(problemFieldBinding.problemId()) {
+							case ProblemReasons.NotVisible : 
+							case ProblemReasons.NonStaticReferenceInStaticContext :
+							case ProblemReasons.NonStaticReferenceInConstructorInvocation :
+								ReferenceBinding declaringClass = problemFieldBinding.declaringClass;
+								FieldBinding exactBinding = declaringClass.getField(problemFieldBinding.name);
+								if (exactBinding != null) {
+									IVariableBinding variableBinding2 = (IVariableBinding) this.compilerBindingsToASTBindings.get(exactBinding);
+									if (variableBinding2 != null) {
+										return variableBinding2;
+									}
+									variableBinding2 = new VariableBinding(this, exactBinding);
+									this.compilerBindingsToASTBindings.put(exactBinding, variableBinding2);
+									return variableBinding2;
+								}
+								break;
+						}
+					}
+	 			}				
 			}
 		} else if (node instanceof QualifiedSuperReference) {
 			QualifiedSuperReference qualifiedSuperReference = (QualifiedSuperReference) node;
@@ -737,17 +766,15 @@ class DefaultBindingResolver extends BindingResolver {
 						case ProblemReasons.NonStaticReferenceInStaticContext :
 						case ProblemReasons.NonStaticReferenceInConstructorInvocation :
 							ReferenceBinding declaringClass = problemFieldBinding.declaringClass;
-							if (variableBinding != null) {
-								FieldBinding exactBinding = declaringClass.getField(problemFieldBinding.name);
-								if (exactBinding != null) {
-									IVariableBinding variableBinding2 = (IVariableBinding) this.compilerBindingsToASTBindings.get(exactBinding);
-									if (variableBinding2 != null) {
-										return variableBinding2;
-									}
-									variableBinding2 = new VariableBinding(this, exactBinding);
-									this.compilerBindingsToASTBindings.put(exactBinding, variableBinding2);
+							FieldBinding exactBinding = declaringClass.getField(problemFieldBinding.name);
+							if (exactBinding != null) {
+								IVariableBinding variableBinding2 = (IVariableBinding) this.compilerBindingsToASTBindings.get(exactBinding);
+								if (variableBinding2 != null) {
 									return variableBinding2;
 								}
+								variableBinding2 = new VariableBinding(this, exactBinding);
+								this.compilerBindingsToASTBindings.put(exactBinding, variableBinding2);
+								return variableBinding2;
 							}
 							break;
 					}
