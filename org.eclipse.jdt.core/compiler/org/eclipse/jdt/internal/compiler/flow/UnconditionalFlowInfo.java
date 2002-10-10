77f63ea750ed0364fe2343040edb02da4f79c989
Philipe Mulet <pmulet>
2002-10-10 13:47:29 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/flow/UnconditionalFlowInfo.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/flow/UnconditionalFlowInfo.java
@@ -133,9 +133,7 @@ public boolean complainIfUnreachable(Statement statement, BlockScope scope) {
 	boolean isDeadEnd;
 	if ((isDeadEnd = (this == DeadEnd)) || isFakeReachable) {
 		statement.bits &= ~AstNode.IsReachableMASK;
-		/* EXTRA REFERENCE RECORDING
-		statement.recordUnreachableReferences(scope.referenceType()); // scopes cannot have an enclosingMethod slot since there are class scopes
-		*/
+
 		if (isDeadEnd)
 			scope.problemReporter().unreachableCode(statement);
 		return isDeadEnd;
