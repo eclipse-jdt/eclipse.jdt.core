e24143dc5fd218ff8d1e0d7f3686a79c1d2dd98d
Philipe Mulet <pmulet>
2002-11-19 14:32:46 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/CompilationResult.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/CompilationResult.java
@@ -56,270 +56,289 @@ public class CompilationResult {
 	public boolean hasBeenAccepted = false;
 	public char[] fileName;
 	
-public CompilationResult(
-	char[] fileName,
-	int unitIndex, 
-	int totalUnitsKnown,
-	int maxProblemPerUnit){
-
-	this.fileName = fileName;
-	this.unitIndex = unitIndex;
-	this.totalUnitsKnown = totalUnitsKnown;
-	this.maxProblemPerUnit = maxProblemPerUnit;
-
-}
-
-public CompilationResult(
-	ICompilationUnit compilationUnit,
-	int unitIndex, 
-	int totalUnitsKnown,
-	int maxProblemPerUnit){
-
-	this.fileName = compilationUnit.getFileName();
-	this.compilationUnit = compilationUnit;
-	this.unitIndex = unitIndex;
-	this.totalUnitsKnown = totalUnitsKnown;
-	this.maxProblemPerUnit = maxProblemPerUnit;
-
-}
-private int computePriority(IProblem problem){
-
-	final int P_STATIC = 1000;
-	final int P_OUTSIDE_METHOD = 4000;
-	final int P_FIRST_ERROR = 2000;
-	final int P_ERROR = 10000;
+	public CompilationResult(
+		char[] fileName,
+		int unitIndex, 
+		int totalUnitsKnown,
+		int maxProblemPerUnit){
 	
-	int priority = 1000 - problem.getSourceLineNumber(); // early problems first
-	if (priority < 0) priority = 0;
-	if (problem.isError()){
-		priority += P_ERROR;
+		this.fileName = fileName;
+		this.unitIndex = unitIndex;
+		this.totalUnitsKnown = totalUnitsKnown;
+		this.maxProblemPerUnit = maxProblemPerUnit;
 	}
-	ReferenceContext context = problemsMap == null ? null : (ReferenceContext) problemsMap.get(problem);
-	if (context != null){
-		if (context instanceof AbstractMethodDeclaration){
-			AbstractMethodDeclaration method = (AbstractMethodDeclaration) context;
-			if (method.isStatic()) {
-				priority += P_STATIC;
+	
+	public CompilationResult(
+		ICompilationUnit compilationUnit,
+		int unitIndex, 
+		int totalUnitsKnown,
+		int maxProblemPerUnit){
+	
+		this.fileName = compilationUnit.getFileName();
+		this.compilationUnit = compilationUnit;
+		this.unitIndex = unitIndex;
+		this.totalUnitsKnown = totalUnitsKnown;
+		this.maxProblemPerUnit = maxProblemPerUnit;
+	}
+
+	private int computePriority(IProblem problem){
+	
+		final int P_STATIC = 1000;
+		final int P_OUTSIDE_METHOD = 4000;
+		final int P_FIRST_ERROR = 2000;
+		final int P_ERROR = 10000;
+		
+		int priority = 1000 - problem.getSourceLineNumber(); // early problems first
+		if (priority < 0) priority = 0;
+		if (problem.isError()){
+			priority += P_ERROR;
+		}
+		ReferenceContext context = problemsMap == null ? null : (ReferenceContext) problemsMap.get(problem);
+		if (context != null){
+			if (context instanceof AbstractMethodDeclaration){
+				AbstractMethodDeclaration method = (AbstractMethodDeclaration) context;
+				if (method.isStatic()) {
+					priority += P_STATIC;
+				}
+			} else {
+			priority += P_OUTSIDE_METHOD;
 			}
 		} else {
-		priority += P_OUTSIDE_METHOD;
+			priority += P_OUTSIDE_METHOD;
 		}
-	} else {
-		priority += P_OUTSIDE_METHOD;
+		if (firstErrorsMap.containsKey(problem)){
+			priority += P_FIRST_ERROR;
+		}
+		return priority;
 	}
-	if (firstErrorsMap.containsKey(problem)){
-		priority += P_FIRST_ERROR;
-	}
-	return priority;
-}
-public ClassFile[] getClassFiles() {
-	Enumeration enum = compiledTypes.elements();
-	ClassFile[] classFiles = new ClassFile[compiledTypes.size()];
-	int index = 0;
-	while (enum.hasMoreElements()){
-		classFiles[index++] = (ClassFile)enum.nextElement();
-	}
-	return classFiles;	
-}
-/**
- * Answer the initial compilation unit corresponding to the present compilation result
- */
-public ICompilationUnit getCompilationUnit(){
-	return compilationUnit;
-}
-/**
- * Answer the initial file name
- */
-public char[] getFileName(){
-	return fileName;
-}
 
-/**
- * Answer the errors encountered during compilation.
- */
-public IProblem[] getErrors() {
-
-	IProblem[] problems = getProblems();
-	int errorCount = 0;
-	for (int i = 0; i < this.problemCount; i++) {
-		if (problems[i].isError()) errorCount++;
+	public ClassFile[] getClassFiles() {
+		Enumeration enum = compiledTypes.elements();
+		ClassFile[] classFiles = new ClassFile[compiledTypes.size()];
+		int index = 0;
+		while (enum.hasMoreElements()){
+			classFiles[index++] = (ClassFile)enum.nextElement();
+		}
+		return classFiles;	
 	}
-	if (errorCount == this.problemCount) return problems;
-	IProblem[] errors = new IProblem[errorCount];
-	int index = 0;
-	for (int i = 0; i < this.problemCount; i++) {
-		if (problems[i].isError()) errors[index++] = problems[i];
-	}
-	return errors;
-}
 
-/**
- * Answer the problems (errors and warnings) encountered during compilation.
- *
- * This is not a compiler internal API - it has side-effects !
- * It is intended to be used only once all problems have been detected,
- * and makes sure the problems slot as the exact size of the number of
- * problems.
- */
-public IProblem[] getProblems() {
+	/**
+	 * Answer the initial compilation unit corresponding to the present compilation result
+	 */
+	public ICompilationUnit getCompilationUnit(){
+		return compilationUnit;
+	}
+
+	/**
+	 * Answer the initial file name
+	 */
+	public char[] getFileName(){
+		return fileName;
+	}
 	
-	// Re-adjust the size of the problems if necessary.
-	if (problems != null) {
-
-		if (this.problemCount != problems.length) {
-			System.arraycopy(problems, 0, (problems = new IProblem[problemCount]), 0, problemCount);
+	/**
+	 * Answer the errors encountered during compilation.
+	 */
+	public IProblem[] getErrors() {
+	
+		IProblem[] problems = getProblems();
+		int errorCount = 0;
+		for (int i = 0; i < this.problemCount; i++) {
+			if (problems[i].isError()) errorCount++;
 		}
-
-		if (this.maxProblemPerUnit > 0 && this.problemCount > this.maxProblemPerUnit){
-			quickPrioritize(problems, 0, problemCount - 1);
-			this.problemCount = this.maxProblemPerUnit;
-			System.arraycopy(problems, 0, (problems = new IProblem[problemCount]), 0, problemCount);
+		if (errorCount == this.problemCount) return problems;
+		IProblem[] errors = new IProblem[errorCount];
+		int index = 0;
+		for (int i = 0; i < this.problemCount; i++) {
+			if (problems[i].isError()) errors[index++] = problems[i];
 		}
-
-		// Sort problems per source positions.
-		quickSort(problems, 0, problems.length-1);
+		return errors;
 	}
-	return problems;
-}
-
-public boolean hasErrors() {
-	if (problems != null)
-		for (int i = 0; i < problemCount; i++) {
-			if (problems[i].isError())
-				return true;
+	
+	/**
+	 * Answer the problems (errors and warnings) encountered during compilation.
+	 *
+	 * This is not a compiler internal API - it has side-effects !
+	 * It is intended to be used only once all problems have been detected,
+	 * and makes sure the problems slot as the exact size of the number of
+	 * problems.
+	 */
+	public IProblem[] getProblems() {
+		
+		// Re-adjust the size of the problems if necessary.
+		if (problems != null) {
+	
+			if (this.problemCount != problems.length) {
+				System.arraycopy(problems, 0, (problems = new IProblem[problemCount]), 0, problemCount);
+			}
+	
+			if (this.maxProblemPerUnit > 0 && this.problemCount > this.maxProblemPerUnit){
+				quickPrioritize(problems, 0, problemCount - 1);
+				this.problemCount = this.maxProblemPerUnit;
+				System.arraycopy(problems, 0, (problems = new IProblem[problemCount]), 0, problemCount);
+			}
+	
+			// Sort problems per source positions.
+			quickSort(problems, 0, problems.length-1);
 		}
-	return false;
-}
-public boolean hasProblems() {
-	return problemCount != 0;
-}
-public boolean hasSyntaxError(){
-	if (problems != null)
-		for (int i = 0; i < problemCount; i++) {
-			IProblem problem = problems[i];
-			if ((problem.getID() & IProblem.Syntax) != 0 && problem.isError())
-				return true;
-		}
-	return false;
-}
-public boolean hasWarnings() {
-	if (problems != null)
-		for (int i = 0; i < problemCount; i++) {
-			if (problems[i].isWarning())
-				return true;
-		}
-	return false;
-}
-
-private static void quickSort(IProblem[] list, int left, int right) {
-	if (left >= right) return;
-
-	// sort the problems by their source start position... starting with 0
-	int original_left = left;
-	int original_right = right;
-	int mid = list[(left + right) / 2].getSourceStart();
-	do {
-		while (list[left].getSourceStart() < mid)
-			left++;
-		while (mid < list[right].getSourceStart())
-			right--;
-		if (left <= right) {
-			IProblem tmp = list[left];
-			list[left] = list[right];
-			list[right] = tmp;
-			left++;
-			right--;
-		}
-	} while (left <= right);
-	if (original_left < right)
-		quickSort(list, original_left, right);
-	if (left < original_right)
-		quickSort(list, left, original_right);
-}
-
-private void quickPrioritize(IProblem[] list, int left, int right) {
-	if (left >= right) return;
-
-	// sort the problems by their priority... starting with the highest priority
-	int original_left = left;
-	int original_right = right;
-	int mid = computePriority(list[(left + right) / 2]);
-	do {
-		while (computePriority(list[right]) < mid)
-			right--;
-		while (mid < computePriority(list[left]))
-			left++;
-		if (left <= right) {
-			IProblem tmp = list[left];
-			list[left] = list[right];
-			list[right] = tmp;
-			left++;
-			right--;
-		}
-	} while (left <= right);
-	if (original_left < right)
-		quickPrioritize(list, original_left, right);
-	if (left < original_right)
-		quickPrioritize(list, left, original_right);
-}
-
-/**
- * For now, remember the compiled type using its compound name.
- */
-public void record(char[] typeName, ClassFile classFile) {
-	compiledTypes.put(typeName, classFile);
-}
-public void record(IProblem newProblem, ReferenceContext referenceContext) {
-	if (problemCount == 0) {
-		problems = new IProblem[5];
-	} else {
-		if (problemCount == problems.length)
-			System.arraycopy(problems, 0, (problems = new IProblem[problemCount * 2]), 0, problemCount);
-	};
-	problems[problemCount++] = newProblem;
-	if (referenceContext != null){
-		if (problemsMap == null) problemsMap = new Hashtable(5);
-		if (firstErrorsMap == null) firstErrorsMap = new Hashtable(5);
-		if (newProblem.isError() && !referenceContext.hasErrors()) firstErrorsMap.put(newProblem, newProblem);
-		problemsMap.put(newProblem, referenceContext);
+		return problems;
 	}
-}
-private static void swap(IProblem arr[], int i, int j) {
-	IProblem tmp;
-	tmp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = tmp;
-}
-public CompilationResult tagAsAccepted(){
-	this.hasBeenAccepted = true;
-	this.problemsMap = null; // flush
-	return this;
-}
+	
+	public boolean hasErrors() {
 
-public String toString(){
-	StringBuffer buffer = new StringBuffer();
-	if (this.fileName != null){
-		buffer.append("Filename : ").append(this.fileName).append('\n'); //$NON-NLS-1$
+		if (problems != null)
+			for (int i = 0; i < problemCount; i++) {
+				if (problems[i].isError())
+					return true;
+			}
+		return false;
 	}
-	if (this.compiledTypes != null){
-		buffer.append("COMPILED type(s)	\n");  //$NON-NLS-1$
-		Enumeration typeNames = this.compiledTypes.keys();
-		while (typeNames.hasMoreElements()) {
-			char[] typeName = (char[]) typeNames.nextElement();
-			buffer.append("\t - ").append(typeName).append('\n');   //$NON-NLS-1$
-			
-		}
-	} else {
-		buffer.append("No COMPILED type\n");  //$NON-NLS-1$
+
+	public boolean hasProblems() {
+
+		return problemCount != 0;
 	}
-	if (problems != null){
-		buffer.append(this.problemCount).append(" PROBLEM(s) detected \n"); //$NON-NLS-1$//$NON-NLS-2$
-		for (int i = 0; i < this.problemCount; i++){
-			buffer.append("\t - ").append(this.problems[i]).append('\n'); //$NON-NLS-1$
+
+	public boolean hasSyntaxError(){
+
+		if (problems != null)
+			for (int i = 0; i < problemCount; i++) {
+				IProblem problem = problems[i];
+				if ((problem.getID() & IProblem.Syntax) != 0 && problem.isError())
+					return true;
+			}
+		return false;
+	}
+
+	public boolean hasWarnings() {
+
+		if (problems != null)
+			for (int i = 0; i < problemCount; i++) {
+				if (problems[i].isWarning())
+					return true;
+			}
+		return false;
+	}
+	
+	private static void quickSort(IProblem[] list, int left, int right) {
+
+		if (left >= right) return;
+	
+		// sort the problems by their source start position... starting with 0
+		int original_left = left;
+		int original_right = right;
+		int mid = list[(left + right) / 2].getSourceStart();
+		do {
+			while (list[left].getSourceStart() < mid)
+				left++;
+			while (mid < list[right].getSourceStart())
+				right--;
+			if (left <= right) {
+				IProblem tmp = list[left];
+				list[left] = list[right];
+				list[right] = tmp;
+				left++;
+				right--;
+			}
+		} while (left <= right);
+		if (original_left < right)
+			quickSort(list, original_left, right);
+		if (left < original_right)
+			quickSort(list, left, original_right);
+	}
+	
+	private void quickPrioritize(IProblem[] list, int left, int right) {
+		
+		if (left >= right) return;
+	
+		// sort the problems by their priority... starting with the highest priority
+		int original_left = left;
+		int original_right = right;
+		int mid = computePriority(list[(left + right) / 2]);
+		do {
+			while (computePriority(list[right]) < mid)
+				right--;
+			while (mid < computePriority(list[left]))
+				left++;
+			if (left <= right) {
+				IProblem tmp = list[left];
+				list[left] = list[right];
+				list[right] = tmp;
+				left++;
+				right--;
+			}
+		} while (left <= right);
+		if (original_left < right)
+			quickPrioritize(list, original_left, right);
+		if (left < original_right)
+			quickPrioritize(list, left, original_right);
+	}
+	
+	/**
+	 * For now, remember the compiled type using its compound name.
+	 */
+	public void record(char[] typeName, ClassFile classFile) {
+
+		compiledTypes.put(typeName, classFile);
+	}
+
+	public void record(IProblem newProblem, ReferenceContext referenceContext) {
+
+		if (problemCount == 0) {
+			problems = new IProblem[5];
+		} else {
+			if (problemCount == problems.length)
+				System.arraycopy(problems, 0, (problems = new IProblem[problemCount * 2]), 0, problemCount);
+		};
+		problems[problemCount++] = newProblem;
+		if (referenceContext != null){
+			if (problemsMap == null) problemsMap = new Hashtable(5);
+			if (firstErrorsMap == null) firstErrorsMap = new Hashtable(5);
+			if (newProblem.isError() && !referenceContext.hasErrors()) firstErrorsMap.put(newProblem, newProblem);
+			problemsMap.put(newProblem, referenceContext);
 		}
-	} else {
-		buffer.append("No PROBLEM\n"); //$NON-NLS-1$
-	} 
-	return buffer.toString();
-}
+	}
+
+	private static void swap(IProblem arr[], int i, int j) {
+
+		IProblem tmp;
+		tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	public CompilationResult tagAsAccepted(){
+
+		this.hasBeenAccepted = true;
+		this.problemsMap = null; // flush
+		return this;
+	}
+	
+	public String toString(){
+
+		StringBuffer buffer = new StringBuffer();
+		if (this.fileName != null){
+			buffer.append("Filename : ").append(this.fileName).append('\n'); //$NON-NLS-1$
+		}
+		if (this.compiledTypes != null){
+			buffer.append("COMPILED type(s)	\n");  //$NON-NLS-1$
+			Enumeration typeNames = this.compiledTypes.keys();
+			while (typeNames.hasMoreElements()) {
+				char[] typeName = (char[]) typeNames.nextElement();
+				buffer.append("\t - ").append(typeName).append('\n');   //$NON-NLS-1$
+				
+			}
+		} else {
+			buffer.append("No COMPILED type\n");  //$NON-NLS-1$
+		}
+		if (problems != null){
+			buffer.append(this.problemCount).append(" PROBLEM(s) detected \n"); //$NON-NLS-1$//$NON-NLS-2$
+			for (int i = 0; i < this.problemCount; i++){
+				buffer.append("\t - ").append(this.problems[i]).append('\n'); //$NON-NLS-1$
+			}
+		} else {
+			buffer.append("No PROBLEM\n"); //$NON-NLS-1$
+		} 
+		return buffer.toString();
+	}
 }
