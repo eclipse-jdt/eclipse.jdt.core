f0f988f5ece8ab0a5ff8315152fd572c7964adb5
Olivier Thomann <othomann>
2002-11-20 17:02:05 +0000

    Reset warning level to JDT/Core default settings

--- org.eclipse.jdt.core/antadapter/org/eclipse/jdt/core/JDTCompilerAdapter.java
+++ org.eclipse.jdt.core/antadapter/org/eclipse/jdt/core/JDTCompilerAdapter.java
@@ -200,10 +200,10 @@ public class JDTCompilerAdapter extends DefaultCompilerAdapter {
 			 */		
 			if (deprecation) {
 				cmd.createArgument().setValue(
-					"-warn:allDeprecation,constructorName,packageDefaultMethod,maskedCatchBlocks,unusedImports,staticReceiver"); //$NON-NLS-1$
+					"-warn:allDeprecation,noOpAssign,constructorName,packageDefaultMethod,maskedCatchBlocks,unusedImports,staticReceiver"); //$NON-NLS-1$
 			} else {
 				cmd.createArgument().setValue(
-					"-warn:constructorName,packageDefaultMethod,maskedCatchBlocks,unusedImports,staticReceiver"); //$NON-NLS-1$
+					"-warn:noOpAssign,constructorName,packageDefaultMethod,maskedCatchBlocks,unusedImports,staticReceiver"); //$NON-NLS-1$
 			}
         }
 
