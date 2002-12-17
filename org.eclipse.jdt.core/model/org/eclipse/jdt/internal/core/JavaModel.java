2b3f7f272869211ccf56fb76abe7591f2a8b895f
Jerome lanneluc <jlanneluc>
2002-12-17 17:10:31 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/JavaModel.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/JavaModel.java
@@ -23,7 +23,6 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
@@ -35,7 +34,6 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 
 /**
