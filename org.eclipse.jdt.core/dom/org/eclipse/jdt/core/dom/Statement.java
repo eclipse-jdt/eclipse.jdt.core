7c3080ba45bd79ddd7d628abfc7780ee747e39e8
Jim Desrivieres <jdesrivieres>
2003-01-21 19:35:25 +0000

    Update copyright notice

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Statement.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Statement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001-2003 International Business Machines Corp. and others. All
+ * Copyright (c) 2001, 2003 International Business Machines Corp. and others. All
  * rights reserved. This program and the accompanying materials are made
  * available under the terms of the Common Public License v1.0 which accompanies
  * this distribution, and is available at http://www.eclipse.org/legal/cpl-v10.
