bb8c48065033e9326f64fa41064a1b4f8da00b0a
Jim Desrivieres <jdesrivieres>
2003-01-31 18:15:08 +0000

    Finalize API for cu sorter (bug 14597)

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/util/CompilationUnitSorter.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/util/CompilationUnitSorter.java
@@ -11,35 +11,17 @@
 
 package org.eclipse.jdt.core.util;
 
-import java.text.Collator;
 import java.util.Comparator;
-import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ArrayType;
-import org.eclipse.jdt.core.dom.BodyDeclaration;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.Initializer;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.Name;
-import org.eclipse.jdt.core.dom.PrimitiveType;
-import org.eclipse.jdt.core.dom.QualifiedName;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SimpleType;
-import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
-import org.eclipse.jdt.core.dom.Type;
-import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.internal.core.SortElementsOperation;
 
 /**
- * Operation for sorting the members of a compilation unit.
+ * Operation for sorting members within a compilation unit.
  * <p>
  * This class provides all functionality via static members; it is not
  * intended to be instantiated or subclassed.
@@ -55,272 +37,6 @@ public final class CompilationUnitSorter {
 	private CompilationUnitSorter() {
 	} 
 	
-	/**
-	 * The class <code>DefaultJavaElementComparator</code> is a standard
-	 * implementation of a comparator.
-	 * <p>
-	 * <ul>
-	 * <li>static fields, arranged alphabetically by name and access modifier
-	 * (public, protected, private, default)</li>
-	 * <li>static initializers in order of appearance</li>
-	 * <li>instance fields, arranged alphabetically by name and access modifier
-	 * (public, protected, private, default)</li>
-	 * <li>instance initializers in order of appearance</li>
-	 * <li>type declarations, arranged alphabetically by name and access modifier
-	 * (public, protected, private, default)</li>
-	 * <li>constructors, arranged by parameter order and access modifier
-	 * (public, protected, private, default)</li>
-	 * <li>methods, arranged by alphabetically by name and parameter order and 
-	 * access modifier (public, protected, private, default)</li>
-	 * </p>
-	 */
-	public static class DefaultJavaElementComparator implements Comparator {
-
-		private static final int STATIC_TYPE_CATEGORY = 0;
-		private static final int STATIC_FIELD_CATEGORY = 1;
-		private static final int STATIC_INITIALIZER_CATEGORY = 2;
-		private static final int STATIC_METHOD_CATEGORY = 3;
-		private static final int TYPE_CATEGORY = 4;
-		private static final int FIELD_CATEGORY = 5;
-		private static final int INITIALIZER_CATEGORY = 6;
-		private static final int CONSTRUCTOR_CATEGORY = 7;
-		private static final int METHOD_CATEGORY = 8;
-		
-		private Collator collator;
-		
-		private int[] categories;
-		
-		/**
-		 * Creates an instance that sorts the various categories of body
-		 * declarations in the following order:
-		 * <ol>
-		 * <li>static types</li>
-		 * <li>static fields </li>
-		 * <li>static initializers</li>
-		 * <li>non-static fields</li>
-		 * <li>instance initializers</li>
-		 * <li>types</li>
-		 * <li>static methods</li>
-		 * <li>constructors</li>
-		 * <li>non-static methods</li>
-		 * </ol>
-		 */
-		public DefaultJavaElementComparator() {
-			// initialize default categories
-			this.categories = new int[] {
-				1, // static type
-				2, // static field
-				3, // static initializer
-				7, // static method
-				6, // type
-				4, // field
-				5, // initializer
-				8, // constructor
-				9  // method
-			};
-			this.collator = Collator.getInstance();
-		}
-
-		/**
-		 * Creates an instance that arranges the various categories of body
-		 * declarations.
-		 * This constructor is used to specify customized values for the different categories.
-		 * They are a convinient way to distinguish AST nodes. 
-		 * The lower a value is, the higher the node will appear in the sorted
-		 * compilation unit.
-		 * <p>
-		 * There are nine categories with theirs default values:
-		 * <ol>
-		 * <li>static types (1)</li>
-		 * <li>static fields (2)</li>
-		 * <li>static initializers (3)</li>
-		 * <li>fields (4) </li>
-		 * <li>initializers (5)</li>
-		 * <li>types (6)</li>
-		 * <li>static methods (7)</li>
-		 * <li>constructors (8)</li>
-		 * <li>methods (9)</li>
-		 * </ol>
-		 * </p>
-		 * 
-		 * @param staticTypeCategory the given value for the static type category
-		 * @param staticFieldCategory the given value for the static field category
-		 * @param staticInitializerCategory the given value for the static initializer category
-		 * @param staticMethodCategory the given value for static the method category
-		 * @param typeCategory the given value for the type category
-		 * @param fieldCategory the given value for field category
-		 * @param initializerCategory the given value for initializer category
-		 * @param constructorCategory the given value for constructor category
-		 * @param methodCategory the given value for method category
-		 */
-		public DefaultJavaElementComparator(
-			int staticTypeCategory,
-			int staticFieldCategory,
-			int staticInitializerCategory,
-			int staticMethodCategory,
-			int typeCategory,
-			int fieldCategory,
-			int initializerCategory,
-			int constructorCategory,
-			int methodCategory) {
-				this.categories = new int[] {
-					staticTypeCategory,
-					staticFieldCategory,
-					staticInitializerCategory,
-					staticMethodCategory,
-					typeCategory,
-					fieldCategory,
-					initializerCategory,
-					constructorCategory,
-					methodCategory
-				};
-			this.collator = Collator.getInstance();
-		}
-
-		/**
-		 * This method is used to retrieve the category for a body declaration node according to the 
-		 * preferences passed at the creation of the comparator.
-		 * 
-		 * @param node the given node
-		 * @return the category corresponding to the given node
-		 * 
-		 * @since 2.1
-		 */
-		private int getCategory(BodyDeclaration node) {
-			switch(node.getNodeType()) {
-				case ASTNode.METHOD_DECLARATION :
-					MethodDeclaration methodDeclaration = (MethodDeclaration) node;
-					if (methodDeclaration.isConstructor()) {
-						return this.categories[CONSTRUCTOR_CATEGORY];
-					}
-					if (Flags.isStatic(methodDeclaration.getModifiers())) {
-						return this.categories[STATIC_METHOD_CATEGORY];
-					} else {
-						return this.categories[METHOD_CATEGORY];
-					}
-				case ASTNode.FIELD_DECLARATION :
-					FieldDeclaration fieldDeclaration = (FieldDeclaration) node;
-					if (Flags.isStatic(fieldDeclaration.getModifiers())) {
-						return this.categories[STATIC_FIELD_CATEGORY];
-					} else {
-						return this.categories[FIELD_CATEGORY];
-					}
-				case ASTNode.TYPE_DECLARATION :
-					TypeDeclaration typeDeclaration = (TypeDeclaration) node;
-					if (Flags.isStatic(typeDeclaration.getModifiers())) {
-						return this.categories[STATIC_TYPE_CATEGORY];
-					} else {
-						return this.categories[TYPE_CATEGORY];
-					}
-				case ASTNode.INITIALIZER :
-					Initializer initializer = (Initializer) node;
-					if (Flags.isStatic(initializer.getModifiers())) {
-						return this.categories[STATIC_INITIALIZER_CATEGORY];
-					} else {
-						return this.categories[INITIALIZER_CATEGORY];
-					}
-			}
-			return 0;
-		}
-	
-		/**
-		 * The <code>DefaultJavaElementComparator</code> implementation of this
-		 * <code>java.util.Comparator</code> method can only be used to compare
-		 * instances of <code>org.eclipse.jdt.core.dom.BodyDeclaration</code>.
-		 * <p>
-		 * The categories of each body declaration are compared. If they are
-		 * in different categories, they are ordered based on their category.
-		 * Body declarations within the same category are ordered by signature
-		 * string. Body declarations with the same signature string are ordered
-		 * by their original relative positions.
-		 * </p>
-		 */
-		public int compare(Object o1, Object o2) {
-			if (!(o1 instanceof BodyDeclaration) && !(o2 instanceof BodyDeclaration)) {
-				throw new ClassCastException();
-			}
-			BodyDeclaration node1 = (BodyDeclaration) o1;
-			BodyDeclaration node2 = (BodyDeclaration) o2;
-			int category1 = getCategory(node1);
-			int category2 = getCategory(node2);
-			
-			if (category1 != category2) {
-				return category1 - category2;
-			}
-			if (o1 == o2) {
-				return 0;
-			}
-			String node1Signature = buildSignature(node1);
-			String node2Signature = buildSignature(node2);
-			if (node1Signature.length() != 0 && node2Signature.length() != 0) {
-				int compare = this.collator.compare(node1Signature, node2Signature);
-				if (compare != 0) {
-					return compare;
-				}
-			}
-			int sourceStart1 = ((Integer) node1.getProperty(CompilationUnitSorter.RELATIVE_ORDER)).intValue();
-			int sourceStart2 = ((Integer) node2.getProperty(CompilationUnitSorter.RELATIVE_ORDER)).intValue();
-			return sourceStart1 - sourceStart2;
-		}
-
-		private String buildSignature(BodyDeclaration node) {
-			switch(node.getNodeType()) {
-				case ASTNode.METHOD_DECLARATION :
-					MethodDeclaration methodDeclaration = (MethodDeclaration) node;
-					StringBuffer buffer = new StringBuffer();
-					buffer.append(methodDeclaration.getName().getIdentifier());
-					final List parameters = methodDeclaration.parameters();
-					int length1 = parameters.size();
-					for (int i = 0; i < length1; i++) {
-						SingleVariableDeclaration parameter = (SingleVariableDeclaration) parameters.get(i);
-						buffer.append(parameter.getName().getIdentifier());
-						Type type = parameter.getType();
-						buffer.append(buildSignature(type));
-					}
-					return buffer.toString();
-				case ASTNode.FIELD_DECLARATION :
-					FieldDeclaration fieldDeclaration = (FieldDeclaration) node;
-					return ((VariableDeclarationFragment) fieldDeclaration.fragments().get(0)).getName().getIdentifier();
-				case ASTNode.INITIALIZER :
-					return ((Integer) node.getProperty(CompilationUnitSorter.RELATIVE_ORDER)).toString();
-				case ASTNode.TYPE_DECLARATION :
-					TypeDeclaration typeDeclaration = (TypeDeclaration) node;
-					return typeDeclaration.getName().getIdentifier();
-			}
-			return null;
-		}
-
-		private String buildSignature(Type type) {
-			switch(type.getNodeType()) {
-				case ASTNode.PRIMITIVE_TYPE :
-					PrimitiveType.Code code = ((PrimitiveType) type).getPrimitiveTypeCode();
-					return code.toString();
-				case ASTNode.ARRAY_TYPE :
-					ArrayType arrayType = (ArrayType) type;
-					StringBuffer buffer = new StringBuffer();
-					buffer.append(buildSignature(arrayType.getElementType()));
-					int dimensions = arrayType.getDimensions();
-					for (int j = 0; j < dimensions; j++) {
-						buffer.append("[]"); //$NON-NLS-1$
-					}
-					return buffer.toString();
-				case ASTNode.SIMPLE_TYPE :
-					SimpleType simpleType = (SimpleType) type;
-					return buildSignature(simpleType.getName());
-			}
-			return null; // should never happen
-		}
-		
-		private String buildSignature(Name name) {
-			if (name.isSimpleName()) {
-				return ((SimpleName) name).getIdentifier();
-			} else {
-				QualifiedName qualifiedName = (QualifiedName) name;
-				return buildSignature(qualifiedName.getQualifier()) + "." + buildSignature(qualifiedName.getName()); //$NON-NLS-1$
-			}
-		}
-	}
-
 	/**
 	 * Name of auxillary property whose value can be used to determine the
 	 * original relative order of two body declarations. This allows a
@@ -346,7 +62,6 @@ public final class CompilationUnitSorter {
 	 * 
 	 * @see #sort
 	 * @see org.eclipse.jdt.core.dom.BodyDeclaration
-	 * @since 2.1
 	 */
 	public static final String RELATIVE_ORDER = "relativeOrder"; //$NON-NLS-1$
 
@@ -354,6 +69,16 @@ public final class CompilationUnitSorter {
 	 * Reorders the declarations in the given compilation unit. The caller is
 	 * responsible for arranging in advance that the given compilation unit is
 	 * a working copy, and for saving the changes afterwards.
+	 * <p>
+	 * <b>Note:</b> Reordering the members within a type declaration might be
+	 * more than a cosmetic change and could have potentially serious
+	 * repercussions. Firstly, the order in which the fields of a type are
+	 * initialized is significant in the Java language; reordering fields
+	 * and initializers may result in compilation errors or change the execution
+	 * behavior of the code. Secondly, reordering a class's members may affect
+	 * how its instances are serialized. This operation should therefore be used
+	 * with caution and due concern for potential negative side effects.
+	 * </p>
 	 * <p>
 	 * The optional <code>positions</code> array contains a non-decreasing 
 	 * ordered list of character-based source positions within the compilation
@@ -366,24 +91,20 @@ public final class CompilationUnitSorter {
 	 * of AST body declarations (subclasses of <code>BodyDeclaration</code>) 
 	 * representing body declarations at the same level. The comparator is
 	 * called on body declarations of nested classes, including anonymous and
-	 * local classes, but always at the same level. The class 
-	 * <code>DefaultJavaElementComparator</code> is the standard comparator, 
-	 * but clients are free to provide their own comparator implementations.
+	 * local classes, but always at the same level. Clients need to provide
+	 * a comparator implementation (there is no standard comparator). The
+	 * <code>RELATIVE_ORDER</code> property attached to these AST nodes afforts
+	 * the comparator a way to preserve the original relative order.
 	 * </p>
 	 * <p>
-	 * The body declarations passed as parameters to the comparator carry 
-	 * signature information only (that is, they are not complete ASTs).
-	 * Clients cannot rely on the AST nodes being properly parented or on
-	 * having source range information.
-	 * The following table describes the information available for each kind
-	 * of body declaration:
+	 * The body declarations passed as parameters to the comparator
+	 * always carry at least the following minimal signature information:
 	 * <br>
 	 * <table border="1" width="80%" cellpadding="5">
 	 *	  <tr>
 	 *	    <td width="20%"><code>TypeDeclaration</code></td>
 	 *	    <td width="50%"><code>modifiers, isInterface, name, superclass,
 	 *	      superInterfaces<br>
-     *		  No bodyDeclarations<br>
      *		  RELATIVE_ORDER property</code></td>
 	 *	  </tr>
 	 *	  <tr>
@@ -399,16 +120,18 @@ public final class CompilationUnitSorter {
 	 *		  parameters
 	 *	      (SingleVariableDeclarations with name and type only),
 	 *		  thrownExceptions<br>
-	 *	      No body<br>
      *		  RELATIVE_ORDER property</code></td>
 	 *	  </tr>
 	 *	  <tr>
 	 *	    <td width="20%"><code>Initializer</code></td>
 	 *	    <td width="50%"><code>modifiers<br>
-	 *	      No body<br>
      *		  RELATIVE_ORDER property</code></td>
 	 *	  </tr>
 	 * </table>
+	 * Clients should rely on the AST nodes being properly parented or on
+	 * having source range information. (Future releases may provide options
+	 * for requesting additional information like source positions, full ASTs,
+	 * non-recursive sorting, etc.)
 	 * </p>
 	 *
 	 * @param compilationUnit the given compilation unit, which must be a 
@@ -418,9 +141,12 @@ public final class CompilationUnitSorter {
 	 * character-based source positions within the original source code for
 	 * the given compilation unit, arranged in non-decreasing order.
 	 * The array is updated in place when this method returns to reflect the
-	 * corresponding source positions in the permuted source code string.
+	 * corresponding source positions in the permuted source code string
+	 * (but not necessarily any longer in non-decreasing order).
 	 * @param comparator the comparator capable of ordering 
-	 * <code>BodyDeclaration</code>s
+	 *   <code>BodyDeclaration</code>s
+	 * @param options bitwise-or of option flags; <code>0</code> for default
+	 * behavior (reserved for future growth)
 	 * @param monitor the progress monitor to notify, or <code>null</code> if
 	 * none
 	 * @exception JavaModelException if the compilation unit could not be
@@ -431,14 +157,16 @@ public final class CompilationUnitSorter {
 	 * <li> A <code>CoreException</code> occurred while accessing the underlying
 	 * resource
 	 * </ul>
-	 * @exception IllegalArgumentException if the given compilation unit is null or if the given
-	 * comparator is null.
+	 * @exception IllegalArgumentException if the given compilation unit is null
+	 * or if the given comparator is null.
 	 * @see org.eclipse.jdt.core.dom.BodyDeclaration
-	 * @see #DefaultJavaElementComparator
 	 * @see #RELATIVE_ORDER
-	 * @since 2.1
 	 */
-	public static void sort(ICompilationUnit compilationUnit, int[] positions, Comparator comparator, IProgressMonitor monitor) throws JavaModelException {
+	public static void sort(ICompilationUnit compilationUnit,
+	        int[] positions,
+	        Comparator comparator,
+	        int options,
+	        IProgressMonitor monitor) throws JavaModelException {
 		if (compilationUnit == null || comparator == null) {
 			throw new IllegalArgumentException();
 		}
@@ -450,5 +178,4 @@ public final class CompilationUnitSorter {
 			throw new JavaModelException(e);
 		}
 	}
-
 }
