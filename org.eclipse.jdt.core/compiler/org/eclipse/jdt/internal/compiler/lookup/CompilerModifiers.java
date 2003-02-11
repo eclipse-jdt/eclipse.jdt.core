a100941090d3ffeb2132febb720cb1f96d6559f2
Philipe Mulet <pmulet>
2003-02-11 13:40:34 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/CompilerModifiers.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/CompilerModifiers.java
@@ -29,7 +29,7 @@ public interface CompilerModifiers extends ClassFileConstants { // modifier cons
 	final int AccSemicolonBody = 0x1000000; // bit25
 	final int AccUnresolved = 0x2000000; // bit26
 	final int AccClearPrivateModifier = 0x4000000; // bit27 might be requested during private access emulation
-	final int AccPrivateUsed = 0x8000000; // bit28 used to diagnose unused private members
 	final int AccBlankFinal = 0x4000000; // bit27 for blank final variables
+	final int AccPrivateUsed = 0x8000000; // bit28 used to diagnose unused private members
 	final int AccVisibilityMASK = AccPublic | AccProtected | AccPrivate;
 }
