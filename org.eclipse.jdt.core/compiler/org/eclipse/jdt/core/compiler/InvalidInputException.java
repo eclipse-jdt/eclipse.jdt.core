816fc3ca6722e100ac387ab402c754444e0f42e1
Olivier Thomann <othomann>
2003-02-11 17:09:35 +0000

    Fix and update missing javadoc

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/core/compiler/InvalidInputException.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/core/compiler/InvalidInputException.java
@@ -17,13 +17,13 @@ package org.eclipse.jdt.core.compiler;
 public class InvalidInputException extends Exception {
 
 	/**
-	 * Creates a new exception with no detail message
+	 * Creates a new exception with no detail message.
 	 */
 	public InvalidInputException() {
 		super();
 	}
 	/**
-	 * Creates a new exception with the given detail message
+	 * Creates a new exception with the given detail message.
 	 * @param message the detail message
 	 */
 	public InvalidInputException(String message) {
