9cb7ba4f49e54f74790d27de35620c882363e835
Philipe Mulet <pmulet>
2003-02-28 10:38:44 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/ThreadSafetyTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/ThreadSafetyTests.java
@@ -27,24 +27,31 @@ import junit.framework.*;
 public class ThreadSafetyTests extends ModifyingResourceTests {
 
 	public class Semaphore {
-		private int status = 0;
-		public Semaphore() { // mutex by default
-			this(1);
+		
+		private String name = null;
+		private int permissions = 0;
+		
+		public Semaphore(int initialPermisions) {
+			this(null, initialPermisions);
 		}
-		public Semaphore(int status) {
-			this.status = status;
+		public Semaphore(String name, int initialPermissions) {
+			this.name = name;
+			this.permissions = initialPermissions;
 		}
 		public synchronized void acquire() {
-			while (status <= 0){
+			while (this.permissions <= 0){
 				try {
+					if (this.name != null) System.out.println(Thread.currentThread() + " - waiting to acquire: " + this.name); //$NON-NLS-1$
 					wait();
 				} catch(InterruptedException e){
 				}
 			}
-			status--;
+			this.permissions--;
+			if (this.name != null) System.out.println(Thread.currentThread() + " - acquired: " + this.name); //$NON-NLS-1$
 		}
 		public synchronized void release() {
-			if (++status > 0) notifyAll();
+			if (this.name != null) System.out.println(Thread.currentThread() + " - releasing: " + this.name); //$NON-NLS-1$
+			if (++this.permissions > 0) notifyAll();
 		}
 	}
 
@@ -81,16 +88,16 @@ public void testDeadlock01() throws CoreException {
 		JavaModelManager.getJavaModelManager().removePerProjectInfo((JavaProject)project);
 
 		// use a thread to hold the lock, so as to recreate potential deadlock situation
-		final Semaphore permissionToPopulateModel = new Semaphore(0); // first acquisition will wait
-		final Semaphore permissionToModifyResource = new Semaphore(0); // first acquisition to wait
+		final Semaphore step1 = new Semaphore("<1:permission to populate JavaModel inducing containers inits>", 0); // first acquisition will wait
+		final Semaphore step2 = new Semaphore("<2:permission to perform resource modification >", 0); // first acquisition to wait
 		final Semaphore hasCompleted = new Semaphore(0); 
 		
 		ContainerInitializer.setInitializer(new ClasspathInitializerTests.DefaultContainerInitializer(new String[] {"P", ""}){
 			public void initialize(IPath containerPath, IJavaProject project) throws CoreException {
-				permissionToModifyResource.release();
-				System.out.println(Thread.currentThread() + "initializer has started: attempting to acquire workspace lock");
+				step2.release();
+				System.out.println(Thread.currentThread() + " initializer has started: attempting to acquire workspace lock");
 				super.initialize(containerPath, project);
-				System.out.println(Thread.currentThread() + "initializer has finished");
+				System.out.println(Thread.currentThread() + " initializer has finished");
 			}
 		});
 
@@ -101,8 +108,8 @@ public void testDeadlock01() throws CoreException {
 						ResourcesPlugin.getWorkspace().run(new IWorkspaceRunnable() {
 							public void run(IProgressMonitor monitor)	throws CoreException {
 								System.out.println(Thread.currentThread() + " got workspace lock");
-								permissionToPopulateModel.release();
-								permissionToModifyResource.acquire(); // ensure the java model lock is taken
+								step1.release();
+								step2.acquire(); // ensure the java model lock is taken
 								ThreadSafetyTests.this.createFile("/P/X.java", "public class X {}");
 								System.out.println(Thread.currentThread() + " created file X.java");
 							}
@@ -119,13 +126,11 @@ public void testDeadlock01() throws CoreException {
 		Thread attemptPopulateTheJavaModel = new Thread(new Runnable(){
 				public void run() {
 					try {
-						//synchronized(JavaModelManager.getJavaModelManager()) {
-							permissionToPopulateModel.acquire(); // ensure workspace lock is taken already
+							step1.acquire(); // ensure workspace lock is taken already
 							System.out.println(Thread.currentThread() + " about to take Java model lock");
 							// needs the JavaModel lock to populate the project
 							project.getChildren(); // trigger classpath initializer activation (requires workspace lock)
 							System.out.println(Thread.currentThread() + " done populating the model");
-						//}
 					} catch (JavaModelException e) {
 					}
 					hasCompleted.release();
