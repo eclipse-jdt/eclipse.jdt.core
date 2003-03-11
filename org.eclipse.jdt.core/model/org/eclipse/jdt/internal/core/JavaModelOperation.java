98e04796f67b009be6b2990d61d59926eaa1e6f0
Philipe Mulet <pmulet>
2003-03-11 22:02:25 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/JavaModelOperation.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/JavaModelOperation.java
@@ -589,6 +589,9 @@ public abstract class JavaModelOperation implements IWorkspaceRunnable, IProgres
 		ArrayList stack = getCurrentOperationStack();
 		int size = stack.size();
 		if (size > 0) {
+			if (size == 1) { // top level operation 
+				operationStacks.setCurrent(null); // release reference (see http://bugs.eclipse.org/bugs/show_bug.cgi?id=33927)
+			}
 			return (JavaModelOperation)stack.remove(size-1);
 		} else {
 			return null;
