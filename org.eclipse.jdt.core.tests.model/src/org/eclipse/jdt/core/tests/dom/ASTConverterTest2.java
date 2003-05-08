d097b02a9c6be986043696f972134a52826511b1
Olivier Thomann <othomann>
2003-05-08 16:28:35 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterTest2.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterTest2.java
@@ -39,8 +39,7 @@ public class ASTConverterTest2 extends ConverterTestSetup {
 			}
 			return suite;
 		}
-		suite.addTest(new ASTConverterTest2("test0433"));			
-		suite.addTest(new ASTConverterTest2("test0450"));	
+		suite.addTest(new ASTConverterTest2("test0469"));			
 		return suite;
 	}
 	/**
@@ -1841,6 +1840,22 @@ public class ASTConverterTest2 extends ConverterTestSetup {
 		ASTNode foundNode = compilationUnit.findDeclaringNode(variableBinding);
 		assertNotNull("No found node", foundNode);
 		assertEquals("wrong node", fragment, foundNode);
+	}		
+
+	/**
+	 * http://bugs.eclipse.org/bugs/show_bug.cgi?id=36895
+	 */
+	public void test0469() throws JavaModelException {
+		ICompilationUnit sourceUnit = getCompilationUnit("Converter" , "", "codeManipulation", "bug.java"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		ASTNode result = runConversion(sourceUnit, true);
+		CompilationUnit compilationUnit = (CompilationUnit) result;
+		ASTNode node = getASTNode(compilationUnit, 0, 2, 0);
+		assertEquals("No error", 0, compilationUnit.getProblems().length); //$NON-NLS-1$
+		assertNotNull("No node", node);
+		assertTrue("not a variable declaration statement", node.getNodeType() == ASTNode.VARIABLE_DECLARATION_STATEMENT); //$NON-NLS-1$
+		ASTNode parent = node.getParent();
+		assertNotNull(parent);
+		assertTrue("not a block", parent.getNodeType() == ASTNode.BLOCK); //$NON-NLS-1$
 	}		
 }
 
