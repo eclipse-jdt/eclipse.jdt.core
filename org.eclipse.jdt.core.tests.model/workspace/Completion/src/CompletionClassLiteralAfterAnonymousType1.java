8c4313649ddacf063569a681f9a87b3d32edbeda
cvs2svn <platform-releng-dev@eclipse.org>
2003-06-17 12:58:42 +0000

    This commit was manufactured by cvs2svn to create branch 'R2_1_maintenance'.
    Cherrypick from master 2003-06-17 12:58:41 UTC David Audel <daudel> 'bug 35877':
        org.eclipse.jdt.core.tests.model/workspace/Completion/src/CompletionClassLiteralAfterAnonymousType1.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/test0470/A.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/Completion/src/CompletionClassLiteralAfterAnonymousType1.java
@@ -0,0 +1,13 @@
+public class CompletionClassLiteralAfterAnonymousType1 {
+	public class X {
+		void foo() {
+			new X() {
+				void bar() {
+					new Object() {
+					};
+					double.
+				}
+			};
+		}
+	}
+}
