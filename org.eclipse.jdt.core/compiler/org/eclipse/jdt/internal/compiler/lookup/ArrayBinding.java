67166780f6a6dde6a39a05a3f0ecadaf0ce4c7f2
Philipe Mulet <pmulet>
2003-07-10 10:57:38 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ArrayBinding.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ArrayBinding.java
@@ -27,18 +27,18 @@ public ArrayBinding(TypeBinding type, int dimensions) {
 	this.leafComponentType = type;
 	this.dimensions = dimensions;
 }
-/* Answer the receiver's constant pool name.
-*
-* NOTE: This method should only be used during/after code gen.
-*/
+/**
+ * Answer the receiver's constant pool name.
+ * NOTE: This method should only be used during/after code gen.
+ * e.g. '[Ljava/lang/Object;'
+ */
 
-public char[] constantPoolName() /*	[Ljava/lang/Object; */ {
+public char[] constantPoolName() {
 	if (constantPoolName != null)
 		return constantPoolName;
 
 	char[] brackets = new char[dimensions];
-	for (int i = dimensions - 1; i >= 0; i--)
-		brackets[i] = '[';
+	for (int i = dimensions - 1; i >= 0; i--) brackets[i] = '[';
 	return constantPoolName = CharOperation.concat(brackets, leafComponentType.signature());
 }
 String debugName() {
