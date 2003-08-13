7d8d6c28953d8af04b4b07af2c2f4422f738a8b9
Olivier Thomann <othomann>
2003-08-13 21:08:29 +0000

    Release a new task to check debug attribute in a jar or .class file

---
+++ org.eclipse.jdt.core/antadapter/org/eclipse/jdt/core/CheckDebugAttributes.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.eclipse.jdt.core.util.IClassFileReader;
+import org.eclipse.jdt.core.util.ICodeAttribute;
+import org.eclipse.jdt.core.util.IMethodInfo;
+import org.eclipse.jdt.internal.antadapter.Util;
+
+public class CheckDebugAttributes extends Task {
+
+	private String file;
+	private String property;
+	
+	public void execute() throws BuildException {
+		if (this.file == null) {
+			throw new BuildException(Util.getString("checkDebugAttributes.file.argument.cannot.be.null")); //$NON-NLS-1$
+		}
+		if (this.property == null) {
+			throw new BuildException(Util.getString("checkDebugAttributes.property.argument.cannot.be.null")); //$NON-NLS-1$
+		}
+		try {
+			boolean hasDebugAttributes = false;
+			if (org.eclipse.jdt.internal.core.Util.isArchiveFileName(this.file)) {
+				ZipFile jarFile = new ZipFile(this.file);
+				for (Enumeration entries = jarFile.entries(); !hasDebugAttributes && entries.hasMoreElements(); ) {
+					ZipEntry entry = (ZipEntry) entries.nextElement();
+					if (org.eclipse.jdt.internal.core.Util.isClassFileName(entry.getName())) {
+						IClassFileReader classFileReader = ToolFactory.createDefaultClassFileReader(this.file, entry.getName(), IClassFileReader.ALL);
+						hasDebugAttributes = checkClassFile(classFileReader);
+					}
+				}
+			} else if (org.eclipse.jdt.internal.core.Util.isClassFileName(this.file)) {
+				IClassFileReader classFileReader = ToolFactory.createDefaultClassFileReader(this.file, IClassFileReader.ALL);
+				hasDebugAttributes = checkClassFile(classFileReader);
+			} else {
+				throw new BuildException(Util.getString("checkDebugAttributes.file.argument.must.be.a.classfile.or.a.jarfile")); //$NON-NLS-1$
+			}
+			if (hasDebugAttributes) {
+				this.project.setUserProperty(this.property, "has debug"); //$NON-NLS-1$
+			}
+		} catch (IOException e) {
+			throw new BuildException(Util.getString("checkDebugAttributes.ioexception.occured") + this.file); //$NON-NLS-1$
+		}
+	}
+	
+	private boolean checkClassFile(IClassFileReader classFileReader) {
+		IMethodInfo[] methodInfos = classFileReader.getMethodInfos();
+		for (int i = 0, max = methodInfos.length; i < max; i++) {
+			ICodeAttribute codeAttribute = methodInfos[i].getCodeAttribute();
+			if (codeAttribute != null && codeAttribute.getLineNumberAttribute() != null) {
+				return true;
+			}	
+		}
+		return false;
+	}
+
+	public void setFile(String value) {
+		this.file = value;
+	}
+	
+	public void setProperty(String value) {
+		this.property = value;
+	}
+}
