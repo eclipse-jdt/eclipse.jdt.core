7f76334b2bb7b495f563a0716e0291f8c0b0738d
Olivier Thomann <othomann>
2003-09-10 17:30:13 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/Scribe.java
+++ org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/Scribe.java
@@ -430,6 +430,10 @@ public class Scribe {
 		column = 1;
 		needSpace = false;
 		lastNumberOfNewLines = 1;
+		// TODO revisit when removing type member alignment
+		if (this.currentAlignment != null && !this.currentAlignment.name.equals("typeMembers")) { //$NON-NLS-1$
+			this.indentationLevel = this.currentAlignment.breakIndentationLevel;
+		}
 	}
 
 	public void printIndentationIfNecessary() {
