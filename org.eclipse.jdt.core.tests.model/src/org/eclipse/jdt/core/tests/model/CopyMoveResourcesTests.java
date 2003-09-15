3fbf7d7b491dc22f98214e330c97192346d8c2e9
Olivier Thomann <othomann>
2003-09-15 14:17:02 +0000

    Fix for Linux

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CopyMoveResourcesTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CopyMoveResourcesTests.java
@@ -482,11 +482,27 @@ public void testCopyReadOnlyPackageFragment() throws CoreException {
 		assertTrue("Not readOnly", getPackage("/P/src2/p1/p2/p3").getResource().isReadOnly());
 		assertTrue("Is readOnly", getFile("/P/src2/p1/p2/p3/X.java").isReadOnly());
 	} finally {
+		IFile xSrcFile = getFile("/P/src/p1/p2/p3/X.java");
+		if (xSrcFile != null) {
+			xSrcFile.setReadOnly(false);
+		}
 		if (pkg2 != null) {
 			pkg2.getResource().setReadOnly(false);
 		}
 		if (pkgSource != null) {
 			pkgSource.getResource().setReadOnly(false);
+		}
+		IPackageFragment p1Fragment = getPackage("/P/src2/p1");
+		if (p1Fragment != null) {
+			p1Fragment.getResource().setReadOnly(false);
+		}
+		IPackageFragment p3Fragment = getPackage("/P/src2/p1/p2/p3");
+		if (p3Fragment != null) {
+			p3Fragment.getResource().setReadOnly(false);
+		}
+		IFile xFile = getFile("/P/src2/p1/p2/p3/X.java");
+		if (xFile != null) {
+			xFile.setReadOnly(false);
 		}
 		deleteFolder("/P/src/p1");
 	}
@@ -870,11 +886,17 @@ public void testMoveReadOnlyPackageFragment() throws CoreException {
 		assertTrue("Not readOnly", getPackage("/P/src2/p1/p2/p3").getResource().isReadOnly());
 		assertTrue("Is readOnly", getFile("/P/src2/p1/p2/p3/X.java").isReadOnly());
 	} finally {
-		if (pkg2 != null) {
-			pkg2.getResource().setReadOnly(false);
+		IPackageFragment p1Fragment = getPackage("/P/src2/p1");
+		if (p1Fragment != null) {
+			p1Fragment.getResource().setReadOnly(false);
 		}
-		if (pkgSource != null) {
-			pkgSource.getResource().setReadOnly(false);
+		IPackageFragment p3Fragment = getPackage("/P/src2/p1/p2/p3");
+		if (p3Fragment != null) {
+			p3Fragment.getResource().setReadOnly(false);
+		}
+		IFile xFile = getFile("/P/src2/p1/p2/p3/X.java");
+		if (xFile != null) {
+			xFile.setReadOnly(false);
 		}
 		deleteFolder("/P/src/p1");
 	}
