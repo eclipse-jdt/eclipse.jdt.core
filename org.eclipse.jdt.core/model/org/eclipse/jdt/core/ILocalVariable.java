abec512c68cdcfa4af91301c9c10dff0375f274a
Jerome lanneluc <jlanneluc>
2003-10-15 14:09:38 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/ILocalVariable.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/ILocalVariable.java
@@ -12,7 +12,9 @@ package org.eclipse.jdt.core;
 
 /**
  * Represents a local variable declared in a method or an initializer.
- * TODO (jreome) specify how it is created (not a child of IMethod)
+ * <code>ILocalVariable</code> are pseudo-elements created as the result of a <code>ICodeAssist.codeSelect(...)</code>
+ * operation. They are not part of the Java model (<code>exists()</code> always returns <code>false</code>) and
+ * they are not included in the children of an <code>IMethod</code> or an <code>IInitializer</code>.
  * <p>
  * This interface is not intended to be implemented by clients.
  * </p>
@@ -21,6 +23,7 @@ package org.eclipse.jdt.core;
 public interface ILocalVariable extends IJavaElement {
 	/**
 	 * Returns the name of this local variable.
+	 * 
 	 * @return the name of this local variable.
 	 */
 	String getElementName();
