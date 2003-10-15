f7caf68ac16c33eed6ef584180545d8f8d13cf1f
Olivier Thomann <othomann>
2003-10-15 18:09:04 +0000

    Release new formatter tests

---
+++ org.eclipse.jdt.core.tests.model/workspace/Formatter/test213/A_out.java
@@ -0,0 +1,8 @@
+/**
+ * This hook is called if the window's close button was pressed, the close menu was selected,
+ * or the ESCAPE key pressed.
+ * The default implementation calls <code>close()</code>.
+ */
+public void closeBoxPressed() {
+	close();
+}
