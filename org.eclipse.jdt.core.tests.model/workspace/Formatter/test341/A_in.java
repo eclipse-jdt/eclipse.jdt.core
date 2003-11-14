ed4bf436d2b67e95dc545f2f6910c1bd6fb58bc2
cvs2svn <platform-releng-dev@eclipse.org>
2003-11-14 19:30:47 +0000

    This commit was manufactured by cvs2svn to create branch 'JDK_1_5'.
    Cherrypick from master 2003-11-14 19:30:46 UTC Olivier Thomann <othomann> 'Fixes for 46689 and 46690':
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test338/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test338/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test339/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test339/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test340/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test340/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test341/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test341/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test342/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test342/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test343/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test343/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test344/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test344/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test345/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test345/A_out.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/Formatter/test341/A_in.java
@@ -0,0 +1,7 @@
+class Example {
+    void foo() {
+        int a = 1 - -4;
+        int b =-a;
+        Other.check(-a);
+     }
+}
---
+++ org.eclipse.jdt.core.tests.model/workspace/Formatter/test343/A_out.java
@@ -0,0 +1,4 @@
+class Example {
+	int c = 2;
+	int a = 1,b = 2,d;
+}
