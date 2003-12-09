6f8d3d54ade38f50e10dbeb27c38e99389cc7710
Philipe Mulet <pmulet>
2003-12-09 17:23:54 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/JavaConventions.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/JavaConventions.java
@@ -540,7 +540,9 @@ public final class JavaConventions {
 			}	
 		}	
 		// allow custom output nesting in project's output if all source entries have a custom output
-		if (potentialNestedOutput != null && sourceEntryCount > outputCount-1) {
+		if (sourceEntryCount <= outputCount-1) {
+			allowNestingInOutputLocations[0] = true;
+		} else if (potentialNestedOutput != null) {
 			return new JavaModelStatus(IJavaModelStatusConstants.INVALID_CLASSPATH, Util.bind("classpath.cannotNestOutputInOutput", potentialNestedOutput.makeRelative().toString(), outputLocations[0].makeRelative().toString())); //$NON-NLS-1$
 		}
 		
@@ -632,14 +634,13 @@ public final class JavaConventions {
 			}
 
 			// prevent nesting entry inside output location - when distinct from project or a source folder
-			if ((index = indexOfEnclosingPath(entryPath, outputLocations, outputCount)) != -1) {
-				if (!allowNestingInOutputLocations[index]) {
-					// allow nesting in project's output if all source entries have a custom output
-					if (index != 0 || sourceEntryCount > outputCount - 1) {
-						return new JavaModelStatus(IJavaModelStatusConstants.INVALID_CLASSPATH, Util.bind("classpath.cannotNestEntryInOutput", entryPath.makeRelative().toString(), outputLocations[index].makeRelative().toString())); //$NON-NLS-1$
-					}
+			for (int j = 0; j < outputCount; j++){
+				if (allowNestingInOutputLocations[j]) continue;
+				IPath currentOutput = outputLocations[j];
+				if (currentOutput.isPrefixOf(entryPath)) {
+					return new JavaModelStatus(IJavaModelStatusConstants.INVALID_CLASSPATH, Util.bind("classpath.cannotNestEntryInOutput", entryPath.makeRelative().toString(), currentOutput.makeRelative().toString())); //$NON-NLS-1$
 				}
-			}
+			}			
 		}
 		// ensure that no specific output is coincidating with another source folder (only allowed if matching current source folder)
 		// 36465 - for 2.0 backward compatibility, only check specific output locations (the default can still coincidate)
