234e90f1063594cbfe450e6804c5db0a515eda2c
Jerome lanneluc <jlanneluc>
2004-02-02 17:05:43 +0000

    50571

--- org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/indexing/IndexManager.java
+++ org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/indexing/IndexManager.java
@@ -481,18 +481,26 @@ public void saveIndexes() {
 		ReadWriteMonitor monitor = getMonitorFor(index);
 		if (monitor == null) continue; // index got deleted since acquired
 		try {
-			monitor.enterWrite();
-			try {
-				saveIndex(index);
-			} catch(IOException e){
-				if (VERBOSE) {
-					JobManager.verbose("-> got the following exception while saving:"); //$NON-NLS-1$
-					e.printStackTrace();
+			// take read lock before checking if index has changed
+			// don't take write lock yet since it can cause a deadlock (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=50571)
+			monitor.enterRead(); 
+			if (index.hasChanged()) {
+				monitor.exitRead();
+				monitor.enterWrite();
+				try {
+					saveIndex(index);
+				} catch(IOException e){
+					if (VERBOSE) {
+						JobManager.verbose("-> got the following exception while saving:"); //$NON-NLS-1$
+						e.printStackTrace();
+					}
+					//Util.log(e);
+				} finally {
+					monitor.exitWriteEnterRead();
 				}
-				//Util.log(e);
 			}
 		} finally {
-			monitor.exitWrite();
+			monitor.exitRead();
 		}
 	}
 	needToSave = false;
