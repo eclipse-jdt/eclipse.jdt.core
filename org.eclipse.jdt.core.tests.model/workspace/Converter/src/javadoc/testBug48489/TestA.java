802af0b0cecbbf8b77770a0bba7fa4b4732e1b5e
cvs2svn <platform-releng-dev@eclipse.org>
2004-02-04 19:51:30 +0000

    This commit was manufactured by cvs2svn to create branch 'THAW_402'.
    Cherrypick from master 2004-02-04 19:51:29 UTC Olivier Thomann <othomann> 'Add new tests':
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/test013/Test.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug48489/TestA.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug48489/TestB.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug48489/TestC.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug48489/TestD.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug48489/TestE.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug50898/TestA.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug50898/TestB.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test459/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test459/A_out.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test460/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test460/A_out.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug48489/TestA.java
@@ -0,0 +1,11 @@
+package testBug48489;
+import java.util.*;
+public class TestA {
+	/**
+	 * Javadoc comment
+	 */
+	public static void main(String[] args) {
+		/* method main */
+		System.out.println("Hello" + " world"); // comment
+	}
+}
