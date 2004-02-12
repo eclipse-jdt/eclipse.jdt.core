01791ae44abfecd3362b8a1aa803cf35d9b11b9e
cvs2svn <platform-releng-dev@eclipse.org>
2004-02-12 16:35:43 +0000

    This commit was manufactured by cvs2svn to create branch 'THAW_402'.
    Cherrypick from master 2004-02-12 16:35:42 UTC Olivier Thomann <othomann> 'Fix for 51603':
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51363/Test.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51476/X1.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51476/X2.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51476/X3.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51478/X1.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51478/X2.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51478/X3.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51508/X.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51650/X.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51770/X.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test476/A_in.java
        org.eclipse.jdt.core.tests.model/workspace/Formatter/test476/A_out.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51650/X.java
@@ -0,0 +1,9 @@
+package javadoc.testBug51650;
+public class X {
+  /**
+   * @see Exception
+   * {@link Exception}
+   */
+  public void foo() {
+  }
+}
---
+++ org.eclipse.jdt.core.tests.model/workspace/Formatter/test476/A_in.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class FormatTest
+	{
+
+	/************************************
+	* method_one
+	* 
+	* 		nothing special.
+	************************************/
+
+	public void method_one()
+		{
+		int i = 1;
+
+		int j = 2;
+
+		for ( i = 12; i < 100; i++ )
+			{
+			if ( i % 2 == 0 )
+				System.out.println( "xx" );
+			else
+				System.out.println( "yy" );
+
+			j = 0;
+			}
+		
+		} // method_one
+
+	} // class FormatTest
