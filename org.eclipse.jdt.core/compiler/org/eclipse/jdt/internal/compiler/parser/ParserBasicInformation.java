d05b1ec606aa2209241b5050dbcae27980ccb548
Olivier Thomann <othomann>
2004-02-24 21:41:21 +0000

    Update the 1.5 grammar

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/ParserBasicInformation.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/ParserBasicInformation.java
@@ -12,25 +12,23 @@ package org.eclipse.jdt.internal.compiler.parser;
 
 /*An interface that contains static declarations for some basic information
  about the parser such as the number of rules in the grammar, the starting state, etc...*/
-
 public interface ParserBasicInformation {
 
-	public final static int
-	    ERROR_SYMBOL      = 108,
-	    MAX_NAME_LENGTH   = 36,
-	    NUM_STATES        = 911,
-	    NT_OFFSET         = 108,
-	    SCOPE_UBOUND      = 104,
-	    SCOPE_SIZE        = 105,
-	    LA_STATE_OFFSET   = 9902,
-	    MAX_LA            = 1,
-	    NUM_RULES         = 649,
-	    NUM_TERMINALS     = 108,
-	    NUM_NON_TERMINALS = 286,
-	    NUM_SYMBOLS       = 394,
-	    START_STATE       = 1205,
-	    EOFT_SYMBOL       = 59,
-	    EOLT_SYMBOL       = 59,
-	    ACCEPT_ACTION     = 9901,
-	    ERROR_ACTION      = 9902;
+	int ERROR_SYMBOL = 108,
+		MAX_NAME_LENGTH = 36,
+		NUM_STATES = 928,
+		NT_OFFSET = 108,
+		SCOPE_UBOUND = 133,
+		SCOPE_SIZE = 134,
+		LA_STATE_OFFSET = 10948,
+		MAX_LA = 1,
+		NUM_RULES = 658,
+		NUM_TERMINALS = 108,
+		NUM_NON_TERMINALS = 286,
+		NUM_SYMBOLS = 394,
+		START_STATE = 1550,
+		EOFT_SYMBOL = 69,
+		EOLT_SYMBOL = 69,
+		ACCEPT_ACTION = 10947,
+		ERROR_ACTION = 10948;
 }
