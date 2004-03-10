13ff61ca720a271640d5e246e392aaa0319534be
Olivier Thomann <othomann>
2004-03-10 16:55:41 +0000

    Stream 1.5 - hack to support current javac implementation

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/AnnotationComponentValue.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/AnnotationComponentValue.java
@@ -61,6 +61,7 @@ public class AnnotationComponentValue extends ClassFileStruct implements IAnnota
 						case IConstantPoolConstant.CONSTANT_Double :
 						case IConstantPoolConstant.CONSTANT_Integer :
 						case IConstantPoolConstant.CONSTANT_String :
+						case IConstantPoolConstant.CONSTANT_Utf8 : //TODO should be Constant_String
 							break;
 						default :
 							throw new ClassFormatException(ClassFormatException.INVALID_CONSTANT_POOL_ENTRY);
