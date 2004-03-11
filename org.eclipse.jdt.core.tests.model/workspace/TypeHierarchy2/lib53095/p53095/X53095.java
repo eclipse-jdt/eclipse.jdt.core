bfce2090aa2cc7a9bcadb6f98ccc58514df10a1b
cvs2svn <platform-releng-dev@eclipse.org>
2004-03-11 20:12:24 +0000

    This commit was manufactured by cvs2svn to create branch 'JDK_1_5'.
    Cherrypick from master 2004-03-11 20:12:23 UTC Jim Desrivieres <jdesrivieres> 'Reparent Annotation under Expression; replace ExtendedModifier by IExtendedModifier':
        org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/TypeHierarchyTests2.java
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy2/.classpath
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy2/.project
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy2/lib53095/p53095/X53095.class
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy2/lib53095/p53095/X53095.java
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy2/test54043.jar
        org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/IExtendedModifier.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy2/lib53095/p53095/X53095.java
@@ -0,0 +1,3 @@
+package p53095;
+public class X53095 extends RuntimeException {
+}
---
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/IExtendedModifier.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core.dom;
+
+/**
+ * Common interface for AST nodes that represent modifiers or
+ * annotations.
+ * <pre>
+ * ExtendedModifier:
+ *   Modifier
+ *   Annotation
+ * </pre>
+ * <p>
+ * Note: Support for annotation metadata is an experimental language feature 
+ * under discussion in JSR-175 and under consideration for inclusion
+ * in the 1.5 release of J2SE. The support here is therefore tentative
+ * and subject to change.
+ * </p>
+ * @since 3.0
+ */
+public interface IExtendedModifier {
+	
+	/**
+	 * Returns whether this extended modifier is a standard modifier.
+	 * 
+	 * @return <code>true</code> if this is a standard modifier
+	 * (instance of {@link Modifier}), and <code>false</code> otherwise
+	 */ 
+	public boolean isModifier();
+	
+	/**
+	 * Returns whether this extended modifier is an annotation.
+	 * 
+	 * @return <code>true</code> if this is an annotation
+	 * (instance of a subclass of {@link Annotation}), and 
+	 * <code>false</code> otherwise
+	 */ 
+	public boolean isAnnotation();
+}
+
