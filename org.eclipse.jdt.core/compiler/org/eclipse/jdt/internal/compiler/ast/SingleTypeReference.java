5905143c866a5ab2f0ec618e5e06095f39d5558f
Philipe Mulet <pmulet>
2004-03-29 14:39:43 +0000

    1.5

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/SingleTypeReference.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/SingleTypeReference.java
@@ -49,14 +49,24 @@ public class SingleTypeReference extends TypeReference {
 
 	public TypeBinding resolveTypeEnclosing(BlockScope scope, ReferenceBinding enclosingType) {
 
-		ReferenceBinding memberTb = scope.getMemberType(token, enclosingType);
-		if (!memberTb.isValidBinding()) {
-			scope.problemReporter().invalidEnclosingType(this, memberTb, enclosingType);
+		ReferenceBinding memberType = scope.getMemberType(token, enclosingType);
+		if (!memberType.isValidBinding()) {
+			scope.problemReporter().invalidEnclosingType(this, memberType, enclosingType);
 			return null;
 		}
-		if (isTypeUseDeprecated(memberTb, scope))
-			scope.problemReporter().deprecatedType(memberTb, this);
-		return this.resolvedType = memberTb;
+		if (isTypeUseDeprecated(memberType, scope)) {
+			scope.problemReporter().deprecatedType(memberType, this);
+		}
+		// check raw type
+		if (memberType.isArrayType()) {
+		    TypeBinding leafComponentType = memberType.leafComponentType();
+		    if (leafComponentType.isGenericType()) { // raw type
+		        return this.resolvedType = scope.createArray(scope.createRawType((ReferenceBinding)leafComponentType), memberType.dimensions());
+		    }
+		} else if (memberType.isGenericType()) {
+	        return this.resolvedType = scope.createRawType(memberType); // raw type
+		}			
+		return this.resolvedType = memberType;
 	}
 
 	public void traverse(ASTVisitor visitor, BlockScope scope) {
