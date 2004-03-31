9434296f2033162ed6e8a05e622a7f5432c377f2
Jim Desrivieres <jdesrivieres>
2004-03-31 18:49:43 +0000

    Fix comment

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ClassInstanceCreation.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ClassInstanceCreation.java
@@ -28,8 +28,8 @@ import java.util.List;
  * <pre>
  * ClassInstanceCreation:
  *        [ Expression <b>.</b> ]
- *            [ <b>&lt;</b> Type { <b>,</b> Type } <b>&gt;</b> ]
- *            <b>new</b> Type <b>(</b> [ Expression { <b>,</b> Expression } ] <b>)</b>
+ *            <b>new</b> [ <b>&lt;</b> Type { <b>,</b> Type } <b>&gt;</b> ]
+ *            Type <b>(</b> [ Expression { <b>,</b> Expression } ] <b>)</b>
  *            [ AnonymousClassDeclaration ]
  * </pre>
  * <p>
