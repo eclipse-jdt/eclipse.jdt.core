fd5977cf9eb578c95407fe9d481762633664a0d9
cvs2svn <platform-releng-dev@eclipse.org>
2004-04-02 10:12:36 +0000

    This commit was manufactured by cvs2svn to create branch 'JDK_1_5'.
    Cherrypick from master 2004-04-02 10:12:35 UTC Jerome lanneluc <jlanneluc> '29964':
        org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/InclusionPatternsTests.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0541/A.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0541/B.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0541/A.java
@@ -0,0 +1,11 @@
+package test0541;
+
+class A {
+    private int[] tab = new int[] {
+        B.CODNIVEL3, B.NOMNIVEL3, B.CODAUTORIZA,
+        B.NOMUSRCIA, B.TIPAUTORIZA, B.NOMAUTORIZA,
+        B.CODCTOAUTORIZA, B.NOMCTOAUTORIZA, B.CODMON,
+        B.NOMMON, B.MCAINH, B.IMPORTEAUTORIZA,
+        B.NUMDECIMALES
+    };   
+}
---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0541/B.java
@@ -0,0 +1,21 @@
+package test0541;
+
+public class B {
+    protected static final int NUMSECUK = 0;
+    protected static final int CODCIA = 1;
+    protected static final int CODNIVEL3 = 2;
+    protected static final int NOMNIVEL3 = 3;
+    protected static final int CODAUTORIZA = 4;
+    protected static final int NOMUSRCIA = 5;
+    protected static final int TIPAUTORIZA = 6;
+    protected static final int NOMAUTORIZA = 7;
+    protected static final int CODCTOAUTORIZA = 8;
+    protected static final int NOMCTOAUTORIZA = 9;
+    protected static final int IMPORTEAUTORIZA = 10;
+    protected static final int CODMON = 11;
+    protected static final int NOMMON = 12;
+    protected static final int NUMDECIMALES = 13;
+    protected static final int CODUSR = 14;
+    protected static final int FECACTU = 15;
+    protected static final int MCAINH = 16;
+}
