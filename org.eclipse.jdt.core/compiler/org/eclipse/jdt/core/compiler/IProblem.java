75905d5e01136badf9032b141692de0f24296eee
Jim Desrivieres <jdesrivieres>
2004-04-05 18:00:25 +0000

    Add missing @since 3.0 tag to IProblem.Javadoc

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/core/compiler/IProblem.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/core/compiler/IProblem.java
@@ -196,6 +196,9 @@ public interface IProblem {
 	int ImportRelated = 0x10000000;
 	int Internal = 0x20000000;
 	int Syntax = 0x40000000;
+	/**
+	 * @since 3.0
+	 */
 	int Javadoc = 0x80000000;
 	
 	/**
