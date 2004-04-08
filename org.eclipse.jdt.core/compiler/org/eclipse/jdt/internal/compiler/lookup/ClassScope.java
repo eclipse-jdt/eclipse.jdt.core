0cef88990b851def8b3e927443996ca444d24266
Philipe Mulet <pmulet>
2004-04-08 10:36:34 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ClassScope.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ClassScope.java
@@ -311,33 +311,46 @@ public class ClassScope extends Scope {
 		} else if (sourceType.isLocalType()) {
 			if (sourceType.isAnonymousType()) 
 			    modifiers |= AccFinal;
-			MethodScope methodScope = methodScope();
-			ReferenceContext refContext = methodScope.referenceContext;
-			if (refContext instanceof TypeDeclaration) {
-			    
-				SourceTypeBinding type = ((TypeDeclaration) refContext).binding;
-
-				// inside field declaration ? check field modifier to see if deprecated
-				if (methodScope.initializedField != null) {
-						// currently inside this field initialization
-					if (methodScope.initializedField.isViewedAsDeprecated() && !sourceType.isDeprecated()){
-						modifiers |= AccDeprecatedImplicitly;
-					}
-				} else {
-					if (type.isStrictfp())
-						modifiers |= AccStrictfp;
-					if (type.isViewedAsDeprecated() && !sourceType.isDeprecated()) 
-						modifiers |= AccDeprecatedImplicitly;
-				}					
-			} else {
-				MethodBinding method = ((AbstractMethodDeclaration) refContext).binding;
-				if (method != null){
-					if (method.isStrictfp())
-						modifiers |= AccStrictfp;
-					if (method.isViewedAsDeprecated() && !sourceType.isDeprecated())
-						modifiers |= AccDeprecatedImplicitly;
+			Scope scope = this;
+			do {
+				switch (scope.kind) {
+					case METHOD_SCOPE :
+						MethodScope methodScope = (MethodScope) scope;
+						if (methodScope.isInsideInitializer()) {
+							SourceTypeBinding type = ((TypeDeclaration) methodScope.referenceContext).binding;
+			
+							// inside field declaration ? check field modifier to see if deprecated
+							if (methodScope.initializedField != null) {
+									// currently inside this field initialization
+								if (methodScope.initializedField.isViewedAsDeprecated() && !sourceType.isDeprecated()){
+									modifiers |= AccDeprecatedImplicitly;
+								}
+							} else {
+								if (type.isStrictfp())
+									modifiers |= AccStrictfp;
+								if (type.isViewedAsDeprecated() && !sourceType.isDeprecated()) 
+									modifiers |= AccDeprecatedImplicitly;
+							}					
+						} else {
+							MethodBinding method = ((AbstractMethodDeclaration) methodScope.referenceContext).binding;
+							if (method != null){
+								if (method.isStrictfp())
+									modifiers |= AccStrictfp;
+								if (method.isViewedAsDeprecated() && !sourceType.isDeprecated())
+									modifiers |= AccDeprecatedImplicitly;
+							}
+						}
+						break;
+					case CLASS_SCOPE :
+						// local member
+						if (enclosingType.isStrictfp())
+							modifiers |= AccStrictfp;
+						if (enclosingType.isViewedAsDeprecated() && !sourceType.isDeprecated())
+							modifiers |= AccDeprecatedImplicitly;
+						break;
 				}
-			}
+				scope = scope.parent;
+			} while (scope != null);
 		}
 		// after this point, tests on the 16 bits reserved.
 		int realModifiers = modifiers & AccJustFlag;
