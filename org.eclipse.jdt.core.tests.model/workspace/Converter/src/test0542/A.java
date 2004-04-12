05ce5f1c5ad158b7a2b3a23dd3336aa7976318bd
cvs2svn <platform-releng-dev@eclipse.org>
2004-04-12 16:22:20 +0000

    This commit was manufactured by cvs2svn to create branch 'JDK_1_5'.
    Cherrypick from master 2004-04-12 16:22:19 UTC Olivier Thomann <othomann> 'HEAD - Added test for constant value. ASTConverterTest2.test0542':
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0542/A.java
        org.eclipse.jdt.core.tests.model/workspace/Resolve/src/ResolveEndOfFile.java
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchyNotification/src/p3/A.java
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchyNotification/src/p3/B.java

---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0542/A.java
@@ -0,0 +1,14 @@
+package test0542;
+
+public class A {
+	private static final String STRING_FIELD = "Hello world!";
+	private static final boolean BOOLEAN_FIELD = true;
+	private static final byte BYTE_FIELD = 1;
+	private static final char CHAR_FIELD = '{';
+	private static final double DOUBLE_FIELD = 3.1415;
+	private static final float FLOAT_FIELD = 3.14159f;
+	private static final int INT_FIELD = Integer.MAX_VALUE; //0x7fffffff
+	private static final long LONG_FIELD = 34L;
+	private static final short SHORT_FIELD = 130;
+	private int int_field = Integer.MAX_VALUE; //0x7fffffff
+}
---
+++ org.eclipse.jdt.core.tests.model/workspace/TypeHierarchyNotification/src/p3/A.java
@@ -0,0 +1,4 @@
+package p3;
+/* Test case for bug 51867 An anonymous type is missing in type hierarchy when editor is modified */
+public class A {
+}
