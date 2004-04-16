8b646a2b78393ed9ea2f29131035a93ec637cf8c
Jerome lanneluc <jlanneluc>
2004-04-16 11:14:24 +0000

    Improve search participant API

--- org.eclipse.jdt.core/search/org/eclipse/jdt/core/search/SearchDocument.java
+++ org.eclipse.jdt.core/search/org/eclipse/jdt/core/search/SearchDocument.java
@@ -11,30 +11,48 @@
 package org.eclipse.jdt.core.search;
 
 /**
- * TODO (jerome) spec
+ * A search document encapsulates a content to be either indexed or searched in.
+ * A search particpant creates a search document.
+ * 
  * @since 3.0
  */
 public class SearchDocument {
 	protected String documentPath;
 	protected SearchParticipant participant;
+	
+	/**
+	 * Internal field: do not use
+	 */
 	public org.eclipse.jdt.internal.core.index.Index index;
 
+	/**
+	 * Creates a new search document.
+	 * 
+	 * @param documentPath the path to the document on disk or <code>null</code> if not provided
+	 * @param participant the participant that creates the search document
+	 */
 	public SearchDocument(String documentPath, SearchParticipant participant) {
 		this.documentPath = documentPath;
 		this.participant = participant;
 	}
 
 	/**
+	 * Returns the contents of this document.
 	 * Contents may be different from actual resource at corresponding document path,
 	 * in case of preprocessing.
+	 * 
+	 * @return the contents of this document.
 	 */
 	public byte[] getByteContents() {
 		return null;
 	}
 
 	/**
+	 * Returns the contents of this document.
 	 * Contents may be different from actual resource at corresponding document path,
 	 * in case of preprocessing.
+	 * 
+	 * @return the contents of this document.
 	 */
 	public char[] getCharContents() {
 		return null;
@@ -42,6 +60,8 @@ public class SearchDocument {
 
 	/**
 	 * Returns the encoding for this document
+	 * 
+	 * @return the encoding for this document
 	 */
 	public String getEncoding() {
 		return null;
@@ -49,13 +69,17 @@ public class SearchDocument {
 
 	/**
 	 * Returns the participant that created this document
+	 * 
+	 * @return the participant that created this document
 	 */
 	public SearchParticipant getParticipant() {
 		return this.participant;
 	}
 	
 	/**
-	 * Path to the original document to publicly mention in index or search results.
+	 * Returns the path to the original document to publicly mention in index or search results.
+	 * 
+	 * @return the path to the document
 	 */	
 	public String getPath() {
 		return this.documentPath;
