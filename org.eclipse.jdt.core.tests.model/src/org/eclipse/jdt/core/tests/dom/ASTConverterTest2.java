0ed7a0c4b1db768a45ce9259dfbd149ef35e5448
Olivier Thomann <othomann>
2004-04-26 19:38:13 +0000

    HEAD - Add test case for 59848

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterTest2.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterTest2.java
@@ -95,7 +95,7 @@ public class ASTConverterTest2 extends ConverterTestSetup {
 			return new Suite(ASTConverterTest2.class);		
 		}
 		TestSuite suite = new Suite(ASTConverterTest2.class.getName());
-		suite.addTest(new ASTConverterTest2("test0545"));
+		suite.addTest(new ASTConverterTest2("test0546"));
 		return suite;
 	}
 	/**
@@ -4680,5 +4680,48 @@ public class ASTConverterTest2 extends ConverterTestSetup {
 		typeDeclaration = (TypeDeclaration) node;
 		typeBinding = typeDeclaration.resolveBinding();
 		assertEquals("Wrong key", "test0545/Test", typeBinding.getKey());
+	}
+	
+	/**
+	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=59848
+	 */
+	public void test0546() throws JavaModelException {
+		ICompilationUnit sourceUnit = getCompilationUnit("Converter", "src", "test0546", "A.java"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		ASTNode result = runConversion(sourceUnit, true);
+		final CompilationUnit unit = (CompilationUnit) result;
+		assertEquals("Wrong number of problems", 0, unit.getProblems().length); //$NON-NLS-1$
+		ASTNode node = getASTNode(unit, 1, 0, 0);
+		assertEquals("not a variable declaration", ASTNode.VARIABLE_DECLARATION_STATEMENT, node.getNodeType()); //$NON-NLS-1$
+		VariableDeclarationStatement variableDeclarationStatement = (VariableDeclarationStatement) node;
+		List fragments = variableDeclarationStatement.fragments();
+		assertEquals("Wrong size", 1, fragments.size());
+		VariableDeclarationFragment fragment = (VariableDeclarationFragment) fragments.get(0);
+		IVariableBinding variableBinding = fragment.resolveBinding();
+		ITypeBinding typeBinding = variableBinding.getType();
+		assertTrue("An anonymous type binding", !typeBinding.isAnonymous());
+		Expression initializer = fragment.getInitializer();
+		assertEquals("not a class instance creation", ASTNode.CLASS_INSTANCE_CREATION, initializer.getNodeType()); //$NON-NLS-1$
+		ClassInstanceCreation classInstanceCreation = (ClassInstanceCreation) initializer;
+		AnonymousClassDeclaration anonymousClassDeclaration = classInstanceCreation.getAnonymousClassDeclaration();
+		ITypeBinding typeBinding2 = anonymousClassDeclaration.resolveBinding();
+		assertTrue("Not an anonymous type binding", typeBinding2.isAnonymous());
+		ITypeBinding typeBinding3 = classInstanceCreation.resolveTypeBinding();
+		assertTrue("Not an anonymous type binding", typeBinding3.isAnonymous());
+		node = getASTNode(unit, 1, 0, 1);
+		assertEquals("not a expression statement", ASTNode.EXPRESSION_STATEMENT, node.getNodeType()); //$NON-NLS-1$
+		ExpressionStatement statement = (ExpressionStatement) node;
+		Expression expression = statement.getExpression();
+		assertEquals("not a method invocation", ASTNode.METHOD_INVOCATION, expression.getNodeType()); //$NON-NLS-1$
+		MethodInvocation methodInvocation = (MethodInvocation) expression;
+		Expression expression2 = methodInvocation.getExpression();
+		assertEquals("not a simple name", ASTNode.SIMPLE_NAME, expression2.getNodeType()); //$NON-NLS-1$
+		SimpleName simpleName = (SimpleName) expression2;
+		ITypeBinding typeBinding4 = simpleName.resolveTypeBinding();
+		assertTrue("An anonymous type binding", !typeBinding4.isAnonymous());		
+		Name name = classInstanceCreation.getName();
+		IBinding binding = name.resolveBinding();
+		assertEquals("Wrong type", IBinding.TYPE, binding.getKind());
+		ITypeBinding typeBinding5 = (ITypeBinding) binding;
+		assertTrue("An anonymous type binding", !typeBinding5.isAnonymous());		
 	}
 }
