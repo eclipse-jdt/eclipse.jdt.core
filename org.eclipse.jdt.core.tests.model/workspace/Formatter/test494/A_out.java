d953024e04caa4804ce7ee43e1c0db8cc73acdeb
Olivier Thomann <othomann>
2004-04-29 15:24:56 +0000

    HEAD - Fixes for 58565 and 58724

---
+++ org.eclipse.jdt.core.tests.model/workspace/Formatter/test494/A_out.java
@@ -0,0 +1,25 @@
+public class A {
+   public void setBorderType( JComponent c, int borderType ) {
+      myOtherData.setBorderType( borderType );
+      if ( c != null ) {
+         switch ( borderType ) {
+            case 0 : // none
+               c.setBorder( null );
+               break;
+            case 1 : // line
+               c.setBorder( BorderFactory.createLineBorder( Color.black ) );
+               break;
+            case 3 : // bevel lowered
+               c.setBorder( BorderFactory.createLoweredBevelBorder() );
+               break;
+            case 4 : // bevel raised
+               c.setBorder( BorderFactory.createRaisedBevelBorder() );
+               break;
+            case 5 : // etched
+               c.setBorder( BorderFactory.createEtchedBorder() );
+               break;
+            default :
+         } // end switch
+      }
+   }
+}
---
+++ org.eclipse.jdt.core.tests.model/workspace/Formatter/test496/A_out.java
@@ -0,0 +1,8 @@
+public class A {
+
+    void doX() {
+        if (1 > 0) { return; //
+        }
+        return;
+    }
+}
