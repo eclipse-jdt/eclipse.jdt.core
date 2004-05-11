4fdd7197b3384381fd94c4e9bd7106d6b2a293c2
cvs2svn <platform-releng-dev@eclipse.org>
2004-05-11 11:21:17 +0000

    This commit was manufactured by cvs2svn to create branch 'JDK_1_5'.
    Cherrypick from master 2004-05-11 11:21:16 UTC Jerome lanneluc <jlanneluc> '60365':
        org.eclipse.jdt.core.tests.model/workspace/Completion/src2/CompletionKeywordThis15.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51600/Test.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51600/Test51617.java
        org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51617/Test.java
        org.eclipse.jdt.core.tests.model/workspace/Encoding/src/testUTF8BOM/Test.java
        org.eclipse.jdt.core.tests.model/workspace/TypeHierarchy/test60365.jar

---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/javadoc/testBug51600/Test51617.java
@@ -0,0 +1,13 @@
+package javadoc.testBug51600;
+/**
+ * This bug is a duplicate of 51600...
+ */
+public class Test51617 {
+	/**
+	 * Sets the name of a TestCase
+	 * @exception NullPointerException
+	 * @param name The name to set
+	 */
+	public void setName1(String name) {
+	}
+}
