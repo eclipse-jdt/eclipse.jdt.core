d7956e7c6003328479aec6af7533d26b1fd09fcd
ptff <ptff>
2004-05-25 10:53:14 +0000

    51660 (fix issue with ':')

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/AbstractCommentParser.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/AbstractCommentParser.java
@@ -211,6 +211,7 @@ public abstract class AbstractCommentParser {
 											case '%':
 											case '&':
 											case '\'':
+											case ':':
 											case '-':
 											case '<' :
 											case '>':
