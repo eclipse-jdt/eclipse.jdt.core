e9de93c88cb5b7a2ff0691598dd2ddf4021e925a
ptff <ptff>
2004-05-25 10:38:58 +0000

    51660

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterJavadocTest.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterJavadocTest.java
@@ -60,7 +60,8 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	// Unicode tests
 	protected static boolean unicode = false;
 	// Unix tests
-	protected static boolean unix = false;
+	final boolean unix;
+	static final String UNIX_SUPPORT = System.getProperty("unix");
 	// Doc Comment support
 	static final String DOC_COMMENT_SUPPORT = System.getProperty("doc.support");
 	final String docCommentSupport;
@@ -91,15 +92,16 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * @param name
 	 * @param support
 	 */
-	public ASTConverterJavadocTest(String name, String support) {
+	public ASTConverterJavadocTest(String name, String support, String unix) {
 		super(name);
 		this.docCommentSupport = support;
+		this.unix = "true".equals(unix);
 	}
 	/**
 	 * @param name
 	 */
 	public ASTConverterJavadocTest(String name) {
-		this(name, JavaCore.ENABLED);
+		this(name, JavaCore.ENABLED, UNIX_SUPPORT);
 	}
 
 	public static Test suite() {
@@ -108,10 +110,10 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 //		if ("true".equals(param)) {
 //			unicode = true;
 //		}
-		String param = System.getProperty("unix");
-		if ("true".equals(param)) {
-			unix = true;
-		}
+//		String param = System.getProperty("unix");
+//		if ("true".equals(param)) {
+//			unix = true;
+//		}
 		if (true) {
 			if (DOC_COMMENT_SUPPORT == null) {
 				buildSuite(suite, JavaCore.ENABLED);
@@ -125,7 +127,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 
 		// Run test cases subset
 		System.err.println("WARNING: only subset of tests will be executed!!!");
-		suite.addTest(new ASTConverterJavadocTest("testBug63044"));
+		suite.addTest(new ASTConverterJavadocTest("testBug51660"));
 		return suite;
 	}
 
@@ -134,7 +136,15 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 		Method[] methods = c.getMethods();
 		for (int i = 0, max = methods.length; i < max; i++) {
 			if (methods[i].getName().startsWith("test")) { //$NON-NLS-1$
-				suite.addTest(new ASTConverterJavadocTest(methods[i].getName(), support));
+				suite.addTest(new ASTConverterJavadocTest(methods[i].getName(), support, UNIX_SUPPORT));
+			}
+		}
+		// when unix support not specified, also run using unix format
+		if (UNIX_SUPPORT == null) {
+			for (int i = 0, max = methods.length; i < max; i++) {
+				if (methods[i].getName().startsWith("test")) { //$NON-NLS-1$
+					suite.addTest(new ASTConverterJavadocTest(methods[i].getName(), support, "true"));
+				}
 			}
 		}
 	}
@@ -143,7 +153,8 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 * @see junit.framework.TestCase#getName()
 	 */
 	public String getName() {
-		return "Doc "+this.docCommentSupport+" - "+super.getName();
+		String strUnix = this.unix ? " - Unix" : "";
+		return "Doc "+this.docCommentSupport+strUnix+" - "+super.getName();
 	}
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
@@ -1237,7 +1248,9 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 			if (length > 0) {
 				problems.append("  - "+this.prefix+length+" problems:"); //$NON-NLS-1$
 				for (int i = 0; i < problemsList.length; i++) {
-					problems.append("	+ "+problemsList[i]);
+					problems.append("	+ ");
+					problems.append(problemsList[i]);
+					problems.append("\n");
 				}
 			}
 		}
@@ -1875,6 +1888,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 */
 	public void testBug53075() throws JavaModelException {
 		ICompilationUnit unit = getCompilationUnit("Converter" , "src", "javadoc.testBug53075", "X.java"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		boolean pb = this.packageBinding;
 		this.packageBinding = false;
 		CompilationUnit compilUnit = verifyComments(unit);
 		if (this.docCommentSupport.equals(JavaCore.ENABLED)) {
@@ -1886,6 +1900,7 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 			tagElement = (TagElement) docComment.tags().get(1);
 			assumeEquals("Wrong tag type!", TagElement.TAG_LINKPLAIN, tagElement.getTagName());
 		}
+		this.packageBinding = pb;
 	}
 
 	/**
@@ -1954,5 +1969,100 @@ public class ASTConverterJavadocTest extends ConverterTestSetup {
 	 */
 	public void testBug63044() throws JavaModelException {
 		verifyComments("testBug63044");
+	}
+
+	/**
+	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=51660
+	 */
+	public void testBug51660() throws JavaModelException {
+		this.stopOnFailure = false;
+		ICompilationUnit unit = getCompilationUnit("Converter" , "src", "javadoc.testBug51660", "Test.java"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		CompilationUnit compilUnit = verifyComments(unit);
+		if (this.docCommentSupport.equals(JavaCore.ENABLED)) {
+			String[] tagNames = {
+				"@ejb",
+				"@ejb\"bean test non-java id character '\"",
+				"@ejb",
+				"@ejb",
+				"@ejb",
+				"@ejb",
+				"@ejb(bean",
+				"@ejb)bean",
+				"@ejb*bean",
+				"@ejb+bean",
+				"@ejb,bean",
+				"@ejb",
+				"@ejb.bean",
+				"@ejb/bean",
+				"@ejb:bean",
+				"@ejb;bean",
+				"@ejb",
+				"@ejb=bean",
+				"@ejb",
+				"@ejb?bean",
+				"@ejb",
+				"@ejb[bean",
+				"@ejb",
+				"@ejb]bean",
+				"@ejb^bean",
+				"@ejb",
+				"@ejb{bean",
+				"@ejb|bean",
+				"@ejb",
+				"@ejb~bean",
+				"@ejb",
+				"@ejb",
+				"@unknown"
+			};
+			String[] tagTexts = {
+				"!bean test non-java id character '!' (val=33) in tag name",
+				"' (val=34) in tag name",
+				"#bean test non-java id character '#' (val=35) in tag name",
+				"%bean test non-java id character '%' (val=37) in tag name",
+				"&bean test non-java id character '&' (val=38) in tag name",
+				"'bean test non-java id character ''' (val=39) in tag name",
+				" test non-java id character '(' (val=40) in tag name",
+				" test non-java id character ')' (val=41) in tag name",
+				" test non-java id character '*' (val=42) in tag name",
+				" test non-java id character '+' (val=43) in tag name",
+				" test non-java id character ',' (val=44) in tag name",
+				"-bean test non-java id character '-' (val=45) in tag name",
+				" test non-java id character '.' (val=46) in tag name",
+				" test non-java id character '/' (val=47) in tag name",
+				" test non-java id character ':' (val=58) in tag name",
+				" test non-java id character ';' (val=59) in tag name",
+				"<bean test non-java id character '<' (val=60) in tag name",
+				" test non-java id character '=' (val=61) in tag name",
+				">bean test non-java id character '>' (val=62) in tag name",
+				" test non-java id character '?' (val=63) in tag name",
+				"@bean test non-java id character '@' (val=64) in tag name",
+				" test non-java id character '[' (val=91) in tag name",
+				"\\bean test non-java id character '\\' (val=92) in tag name",
+				" test non-java id character ']' (val=93) in tag name",
+				" test non-java id character '^' (val=94) in tag name",
+				"`bean test non-java id character '`' (val=96) in tag name",
+				" test non-java id character '{' (val=123) in tag name",
+				" test non-java id character '|' (val=124) in tag name",
+				"}bean test non-java id character '}' (val=125) in tag name",
+				" test non-java id character '~' (val=126) in tag name",
+				"¦bean test non-java id character '¦' (val=166) in tag name",
+				"§bean test non-java id character '§' (val=167) in tag name",
+				" test java id"
+			};
+			Comment comment = (Comment) compilUnit.getCommentList().get(0);
+			assumeTrue(this.prefix+"Comment should be a javadoc comment ", comment.isDocComment());
+			Javadoc docComment = (Javadoc) comment;
+			int size = docComment.tags().size();
+			for (int i=0; i<size; i++) {
+				TagElement tagElement = (TagElement) docComment.tags().get(i);
+				assumeEquals("Wrong tag name for:"+tagElement, tagNames[i], tagElement.getTagName());
+				assumeEquals("Wrong fragments size for :"+tagElement, 1, tagElement.fragments().size());
+				ASTNode fragment = (ASTNode) tagElement.fragments().get(0);
+				assumeEquals("Wrong fragments type for :"+tagElement, ASTNode.TEXT_ELEMENT, fragment.getNodeType());
+				TextElement textElement = (TextElement) fragment;
+				assumeEquals("Wrong text for tag!", tagTexts[i], textElement.getText());
+			}
+		}
+		this.stopOnFailure = true;
 	}
 }
