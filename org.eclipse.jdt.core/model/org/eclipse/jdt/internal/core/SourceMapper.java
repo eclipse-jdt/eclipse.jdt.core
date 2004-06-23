ccdd34a85499793a78a8b9f93bacff446273bb8b
Jerome lanneluc <jlanneluc>
2004-06-23 14:20:30 +0000

    1.5 - Enable navigation to generic methods in Outline

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/SourceMapper.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/SourceMapper.java
@@ -1035,9 +1035,10 @@ public class SourceMapper
 				unqualifiedName.append(Signature.C_ARRAY);
 				++count;
 			}
-			if (qualifiedName.charAt(count) == Signature.C_RESOLVED) {
+			char currentChar = qualifiedName.charAt(count);
+			if (currentChar == Signature.C_RESOLVED || currentChar == Signature.C_TYPE_VARIABLE) {
 				unqualifiedName.append(Signature.C_UNRESOLVED);
-				String simpleName = Signature.getSimpleName(qualifiedName.substring(count+1));
+				String simpleName = toUnqualifiedSimpleName(qualifiedName, count+1);
 				if(!noDollar) {
 					if(!hasDollar && simpleName.indexOf('$') != -1) {
 						hasDollar = true;
@@ -1241,4 +1242,35 @@ public class SourceMapper
 		}
 		return false;
 	}	
+	
+	private String toUnqualifiedSimpleName(String qualifiedName, int start) {
+		// TODO (jerome) should not create unnecessary String and StringBuffer
+		String simpleName = Signature.getSimpleName(qualifiedName.substring(start));
+		StringBuffer unqualifiedName = new StringBuffer();
+		boolean sigStart = false;
+		for (int i = 0, length = simpleName.length(); i < length; i++) {
+			char current = simpleName.charAt(i);
+			switch (current) {
+				case Signature.C_SUPER:
+				case Signature.C_EXTENDS:
+				case Signature.C_GENERIC_START:
+				case Signature.C_NAME_END:
+					unqualifiedName.append(current);
+					sigStart = true;
+					break;
+				default:
+					if (sigStart) {
+						if (current == Signature.C_TYPE_VARIABLE) {
+							unqualifiedName.append(Signature.C_UNRESOLVED);
+						} else {
+							unqualifiedName.append(current);
+						}
+						sigStart = false;
+					} else {
+						unqualifiedName.append(current);
+					}
+			}
+		}
+		return unqualifiedName.toString();
+	}
 }
