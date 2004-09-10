b3cd104083c304965f66df8d3402606ee1d593b6
Kent Johnson <kjohnson>
2004-09-10 20:34:37 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericTypeTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericTypeTest.java
@@ -7668,4 +7668,18 @@ public class GenericTypeTest extends AbstractRegressionTest {
 			},
 			"42");
 	}
+	// 72644
+	public void test286() {
+		this.runConformTest(
+			new String[] {
+				"X.java",
+				"public class X {\n" + 
+				"	<T extends Object> T foo(Class<T> c) {return null;}\n" + 
+				"}\n" + 
+				"class Y extends X {\n" + 
+				"	<T extends Object> T foo(Class<T> c) {return null;}\n" + 
+				"}"
+			},
+			"");
+	}
 }
