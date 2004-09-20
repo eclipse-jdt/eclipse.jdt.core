b88b1d83a93cec80a32f1da36a2eadc4c0f3c704
Philipe Mulet <pmulet>
2004-09-20 09:45:57 +0000

    74244

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ClassLiteralAccess.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ClassLiteralAccess.java
@@ -85,8 +85,8 @@ public class ClassLiteralAccess extends Expression {
 		}
 		ReferenceBinding classType = scope.getJavaLangClass();
 		if (classType.isGenericType()) {
-		    // Integer.class is of type Class<Integer>
-		    this.resolvedType = scope.createParameterizedType(classType, new TypeBinding[]{ targetType }, null);
+		    // Integer.class is of type Class<Integer>, perform boxing of base types
+		    this.resolvedType = scope.createParameterizedType(classType, new TypeBinding[]{ scope.boxing(targetType) }, null);
 		} else {
 		    this.resolvedType = classType;
 		}
