503d834aa5b0d7138084ea18f778282bd14105f3
Olivier Thomann <othomann>
2004-09-28 19:51:29 +0000

    HEAD - Regression test for 74369

---
+++ org.eclipse.jdt.core.tests.model/workspace/Converter/src/test0572/A.java
@@ -0,0 +1,19 @@
+package test0572;
+
+/**
+ * javadoc comment
+ */
+public class A {
+
+	/**
+	 * javadoc comment
+	 */
+	public void method() {
+		/**
+		 * not javadoc comment
+		 */
+		System.out.println("println");
+		class Local {
+		}
+	}
+}
