dbdb68609ec6a5b2774fe0cf311bcbbacfc99d49
Jerome Lanneluc <jeromel>
2004-10-22 09:31:58 +0000

    Finger print test: Full source workspace build

--- org.eclipse.jdt.core.tests.builder/src/org/eclipse/jdt/core/tests/builder/Tests.java
+++ org.eclipse.jdt.core.tests.builder/src/org/eclipse/jdt/core/tests/builder/Tests.java
@@ -369,7 +369,8 @@ public class Tests extends TestCase {
 
 	/** Sets up this test.
 	 */
-	protected void setUp() {
+	protected void setUp() throws Exception {
+		super.setUp();
 		debugRequestor = new EfficiencyCompilerRequestor();
 		Compiler.DebugRequestor = debugRequestor;
 		if (env == null) {
@@ -385,6 +386,7 @@ public class Tests extends TestCase {
 	protected void tearDown() throws Exception {
 		env.resetWorkspace();
 		JavaCore.setOptions(JavaCore.getDefaultOptions());
+		super.tearDown();
 	}
 
 	public static Test suite() {
