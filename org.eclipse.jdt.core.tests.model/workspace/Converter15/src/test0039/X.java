7aac6f03ecf70666410b0e689c91dafd6d67757a
Olivier Thomann <oliviert>
2004-11-16 21:35:43 +0000

    HEAD - Improve test case

--- org.eclipse.jdt.core.tests.model/workspace/Converter15/src/test0039/X.java
+++ org.eclipse.jdt.core.tests.model/workspace/Converter15/src/test0039/X.java
@@ -1,7 +1,18 @@
 package test0039;
 
+import static test0039.Z.*;
+
+enum Z {
+	A, B, C, D
+}
+@interface Foo {
+    Z[] value();
+}
+@interface Bar {
+    Z value();
+}
 @Foo({A, B, C, D})
-@Bar(A.B)
+@Bar(Z.B)
 public @interface X {
-    Object[] bar();
+    String[] bar();
 }
