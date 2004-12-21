5165b07170df5fe077059eae2a2d75d47fa1df0d
cvs2svn <platform-releng-dev@eclipse.org>
2004-12-21 19:53:12 +0000

    This commit was manufactured by cvs2svn to create branch 'perf_213'.
    Cherrypick from master 2004-12-21 19:53:11 UTC Frederic Fusier <ffusier> 'Wait end of indexing before measure times':
        org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/BuildBatchCompilerTest.java
        org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceASTTests.java
        org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceBuildTests.java
        org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceSearchTests.java
        org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceTypeHierarchyTests.java

---
+++ org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceASTTests.java
@@ -0,0 +1,556 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core.tests.performance;
+
+import java.text.NumberFormat;
+import java.util.List;
+import junit.framework.*;
+import org.eclipse.jdt.core.*;
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.test.performance.Dimension;
+
+
+/**
+ */
+public class FullSourceWorkspaceASTTests extends FullSourceWorkspaceTests {
+
+	/**
+	 * @param name
+	 */
+	public FullSourceWorkspaceASTTests(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		return buildSuite(FullSourceWorkspaceASTTests.class);
+	}
+
+	/**
+	 * Comment Mapper visitor
+	 */
+	class CommentMapperASTVisitor extends ASTVisitor {
+		CompilationUnit compilationUnit;
+		int nodesCount = 0;
+		int extendedStartPositions = 0;
+		int extendedEndPositions = 0;
+
+		public CommentMapperASTVisitor(CompilationUnit unit) {
+			this.compilationUnit = unit;
+		}
+		protected boolean visitNode(ASTNode node) {
+			// get node positions and extended positions
+			int nodeStart = node.getStartPosition();
+			int nodeEnd = node.getLength() - 1 - nodeStart;
+			int extendedStart = this.compilationUnit.getExtendedStartPosition(node);
+			int extendedEnd = this.compilationUnit.getExtendedLength(node) - 1 - extendedStart;
+			// update counters
+			if (extendedStart < nodeStart) this.extendedStartPositions++;
+			if (extendedEnd > nodeEnd) this.extendedEndPositions++;
+			this.nodesCount++;
+			return true;
+		}
+		protected void endVisitNode(ASTNode node) {
+			// do nothing
+		}
+		public boolean visit(AnonymousClassDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(ArrayAccess node) {
+			return visitNode(node);
+		}
+		public boolean visit(ArrayCreation node) {
+			return visitNode(node);
+		}
+		public boolean visit(ArrayInitializer node) {
+			return visitNode(node);
+		}
+		public boolean visit(ArrayType node) {
+			visitNode(node);
+			return false;
+		}
+		public boolean visit(AssertStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(Assignment node) {
+			return visitNode(node);
+		}
+		public boolean visit(Block node) {
+			return visitNode(node);
+		}
+		public boolean visit(BooleanLiteral node) {
+			return visitNode(node);
+		}
+		public boolean visit(BreakStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(CastExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(CatchClause node) {
+			return visitNode(node);
+		}
+		public boolean visit(CharacterLiteral node) {
+			return visitNode(node);
+		}
+		public boolean visit(ClassInstanceCreation node) {
+			return visitNode(node);
+		}
+		public boolean visit(CompilationUnit node) {
+			return visitNode(node);
+		}
+		public boolean visit(ConditionalExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(ConstructorInvocation node) {
+			return visitNode(node);
+		}
+		public boolean visit(ContinueStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(DoStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(EmptyStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(ExpressionStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(FieldAccess node) {
+			return visitNode(node);
+		}
+		public boolean visit(FieldDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(ForStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(IfStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(ImportDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(InfixExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(InstanceofExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(Initializer node) {
+			return visitNode(node);
+		}
+		public boolean visit(Javadoc node) {
+			// do not visit Javadoc tags by default. Use constructor with
+			// boolean to enable.
+			if (super.visit(node)) { return visitNode(node); }
+			return false;
+		}
+		public boolean visit(LabeledStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(MethodDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(MethodInvocation node) {
+			return visitNode(node);
+		}
+		public boolean visit(NullLiteral node) {
+			return visitNode(node);
+		}
+		public boolean visit(NumberLiteral node) {
+			return visitNode(node);
+		}
+		public boolean visit(PackageDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(ParenthesizedExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(PostfixExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(PrefixExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(PrimitiveType node) {
+			return visitNode(node);
+		}
+		public boolean visit(QualifiedName node) {
+			return visitNode(node);
+		}
+		public boolean visit(ReturnStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(SimpleName node) {
+			return visitNode(node);
+		}
+		public boolean visit(SimpleType node) {
+			return visitNode(node);
+		}
+		public boolean visit(StringLiteral node) {
+			return visitNode(node);
+		}
+		public boolean visit(SuperConstructorInvocation node) {
+			return visitNode(node);
+		}
+		public boolean visit(SuperFieldAccess node) {
+			return visitNode(node);
+		}
+		public boolean visit(SuperMethodInvocation node) {
+			return visitNode(node);
+		}
+		public boolean visit(SwitchCase node) {
+			return visitNode(node);
+		}
+		public boolean visit(SwitchStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(SynchronizedStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(ThisExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(ThrowStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(TryStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(TypeDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(TypeDeclarationStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(TypeLiteral node) {
+			return visitNode(node);
+		}
+		public boolean visit(SingleVariableDeclaration node) {
+			return visitNode(node);
+		}
+		public boolean visit(VariableDeclarationExpression node) {
+			return visitNode(node);
+		}
+		public boolean visit(VariableDeclarationStatement node) {
+			return visitNode(node);
+		}
+		public boolean visit(VariableDeclarationFragment node) {
+			return visitNode(node);
+		}
+		public boolean visit(WhileStatement node) {
+			return visitNode(node);
+		}
+		/* since 3.0 */
+		public boolean visit(BlockComment node) {
+			return visitNode(node);
+		}
+		public boolean visit(LineComment node) {
+			return visitNode(node);
+		}
+		public boolean visit(MemberRef node) {
+			return visitNode(node);
+		}
+		public boolean visit(MethodRef node) {
+			return visitNode(node);
+		}
+		public boolean visit(MethodRefParameter node) {
+			return visitNode(node);
+		}
+		public boolean visit(TagElement node) {
+			return visitNode(node);
+		}
+		public boolean visit(TextElement node) {
+			return visitNode(node);
+		}
+		public void endVisit(AnonymousClassDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ArrayAccess node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ArrayCreation node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ArrayInitializer node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ArrayType node) {
+			endVisitNode(node);
+		}
+		public void endVisit(AssertStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(Assignment node) {
+			endVisitNode(node);
+		}
+		public void endVisit(Block node) {
+			endVisitNode(node);
+		}
+		public void endVisit(BooleanLiteral node) {
+			endVisitNode(node);
+		}
+		public void endVisit(BreakStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(CastExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(CatchClause node) {
+			endVisitNode(node);
+		}
+		public void endVisit(CharacterLiteral node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ClassInstanceCreation node) {
+			endVisitNode(node);
+		}
+		public void endVisit(CompilationUnit node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ConditionalExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ConstructorInvocation node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ContinueStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(DoStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(EmptyStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ExpressionStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(FieldAccess node) {
+			endVisitNode(node);
+		}
+		public void endVisit(FieldDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ForStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(IfStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ImportDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(InfixExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(InstanceofExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(Initializer node) {
+			endVisitNode(node);
+		}
+		public void endVisit(Javadoc node) {
+			endVisitNode(node);
+		}
+		public void endVisit(LabeledStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(MethodDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(MethodInvocation node) {
+			endVisitNode(node);
+		}
+		public void endVisit(NullLiteral node) {
+			endVisitNode(node);
+		}
+		public void endVisit(NumberLiteral node) {
+			endVisitNode(node);
+		}
+		public void endVisit(PackageDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ParenthesizedExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(PostfixExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(PrefixExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(PrimitiveType node) {
+			endVisitNode(node);
+		}
+		public void endVisit(QualifiedName node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ReturnStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SimpleName node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SimpleType node) {
+			endVisitNode(node);
+		}
+		public void endVisit(StringLiteral node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SuperConstructorInvocation node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SuperFieldAccess node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SuperMethodInvocation node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SwitchCase node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SwitchStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SynchronizedStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ThisExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(ThrowStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(TryStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(TypeDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(TypeDeclarationStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(TypeLiteral node) {
+			endVisitNode(node);
+		}
+		public void endVisit(SingleVariableDeclaration node) {
+			endVisitNode(node);
+		}
+		public void endVisit(VariableDeclarationExpression node) {
+			endVisitNode(node);
+		}
+		public void endVisit(VariableDeclarationStatement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(VariableDeclarationFragment node) {
+			endVisitNode(node);
+		}
+		public void endVisit(WhileStatement node) {
+			endVisitNode(node);
+		}
+		/* since 3.0 */
+		public void endVisit(BlockComment node) {
+			endVisitNode(node);
+		}
+		public void endVisit(LineComment node) {
+			endVisitNode(node);
+		}
+		public void endVisit(MemberRef node) {
+			endVisitNode(node);
+		}
+		public void endVisit(MethodRef node) {
+			endVisitNode(node);
+		}
+		public void endVisit(MethodRefParameter node) {
+			endVisitNode(node);
+		}
+		public void endVisit(TagElement node) {
+			endVisitNode(node);
+		}
+		public void endVisit(TextElement node) {
+			endVisitNode(node);
+		}
+	}
+
+	private void runAstCreation(int astLevel) throws JavaModelException {
+		tagAsSummary("AST Creation", Dimension.CPU_TIME);
+		int allExtendedStartPositions = 0;
+		int allExtendedEndPositions = 0;
+		int allNodesCount = 0;
+		startMeasuring();
+		if (DEBUG) System.out.println("Creating AST hierarchy for all units of projects:");
+		for (int i = 0; i < ALL_PROJECTS.length; i++) {
+			// Get project compilation units
+			if (DEBUG) System.out.print("\t- "+ALL_PROJECTS[i].getElementName());
+			List units = getProjectCompilationUnits(ALL_PROJECTS[i]);
+			int size = units.size();
+			if (size == 0) {
+				if (DEBUG) System.out.println(": empty!");
+				continue;
+			}
+			List unitsArrays = splitListInSmallArrays(units, 20);
+			int n = unitsArrays.size();
+			if (DEBUG)
+				if (n==1)
+					System.out.print(": "+size+" units to proceed ("+n+" step): ");
+				else
+					System.out.print(": "+size+" units to proceed ("+n+" steps): ");
+			while (unitsArrays.size() > 0) {
+				ICompilationUnit[] unitsArray = (ICompilationUnit[]) unitsArrays.remove(0);
+				if (DEBUG) System.out.print('.');
+				int length = unitsArray.length;
+				CompilationUnit[] compilationUnits = new CompilationUnit[length];
+				// Create AST tree
+				for (int ptr=0; ptr<length; ptr++) {
+					ICompilationUnit unit = unitsArray[ptr];
+					unitsArray[ptr] = null; // release memory handle
+					ASTParser parser = ASTParser.newParser(astLevel);
+					parser.setSource(unit);
+					parser.setResolveBindings(false);
+					ASTNode result = parser.createAST(null);
+					assertEquals("Wrong type for node"+result, result.getNodeType(), ASTNode.COMPILATION_UNIT);
+					compilationUnits[ptr] = (CompilationUnit) result;
+				}
+				for (int ptr=0; ptr<length; ptr++) {
+					CommentMapperASTVisitor visitor = new CommentMapperASTVisitor(compilationUnits[ptr]);
+					compilationUnits[ptr].accept(visitor);
+					allExtendedStartPositions += visitor.extendedStartPositions;
+					allExtendedEndPositions += visitor.extendedEndPositions;
+					allNodesCount += visitor.nodesCount;
+				}
+			}
+			if (DEBUG) System.out.println(" done!");
+		}
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+
+		// Print statistics
+		System.out.println("-------------------------------------");
+		System.out.println("DOM/AST creation performance test statistics:");
+		NumberFormat intFormat = NumberFormat.getIntegerInstance();
+		System.out.println("  - "+intFormat.format(allNodesCount)+" nodes have been parsed.");
+		System.out.println("  - "+intFormat.format(allExtendedStartPositions)+" nodes have extended start position.");
+		System.out.println("  - "+intFormat.format(allExtendedEndPositions)+" nodes have extended end position.");
+		System.out.println("-------------------------------------\n");
+	}
+
+	// Do NOT forget that tests must start with "testPerf"
+	public void testPerfDomAstCreationJLS2() throws JavaModelException {
+		runAstCreation(AST.JLS2);
+	}
+	public void testPerfDomAstCreationJLS3() throws JavaModelException {
+		runAstCreation(AST.JLS3);
+	}
+}
---
+++ org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceSearchTests.java
@@ -0,0 +1,208 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core.tests.performance;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import junit.framework.*;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.search.*;
+import org.eclipse.jdt.internal.core.JavaModelManager;
+import org.eclipse.jdt.internal.core.search.indexing.IndexManager;
+import org.eclipse.jdt.internal.core.search.processing.IJob;
+import org.eclipse.test.performance.Dimension;
+
+
+/**
+ */
+public class FullSourceWorkspaceSearchTests extends FullSourceWorkspaceTests implements IJavaSearchConstants {
+	
+	static int[] REFERENCES = new int[4];
+	private static int COUNT = 0;
+	private static IJavaSearchScope SEARCH_SCOPE;
+
+	/**
+	 * @param name
+	 */
+	public FullSourceWorkspaceSearchTests(String name) {
+		super(name);
+	}
+
+//	static {
+//		TESTS_NAMES = new String[] { "testPerfSearchType" };
+//	}
+	public static Test suite() {
+		Test suite = buildSuite(FullSourceWorkspaceSearchTests.class);
+		COUNT = suite.countTestCases();
+		return suite;
+	}
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.resultCollector = new JavaSearchResultCollector();
+		if (SEARCH_SCOPE == null) {
+			SEARCH_SCOPE = SearchEngine.createJavaSearchScope(ALL_PROJECTS);
+			waitUntilReadyToSearch();
+		}
+	}
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		COUNT--;
+		if (COUNT == 0) {
+			// Print statistics
+			System.out.println("-------------------------------------");
+			System.out.println("Search performance test statistics:");
+			NumberFormat intFormat = NumberFormat.getIntegerInstance();
+			System.out.println("  - "+intFormat.format(REFERENCES[0])+" type references found.");
+			System.out.println("  - "+intFormat.format(REFERENCES[1])+" field references found.");
+			System.out.println("  - "+intFormat.format(REFERENCES[2])+" method references found.");
+			System.out.println("  - "+intFormat.format(REFERENCES[3])+" constructor references found.");
+			System.out.println("-------------------------------------\n");
+		}
+	}
+	/**
+	 * Simple search result collector: only count matches.
+	 */
+	class JavaSearchResultCollector extends SearchRequestor {
+		int count = 0;
+		public void acceptSearchMatch(SearchMatch match) throws CoreException {
+			this.count++;
+		}
+	}
+	
+	protected JavaSearchResultCollector resultCollector;
+
+	protected void search(String patternString, int searchFor, int limitTo, SearchRequestor requestor) throws CoreException {
+		int matchMode = patternString.indexOf('*') != -1 || patternString.indexOf('?') != -1
+			? SearchPattern.R_PATTERN_MATCH
+			: SearchPattern.R_EXACT_MATCH;
+		SearchPattern pattern = SearchPattern.createPattern(
+			patternString, 
+			searchFor,
+			limitTo, 
+			matchMode | SearchPattern.R_CASE_SENSITIVE);
+		new SearchEngine().search(
+			pattern,
+			new SearchParticipant[] {SearchEngine.getDefaultSearchParticipant()},
+			SEARCH_SCOPE,
+			requestor,
+			null);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.core.tests.performance.FullSourceWorkspaceTests#setFullSourceWorkspace()
+	 */
+	private void waitUntilReadyToSearch() throws IOException, CoreException {
+		IndexManager indexManager = JavaModelManager.getJavaModelManager().getIndexManager();
+		IJob doNothing = new IJob() {
+			/**
+			 * Answer true if the job belongs to a given family (tag)
+			 */
+			public boolean belongsTo(String jobFamily) {
+				return true;
+			}
+			/**
+			 * Asks this job to cancel its execution. The cancellation
+			 * can take an undertermined amount of time.
+			 */
+			public void cancel() {
+				// nothing to cancel
+			}
+			/**
+			 * Ensures that this job is ready to run.
+			 */
+			public void ensureReadyToRun() {
+				// always ready to do nothing
+			}
+			/**
+			 * Execute the current job, answer whether it was successful.
+			 */
+			public boolean execute(IProgressMonitor progress) {
+				// always succeed to do nothing
+				return true;
+			}
+		};
+		if (DEBUG) System.out.print("Wait until ready to search..."); //$NON-NLS-1$
+		indexManager.performConcurrentJob(doNothing, IJavaSearchConstants.WAIT_UNTIL_READY_TO_SEARCH, null);
+		if (DEBUG) {
+			if (indexManager.awaitingJobsCount() == 0)
+				System.out.println("done"); //$NON-NLS-1$
+			else
+				System.err.println(" KO: remaining jobs="+indexManager.awaitingJobsCount()); //$NON-NLS-1$
+		}
+	}
+
+	// Do NOT forget that tests must start with "testPerf"
+	public void testPerfSearchType() throws CoreException {
+		tagAsSummary("Search Type all occurences", Dimension.CPU_TIME);
+		startMeasuring();
+		search(
+//			"String",  > 65000 macthes: needs -Xmx512M
+//			"Object", 13497 matches: needs -Xmx128M
+//			"IResource", 5886 macthes: fails needs ?
+			"JavaCore", // 2145 m	atches
+			TYPE,
+			ALL_OCCURRENCES, 
+			this.resultCollector);
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+		// store counter
+		REFERENCES[0] = this.resultCollector.count;
+	}
+	public void testPerfSearchField() throws CoreException {
+		tagAsSummary("Search Field all occurences", Dimension.CPU_TIME);
+		startMeasuring();
+		search(
+			"FILE", 
+			FIELD,
+			ALL_OCCURRENCES, 
+			this.resultCollector);
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+		// store counter
+		REFERENCES[1] = this.resultCollector.count;
+	}
+	public void testPerfSearchMethod() throws CoreException {
+		tagAsSummary("Search Method all occurences", Dimension.CPU_TIME);
+		startMeasuring();
+		search(
+			"equals", 
+			METHOD,
+			ALL_OCCURRENCES, 
+			this.resultCollector);
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+		// store counter
+		REFERENCES[2] = this.resultCollector.count;
+	}
+	public void testPerfSearchConstructor() throws CoreException {
+		tagAsSummary("Search Constructor all occurences", Dimension.CPU_TIME);
+		startMeasuring();
+		search(
+			"String", 
+			CONSTRUCTOR,
+			ALL_OCCURRENCES, 
+			this.resultCollector);
+		stopMeasuring();
+		commitMeasurements();
+		assertPerformance();
+		// store counter
+		REFERENCES[3] = this.resultCollector.count;
+	}
+}
