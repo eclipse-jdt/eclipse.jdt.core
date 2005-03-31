87d174d7d7847aa475874fa21be175beee417fd9
Frederic Fusier <ffusier>
2005-03-31 13:22:07 +0000

    perf_301 - Synch with HEAD (3.1 M6)

--- org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/AllPerformanceTests.java
+++ org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/AllPerformanceTests.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -17,10 +17,13 @@ import junit.framework.TestCase;
 
 public class AllPerformanceTests extends TestCase {
 
+	static String LENGTH = System.getProperty("length", "0");
+
 	public static Class[] getAllTestClasses() {
 		return new Class[] {
 			FullSourceWorkspaceSearchTests.class, // run this test first to be sure that indexing is finished
 			FullSourceWorkspaceBuildTests.class,
+//			FullSourceWorkspaceCompletionTests.class,
 			FullSourceWorkspaceASTTests.class,
 			FullSourceWorkspaceTypeHierarchyTests.class
 		};
@@ -28,13 +31,19 @@ public class AllPerformanceTests extends TestCase {
 	public static Test suite() {
 		PerformanceTestSuite perfSuite = new PerformanceTestSuite(AllPerformanceTests.class.getName());
 		Class[] testSuites = getAllTestClasses();
-		int length = testSuites.length;
+		int length = 0;
+		try {
+			length = Integer.parseInt(LENGTH);
+			if (length<=0 || length>testSuites.length)
+				length = testSuites.length;
+		} catch (NumberFormatException e1) {
+			length = testSuites.length;
+		}
 		for (int i = 0; i < length; i++) {
 			Class testClass = testSuites[i];
 			// call the suite() method and add the resulting suite to the suite
 			try {
-				Method suiteMethod = testClass.getDeclaredMethod(
-						"suite", new Class[0]); //$NON-NLS-1$
+				Method suiteMethod = testClass.getDeclaredMethod("suite", new Class[0]); //$NON-NLS-1$
 				Test suite = (Test) suiteMethod.invoke(null, new Object[0]);
 				perfSuite.addTest(suite);
 			} catch (IllegalAccessException e) {
