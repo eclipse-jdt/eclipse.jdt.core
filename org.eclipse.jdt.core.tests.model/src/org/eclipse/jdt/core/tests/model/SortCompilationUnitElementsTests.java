579829d4867b5583bfe72f32f009d0dbea13c114
Frederic Fusier <ffusier>
2005-04-20 16:47:04 +0000

    HEAD - Fix copyrights cpl-v10

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/SortCompilationUnitElementsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/SortCompilationUnitElementsTests.java
@@ -1251,15 +1251,16 @@ public void test022() throws CoreException {
 	try {
 		this.createFile(
 			"/P/src/BuildUtilities.java",
-			"/**********************************************************************\n" +
-			" * Copyright (c) 2004 IBM Corporation and others. All rights reserved.   This\n" +
-			" * program and the accompanying materials are made available under the terms of\n" +
-			" * the Common Public License v1.0 which accompanies this distribution, and is\n" +
-			" * available at http://www.eclipse.org/legal/cpl-v10.html\n" +
-			" * \n" +
-			" * Contributors: \n" +
-			" * IBM - Initial API and implementation\n" +
-			" **********************************************************************/\n" +
+			"/*******************************************************************************" +
+			" * Copyright (c) 2000, 2005 IBM Corporation and others." +
+			" * All rights reserved. This program and the accompanying materials" +
+			" * are made available under the terms of the Eclipse Public License v1.0" +
+			" * which accompanies this distribution, and is available at" +
+			" * http://www.eclipse.org/legal/epl-v10.html" +
+			" *" +
+			" * Contributors:" +
+			" *     IBM Corporation - initial API and implementation" +
+			" *******************************************************************************/" +
 			"import java.util.HashSet;\n" +
 			"import org.eclipse.core.resources.ICommand;\n" +
 			"import org.eclipse.core.resources.IProject;\n" +
@@ -1419,15 +1420,16 @@ public void test022() throws CoreException {
 			"}"
 		);
 		String expectedResult = 
-			"/**********************************************************************\n" +
-			" * Copyright (c) 2004 IBM Corporation and others. All rights reserved.   This\n" +
-			" * program and the accompanying materials are made available under the terms of\n" +
-			" * the Common Public License v1.0 which accompanies this distribution, and is\n" +
-			" * available at http://www.eclipse.org/legal/cpl-v10.html\n" +
-			" * \n" +
-			" * Contributors: \n" +
-			" * IBM - Initial API and implementation\n" +
-			" **********************************************************************/\n" +
+			"/*******************************************************************************" +
+			" * Copyright (c) 2000, 2005 IBM Corporation and others." +
+			" * All rights reserved. This program and the accompanying materials" +
+			" * are made available under the terms of the Eclipse Public License v1.0" +
+			" * which accompanies this distribution, and is available at" +
+			" * http://www.eclipse.org/legal/epl-v10.html" +
+			" *" +
+			" * Contributors:" +
+			" *     IBM Corporation - initial API and implementation" +
+			" *******************************************************************************/" +
 			"import java.util.HashSet;\n" +
 			"import org.eclipse.core.resources.ICommand;\n" +
 			"import org.eclipse.core.resources.IProject;\n" +
