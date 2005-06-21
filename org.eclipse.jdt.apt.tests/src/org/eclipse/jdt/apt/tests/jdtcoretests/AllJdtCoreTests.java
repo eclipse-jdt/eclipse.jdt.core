41a8198978eafefd7ac9d52b3cf57e10d1d7644a
mkaufman <mkaufman>
2005-06-21 20:57:08 +0000

    - reordered tests so that the model tests run first.  If not, the model tests fail.  This is being tracked by Bugzilla 101144.

--- org.eclipse.jdt.apt.tests/src/org/eclipse/jdt/apt/tests/jdtcoretests/AllJdtCoreTests.java
+++ org.eclipse.jdt.apt.tests/src/org/eclipse/jdt/apt/tests/jdtcoretests/AllJdtCoreTests.java
@@ -12,11 +12,45 @@
 
 package org.eclipse.jdt.apt.tests.jdtcoretests;
 
-public class AllJdtCoreTests extends org.eclipse.jdt.core.tests.RunJDTCoreTests {
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.jdt.core.tests.RunBuilderTests;
+import org.eclipse.jdt.core.tests.RunCompilerTests;
+import org.eclipse.jdt.core.tests.RunDOMTests;
+import org.eclipse.jdt.core.tests.RunFormatterTests;
+import org.eclipse.jdt.core.tests.RunJDTCoreTests;
+import org.eclipse.jdt.core.tests.RunModelTests;
+
+//
+//  BUGZILLA 101144
+// 
+// This code should look like this when bug 101144 is fixed:
+//
+// public class AllJdtCoreTests extends org.eclipse.jdt.core.tests.RunJDTCoreTests {
+//
+//    public AllJdtCoreTests(String testName) { super(testName); }
+// }
+//
+
+public class AllJdtCoreTests extends TestCase {
 	
 	public AllJdtCoreTests(String testName) 
 	{
 		super(testName);
 	}
+	
+		public static Test suite() {
+			TestSuite suite = new TestSuite(RunJDTCoreTests.class.getName());
+			suite.addTest(RunModelTests.suite());
+			suite.addTest(RunBuilderTests.suite());
+			suite.addTest(RunCompilerTests.suite());
+			suite.addTest(RunDOMTests.suite());
+			suite.addTest(RunFormatterTests.suite());
+
+			return suite;
+		}
 }
+	
 
