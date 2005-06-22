78489a1112068ad791184216a4e6b3439ea59739
Philipe Mulet <pmulet>
2005-06-22 09:39:45 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/AbstractASTTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/AbstractASTTests.java
@@ -429,7 +429,7 @@ public class AbstractASTTests extends ModifyingResourceTests {
 	 * Resolve the bindings of the nodes marked with *start?* and *end?*.
 	 */
 	protected IBinding[] resolveBindings(String contents, ICompilationUnit cu) throws JavaModelException {
-		ASTNode[] nodes = buildASTs(contents, cu);
+		ASTNode[] nodes = buildASTs(contents, cu, false /* do not report errors */);
 		if (nodes == null) return null;
 		int length = nodes.length;
 		IBinding[] result = new IBinding[length];
