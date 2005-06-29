942207851a6bad82daa18a8833b114f361d67c7e
Olivier Thomann <oliviert>
2005-06-29 13:21:31 +0000

    3.1. maintenance stream - Fix for 101885

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/SortCompilationUnitElementsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/SortCompilationUnitElementsTests.java
@@ -1878,4 +1878,30 @@ public void test028() throws CoreException {
 		this.deleteFile("/P/src/X.java");
 	}
 }
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=101885
+public void test029() throws CoreException {
+	try {
+		this.createFile(
+			"/P/src/X.java",
+			"public enum X {\n" + 
+			"	Z, A, C, B;\n" + 
+			"}"
+		);
+		String expectedResult = 
+			"public enum X {\n" + 
+			"	Z, A, C, B;\n" + 
+			"}";
+		sortUnit(AST.JLS3, this.getCompilationUnit("/P/src/X.java"), expectedResult, false, new Comparator() {
+			public int compare(Object o1, Object o2) {
+				BodyDeclaration bodyDeclaration1 = (BodyDeclaration) o1;
+				BodyDeclaration bodyDeclaration2 = (BodyDeclaration) o2;
+				final int sourceStart1 = ((Integer) bodyDeclaration1.getProperty(CompilationUnitSorter.RELATIVE_ORDER)).intValue();
+				final int sourceStart2 = ((Integer) bodyDeclaration2.getProperty(CompilationUnitSorter.RELATIVE_ORDER)).intValue();
+				return sourceStart1 - sourceStart2;
+			}
+		});
+	} finally {
+		this.deleteFile("/P/src/X.java");
+	}
+}
 }
