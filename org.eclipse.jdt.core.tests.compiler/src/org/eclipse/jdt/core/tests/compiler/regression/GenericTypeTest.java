500f67d7a929b44db04ad6aaec96f39e2f081a00
Olivier Thomann <oliviert>
2005-08-02 20:16:56 +0000

    HEAD - Fix for 105635

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericTypeTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericTypeTest.java
@@ -31,7 +31,7 @@ public class GenericTypeTest extends AbstractComparableTest {
 	// All specified tests which does not belong to the class are skipped...
 	static {
 //		TESTS_NAMES = new String[] { "test788" };
-//		TESTS_NUMBERS = new int[] { 788 };
+//		TESTS_NUMBERS = new int[] { 793, 794 };
 //		TESTS_RANGE = new int[] { 514, -1 };
 	}
 	public static Test suite() {
@@ -22923,6 +22923,59 @@ public void test792() {
 			"}\n",
 		},
 		"");
+}
+//https://bugs.eclipse.org/bugs/show_bug.cgi?id=105635
+public void test793() {
+	this.runNegativeTest(
+		new String[] {
+			"X.java",
+			"class X { \n" +
+			"	public java.util.List<Integer> i,j[],k;\n" +
+			"	void m() {\n" +
+			"		  i[0] = null;\n" +
+			"		  j[0] = null;\n" +
+			"		  k[0] = null;\n" +
+			"	}\n" +
+			"}",
+		},
+		"----------\n" + 
+		"1. ERROR in X.java (at line 4)\n" + 
+		"	i[0] = null;\n" + 
+		"	^^^^\n" + 
+		"The type of the expression must be an array type but it resolved to List<Integer>\n" + 
+		"----------\n" + 
+		"2. ERROR in X.java (at line 6)\n" + 
+		"	k[0] = null;\n" + 
+		"	^^^^\n" + 
+		"The type of the expression must be an array type but it resolved to List<Integer>\n" + 
+		"----------\n");
+}
+//https://bugs.eclipse.org/bugs/show_bug.cgi?id=105635
+public void test794() {
+	this.runNegativeTest(
+		new String[] {
+			"X.java",
+			"import java.util.List;\n" +
+			"class X { \n" +
+			"	public List<Integer> i,j[],k;\n" +
+			"	void m() {\n" +
+			"		  i[0] = null;\n" +
+			"		  j[0] = null;\n" +
+			"		  k[0] = null;\n" +
+			"	}\n" +
+			"}",
+		},
+		"----------\n" + 
+		"1. ERROR in X.java (at line 5)\n" + 
+		"	i[0] = null;\n" + 
+		"	^^^^\n" + 
+		"The type of the expression must be an array type but it resolved to List<Integer>\n" + 
+		"----------\n" + 
+		"2. ERROR in X.java (at line 7)\n" + 
+		"	k[0] = null;\n" + 
+		"	^^^^\n" + 
+		"The type of the expression must be an array type but it resolved to List<Integer>\n" + 
+		"----------\n");
 }
 }
 
--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ParameterizedSingleTypeReference.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ParameterizedSingleTypeReference.java
@@ -45,8 +45,7 @@ public class ParameterizedSingleTypeReference extends ArrayTypeReference {
 	 * @see org.eclipse.jdt.internal.compiler.ast.TypeReference#copyDims(int)
 	 */
 	public TypeReference copyDims(int dim) {
-		this.dimensions = dim;
-		return this;
+		return new ParameterizedSingleTypeReference(token, typeArguments, dim, (((long)sourceStart)<<32)+sourceEnd);
 	}
 
 	/**
