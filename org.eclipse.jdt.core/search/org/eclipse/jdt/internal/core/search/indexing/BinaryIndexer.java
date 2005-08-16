d5037ff9ecee16ca60b86ad580ca6b3f8e03ee6a
Olivier Thomann <oliviert>
2005-08-16 20:40:33 +0000

    HEAD - Fix for 107124

--- org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/indexing/BinaryIndexer.java
+++ org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/indexing/BinaryIndexer.java
@@ -440,6 +440,9 @@ public class BinaryIndexer extends AbstractIndexer implements SuffixConstants {
 	public void indexDocument() {
 		try {
 			byte[] contents = this.document.getByteContents();
+			// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=107124
+			// contents can potentially be null if a IOException occurs while retrieving the contents
+			if (contents == null) return;
 			ClassFileReader reader = new ClassFileReader(contents, this.document.getPath().toCharArray());
 	
 			// first add type references
