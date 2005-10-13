6acf9b31e0f4fe4bc956ec43aedefcc09e485bb3
Jerome Lanneluc <jeromel>
2005-10-13 11:52:05 +0000

    Remove unused import

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/Openable.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/Openable.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.core;
 
-import java.util.*;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
