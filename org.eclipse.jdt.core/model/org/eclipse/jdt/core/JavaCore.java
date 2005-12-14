6d4eb8ff27f388c73bd6fad6e491d4286888ed67
Olivier Thomann <oliviert>
2005-12-14 14:28:39 +0000

    HEAD - Fix for 120545

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/JavaCore.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/JavaCore.java
@@ -2475,10 +2475,10 @@ public final class JavaCore extends Plugin {
 	 *     - possible values:   { "enabled", "disabled" }
 	 *     - default:           "enabled"
 	 *     
-	 *	CODEASSIST / Set the timeout value for retrieving the method's parameter names from javadoc
+	 *	Set the timeout value for retrieving the method's parameter names from javadoc
 	 *    Timeout in milliseconds to retrieve the method's parameter names from javadoc.
 	 *    If the value is 0, then it means don't fetch the parameter names (use default values).
-	 *     - option id:         "org.eclipse.jdt.core.codeAssist.timeoutForParameterNameFromAttachedJavadoc"
+	 *     - option id:         "org.eclipse.jdt.core.timeoutForParameterNameFromAttachedJavadoc"
 	 *     - possible values:	"&lt;n&gt;", where n is an integer greater or equals to 0
 	 *     - default:           "50"
 	 * </pre>
