b9048812dd88e4e03993f3f1485c45b1b5d06172
Walter Harley <wharley>
2006-01-05 02:12:29 +0000

    wharley - fix potential race condition where two threads compete to create the generated source folder.

--- org.eclipse.jdt.apt.core/src/org/eclipse/jdt/apt/core/internal/util/FileSystemUtil.java
+++ org.eclipse.jdt.apt.core/src/org/eclipse/jdt/apt/core/internal/util/FileSystemUtil.java
@@ -52,11 +52,23 @@ public final class FileSystemUtil
     }    
   
     public static void makeDerivedParentFolders (IContainer container) throws CoreException {
-		if ((container instanceof IFolder) && !container.exists()) {
-			makeDerivedParentFolders(container.getParent());
-			((IFolder) container).create(true, true, null);
-			container.setDerived(true);
-		}
+    	// synchronize the "does it exist - if not, create it" sequence.
+    	synchronized(container.getProject()) {
+			if ((container instanceof IFolder) && !container.exists()) {
+				makeDerivedParentFolders(container.getParent());
+		    	try {
+		    		((IFolder)container).create(true, true, null);
+		    	}
+		    	catch (CoreException e) {
+		    		// Ignore race condition where another thread created the folder at the
+		    		// same time, causing checkDoesNotExist() to throw within create(). 
+		    		if (!container.exists()) {
+		    			throw e;
+		    		}
+		    	}
+				container.setDerived(true);
+			}
+    	}
     }
     
     /**
