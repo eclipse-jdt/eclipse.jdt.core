a0d5fdb263316467393d69bcc00eba6f15d0b353
Frederic Fusier <ffusier>
2006-01-11 18:34:20 +0000

    HEAD - 118823 (put back failing test)

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/ReconcilerTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/ReconcilerTests.java
@@ -2664,8 +2664,7 @@ public void testBug36032c() throws CoreException, InterruptedException {
  * Bug 118823: [model] Secondary types cache not reset while removing _all_ secondary types from CU
  * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=118823"
  */
-// TODO (frederic) enable to trace problem on releng Linux test box
-public void _testBug118823() throws CoreException, InterruptedException, IOException {
+public void testBug118823() throws CoreException, InterruptedException, IOException {
 	try {
 		// Resources creation
 		IJavaProject project = createJavaProject("P1", new String[] {""}, new String[] {"JCL_LIB"}, "bin");
