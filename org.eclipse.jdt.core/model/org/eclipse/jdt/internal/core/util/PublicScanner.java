9814bb06776e7a6f85cab0ee3f3e1e621c043d2c
mdaniel <mdaniel>
2006-02-13 09:32:57 +0000

    HEAD - 110030 Mirrored from Scanner.

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/PublicScanner.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/PublicScanner.java
@@ -153,6 +153,10 @@ public class PublicScanner implements IScanner, ITerminalSymbols {
 	private NLSTag[] nlsTags = null;
 	protected int nlsTagsPtr;
 	public boolean checkNonExternalizedStringLiterals;
+	
+	// support for tagging references as non null
+	boolean checkNullReferences;
+	public boolean gotNonNullTag;
 
 	// generic support
 	public boolean returnOnlyGreater = false;
@@ -196,7 +200,8 @@ public PublicScanner(
 	long complianceLevel,
 	char[][] taskTags,
 	char[][] taskPriorities,
-	boolean isTaskCaseSensitive) {
+	boolean isTaskCaseSensitive,
+	boolean checkNullReferences) {
 
 	this.eofPosition = Integer.MAX_VALUE;
 	this.tokenizeComments = tokenizeComments;
@@ -207,6 +212,28 @@ public PublicScanner(
 	this.taskTags = taskTags;
 	this.taskPriorities = taskPriorities;
 	this.isTaskCaseSensitive = isTaskCaseSensitive;
+	this.checkNullReferences = checkNullReferences;
+}
+
+public PublicScanner(
+	boolean tokenizeComments, 
+	boolean tokenizeWhiteSpace, 
+	boolean checkNonExternalizedStringLiterals, 
+	long sourceLevel,
+	long complianceLevel,
+	char[][] taskTags,
+	char[][] taskPriorities,
+	boolean isTaskCaseSensitive) {
+	this(
+		tokenizeComments,
+		tokenizeWhiteSpace,
+		checkNonExternalizedStringLiterals,
+		sourceLevel,
+		complianceLevel,
+		taskTags,
+		taskPriorities,
+		isTaskCaseSensitive,
+		false);
 }
 
 public PublicScanner(
@@ -226,7 +253,8 @@ public PublicScanner(
 		sourceLevel,
 		taskTags,
 		taskPriorities,
-		isTaskCaseSensitive);
+		isTaskCaseSensitive,
+		false);
 }
 
 public  final boolean atEnd() {
@@ -235,6 +263,72 @@ public  final boolean atEnd() {
 
 	return this.source.length == this.currentPosition;
 }
+
+// Check presence of non null tags.
+private final void checkNonNullTag(int commentStart, int commentEnd) {
+	for (int i = commentStart + 2, state = 0; state < 9;) {
+		if (i >= commentEnd || i >= this.eofPosition) {
+			return;
+		}
+		char currentChar = this.source[i++];
+		switch (state) {
+			case 0: // start
+				if (currentChar == 'N') {
+					state++;
+				}
+				else if (!CharOperation.isWhitespace(currentChar)) {
+					return;
+				}
+				continue;
+			case 1: // got a N
+				switch (currentChar) {
+					case 'O':
+						state++;
+						continue;
+					case 'N':
+						state = 8;
+						continue;
+					default:
+						return;
+				}
+				// never get there
+			case 2: // got NO
+			case 4: // got NON-
+				if (currentChar == 'N') {
+					state++;
+					continue;
+				}
+				return;
+			case 3: // got NON
+				if (currentChar == '-') {
+					state++;
+					continue;
+				}
+				return;
+			case 5: // got NON-N
+				if (currentChar == 'U') {
+					state++;
+					continue;
+				}
+				return;
+			case 6: // got NON-NU
+			case 7: // got NON-NUL
+				if (currentChar == 'L') {
+					state++;
+					continue;
+				}
+				return;
+			case 8:	// got NON-NULL or NN
+				if (currentChar != '*' && !CharOperation.isWhitespace(currentChar)) {
+					return;
+				}
+				state = 9; // got a marker
+		}
+	}
+	// got a non null marker
+	this.gotNonNullTag = true;
+}
+
 // chech presence of task: tags
 // TODO (frederic) see if we need to take unicode characters into account...
 public void checkTaskTag(int commentStart, int commentEnd) throws InvalidInputException {
@@ -1280,6 +1374,10 @@ public int getNextToken() throws InvalidInputException {
 							   	}
 								recordComment(TokenNameCOMMENT_LINE);
 								if (this.taskTags != null) checkTaskTag(this.startPosition, this.currentPosition);
+								if (this.checkNullReferences) {
+									checkNonNullTag(this.startPosition, 
+											this.currentPosition);
+								}
 								if ((this.currentCharacter == '\r') || (this.currentCharacter == '\n')) {
 									if (this.checkNonExternalizedStringLiterals) {
 										parseTags();
@@ -1299,6 +1397,10 @@ public int getNextToken() throws InvalidInputException {
 								this.currentPosition--;
 								recordComment(TokenNameCOMMENT_LINE);
 								if (this.taskTags != null) checkTaskTag(this.startPosition, this.currentPosition);
+								if (this.checkNullReferences) {
+									checkNonNullTag(this.startPosition, 
+											this.currentPosition);
+								}
 								if (this.checkNonExternalizedStringLiterals) {
 									parseTags();
 								}
@@ -1404,6 +1506,10 @@ public int getNextToken() throws InvalidInputException {
 								recordComment(token);
 								this.commentTagStarts[this.commentPtr] = firstTag;
 								if (this.taskTags != null) checkTaskTag(this.startPosition, this.currentPosition);
+								if (this.checkNullReferences) {
+									checkNonNullTag(this.startPosition, 
+											this.currentPosition);
+								}
 								if (this.tokenizeComments) {
 									/*
 									if (isJavadoc)
