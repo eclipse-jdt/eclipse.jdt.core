2bc8ee5cd6fc6801576741465b39e1e9af899d1a
Philipe Mulet <pmulet>
2006-02-19 15:30:18 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericTypeTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericTypeTest.java
@@ -25937,8 +25937,7 @@ public void test838() {
 		"");
 }
 //https://bugs.eclipse.org/bugs/show_bug.cgi?id=109118
-// TODO need to be updated regarding fix for 128389
-public void _test839() {
+public void test839() {
 	this.runConformTest(
 		new String[] {
 			"com/test/Tester.java",
@@ -25965,7 +25964,7 @@ public void _test839() {
 			"		throw new MyException();\n" + 
 			"	}\n" + 
 			"\n" + 
-			"	public class MyException extends Exception {\n" + 
+			"	public static class MyException extends Exception {\n" + 
 			"		\n" + 
 			"		public MyException() {\n" + 
 			"			super();\n" + 
