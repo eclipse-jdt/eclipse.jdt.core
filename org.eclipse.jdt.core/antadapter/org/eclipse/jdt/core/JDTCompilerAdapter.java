8bf9455f62816ce90f9b647400e93bfa5d632a6f
Olivier Thomann <oliviert>
2006-03-29 15:21:14 +0000

    HEAD - Fix for 133748

--- org.eclipse.jdt.core/antadapter/org/eclipse/jdt/core/JDTCompilerAdapter.java
+++ org.eclipse.jdt.core/antadapter/org/eclipse/jdt/core/JDTCompilerAdapter.java
@@ -567,7 +567,7 @@ public class JDTCompilerAdapter extends DefaultCompilerAdapter {
             if (dir.exists() && dir.isDirectory()) {
                 FileSet fs = new FileSet();
                 fs.setDir(dir);
-                fs.setIncludes("*"); //$NON-NLS-1$
+                fs.setIncludes("*.zip,*.jar"); //$NON-NLS-1$
                 classpath.addFileset(fs);
             }
         }
