752dc22f4237db487e590b76a74f04b6451ef39b
thanson <thanson>
2006-04-11 18:19:54 +0000

    Fixed warning.

--- org.eclipse.jdt.apt.tests/src/org/eclipse/jdt/apt/tests/AptReconcileTests.java
+++ org.eclipse.jdt.apt.tests/src/org/eclipse/jdt/apt/tests/AptReconcileTests.java
@@ -12,17 +12,11 @@
 
 package org.eclipse.jdt.apt.tests;
 
-import java.io.File;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.core.resources.*;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.apt.core.AptPlugin;
 import org.eclipse.jdt.apt.core.internal.generatedfile.GeneratedFileManager;
 import org.eclipse.jdt.apt.core.util.AptConfig;
@@ -30,9 +24,7 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.WorkingCopyOwner;
-import org.eclipse.jdt.core.tests.model.CompilationUnitTests;
 import org.eclipse.jdt.core.tests.model.ModifyingResourceTests;
-import org.eclipse.jdt.core.tests.util.Util;
 
 public class AptReconcileTests extends ModifyingResourceTests
 {
@@ -449,6 +441,4 @@ public class AptReconcileTests extends ModifyingResourceTests
 	public static final String	TEST_FOLDER		= "/" + TEST_PROJECT //$NON-NLS-1$
 													+ "/src/test"; //$NON-NLS-1$
 	
-	private File				_classesJarFile;
-
 }
