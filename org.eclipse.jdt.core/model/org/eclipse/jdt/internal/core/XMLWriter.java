4363912f18eb1d976122576b794dd16e256f3f9f
Jerome Lanneluc <jeromel>
2006-06-22 07:57:43 +0000

    *** empty log message ***

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/XMLWriter.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/XMLWriter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,7 +18,6 @@ import org.eclipse.jdt.internal.core.util.Util;
  * @since 3.0
  */
 class XMLWriter extends GenericXMLWriter {
-	/* constants */
 
 	public XMLWriter(Writer writer, IJavaProject project, boolean printXmlVersion) {
 		super(writer, Util.getLineSeparator((String) null, project), printXmlVersion);
