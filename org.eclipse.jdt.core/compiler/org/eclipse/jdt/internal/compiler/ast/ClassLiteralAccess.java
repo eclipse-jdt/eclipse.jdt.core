d2c5dc16b658319d6e93a35d91f01c110614fa67
Olivier Thomann <oliviert>
2006-11-09 01:37:15 +0000

    3.2 maintenance - Fix for 163600

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ClassLiteralAccess.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/ClassLiteralAccess.java
@@ -36,11 +36,10 @@ public class ClassLiteralAccess extends Expression {
 		FlowInfo flowInfo) {
 
 		// if reachable, request the addition of a synthetic field for caching the class descriptor
-		SourceTypeBinding sourceType =
-			currentScope.outerMostMethodScope().enclosingSourceType();
-		if ((!(sourceType.isInterface()
-				// no field generated in interface case (would'nt verify) see 1FHHEZL
-				|| sourceType.isBaseType()))
+		SourceTypeBinding sourceType = currentScope.outerMostClassScope().enclosingSourceType();
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=22334
+		if (!sourceType.isInterface()
+				&& !sourceType.isBaseType()
 				&& currentScope.compilerOptions().sourceLevel < ClassFileConstants.JDK1_5) {
 			syntheticField = sourceType.addSyntheticFieldForClassLiteral(targetType, currentScope);
 		}
