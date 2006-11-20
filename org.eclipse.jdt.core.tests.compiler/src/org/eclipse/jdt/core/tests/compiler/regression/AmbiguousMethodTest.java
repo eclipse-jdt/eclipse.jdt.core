b0673075374f5e3d492b8bf3cb6c4f6a7d2a3dcc
mdaniel <mdaniel>
2006-11-20 12:32:55 +0000

    HEAD - (inactive) test case for bug 163370 (test044)

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AmbiguousMethodTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AmbiguousMethodTest.java
@@ -1509,4 +1509,27 @@ public void test043() {
 		"The return type is incompatible with J.foo(float)\n" + 
 		"----------\n");
 }
+
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=163370
+public void _test044() {
+	this.runConformTest(
+		new String[] {
+			"X.java",
+			"interface I<E> {}\n" + 
+			"class Y<E> {}\n" + 
+			"public class X<E extends Y<E>> implements I<E> {\n" + 
+			"  public static <E extends Y<E>> X<E> bar(X<E> s) {\n" + 
+			"    return null;\n" + 
+			"  }\n" + 
+			"  public static <E extends Y<E>> X<E> bar(I<E> c) {\n" + 
+			"    return null;\n" + 
+			"  }\n" + 
+			"  public static <E extends Y<E>> X<E> foo(X<E> s) {\n" + 
+			"    X<E> result = bar(s);\n" + 
+			"    return result;\n" + 
+			"  }\n" + 
+			"}"
+		},
+		"");
+}
 }
