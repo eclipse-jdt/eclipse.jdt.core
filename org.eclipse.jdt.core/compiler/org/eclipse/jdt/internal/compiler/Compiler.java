ec10ecc7c18b8fb0bff4ab64d12369772e534d3d
Kent Johnson <kent>
2007-01-09 16:26:50 +0000

    162478 in 3.2.2 stream

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/Compiler.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/Compiler.java
@@ -641,6 +641,7 @@ public class Compiler implements ITypeRequestor, ProblemSeverities {
 				// binding resolution
 				lookupEnvironment.completeTypeBindings();
 			}
+			this.lookupEnvironment.unitBeingCompleted = unit;
 			this.parser.getMethodBodies(unit);
 			if (unit.scope != null) {
 				// fault in fields & methods
@@ -672,6 +673,7 @@ public class Compiler implements ITypeRequestor, ProblemSeverities {
 			this.handleInternalException(e, unit, null);
 			throw e; // rethrow
 		} finally {
+			this.lookupEnvironment.unitBeingCompleted = null;
 			// No reset is performed there anymore since,
 			// within the CodeAssist (or related tools),
 			// the compiler may be called *after* a call
