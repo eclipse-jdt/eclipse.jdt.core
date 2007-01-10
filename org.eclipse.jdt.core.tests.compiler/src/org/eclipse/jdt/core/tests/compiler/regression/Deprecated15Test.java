a41e2ce3ae2ca6eb45aa96f0443456d1a4c72840
Philipe Mulet <pmulet>
2007-01-10 14:48:10 +0000

    *** empty log message ***

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/Deprecated15Test.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/Deprecated15Test.java
@@ -136,9 +136,14 @@ public void test002() {
 		"1. ERROR in p\\M1.java (at line 4)\n" + 
 		"	a.N1.N2.N3 m = null;\n" + 
 		"	^^^^^^^^^^\n" + 
+		"The type N1.N2 is deprecated\n" + 
+		"----------\n" + 
+		"2. ERROR in p\\M1.java (at line 4)\n" + 
+		"	a.N1.N2.N3 m = null;\n" + 
+		"	^^^^^^^^^^\n" + 
 		"The type N1.N2.N3 is deprecated\n" + 
 		"----------\n" + 
-		"2. ERROR in p\\M1.java (at line 5)\n" + 
+		"3. ERROR in p\\M1.java (at line 5)\n" + 
 		"	m.foo();\n" + 
 		"	^^^^^^^\n" + 
 		"The method foo() from the type N1.N2.N3 is deprecated\n" + 
