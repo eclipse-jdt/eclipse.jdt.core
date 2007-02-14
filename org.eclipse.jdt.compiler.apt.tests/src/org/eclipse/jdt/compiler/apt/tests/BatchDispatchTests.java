9b273f1da9b2de9c2343ae145581432c1143c1d0
Walter Harley <wharley>
2007-02-14 02:05:50 +0000

    More test code cleanup

--- org.eclipse.jdt.compiler.apt.tests/src/org/eclipse/jdt/compiler/apt/tests/BatchDispatchTests.java
+++ org.eclipse.jdt.compiler.apt.tests/src/org/eclipse/jdt/compiler/apt/tests/BatchDispatchTests.java
@@ -12,28 +12,14 @@
 package org.eclipse.jdt.compiler.apt.tests;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.ServiceLoader;
 
-import javax.tools.FileObject;
-import javax.tools.ForwardingJavaFileManager;
 import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
-import javax.tools.JavaCompiler.CompilationTask;
-import javax.tools.JavaFileObject.Kind;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
 
 /**
  * Test the ability to execute annotation processors in batch mode, including 
@@ -43,14 +29,6 @@ import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
  * @since 3.3
  */
 public class BatchDispatchTests extends TestCase {
-	
-	private static boolean _verbose = false; 
-	
-	// relative to plugin directory
-	protected static final String _processorJarName = "export\\apttestprocessors.jar";
-	
-	// locations to copy and generate files
-	protected static final String _tmpFolder = System.getProperty("java.io.tmpdir") + "eclipse-temp";
 	
 	private static final String[] ONE_ARG_OPTIONS = {
 		"-s",
@@ -65,15 +43,6 @@ public class BatchDispatchTests extends TestCase {
 	};
 
 
-	protected JavaCompiler _eclipseCompiler;
-	
-	protected String _tmpSrcFolderName;
-	protected File _tmpSrcDir;
-	protected String _tmpBinFolderName;
-	protected File _tmpBinDir;
-	protected String _tmpGenFolderName;
-	protected File _tmpGenDir;
-	
 	public static TestSuite suite() {
 		TestSuite suite = new TestSuite();
 		suite.addTestSuite(BatchDispatchTests.class);
@@ -87,65 +56,37 @@ public class BatchDispatchTests extends TestCase {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		
-		_tmpBinFolderName = _tmpFolder + File.separator + "bin";
-		_tmpBinDir = new File(_tmpBinFolderName);
-		deleteTree(_tmpBinDir); // remove existing contents
-		_tmpBinDir.mkdirs();
-		assert _tmpBinDir.exists() : "couldn't mkdirs " + _tmpBinFolderName;
-		
-		_tmpGenFolderName = _tmpFolder + File.separator + "gen-src";
-		_tmpGenDir = new File(_tmpGenFolderName);
-		deleteTree(_tmpGenDir); // remove existing contents
-		_tmpGenDir.mkdirs();
-		assert _tmpGenDir.exists() : "couldn't mkdirs " + _tmpGenFolderName;
-		
-		_tmpSrcFolderName = _tmpFolder + File.separator + "src";
-		_tmpSrcDir = new File(_tmpSrcFolderName);
-		deleteTree(_tmpSrcDir); // remove existing contents
-		_tmpSrcDir.mkdirs();
-		assert _tmpSrcDir.exists() : "couldn't mkdirs " + _tmpSrcFolderName;
-		
-		File processorJar = new File(_processorJarName);
-		assertTrue("Couldn't find processor jar at " + processorJar.getAbsolutePath(), processorJar.exists());
-		
-		ServiceLoader<JavaCompiler> javaCompilerLoader = ServiceLoader.load(JavaCompiler.class);//, EclipseCompiler.class.getClassLoader());
-		int compilerCounter = 0;
-		for (JavaCompiler javaCompiler : javaCompilerLoader) {
-			compilerCounter++;
-			if (javaCompiler instanceof EclipseCompiler) {
-				_eclipseCompiler = javaCompiler;
-			}
-	     }
-		assertEquals("Only one compiler available", 1, compilerCounter);
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
+		BatchTestUtils.init();
 	}
 
 	/**
 	 * Verify that Eclipse compiler properly supports apt-related command line options
 	 */
 	public void testCheckOptions() {
-		assertNotNull("No compiler found", _eclipseCompiler);
 		for (String option : ONE_ARG_OPTIONS) {
-			assertEquals(option + " requires 1 argument", 1, _eclipseCompiler.isSupportedOption(option));
+			assertEquals(option + " requires 1 argument", 1, BatchTestUtils.getEclipseCompiler().isSupportedOption(option));
 		}
 		for (String option : ZERO_ARG_OPTIONS) {
-			assertEquals(option + " requires no argument", 0, _eclipseCompiler.isSupportedOption(option));
+			assertEquals(option + " requires no argument", 0, BatchTestUtils.getEclipseCompiler().isSupportedOption(option));
 		}
 	}
 	
+	/**
+	 * Veriy that processor sees correct environment options
+	 * (sanity check with system compiler)
+	 */
 	public void testProcessorArgumentsWithSystemCompiler() {
 		// System compiler
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		internalTestProcessorArguments(compiler);
 	}
 	
+	/**
+	 * Veriy that processor sees correct environment options
+	 * when called from Eclipse compiler
+	 */
 	public void testProcessorArgumentsWithEclipseCompiler() {
-		JavaCompiler compiler = _eclipseCompiler;
+		JavaCompiler compiler = BatchTestUtils.getEclipseCompiler();
 		internalTestProcessorArguments(compiler);
 	}
 
@@ -165,7 +106,7 @@ public class BatchDispatchTests extends TestCase {
 	 */
 	public void testCompilerOneClassWithEclipseCompiler() {
 		// Eclipse compiler
-		JavaCompiler compiler = _eclipseCompiler;
+		JavaCompiler compiler = BatchTestUtils.getEclipseCompiler();
 		internalTestGenerateClass(compiler);
 	}
 	
@@ -173,162 +114,55 @@ public class BatchDispatchTests extends TestCase {
 	 * Verify that if a type has two annotations, both processors are run.
 	 */
 	public void testTwoAnnotations() {
-		File inputFile = TestUtils.copyResource("targets/dispatch/TwoAnnotations.java.txt", _tmpSrcFolderName, "TwoAnnotations.java");
+		File inputFile = TestUtils.copyResource("targets/dispatch/TwoAnnotations.java.txt", BatchTestUtils.getSrcFolderName(), "TwoAnnotations.java");
 		
 		List<String> options = new ArrayList<String>();
 		// See corresponding list in CheckArgsProc processor.
 		// Processor will throw IllegalStateException if it detects a mismatch.
 		options.add("-Afoo=bar");
 		options.add("-Anovalue");
-		compileOneClass(_eclipseCompiler, inputFile, options);
+		BatchTestUtils.compileOneClass(BatchTestUtils.getEclipseCompiler(), inputFile, options);
 		
 		// check that the src and class files were generated
- 		File genSrcFile = TestUtils.concatPath(_tmpGenFolderName, "gen", "TwoAnnotationsGen.java");
+ 		File genSrcFile = TestUtils.concatPath(BatchTestUtils.getGenFolderName(), "gen", "TwoAnnotationsGen.java");
  		assertTrue("generated src file does not exist", genSrcFile.exists());
  		
- 		File classFile = TestUtils.concatPath(_tmpBinFolderName, "targets", "dispatch", "TwoAnnotations.class");
+ 		File classFile = TestUtils.concatPath(BatchTestUtils.getBinFolderName(), "targets", "dispatch", "TwoAnnotations.class");
  		assertTrue("ordinary src file was not compiled", classFile.exists());
  		
- 		File genClassFile = TestUtils.concatPath(_tmpBinFolderName, "gen", "TwoAnnotationsGen.class");
+ 		File genClassFile = TestUtils.concatPath(BatchTestUtils.getBinFolderName(), "gen", "TwoAnnotationsGen.class");
  		assertTrue("generated src file was not compiled", genClassFile.exists());
 	}
 	
-	/*
-	 * Clean up the compiler
-	 */
-	public void testCleanUp() {
-		_eclipseCompiler = null;
-	}
-	
+	// Called with system compiler and Eclipse compiler
 	private void internalTestGenerateClass(JavaCompiler compiler) {
-		File inputFile = TestUtils.copyResource("targets/dispatch/HasGenClass.java.txt", _tmpSrcFolderName, "HasGenClass.java");
+		File inputFile = TestUtils.copyResource("targets/dispatch/HasGenClass.java.txt", BatchTestUtils.getSrcFolderName(), "HasGenClass.java");
 		
 		List<String> options = new ArrayList<String>();
-		compileOneClass(compiler, inputFile, options);
+		BatchTestUtils.compileOneClass(compiler, inputFile, options);
 		
-		// check that the src and class files were generated
- 		File genSrcFile = TestUtils.concatPath(_tmpGenFolderName, "gen", "HgcGen.java");
+		// check that the gen-src and class files were generated
+ 		File genSrcFile = TestUtils.concatPath(BatchTestUtils.getGenFolderName(), "gen", "HgcGen.java");
  		assertTrue("generated src file does not exist", genSrcFile.exists());
  		
- 		File classFile = TestUtils.concatPath(_tmpBinFolderName, "targets", "dispatch", "HasGenClass.class");
+ 		File classFile = TestUtils.concatPath(BatchTestUtils.getBinFolderName(), "targets", "dispatch", "HasGenClass.class");
  		assertTrue("ordinary src file was not compiled", classFile.exists());
  		
- 		File genClassFile = TestUtils.concatPath(_tmpBinFolderName, "gen", "HgcGen.class");
+ 		File genClassFile = TestUtils.concatPath(BatchTestUtils.getBinFolderName(), "gen", "HgcGen.class");
  		assertTrue("generated src file was not compiled", genClassFile.exists());
 		
 	}
 	
+	// Called with system compiler and Eclipse compiler
 	private void internalTestProcessorArguments(JavaCompiler compiler) {
-		File inputFile = TestUtils.copyResource("targets/dispatch/HasCheckArgs.java.txt", _tmpSrcFolderName, "HasCheckArgs.java");
+		File inputFile = TestUtils.copyResource("targets/dispatch/HasCheckArgs.java.txt", BatchTestUtils.getSrcFolderName(), "HasCheckArgs.java");
 		
 		List<String> options = new ArrayList<String>();
 		// See corresponding list in CheckArgsProc processor.
 		// Processor will throw IllegalStateException if it detects a mismatch.
 		options.add("-Afoo=bar");
 		options.add("-Anovalue");
-		compileOneClass(compiler, inputFile, options);
-	}
-
-	/**
-	 * Recursively delete the contents of a directory, including any subdirectories.
-	 * This is not optimized to handle very large or deep directory trees efficiently.
-	 * @param f is either a normal file (which will be deleted) or a directory
-	 * (which will be emptied and then deleted).
-	 */
-	protected static void deleteTree(File f)
-	{
-		if (null == f) {
-			return;
-		}
-		File[] children = f.listFiles();
-		if (null != children) {
-			// if f has any children, (recursively) delete them
-			for (File child : children) {
-				deleteTree(child);
-			}
-		}
-		// At this point f is either a normal file or an empty directory
-		f.delete();
-	}
-
-	/**
-	 * Get a JavaFileManager that can spit out diagnostic info.
-	 */
-	private ForwardingJavaFileManager<StandardJavaFileManager> getForwardingFileManager(
-			StandardJavaFileManager manager) {
-		ForwardingJavaFileManager<StandardJavaFileManager> forwardingJavaFileManager = new ForwardingJavaFileManager<StandardJavaFileManager>(manager) {
-			@Override
-			public FileObject getFileForInput(Location location, String packageName, String relativeName)
-					throws IOException {
-				if (_verbose)
-					System.out.println("Create file for input : " + packageName + " " + relativeName + " in location " + location);
-				return super.getFileForInput(location, packageName, relativeName);
-			}
-			@Override
-			public JavaFileObject getJavaFileForInput(Location location, String className, Kind kind)
-					throws IOException {
-				if (_verbose)
-					System.out.println("Create java file for input : " + className + " in location " + location);
-				return super.getJavaFileForInput(location, className, kind);
-			}
-			@Override
-			public JavaFileObject getJavaFileForOutput(Location location,
-					String className,
-					Kind kind,
-					FileObject sibling) throws IOException {
-				
-				if (_verbose) {
-					if (null != sibling) {
-						System.out.println("Create .class file for " + className + " in location " + location + " with sibling " + sibling.toUri());
-					}
-					else {
-						System.out.println("Create .class file for " + className + " in location " + location);
-					}
-				}
-				JavaFileObject javaFileForOutput = super.getJavaFileForOutput(location, className, kind, sibling);
-				if (_verbose)
-					System.out.println(javaFileForOutput.toUri());
-				return javaFileForOutput;
-			}
-		};
-		return forwardingJavaFileManager;
-	}
-
-	/**
-	 * Create a class that contains an annotation that generates another class, 
-	 * and compile it.  Verify that generation and compilation succeeded.
-	 */
-	private void compileOneClass(JavaCompiler compiler, File inputFile, List<String> options) {
-		StandardJavaFileManager manager = compiler.getStandardFileManager(null, Locale.getDefault(), Charset.defaultCharset());
-		ForwardingJavaFileManager<StandardJavaFileManager> forwardingJavaFileManager = getForwardingFileManager(manager);
-	
-		
-		// create new list containing inputfile
-		List<File> files = new ArrayList<File>();
-		files.add(inputFile);
-		Iterable<? extends JavaFileObject> units = manager.getJavaFileObjectsFromFiles(files);
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter printWriter = new PrintWriter(stringWriter);
-		
-		if (compiler == _eclipseCompiler) {
-			options.add("-6.0"); // not the default for Eclipse compiler
-		}
-		options.add("-d");
-		options.add(_tmpBinFolderName);
-		options.add("-s");
-		options.add(_tmpGenFolderName);
-		options.add("-cp");
-		options.add(_tmpSrcFolderName + File.pathSeparator + _tmpGenFolderName + File.pathSeparator + _processorJarName);
-		options.add("-processorpath");
-		options.add(_processorJarName);
-		options.add("-XprintRounds");
-		CompilationTask task = compiler.getTask(printWriter, forwardingJavaFileManager, null, options, null, units);
-		Boolean result = task.call();
-		
-		if (!result.booleanValue()) {
-			System.err.println("Compilation failed: " + stringWriter.getBuffer().toString());
-	 		assertTrue("Compilation failed ", false);
-		}
+		BatchTestUtils.compileOneClass(compiler, inputFile, options);
 	}
 
 }
---
+++ org.eclipse.jdt.compiler.apt.tests/src/org/eclipse/jdt/compiler/apt/tests/BatchTestUtils.java
@@ -0,0 +1,163 @@
+/*******************************************************************************
+ * Copyright (c) 2007 BEA Systems, Inc. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    wharley@bea.com - initial API and implementation
+ *    
+ *******************************************************************************/
+
+package org.eclipse.jdt.compiler.apt.tests;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ServiceLoader;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.JavaCompiler.CompilationTask;
+
+import junit.framework.Assert;
+
+import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
+
+/**
+ * Helper class to support compilation and results checking for tests running in batch mode.  
+ * @since 3.3
+ */
+public class BatchTestUtils {
+	// relative to plugin directory
+	private static final String _processorJarName = "export\\apttestprocessors.jar";
+	
+	// locations to copy and generate files
+	private static final String _tmpFolder = System.getProperty("java.io.tmpdir") + "eclipse-temp";
+	
+	private static JavaCompiler _eclipseCompiler;
+	
+	private static String _tmpSrcFolderName;
+	private static File _tmpSrcDir;
+	private static String _tmpBinFolderName;
+	private static File _tmpBinDir;
+	private static String _tmpGenFolderName;
+	private static File _tmpGenDir;
+	
+	/**
+	 * Create a class that contains an annotation that generates another class, 
+	 * and compile it.  Verify that generation and compilation succeeded.
+	 */
+	public static void compileOneClass(JavaCompiler compiler, File inputFile, List<String> options) {
+		StandardJavaFileManager manager = compiler.getStandardFileManager(null, Locale.getDefault(), Charset.defaultCharset());
+	
+		
+		// create new list containing inputfile
+		List<File> files = new ArrayList<File>();
+		files.add(inputFile);
+		Iterable<? extends JavaFileObject> units = manager.getJavaFileObjectsFromFiles(files);
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stringWriter);
+		
+		if (compiler == _eclipseCompiler) {
+			options.add("-6.0"); // not the default for Eclipse compiler
+		}
+		options.add("-d");
+		options.add(_tmpBinFolderName);
+		options.add("-s");
+		options.add(_tmpGenFolderName);
+		options.add("-cp");
+		options.add(_tmpSrcFolderName + File.pathSeparator + _tmpGenFolderName + File.pathSeparator + _processorJarName);
+		options.add("-processorpath");
+		options.add(_processorJarName);
+		options.add("-XprintRounds");
+		CompilationTask task = compiler.getTask(printWriter, manager, null, options, null, units);
+		Boolean result = task.call();
+		
+		if (!result.booleanValue()) {
+			System.err.println("Compilation failed: " + stringWriter.getBuffer().toString());
+	 		Assert.assertTrue("Compilation failed ", false);
+		}
+	}
+	
+	public static String getBinFolderName() {
+		return _tmpBinFolderName;
+	}
+
+	public static JavaCompiler getEclipseCompiler() {
+		return _eclipseCompiler;
+	}
+	
+	public static String getGenFolderName() {
+		return _tmpGenFolderName;
+	}
+	
+	public static String getSrcFolderName() {
+		return _tmpSrcFolderName;
+	}
+	
+	/**
+	 * Load Eclipse compiler and create temporary directories on disk
+	 */
+	public static void init() 
+	{
+		_tmpBinFolderName = _tmpFolder + File.separator + "bin";
+		_tmpBinDir = new File(_tmpBinFolderName);
+		BatchTestUtils.deleteTree(_tmpBinDir); // remove existing contents
+		_tmpBinDir.mkdirs();
+		assert _tmpBinDir.exists() : "couldn't mkdirs " + _tmpBinFolderName;
+		
+		_tmpGenFolderName = _tmpFolder + File.separator + "gen-src";
+		_tmpGenDir = new File(_tmpGenFolderName);
+		BatchTestUtils.deleteTree(_tmpGenDir); // remove existing contents
+		_tmpGenDir.mkdirs();
+		assert _tmpGenDir.exists() : "couldn't mkdirs " + _tmpGenFolderName;
+		
+		_tmpSrcFolderName = _tmpFolder + File.separator + "src";
+		_tmpSrcDir = new File(_tmpSrcFolderName);
+		BatchTestUtils.deleteTree(_tmpSrcDir); // remove existing contents
+		_tmpSrcDir.mkdirs();
+		assert _tmpSrcDir.exists() : "couldn't mkdirs " + _tmpSrcFolderName;
+		
+		File processorJar = new File(_processorJarName);
+		Assert.assertTrue("Couldn't find processor jar at " + processorJar.getAbsolutePath(), processorJar.exists());
+		
+		ServiceLoader<JavaCompiler> javaCompilerLoader = ServiceLoader.load(JavaCompiler.class);//, EclipseCompiler.class.getClassLoader());
+		int compilerCounter = 0;
+		for (JavaCompiler javaCompiler : javaCompilerLoader) {
+			compilerCounter++;
+			if (javaCompiler instanceof EclipseCompiler) {
+				_eclipseCompiler = javaCompiler;
+			}
+	     }
+		Assert.assertEquals("Only one compiler available", 1, compilerCounter);
+	}
+
+	/**
+	 * Recursively delete the contents of a directory, including any subdirectories.
+	 * This is not optimized to handle very large or deep directory trees efficiently.
+	 * @param f is either a normal file (which will be deleted) or a directory
+	 * (which will be emptied and then deleted).
+	 */
+	public static void deleteTree(File f)
+	{
+		if (null == f) {
+			return;
+		}
+		File[] children = f.listFiles();
+		if (null != children) {
+			// if f has any children, (recursively) delete them
+			for (File child : children) {
+				deleteTree(child);
+			}
+		}
+		// At this point f is either a normal file or an empty directory
+		f.delete();
+	}
+}
