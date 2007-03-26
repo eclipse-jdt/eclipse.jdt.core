011e8e3a52083052e5657e005c0f9c924e859fa5
Jerome Lanneluc <jeromel>
2007-03-26 12:02:35 +0000

    HEAD - remove deprecation warnings

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/TestPessimisticProvider.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/TestPessimisticProvider.java
@@ -11,6 +11,8 @@
 package org.eclipse.jdt.core.tests.model;
 
 import org.eclipse.core.resources.*;
+import org.eclipse.core.resources.team.FileModificationValidationContext;
+import org.eclipse.core.resources.team.FileModificationValidator;
 import org.eclipse.core.runtime.*;
 import org.eclipse.jdt.internal.core.util.Util;
 import org.eclipse.team.core.RepositoryProvider;
@@ -18,18 +20,13 @@ import org.eclipse.team.core.RepositoryProvider;
 /**
  * Repository provider that can be configured to be pessimistic.
  */
-public class TestPessimisticProvider extends RepositoryProvider implements IFileModificationValidator {
-	private static TestPessimisticProvider soleInstance;
+public class TestPessimisticProvider extends RepositoryProvider {
 	
 	public static final String NATURE_ID = "org.eclipse.jdt.core.tests.model.pessimisticnature";
 	
 	public static boolean markWritableOnEdit;
 	public static boolean markWritableOnSave;
 
-	public TestPessimisticProvider() {
-		soleInstance = this;
-	}
-	
 	public void configureProject() {
 	}
 
@@ -40,56 +37,54 @@ public class TestPessimisticProvider extends RepositoryProvider implements IFile
 	public void deconfigure() {
 	}
 	
-	/*
-	 * @see IRepositoryProvider#getFileModificationValidator()
-	 */
-	public IFileModificationValidator getFileModificationValidator() {
-		return soleInstance;
+	public FileModificationValidator getFileModificationValidator2() {
+		return new FileModificationValidator() {
+			public IStatus validateEdit(final IFile[] files, FileModificationValidationContext context) {
+				if (TestPessimisticProvider.markWritableOnEdit) {
+					try {
+						ResourcesPlugin.getWorkspace().run(
+							new IWorkspaceRunnable() {
+								public void run(IProgressMonitor monitor)	{
+									for (int i = 0, length = files.length; i < length; i++) {
+										try {
+											setReadOnly(files[i], false);
+										} catch (CoreException e) {
+											e.printStackTrace();
+										}
+									}
+								}
+							},
+							null);
+					} catch (CoreException e) {
+						e.printStackTrace();
+						return e.getStatus();
+					}
+				} 
+				return Status.OK_STATUS;
+			}
+		
+			public IStatus validateSave(IFile file) {
+				if (TestPessimisticProvider.markWritableOnSave) {
+					try {
+						setReadOnly(file, false);
+					} catch (CoreException e) {
+						e.printStackTrace();
+						return e.getStatus();
+					}
+				}
+				return Status.OK_STATUS;
+			}
+		
+			void setReadOnly(IResource resource, boolean readOnly) throws CoreException {
+				if (Util.isReadOnlySupported()) {
+					ResourceAttributes resourceAttributes = resource.getResourceAttributes();
+					if (resourceAttributes != null) {
+						resourceAttributes.setReadOnly(readOnly);
+						resource.setResourceAttributes(resourceAttributes);
+					}		
+				}
+			}
+		};
 	}
 	
-	public IStatus validateEdit(final IFile[] files, Object context) {
-		if (markWritableOnEdit) {
-			try {
-				ResourcesPlugin.getWorkspace().run(
-					new IWorkspaceRunnable() {
-						public void run(IProgressMonitor monitor)	{
-							for (int i = 0, length = files.length; i < length; i++) {
-								try {
-									setReadOnly(files[i], false);
-								} catch (CoreException e) {
-									e.printStackTrace();
-								}
-							}
-						}
-					},
-					null);
-			} catch (CoreException e) {
-				e.printStackTrace();
-				return e.getStatus();
-			}
-		} 
-		return Status.OK_STATUS;
-	}
-
-	public IStatus validateSave(IFile file) {
-		if (markWritableOnSave) {
-			try {
-				setReadOnly(file, false);
-			} catch (CoreException e) {
-				e.printStackTrace();
-				return e.getStatus();
-			}
-		}
-		return Status.OK_STATUS;
-	}
-
-	public void setReadOnly(IResource resource, boolean readOnly) throws CoreException {
-		if (Util.isReadOnlySupported()) {
-			ResourceAttributes resourceAttributes = resource.getResourceAttributes();
-			if (resourceAttributes != null) {
-				resourceAttributes.setReadOnly(readOnly);
-				resource.setResourceAttributes(resourceAttributes);
-			}		
-		}
-	}
 }
