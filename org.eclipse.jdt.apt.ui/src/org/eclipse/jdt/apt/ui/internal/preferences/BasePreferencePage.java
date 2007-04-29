5968239a533ddcecfe4f0e19826f01eca0bd5d0c
Walter Harley <wharley>
2007-04-29 04:57:23 +0000

    Update copyrights

--- org.eclipse.jdt.apt.ui/src/org/eclipse/jdt/apt/ui/internal/preferences/BasePreferencePage.java
+++ org.eclipse.jdt.apt.ui/src/org/eclipse/jdt/apt/ui/internal/preferences/BasePreferencePage.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2005 BEA Systems, Inc.
+ * Copyright (c) 2005, 2006 BEA Systems, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   {INITIAL_AUTHOR} - initial API and implementation
+ *   wharley@bea.com - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.jdt.apt.ui.internal.preferences;
