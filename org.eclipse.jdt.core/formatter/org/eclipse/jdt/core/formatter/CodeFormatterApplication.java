b0065f60b83aeac5c6920c9b0be261e7e018fd12
Olivier Thomann <oliviert>
2007-05-10 16:10:43 +0000

    HEAD - Fix copyrights

--- org.eclipse.jdt.core/formatter/org/eclipse/jdt/core/formatter/CodeFormatterApplication.java
+++ org.eclipse.jdt.core/formatter/org/eclipse/jdt/core/formatter/CodeFormatterApplication.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2004 Ben Konrath <ben@bagu.org>
  * Copyright (c) 2006 Red Hat Incorporated
+ * Copyright (c) 2007 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
