a1f9825a7f8de06824875cf094f49ab35bbb1b56
cvs2svn <platform-releng-dev@eclipse.org>
2007-06-27 23:19:20 +0000

    This commit was manufactured by cvs2svn to create branch 'R3_3_maintenance'.
    Sprout from master 2007-06-21 13:15:13 UTC Olivier Thomann <oliviert> 'HEAD - Fix for 193570'
    Cherrypick from master 2007-06-27 23:19:17 UTC Walter Harley <wharley> 'Initial commit of IDE test code.  Contains some processors and annotations for manual testing, but as yet no automated tests.':
        org.eclipse.jdt.apt.pluggable.tests/.classpath
        org.eclipse.jdt.apt.pluggable.tests/.cvsignore
        org.eclipse.jdt.apt.pluggable.tests/.project
        org.eclipse.jdt.apt.pluggable.tests/META-INF/MANIFEST.MF
        org.eclipse.jdt.apt.pluggable.tests/META-INF/services/javax.annotation.processing.Processor
        org.eclipse.jdt.apt.pluggable.tests/about.html
        org.eclipse.jdt.apt.pluggable.tests/annotations.jardesc
        org.eclipse.jdt.apt.pluggable.tests/build.properties
        org.eclipse.jdt.apt.pluggable.tests/plugin.properties
        org.eclipse.jdt.apt.pluggable.tests/plugin.xml
        org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/Apt6TestsPlugin.java
        org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/TestAll.java
        org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/annotations/GenClass6.java
        org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/annotations/Message6.java
        org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/processors/genclass6/GenClass6Proc.java
        org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/processors/message6/Message6Proc.java
        org.eclipse.jdt.apt.pluggable.tests/test.xml
        org.eclipse.jdt.compiler.apt/src/org/eclipse/jdt/internal/compiler/apt/model/ElementImpl.java

---
+++ org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/Apt6TestsPlugin.java
@@ -0,0 +1,11 @@
+package org.eclipse.jdt.apt.pluggable.tests;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class Apt6TestsPlugin extends Plugin {
+
+	public Apt6TestsPlugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
