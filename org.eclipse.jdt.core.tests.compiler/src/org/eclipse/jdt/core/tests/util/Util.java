b491247e872902f1348e11c21d1da6d6ba1402ac
Olivier Thomann <oliviert>
2007-07-05 15:15:14 +0000

    revert fix till Philippe approval

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/util/Util.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/util/Util.java
@@ -639,75 +639,47 @@ public static int getFreePort() {
  * Example of use: [org.eclipse.jdt.core.tests.util.Util.getJavaClassLib()]
 */
 public static String[] getJavaClassLibs() {
-	// check bootclasspath properties for Sun, JRockit and Harmony VMs
-	String bootclasspathProperty = System.getProperty("sun.boot.class.path"); //$NON-NLS-1$
-	if ((bootclasspathProperty == null) || (bootclasspathProperty.length() == 0)) {
-		// IBM J9 VMs
-		bootclasspathProperty = System.getProperty("vm.boot.class.path"); //$NON-NLS-1$
-		if ((bootclasspathProperty == null) || (bootclasspathProperty.length() == 0)) {
-			// Harmony using IBM VME
-			bootclasspathProperty = System.getProperty("org.apache.harmony.boot.class.path"); //$NON-NLS-1$
-		}
-	}
-	String[] jars = null;
-	if ((bootclasspathProperty != null) && (bootclasspathProperty.length() != 0)) {
-		StringTokenizer tokenizer = new StringTokenizer(bootclasspathProperty, File.pathSeparator);
-		final int size = tokenizer.countTokens();
-		jars = new String[size];
-		int i = 0;
-		while (tokenizer.hasMoreTokens()) {
-			final String fileName = toNativePath(tokenizer.nextToken());
-			if (new File(fileName).exists()) {
-				jars[i] = fileName;
-				i++;
+    String jreDir = getJREDirectory();
+    final String osName = System.getProperty("os.name");
+    if (jreDir == null) {
+        return new String[] {};
+    }
+    if (osName.startsWith("Mac")) {
+        return new String[] {
+            toNativePath(jreDir + "/../Classes/classes.jar")
+        };
+    }
+    final String vmName = System.getProperty("java.vm.name");
+    if ("J9".equals(vmName)) {
+        return new String[] {
+            toNativePath(jreDir + "/lib/jclMax/classes.zip")
+        };
+    }
+	if ("DRLVM".equals(vmName)) {
+		FilenameFilter jarFilter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".jar") & !name.endsWith("-src.jar");
 			}
+		};
+		String[] jars = new File(jreDir + "/lib/boot/").list(jarFilter);
+		for (int i = 0; i < jars.length; i++) {
+			jars[i] = toNativePath(jreDir + "/lib/boot/" + jars[i]);
 		}
-		if (size != i) {
-			// resize
-			System.arraycopy(jars, 0, (jars = new String[i]), 0, i);
-		}
-	} else {
-		String jreDir = getJREDirectory();
-		final String osName = System.getProperty("os.name");
-		if (jreDir == null) {
-			return new String[] {};
-		}
-		if (osName.startsWith("Mac")) {
-			return new String[] {
-					toNativePath(jreDir + "/../Classes/classes.jar")
-			};
-		}
-		final String vmName = System.getProperty("java.vm.name");
-		if ("J9".equals(vmName)) {
-			return new String[] {
-					toNativePath(jreDir + "/lib/jclMax/classes.zip")
-			};
-		}
-		String[] jarsNames = null;
-		ArrayList paths = new ArrayList();
-		if ("DRLVM".equals(vmName)) {
-			FilenameFilter jarFilter = new FilenameFilter() {
-				public boolean accept(File dir, String name) {
-					return name.endsWith(".jar") & !name.endsWith("-src.jar");
-				}
-			};
-			jarsNames = new File(jreDir + "/lib/boot/").list(jarFilter);
-			addJarEntries(jreDir + "/lib/boot/", jarsNames, paths);
-		} else {
-			jarsNames = new String[] {
-					"/lib/vm.jar",
-					"/lib/rt.jar",
-					"/lib/core.jar",
-					"/lib/security.jar",
-					"/lib/xml.jar",
-					"/lib/graphics.jar"
-			};
-			addJarEntries(jreDir, jarsNames, paths);
-		}
-		jars = new String[paths.size()];
-		paths.toArray(jars);
+		return jars;
 	}
-	return jars;
+    ArrayList paths = new ArrayList();
+    String[] jarsNames = new String[] {
+    		"/lib/vm.jar",
+    		"/lib/rt.jar",
+    		"/lib/core.jar",
+    		"/lib/security.jar",
+    		"/lib/xml.jar",
+    		"/lib/graphics.jar"
+    };
+    addJarEntries(jreDir, jarsNames, paths);
+    String[] result = new String[paths.size()];
+    paths.toArray(result);
+    return result;
 }
 private static void addJarEntries(String jreDir, String[] jarNames, ArrayList paths) {
 	for (int i = 0, max = jarNames.length; i < max; i++) {
@@ -719,14 +691,15 @@ private static void addJarEntries(String jreDir, String[] jarNames, ArrayList pa
 	}
 }
 public static String getJavaClassLibsAsString() {
-	String[] classLibs = getJavaClassLibs();
-	StringBuffer buffer = new StringBuffer();
-	for (int i = 0, max = classLibs.length; i < max; i++) {
-		buffer
-		.append(classLibs[i])
-		.append(File.pathSeparatorChar);
-	}
-	return buffer.toString();
+    String[] classLibs = getJavaClassLibs();
+    StringBuffer buffer = new StringBuffer();
+    for (int i = 0, max = classLibs.length; i < max; i++) {
+        buffer
+            .append(classLibs[i])
+            .append(File.pathSeparatorChar);
+
+    }
+    return buffer.toString();
 }
 /**
  * Returns the JRE directory this tests are running on.
