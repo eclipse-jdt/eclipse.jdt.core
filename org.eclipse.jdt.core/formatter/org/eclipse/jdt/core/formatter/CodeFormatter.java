06cb2fb2cd016c5bb9806c77f9b924461e74f729
Eric Jodet <erjodet>
2007-11-07 12:53:12 +0000

    HEAD - 208234

--- org.eclipse.jdt.core/formatter/org/eclipse/jdt/core/formatter/CodeFormatter.java
+++ org.eclipse.jdt.core/formatter/org/eclipse/jdt/core/formatter/CodeFormatter.java
@@ -16,7 +16,9 @@ import org.eclipse.text.edits.TextEdit;
 
 /**
  * Specification for a generic source code formatter.
- * 
+ * <p>
+ * This class is not intended to be instantiated or subclassed by clients.
+ * </p>
  * @since 3.0
  */
 public abstract class CodeFormatter {
