ffd901457a8019a34d01704f9139e1e52b8a6906
Olivier Thomann <oliviert>
2008-01-17 20:30:37 +0000

    HEAD - Complement for bug 206423. Missing @since tag

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/core/compiler/CharOperation.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/core/compiler/CharOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2855,6 +2855,7 @@ public static final int occurencesOf(
  * @return the int value of a subpart of array
  * @throws NumberFormatException if the designated subpart of array does not
  *         parse to an int
+ * @since 3.4
  */
 public static final int parseInt(char[] array, int start, int length) throws NumberFormatException {
 	if (length == 1) {
