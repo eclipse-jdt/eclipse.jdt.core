0cf161d418836dfc8cccd8823a224663dd8ed2a0
Frederic Fusier <ffusier>
2008-04-23 22:42:24 +0000

    HEAD - 102780 and 227043 fixes tuning

---
+++ org.eclipse.jdt.core.tests.model/workspace/FormatterJavadoc/test/wksp/eclipse/out/dont_indent_tags/X13.java
@@ -0,0 +1,16 @@
+package test.wksp.eclipse;
+
+public class X13 {
+
+	/**
+	 * Returns the string from the plugin's resource bundle, or 'key' if not
+	 * found.
+	 * 
+	 * @param key
+	 * the resource string key
+	 * @return the resource string for the given key
+	 */
+	String foo(String key) {
+		return null;
+	}
+}
