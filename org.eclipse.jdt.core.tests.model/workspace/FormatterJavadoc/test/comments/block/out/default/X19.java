3a9d0e9ebb77a4184e107b2ffacf2122d4d188cf
Frederic Fusier <ffusier>
2008-05-08 19:03:27 +0000

    HEAD - 230944

---
+++ org.eclipse.jdt.core.tests.model/workspace/FormatterJavadoc/test/comments/block/out/default/X19.java
@@ -0,0 +1,15 @@
+package test.comments.block;
+
+public class X19 {
+
+	/*- 
+	 * Here is a block comment with some very special
+	 * formatting that I want indent(1) to ignore.
+	 *
+	 *    one
+	 *        two
+	 *            three
+	 */
+	void foo() {
+	}
+}
