a9a704ef7949092931ba031e7411e0f023ad75bb
Philipe Mulet <pmulet>
2008-07-07 15:43:13 +0000

    239229

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ProblemTypeAndMethodTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ProblemTypeAndMethodTest.java
@@ -11,6 +11,7 @@
 package org.eclipse.jdt.core.tests.compiler.regression;
 
 import java.io.File;
+import java.util.Map;
 
 import junit.framework.Test;
 
@@ -20,6 +21,7 @@ import org.eclipse.jdt.core.util.ClassFileBytesDisassembler;
 import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
 import org.eclipse.jdt.internal.compiler.classfmt.ClassFileReader;
 import org.eclipse.jdt.internal.compiler.env.IBinaryMethod;
+import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 
 public class ProblemTypeAndMethodTest extends AbstractRegressionTest {
 public ProblemTypeAndMethodTest(String name) {
@@ -3794,4 +3796,93 @@ public void test080() {
 			"IOException cannot be resolved to a type\n" + 
 			"----------\n");
 }
+//https://bugs.eclipse.org/bugs/show_bug.cgi?id=239758
+public void _test081() {
+	if (this.complianceLevel <= ClassFileConstants.JDK1_4) return;
+	Map customOptions = getCompilerOptions();
+	customOptions.put(	CompilerOptions.OPTION_DocCommentSupport, CompilerOptions.ENABLED);	
+	customOptions.put(CompilerOptions.OPTION_ReportInvalidJavadoc, CompilerOptions.ERROR);
+	customOptions.put(CompilerOptions.OPTION_ReportInvalidJavadocTags, CompilerOptions.ENABLED);
+	this.runNegativeTest(
+			new String[] {
+				"com/ost/util/report/Matrix.java", // =================
+				"package com.ost.util.report;\n" + 
+				"import java.io.Serializable;\n" + 
+				"import com.ost.util.report.exceptions.InvalidRowSizeException;\n" + 
+				"public class Matrix<T> implements Serializable {\n" + 
+				"	/**\n" + 
+				"	 * @see exceptions.InvalidRowSizeException2\n" + 
+				"	 */\n" + 
+				"	public synchronized final void addRow(Object[] row){\n" + 
+				"			throw new InvalidRowSizeException();\n" + 
+				"	}\n" + 
+				"}\n",
+				"com/ost/util/report/FilterConstraintSpecification.java", // =================
+				"package com.ost.util.report;\n" + 
+				"import java.io.Serializable;\n" + 
+				"import com.ost.util.report.exceptions.MalformedFilterConstraintSpecification;\n" + 
+				"public final class FilterConstraintSpecification implements Serializable, Cloneable {\n" + 
+				"	private final void makeConstraint(){\n" + 
+				"		throw new MalformedFilterConstraintSpecification();\n" + 
+				"	}\n" + 
+				"}\n",
+				"com/ost/util/report/exceptions/MalformedFilterConstraintSpecification.java", // =================
+				"package com.ost.util.report.exceptions;\n" + 
+				"public class MalformedFilterConstraintSpecification extends RuntimeException {\n" + 
+				"	/** Creates a new instance of MalformedFilterConstraintSpecification */\n" + 
+				"	public MalformedFilterConstraintSpecification() {\n" + 
+				"		super();\n" + 
+				"	}\n" + 
+				"	/* Creates a new instance of MalformedFilterConstraintSpecification */\n" + 
+				"	public MalformedFilterConstraintSpecification(String message) {\n" + 
+				"		super(message);\n" + 
+				"	}\n" + 
+				"}\n",
+				"com/ost/util/report/exceptions/InvalidRowSizeException.java", // =================
+				"package com.ost.util.report.exceptions;\n" + 
+				"public class InvalidRowSizeException extends RuntimeException {\n" + 
+				"	/** Creates a new instance of InvalidRowSizeException */\n" + 
+				"	public InvalidRowSizeException() {\n" + 
+				"		super();\n" + 
+				"	}\n" + 
+				"	/* Creates a new instance of InvalidRowSizeException */\n" + 
+				"	public InvalidRowSizeException(String message) {\n" + 
+				"		super(message);\n" + 
+				"	}\n" + 
+				"}\n"				
+			},
+			"----------\n" + 
+			"1. WARNING in com\\ost\\util\\report\\Matrix.java (at line 4)\n" + 
+			"	public class Matrix<T> implements Serializable {\n" + 
+			"	             ^^^^^^\n" + 
+			"The serializable class Matrix does not declare a static final serialVersionUID field of type long\n" + 
+			"----------\n" + 
+			"2. ERROR in com\\ost\\util\\report\\Matrix.java (at line 6)\n" + 
+			"	* @see exceptions.InvalidRowSizeException2\n" + 
+			"	       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+			"Javadoc: exceptions cannot be resolved to a type\n" + 
+			"----------\n" + 
+			"----------\n" + 
+			"1. WARNING in com\\ost\\util\\report\\FilterConstraintSpecification.java (at line 4)\n" + 
+			"	public final class FilterConstraintSpecification implements Serializable, Cloneable {\n" + 
+			"	                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+			"The serializable class FilterConstraintSpecification does not declare a static final serialVersionUID field of type long\n" + 
+			"----------\n" + 
+			"----------\n" + 
+			"1. WARNING in com\\ost\\util\\report\\exceptions\\MalformedFilterConstraintSpecification.java (at line 2)\n" + 
+			"	public class MalformedFilterConstraintSpecification extends RuntimeException {\n" + 
+			"	             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+			"The serializable class MalformedFilterConstraintSpecification does not declare a static final serialVersionUID field of type long\n" + 
+			"----------\n" + 
+			"----------\n" + 
+			"1. WARNING in com\\ost\\util\\report\\exceptions\\InvalidRowSizeException.java (at line 2)\n" + 
+			"	public class InvalidRowSizeException extends RuntimeException {\n" + 
+			"	             ^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+			"The serializable class InvalidRowSizeException does not declare a static final serialVersionUID field of type long\n" + 
+			"----------\n",
+			null,
+			false,
+			customOptions);
+}
+
 }
