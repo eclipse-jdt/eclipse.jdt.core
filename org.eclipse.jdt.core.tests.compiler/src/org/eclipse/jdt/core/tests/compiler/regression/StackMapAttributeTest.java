da1b268914409ccc196058300ff04e7c889248e2
Olivier Thomann <oliviert>
2008-07-08 14:06:13 +0000

    HEAD - Fix for 237931

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/StackMapAttributeTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/StackMapAttributeTest.java
@@ -6096,4 +6096,25 @@ public class StackMapAttributeTest extends AbstractRegressionTest {
 			},
 		"SUCCESS");
 	}
+
+	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=237931
+	public void test039() {
+		this.runConformTest(
+			new String[] {
+				"X.java",
+				"public class X {\n" + 
+				"	public String[][] foo(String s) {\n" + 
+				"		return\n" + 
+				"			new String[][] { {\" \", s != null ? s : \"\" },\n" + 
+				"				{\" \", s != null ? s : \"\" },\n" + 
+				"				{\" \", s != null ? s : \"\" },\n" + 
+				"				{\" \", s != null ? s : \"\" } };\n" + 
+				"	}\n" + 
+				"	public static void main(String[] args) {\n" + 
+				"		System.out.println(\"SUCCESS\");\n" + 
+				"	}\n" + 
+				"}",
+			},
+		"SUCCESS");
+	}
 }
