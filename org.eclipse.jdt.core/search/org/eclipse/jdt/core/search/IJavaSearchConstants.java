32182d99a9e4095a7eee846288e3e10ca04721a2
Frederic Fusier <ffusier>
2008-08-28 13:38:40 +0000

    HEAD - 239972

--- org.eclipse.jdt.core/search/org/eclipse/jdt/core/search/IJavaSearchConstants.java
+++ org.eclipse.jdt.core/search/org/eclipse/jdt/core/search/IJavaSearchConstants.java
@@ -385,7 +385,8 @@ public interface IJavaSearchConstants {
 	int WILDCARD_BOUND_TYPE_REFERENCE = 0x80000;
 
 	/**
-	 * Return only type references used in an <code>instance of</code> condition.
+	 * Return only type references used as a type of an <code>instanceof</code>
+	 * expression.
 	 * <p>
 	 * When this flag is set, only {@link TypeReferenceMatch} matches will be
 	 * returned.
