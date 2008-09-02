295ea3e15a4e4aa9bcf6dce19d6e64297b9eb72c
Walter Harley <wharley>
2008-09-02 16:10:11 +0000

    Bug 245746 - NPE when calling GetTypeDeclaration() with nonexistent type

--- org.eclipse.jdt.apt.tests/src/org/eclipse/jdt/apt/tests/MirrorDeclarationTests.java
+++ org.eclipse.jdt.apt.tests/src/org/eclipse/jdt/apt/tests/MirrorDeclarationTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 BEA Systems, Inc. 
+ * Copyright (c) 2005, 2008 BEA Systems, Inc. and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -275,6 +275,10 @@ public class MirrorDeclarationTests extends APTTestBase {
 		
 		assertTrue(pos.column() == 32);
 		assertTrue(pos.line() == 3);
+		
+		// Bad type should gracefully return null
+		TypeDeclaration tBad = env.getTypeDeclaration("test.XYZ");
+		assertTrue(null == tBad);
 	}
 
 }
