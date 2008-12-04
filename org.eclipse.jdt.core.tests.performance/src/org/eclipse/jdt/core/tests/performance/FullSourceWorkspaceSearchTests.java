1fb602b75e740fee79145d5e2e5fdf04a9fb5ee8
Frederic Fusier <ffusier>
2008-12-04 17:17:18 +0000

    HEAD - Fix dead code

--- org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceSearchTests.java
+++ org.eclipse.jdt.core.tests.performance/src/org/eclipse/jdt/core/tests/performance/FullSourceWorkspaceSearchTests.java
@@ -204,9 +204,6 @@ public class FullSourceWorkspaceSearchTests extends FullSourceWorkspaceTests imp
 	 *
 	 * First wait that already started indexing jobs ends before performing test and measure.
 	 * Consider this initial indexing jobs as warm-up for this test.
-	 *
-	 * TODO (frederic) After 3.3, activate several iteration for this test to have more accurate results,
-	 * 	then rename the test as numbers will be different...
 	 */
 	public void testIndexing() throws CoreException {
 		tagAsSummary("Indexing all workspace projects", false); // do NOT put in fingerprint
@@ -214,28 +211,23 @@ public class FullSourceWorkspaceSearchTests extends FullSourceWorkspaceTests imp
 		// Wait for indexing end (we use initial indexing as warm-up)
 		AbstractJavaModelTests.waitUntilIndexesReady();
 
-		// Measures
-		int measures = false ? MEASURES_COUNT/2 : 1;
-		for (int i=0; i<measures; i++) {
+		// Remove project previous indexing
+		INDEX_MANAGER.removeIndexFamily(new Path(""));
+		INDEX_MANAGER.reset();
 
-			// Remove project previous indexing
-			INDEX_MANAGER.removeIndexFamily(new Path(""));
-			INDEX_MANAGER.reset();
+		// Clean memory
+		runGc();
 
-			// Clean memory
-			runGc();
-
-			// Restart brand new indexing
-			INDEX_MANAGER.request(new Measuring(true/*start measuring*/));
-			for (int j=0, length=ALL_PROJECTS.length; j<length; j++) {
-				INDEX_MANAGER.indexAll(ALL_PROJECTS[j].getProject());
-			}
-			AbstractJavaModelTests.waitUntilIndexesReady();
-
-			// end measure
-			INDEX_MANAGER.request(new Measuring(false /*end measuring*/));
-			AbstractJavaModelTests.waitUntilIndexesReady();
+		// Restart brand new indexing
+		INDEX_MANAGER.request(new Measuring(true/*start measuring*/));
+		for (int j=0, length=ALL_PROJECTS.length; j<length; j++) {
+			INDEX_MANAGER.indexAll(ALL_PROJECTS[j].getProject());
 		}
+		AbstractJavaModelTests.waitUntilIndexesReady();
+
+		// end measure
+		INDEX_MANAGER.request(new Measuring(false /*end measuring*/));
+		AbstractJavaModelTests.waitUntilIndexesReady();
 
 		// Commit
 		commitMeasurements();
