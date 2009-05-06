b6c213d292f2a14fc7e944fa6a9473734abc07ab
Olivier Thomann <oliviert>
2009-05-06 21:27:38 +0000

    HEAD - Fix for 274114

--- org.eclipse.jdt.compiler.tool/src/org/eclipse/jdt/internal/compiler/tool/Options.java
+++ org.eclipse.jdt.compiler.tool/src/org/eclipse/jdt/internal/compiler/tool/Options.java
@@ -55,6 +55,9 @@ public final class Options {
 		Options.ZERO_ARGUMENT_OPTIONS.add("-1.6");//$NON-NLS-1$
 		Options.ZERO_ARGUMENT_OPTIONS.add("-6");//$NON-NLS-1$
 		Options.ZERO_ARGUMENT_OPTIONS.add("-6.0");//$NON-NLS-1$
+		Options.ZERO_ARGUMENT_OPTIONS.add("-1.7");//$NON-NLS-1$
+		Options.ZERO_ARGUMENT_OPTIONS.add("-7");//$NON-NLS-1$
+		Options.ZERO_ARGUMENT_OPTIONS.add("-7.0");//$NON-NLS-1$
 		Options.ZERO_ARGUMENT_OPTIONS.add("-proc:only");//$NON-NLS-1$
 		Options.ZERO_ARGUMENT_OPTIONS.add("-proc:none");//$NON-NLS-1$
 		Options.ZERO_ARGUMENT_OPTIONS.add("-XprintProcessorInfo");//$NON-NLS-1$
@@ -139,86 +142,95 @@ public final class Options {
 			while (tokenizer.hasMoreTokens()) {
 				String token = tokenizer.nextToken();
 				tokenCounter++;
-				if ("constructorName".equals(token)//$NON-NLS-1$
-						|| token.equals("pkgDefaultMethod")//$NON-NLS-1$
-						|| token.equals("packageDefaultMethod")//$NON-NLS-1$
+				if (token.equals("allDeadCode")//$NON-NLS-1$
+						|| token.equals("allDeprecation")//$NON-NLS-1$
+						|| token.equals("allJavadoc")//$NON-NLS-1$
+						|| token.equals("assertIdentifier")//$NON-NLS-1$
+						|| token.equals("boxing")//$NON-NLS-1$
+						|| token.equals("charConcat")//$NON-NLS-1$
+						|| token.equals("compareIdentical")//$NON-NLS-1$
+						|| token.equals("conditionAssign")//$NON-NLS-1$
+						|| token.equals("constructorName")//$NON-NLS-1$
+						|| token.equals("deadCode")//$NON-NLS-1$
+						|| token.equals("dep-ann")//$NON-NLS-1$
+						|| token.equals("deprecation")//$NON-NLS-1$
+						|| token.equals("discouraged")//$NON-NLS-1$
+						|| token.equals("emptyBlock")//$NON-NLS-1$
+						|| token.equals("enumIdentifier")//$NON-NLS-1$
+						|| token.equals("enumSwitch")//$NON-NLS-1$
+						|| token.equals("fallthrough")//$NON-NLS-1$
+						|| token.equals("fieldHiding")//$NON-NLS-1$
+						|| token.equals("finalBound")//$NON-NLS-1$
+						|| token.equals("finally")//$NON-NLS-1$
+						|| token.equals("forbidden")//$NON-NLS-1$
+						|| token.equals("hashCode")//$NON-NLS-1$
+						|| token.equals("hiding")//$NON-NLS-1$
+						|| token.equals("incomplete-switch")//$NON-NLS-1$
+						|| token.equals("indirectStatic")//$NON-NLS-1$
+						|| token.equals("interfaceNonInherited")//$NON-NLS-1$
+						|| token.equals("intfAnnotation")//$NON-NLS-1$
+						|| token.equals("intfNonInherited")//$NON-NLS-1$
+						|| token.equals("intfRedundant")//$NON-NLS-1$
+						|| token.equals("javadoc")//$NON-NLS-1$
+						|| token.equals("localHiding")//$NON-NLS-1$
 						|| token.equals("maskedCatchBlock")//$NON-NLS-1$
 						|| token.equals("maskedCatchBlocks")//$NON-NLS-1$
-						|| token.equals("deprecation")//$NON-NLS-1$
-						|| token.equals("allDeprecation")//$NON-NLS-1$
-						|| token.equals("unusedLocal")//$NON-NLS-1$
-						|| token.equals("unusedLocals")//$NON-NLS-1$
+						|| token.equals("nls")//$NON-NLS-1$
+						|| token.equals("noEffectAssign")//$NON-NLS-1$
+						|| token.equals("noImplicitStringConversion")//$NON-NLS-1$
+						|| token.equals("null")//$NON-NLS-1$
+						|| token.equals("nullDereference")//$NON-NLS-1$
+						|| token.equals("over-ann")//$NON-NLS-1$
+						|| token.equals("packageDefaultMethod")//$NON-NLS-1$
+						|| token.equals("paramAssign")//$NON-NLS-1$
+						|| token.equals("pkgDefaultMethod")//$NON-NLS-1$
+						|| token.equals("raw")//$NON-NLS-1$
+						|| token.equals("semicolon")//$NON-NLS-1$
+						|| token.equals("serial")//$NON-NLS-1$
+						|| token.equals("specialParamHiding")//$NON-NLS-1$
+						|| token.equals("static-access")//$NON-NLS-1$
+						|| token.equals("staticReceiver")//$NON-NLS-1$
+						|| token.equals("super")//$NON-NLS-1$
+						|| token.equals("suppress")//$NON-NLS-1$
+						|| token.equals("syncOverride")//$NON-NLS-1$
+						|| token.equals("synthetic-access")//$NON-NLS-1$
+						|| token.equals("syntheticAccess")//$NON-NLS-1$
+						|| token.equals("typeHiding")//$NON-NLS-1$
+						|| token.equals("unchecked")//$NON-NLS-1$
+						|| token.equals("unnecessaryElse")//$NON-NLS-1$
+						|| token.equals("unqualified-field-access")//$NON-NLS-1$
+						|| token.equals("unqualifiedField")//$NON-NLS-1$
+						|| token.equals("unsafe")//$NON-NLS-1$
+						|| token.equals("unused")//$NON-NLS-1$
 						|| token.equals("unusedArgument")//$NON-NLS-1$
 						|| token.equals("unusedArguments")//$NON-NLS-1$
 						|| token.equals("unusedImport")//$NON-NLS-1$
 						|| token.equals("unusedImports")//$NON-NLS-1$
-						|| token.equals("unusedPrivate")//$NON-NLS-1$
 						|| token.equals("unusedLabel")//$NON-NLS-1$
-						|| token.equals("localHiding")//$NON-NLS-1$
-						|| token.equals("fieldHiding")//$NON-NLS-1$
-						|| token.equals("specialParamHiding")//$NON-NLS-1$
-						|| token.equals("conditionAssign")//$NON-NLS-1$
-						|| token.equals("syntheticAccess")//$NON-NLS-1$
-						|| token.equals("synthetic-access")//$NON-NLS-1$
-						|| token.equals("nls")//$NON-NLS-1$
-						|| token.equals("staticReceiver")//$NON-NLS-1$
-						|| token.equals("indirectStatic")//$NON-NLS-1$
-						|| token.equals("noEffectAssign")//$NON-NLS-1$
-						|| token.equals("intfNonInherited")//$NON-NLS-1$
-						|| token.equals("interfaceNonInherited")//$NON-NLS-1$
-						|| token.equals("charConcat")//$NON-NLS-1$
-						|| token.equals("noImplicitStringConversion")//$NON-NLS-1$
-						|| token.equals("semicolon")//$NON-NLS-1$
-						|| token.equals("serial")//$NON-NLS-1$
-						|| token.equals("emptyBlock")//$NON-NLS-1$
-						|| token.equals("uselessTypeCheck")//$NON-NLS-1$
-						|| token.equals("unchecked")//$NON-NLS-1$
-						|| token.equals("unsafe")//$NON-NLS-1$
-						|| token.equals("raw")//$NON-NLS-1$
-						|| token.equals("finalBound")//$NON-NLS-1$
-						|| token.equals("suppress")//$NON-NLS-1$
-						|| token.equals("warningToken")//$NON-NLS-1$
-						|| token.equals("unnecessaryElse")//$NON-NLS-1$
-						|| token.equals("javadoc")//$NON-NLS-1$
-						|| token.equals("allJavadoc")//$NON-NLS-1$
-						|| token.equals("assertIdentifier")//$NON-NLS-1$
-						|| token.equals("enumIdentifier")//$NON-NLS-1$
-						|| token.equals("finally")//$NON-NLS-1$
+						|| token.equals("unusedLocal")//$NON-NLS-1$
+						|| token.equals("unusedLocals")//$NON-NLS-1$
+						|| token.equals("unusedPrivate")//$NON-NLS-1$
 						|| token.equals("unusedThrown")//$NON-NLS-1$
-						|| token.equals("unqualifiedField")//$NON-NLS-1$
-						|| token.equals("unqualified-field-access")//$NON-NLS-1$
-						|| token.equals("typeHiding")//$NON-NLS-1$
+						|| token.equals("unusedTypeArgs")//$NON-NLS-1$
+						|| token.equals("uselessTypeCheck")//$NON-NLS-1$
 						|| token.equals("varargsCast")//$NON-NLS-1$
-						|| token.equals("null")//$NON-NLS-1$
-						|| token.equals("boxing")//$NON-NLS-1$
-						|| token.equals("over-ann")//$NON-NLS-1$
-						|| token.equals("dep-ann")//$NON-NLS-1$
-						|| token.equals("intfAnnotation")//$NON-NLS-1$
-						|| token.equals("enumSwitch")//$NON-NLS-1$
-						|| token.equals("incomplete-switch")//$NON-NLS-1$
-						|| token.equals("hiding")//$NON-NLS-1$
-						|| token.equals("static-access")//$NON-NLS-1$
-						|| token.equals("unused")//$NON-NLS-1$
-						|| token.equals("paramAssign")//$NON-NLS-1$
-						|| token.equals("discouraged")//$NON-NLS-1$
-						|| token.equals("forbidden")//$NON-NLS-1$
-						|| token.equals("fallthrough")) {//$NON-NLS-1$
+						|| token.equals("warningToken")) {//$NON-NLS-1$
 					continue;
-    			} else if (token.equals("tasks")) {//$NON-NLS-1$
-    				String taskTags = "";//$NON-NLS-1$
-    				int start = token.indexOf('(');
-    				int end = token.indexOf(')');
-    				if (start >= 0 && end >= 0 && start < end){
-    					taskTags = token.substring(start+1, end).trim();
-    					taskTags = taskTags.replace('|',',');
-    				}
-    				if (taskTags.length() == 0){
-    					return -1;
-    				}
-    				continue;
-    			} else {
-    				return -1;
-    			}
+				} else if (token.equals("tasks")) {//$NON-NLS-1$
+					String taskTags = "";//$NON-NLS-1$
+					int start = token.indexOf('(');
+					int end = token.indexOf(')');
+					if (start >= 0 && end >= 0 && start < end){
+						taskTags = token.substring(start+1, end).trim();
+						taskTags = taskTags.replace('|',',');
+					}
+					if (taskTags.length() == 0){
+						return -1;
+					}
+					continue;
+				} else {
+					return -1;
+				}
 			}
 			if (tokenCounter == 0) {
 				return -1;
