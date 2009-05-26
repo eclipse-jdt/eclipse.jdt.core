0218d2fde27ae288f3eb227e030a2aee2bd32f49
Walter Harley <wharley>
2009-05-26 16:04:15 +0000

    Copyright fixes

--- org.eclipse.jdt.compiler.apt.tests/resources/targets/model/pa/package-info.java
+++ org.eclipse.jdt.compiler.apt.tests/resources/targets/model/pa/package-info.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009 BEA Systems, Inc.
+ * Copyright (c) 2009 IBM Corporation and others..
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     wharley@bea.com - initial API and implementation
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /**
  * @see #methodIAString(int)
