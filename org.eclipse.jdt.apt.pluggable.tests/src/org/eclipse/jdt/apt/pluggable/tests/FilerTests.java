7d6a60331b61847227c268900dd844a9e95b3ada
Walter Harley <wharley>
2009-08-18 06:16:33 +0000

    Bug 285838 - IdeFilerImpl.createXxx should handle null originating element.

--- org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/FilerTests.java
+++ org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/FilerTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 BEA Systems, Inc. and others
+ * Copyright (c) 2007 - 2009 BEA Systems, Inc. and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -258,6 +258,38 @@ public class FilerTests extends TestBase
 		expectingNoProblems();
 		assertTrue("Processor did not run", ProcessorTestStatus.processorRan());
 		assertEquals("Processor reported errors", ProcessorTestStatus.NO_ERRORS, ProcessorTestStatus.getErrors());
+	}
+	
+	/**
+	 * Call FilerTesterProc.testNullParents(), which checks handling of null originatingElements
+	 * in the Filer.createXxx() methods.
+	 */
+	public void testNullParents() throws Throwable {
+		ProcessorTestStatus.reset();
+		IJavaProject jproj = createJavaProject(_projectName);
+		disableJava5Factories(jproj);
+		IProject proj = jproj.getProject();
+		IPath projPath = proj.getFullPath();
+		
+		env.addClass(projPath.append("src"), "p", "Trigger",
+				"package p;\n" +
+				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
+				"@FilerTestTrigger(test = \"testNullParents\", arg0 = \"t\", arg1 = \"Test\")" +
+				"public class Trigger {\n" +
+				"}"
+		); 
+		
+		AptConfig.setEnabled(jproj, true);
+		
+		fullBuild();
+		expectingNoProblems();
+		assertTrue("Processor did not run", ProcessorTestStatus.processorRan());
+		assertEquals("Processor reported errors", ProcessorTestStatus.NO_ERRORS, ProcessorTestStatus.getErrors());
+
+		expectingFile(proj, ".apt_generated/t/Test.java");
+		expectingFile(proj, ".apt_generated/t/Test.txt");
+		final String[] expectedClasses = { "p.Trigger", "t.Test" };
+		expectingUniqueCompiledClasses(expectedClasses);
 	}
 	
 	/**
