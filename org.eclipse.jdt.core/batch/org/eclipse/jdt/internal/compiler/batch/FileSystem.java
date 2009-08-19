ac6b131788a5b21da5e70feb0bd56c5cbe069fb1
Olivier Thomann <oliviert>
2009-08-19 19:25:16 +0000

    3.5 maintenance - Fix for 285466

--- org.eclipse.jdt.core/batch/org/eclipse/jdt/internal/compiler/batch/FileSystem.java
+++ org.eclipse.jdt.core/batch/org/eclipse/jdt/internal/compiler/batch/FileSystem.java
@@ -187,20 +187,28 @@ private void initializeKnownFileNames(String[] initialFileNames) {
 	}
 	this.knownFileNames = new HashSet(initialFileNames.length * 2);
 	for (int i = initialFileNames.length; --i >= 0;) {
-		char[] fileName = initialFileNames[i].toCharArray();
+		File compilationUnitFile = new File(initialFileNames[i]);
+		char[] fileName = null;
+		try {
+			fileName = compilationUnitFile.getCanonicalPath().toCharArray();
+		} catch (IOException e) {
+			// this should not happen as the file exists
+			continue;
+		}
 		char[] matchingPathName = null;
 		final int lastIndexOf = CharOperation.lastIndexOf('.', fileName);
 		if (lastIndexOf != -1) {
 			fileName = CharOperation.subarray(fileName, 0, lastIndexOf);
 		}
 		CharOperation.replace(fileName, '\\', '/');
-		for (int j = 0; j < this.classpaths.length; j++){
+		for (int j = 0, max = this.classpaths.length; j < max; j++) {
 			char[] matchCandidate = this.classpaths[j].normalizedPath();
 			if (this.classpaths[j] instanceof  ClasspathDirectory &&
 					CharOperation.prefixEquals(matchCandidate, fileName) &&
 					(matchingPathName == null ||
-							matchCandidate.length < matchingPathName.length))
+							matchCandidate.length < matchingPathName.length)) {
 				matchingPathName = matchCandidate;
+			}
 		}
 		if (matchingPathName == null) {
 			this.knownFileNames.add(new String(fileName)); // leave as is...
