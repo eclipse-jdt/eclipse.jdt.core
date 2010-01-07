547ec74f887db29f412ac2b8292c7b54a549baae
Olivier Thomann <oliviert>
2010-01-07 20:46:37 +0000

    3.3 maintenance - Fix for 299053

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaSearchBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaSearchBugsTests.java
@@ -18,6 +18,7 @@ import junit.framework.Test;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.core.compiler.CharOperation;
@@ -25,9 +26,12 @@ import org.eclipse.jdt.core.search.*;
 
 import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
 import org.eclipse.jdt.internal.core.ClassFile;
+import org.eclipse.jdt.internal.core.JavaModelManager;
 import org.eclipse.jdt.internal.core.SourceMethod;
 import org.eclipse.jdt.internal.core.search.AbstractSearchScope;
 import org.eclipse.jdt.internal.core.search.indexing.IIndexConstants;
+import org.eclipse.jdt.internal.core.search.indexing.IndexManager;
+import org.eclipse.jdt.internal.core.search.indexing.IndexRequest;
 import org.eclipse.jdt.internal.core.search.matching.MatchLocator;
 import org.eclipse.jdt.internal.core.search.matching.PatternLocator;
 import org.eclipse.jdt.internal.core.search.matching.TypeDeclarationPattern;
@@ -8146,5 +8150,55 @@ public void testBug209054() throws CoreException {
 	search(method, REFERENCES);
 	assertSearchResults("");
 }
-
+/**
+ * @bug 296343: OOM error caused by java indexing referencing classloader from threadLocal
+ * @test Ensure that indexing thread context class loader is not the application class loader
+ * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=296343"
+ */
+public void testBug296343() throws Exception {
+	simulateExit();
+	class TestClassLoader extends ClassLoader {
+		TestClassLoader(ClassLoader parent) {
+			super(parent);
+		}
+	}
+	TestClassLoader tcl = new TestClassLoader(this.getClass().getClassLoader());
+	ClassLoader cl = Thread.currentThread().getContextClassLoader();
+	try {
+		// set the thread context class loader
+		Thread.currentThread().setContextClassLoader(tcl);
+		simulateRestart();
+		
+		// get the indexing thread
+		class TestIndexRequest extends IndexRequest {
+			public Thread indexingThread = null;
+			public boolean executed = false;
+			public boolean execute(IProgressMonitor progressMonitor) {
+				this.indexingThread = Thread.currentThread();
+				this.executed = true;
+				return true;
+			}
+			TestIndexRequest(Path containerPath, IndexManager indexManager) {
+				super(containerPath, indexManager);
+			}
+		}
+		IndexManager indexManager = JavaModelManager.getJavaModelManager().getIndexManager();
+		TestIndexRequest tir = new TestIndexRequest(new Path(""), indexManager );
+		indexManager.request(tir);
+		int counter = 0;
+		// wait until the Index request gets executed
+		while (!tir.executed) {
+			try {
+				Thread.sleep(100);
+			}
+			catch (InterruptedException ie) {
+				// skip
+			}
+			assertTrue("Index request should have got executed within a 10s delay!", counter++ < 100);
+		}
+		assertFalse(tir.indexingThread.getContextClassLoader() == tcl);
+	} finally {
+		Thread.currentThread().setContextClassLoader(cl);
+	}	
+}
 }
