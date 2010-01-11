be41634e346526062c5eb4b2529e798869ce67a3
Olivier Thomann <oliviert>
2010-01-11 20:30:37 +0000

    JSR_308 - Fix disassembling problem

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/ExtendedAnnotation.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/ExtendedAnnotation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -83,7 +83,7 @@ public class ExtendedAnnotation extends ClassFileStruct implements IExtendedAnno
 		switch(index) {
 			case IExtendedAnnotationConstants.CLASS_EXTENDS_IMPLEMENTS :
 				this.annotationTypeIndex = u2At(classFileBytes, this.readOffset, offset);
-				this.readOffset++;
+				this.readOffset+=2;
 				break;
 			case IExtendedAnnotationConstants.CLASS_EXTENDS_IMPLEMENTS_GENERIC_OR_ARRAY :
 				this.annotationTypeIndex = u2At(classFileBytes, this.readOffset, offset);
