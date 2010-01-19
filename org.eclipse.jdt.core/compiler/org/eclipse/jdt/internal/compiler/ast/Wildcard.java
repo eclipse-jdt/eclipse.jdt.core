7bd8c00da1c60844ad7d83d14b9a054175d3b07a
Olivier Thomann <oliviert>
2010-01-19 17:26:22 +0000

    JSR_308 - Fix whitespaces

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/Wildcard.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/Wildcard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -104,7 +104,7 @@ public class Wildcard extends SingleTypeReference {
 	}
 
 	public TypeBinding resolveTypeArgument(ClassScope classScope, ReferenceBinding genericType, int rank) {
-	    return internalResolveType(classScope, genericType, rank);
+		return internalResolveType(classScope, genericType, rank);
 	}
 
 	public void traverse(ASTVisitor visitor, BlockScope scope) {
