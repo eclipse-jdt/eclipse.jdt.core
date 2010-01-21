6d5d9e32b8895e146fe7b876c8e27635ad72d617
Olivier Thomann <oliviert>
2010-01-21 16:14:10 +0000

    HEAD - Fix toString() (debug mode)

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/Scanner.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/Scanner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3669,7 +3669,7 @@ public String toString() {
 		buffer.append(this.source, 0, this.startPosition);
 	} else {
 		buffer.append("<source beginning>\n...\n"); //$NON-NLS-1$
-		int line = Util.getLineNumber(this.startPosition-1000, this.lineEnds, 0, this.lineEnds.length);
+		int line = Util.getLineNumber(this.startPosition-1000, this.lineEnds, 0, this.linePtr);
 		int lineStart = getLineStart(line);
 		buffer.append(this.source, lineStart, this.startPosition-lineStart);
 	}
