99d9827a13a83fa6bbad43196ec624282b8c65f9
Olivier Thomann <oliviert>
2010-02-03 20:46:58 +0000

    HEAD - Fix for 301562

--- org.eclipse.jdt.compiler.tool.tests/src/org/eclipse/jdt/compiler/tool/tests/CompilerToolTests.java
+++ org.eclipse.jdt.compiler.tool.tests/src/org/eclipse/jdt/compiler/tool/tests/CompilerToolTests.java
@@ -19,13 +19,17 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.ServiceLoader;
+import java.util.Set;
 
 import javax.tools.FileObject;
 import javax.tools.ForwardingJavaFileManager;
 import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
@@ -51,6 +55,8 @@ public class CompilerToolTests extends TestCase {
 	public static TestSuite suite() {
 		TestSuite suite = new TestSuite();
 		suite.addTest(new CompilerToolTests("testInitializeJavaCompiler"));
+		suite.addTest(new CompilerToolTests("testFileManager"));
+		suite.addTest(new CompilerToolTests("testFileManager2"));
 		suite.addTest(new CompilerToolTests("testCheckOptions"));
 		suite.addTest(new CompilerToolTests("testCompilerOneClassWithSystemCompiler"));
 //		suite.addTest(new CompilerToolTests("testCompilerOneClassWithSystemCompiler2"));
@@ -682,6 +688,136 @@ static final String[] FAKE_ZERO_ARG_OPTIONS = new String[] {
 		assertTrue("delete failed", inputFile.delete());
 	}
 
+	public void testFileManager() {
+		String tmpFolder = System.getProperty("java.io.tmpdir");
+		File dir = new File(tmpFolder, "src" + System.currentTimeMillis());
+		dir.mkdirs();
+		File inputFile = new File(dir, "X.java");
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(inputFile));
+			writer.write("public class X {}");
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// ignore
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+		try {
+			//JavaCompiler systemCompiler = ToolProvider.getSystemJavaCompiler();
+			StandardJavaFileManager fileManager = Compiler.getStandardFileManager(null, Locale.getDefault(), Charset.defaultCharset());
+	
+			List<File> fins = new ArrayList<File>();
+			fins.add(dir);
+	
+			JavaFileManager.Location sourceLoc = javax.tools.StandardLocation.SOURCE_PATH;
+				fileManager.setLocation(sourceLoc, fins);
+	
+			Set<JavaFileObject.Kind> fileTypes = new HashSet<JavaFileObject.Kind>();
+			fileTypes.add(JavaFileObject.Kind.SOURCE);
+	
+			Iterable<? extends JavaFileObject> compilationUnits = fileManager.list(sourceLoc, "", fileTypes, true);
+	
+			Iterator<? extends JavaFileObject> it = compilationUnits.iterator();
+			StringBuilder builder = new StringBuilder();
+			while (it.hasNext()) {
+				JavaFileObject next = it.next();
+				String name = next.getName();
+				name = name.replace('\\', '/');
+				int lastIndexOf = name.lastIndexOf('/');
+				builder.append(name.substring(lastIndexOf + 1));
+			}
+			assertEquals("Wrong contents", "X.java", String.valueOf(builder));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// check that the .class file exist for X
+		assertTrue("delete failed", inputFile.delete());
+		assertTrue("delete failed", dir.delete());
+	}
+	public void testFileManager2() {
+		String tmpFolder = System.getProperty("java.io.tmpdir");
+		File dir = new File(tmpFolder, "src" + System.currentTimeMillis());
+		dir.mkdirs();
+		File dir2 = new File(dir, "src2");
+		dir2.mkdirs();
+		File inputFile = new File(dir, "X.java");
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(inputFile));
+			writer.write("public class X {}");
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// ignore
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+		File inputFile2 = new File(dir2, "X2.java");
+		writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(inputFile2));
+			writer.write("public class X2 {}");
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// ignore
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+		try {
+			//JavaCompiler systemCompiler = ToolProvider.getSystemJavaCompiler();
+			StandardJavaFileManager fileManager = Compiler.getStandardFileManager(null, Locale.getDefault(), Charset.defaultCharset());
+	
+			List<File> fins = new ArrayList<File>();
+			fins.add(dir);
+	
+			JavaFileManager.Location sourceLoc = javax.tools.StandardLocation.SOURCE_PATH;
+				fileManager.setLocation(sourceLoc, fins);
+	
+			Set<JavaFileObject.Kind> fileTypes = new HashSet<JavaFileObject.Kind>();
+			fileTypes.add(JavaFileObject.Kind.SOURCE);
+	
+			Iterable<? extends JavaFileObject> compilationUnits = fileManager.list(sourceLoc, "", fileTypes, true);
+	
+			Iterator<? extends JavaFileObject> it = compilationUnits.iterator();
+			StringBuilder builder = new StringBuilder();
+			while (it.hasNext()) {
+				JavaFileObject next = it.next();
+				String name = next.getName();
+				name = name.replace('\\', '/');
+				int lastIndexOf = name.lastIndexOf('/');
+				builder.append(name.substring(lastIndexOf + 1));
+			}
+			assertEquals("Wrong contents", "X2.javaX.java", String.valueOf(builder));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// check that the .class file exist for X
+		assertTrue("delete failed", inputFile2.delete());
+		assertTrue("delete failed", dir2.delete());
+		assertTrue("delete failed", inputFile.delete());
+		assertTrue("delete failed", dir.delete());
+	}
 	/*
 	 * Clean up the compiler
 	 */
