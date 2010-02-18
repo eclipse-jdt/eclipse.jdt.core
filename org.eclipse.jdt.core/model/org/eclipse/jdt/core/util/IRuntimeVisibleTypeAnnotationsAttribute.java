00e3dd9cebfeaba75110f7c1ce9de700012a974c
Olivier Thomann <oliviert>
2010-02-18 01:49:37 +0000

    JSR_308 - Fix @since tag

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/util/IRuntimeVisibleTypeAnnotationsAttribute.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/util/IRuntimeVisibleTypeAnnotationsAttribute.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,7 @@ package org.eclipse.jdt.core.util;
  *
  * This interface may be implemented by clients.
  *
- * @since 3.0
+ * @since 3.6
  */
 public interface IRuntimeVisibleTypeAnnotationsAttribute extends IClassFileAttribute {
 
