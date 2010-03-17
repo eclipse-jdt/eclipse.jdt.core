c501e595381972dc3a7cf1823f0e3b16df719bd3
Olivier Thomann <oliviert>
2010-03-17 17:29:32 +0000

    JSR_308 - Fix for 182459

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterTestAST3_2.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverterTestAST3_2.java
@@ -7689,11 +7689,10 @@ public class ASTConverterTestAST3_2 extends ConverterTestSetup {
 				true);
 			assertEquals("Not a compilation unit", ASTNode.COMPILATION_UNIT, node.getNodeType());
 			CompilationUnit unit = (CompilationUnit) node;
-			assertProblemsSize(
-					unit,
+			assertEquals(
+					"Wrong problem size",
 					2,
-					"Object.equ cannot be resolved to a type\n" + 
-					"Syntax error, insert \"VariableDeclarators\" to complete LocalVariableDeclaration");
+					unit.getProblems().length);
 			node = getASTNode(unit, 0, 0);
 			assertEquals("Not a field declaration statement", ASTNode.INITIALIZER, node.getNodeType());
 			Initializer initializer = (Initializer) node;
