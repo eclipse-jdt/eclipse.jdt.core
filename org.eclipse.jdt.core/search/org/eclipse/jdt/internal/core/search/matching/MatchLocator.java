b2a72ff754b06cb3bacbbfdf4dfb68bbe32ad741
Frederic Fusier <ffusier>
2010-05-06 11:50:03 +0000

    JSR_308 - 307040

--- org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/matching/MatchLocator.java
+++ org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/matching/MatchLocator.java
@@ -725,7 +725,13 @@ protected IType createTypeHandle(String simpleTypeName) {
 	return classFile.getType();
 }
 protected boolean encloses(IJavaElement element) {
-	return element != null && this.scope.encloses(element);
+	if (element != null) {
+		if (this.scope instanceof HierarchyScope)
+			return ((HierarchyScope)this.scope).encloses(element, this.progressMonitor);
+		else 
+			return this.scope.encloses(element);
+	}
+	return false;
 }
 /* (non-Javadoc)
  * Return info about last type argument of a parameterized type reference.
