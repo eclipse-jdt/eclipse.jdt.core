0932d0fb4e43b1379b1c286eb1af8bd99bf0e6e1
Olivier Thomann <oliviert>
2010-05-06 20:56:52 +0000

    JSR_308 - Fix for 311849

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AnnotationTest.java
@@ -45,7 +45,7 @@ public class AnnotationTest extends AbstractComparableTest {
 	// All specified tests which do not belong to the class are skipped...
 	static {
 //		TESTS_NAMES = new String[] { "test127" };
-//		TESTS_NUMBERS = new int[] { 286 };
+//		TESTS_NUMBERS = new int[] { 287, 288 };
 //		TESTS_RANGE = new int[] { 249, -1 };
 	}
 
@@ -9516,6 +9516,60 @@ public void test286() {
 		true,
 		null,
 		raiseDeprecationReduceInvalidJavadocSeverity,
+		null);
+}
+//https://bugs.eclipse.org/bugs/show_bug.cgi?id=304031
+public void test287() {
+	Map options = getCompilerOptions();
+	options.put(CompilerOptions.OPTION_ReportUncheckedTypeOperation, CompilerOptions.ERROR);
+	options.put(CompilerOptions.OPTION_SuppressOptionalErrors, CompilerOptions.ENABLED);
+	this.runConformTest(
+		new String[] {
+				"X.java",
+				"import java.util.ArrayList;\n" + 
+				"\n" + 
+				"public class X {\n" + 
+				"	@SuppressWarnings(\"rawtypes\")\n" + 
+				"	void foo(ArrayList arg) {\n" + 
+				"		for (\n" + 
+				"			@SuppressWarnings(\"unchecked\")\n" +
+				"			boolean a= arg.add(1), b= arg.add(1);\n" + 
+				"			Boolean.FALSE;\n" + 
+				"		) {\n" + 
+				"			System.out.println(a && b);\n" + 
+				"		}\n" + 
+				"	}\n" + 
+				"}",
+		},
+		"",
+		null,
+		true,
+		null,
+		options,
+		null);
+}
+//https://bugs.eclipse.org/bugs/show_bug.cgi?id=304031
+public void test288() {
+	Map options = getCompilerOptions();
+	options.put(CompilerOptions.OPTION_ReportUncheckedTypeOperation, CompilerOptions.ERROR);
+	options.put(CompilerOptions.OPTION_SuppressOptionalErrors, CompilerOptions.ENABLED);
+	this.runConformTest(
+		new String[] {
+				"X.java",
+				"import java.util.ArrayList;\n" + 
+				"\n" + 
+				"public class X {\n" + 
+				"	@SuppressWarnings(\"rawtypes\")\n" + 
+				"	ArrayList arg;\n" + 
+				"	@SuppressWarnings(\"unchecked\")\n" + 
+				"	boolean a= arg.add(1), b= arg.add(1);\n" + 
+				"}",
+		},
+		"",
+		null,
+		true,
+		null,
+		options,
 		null);
 }
 }
