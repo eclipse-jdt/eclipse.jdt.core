e2bec651319bc13e17c21c9c3e9db519100479b3
Olivier Thomann <oliviert>
2011-01-25 14:11:09 +0000

    HEAD - Fixed bug 327143: IndexManager should not accept new jobs if the processing thread is null

--- org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/processing/JobManager.java
+++ org.eclipse.jdt.core/search/org/eclipse/jdt/internal/core/search/processing/JobManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -226,7 +226,8 @@ public abstract class JobManager implements Runnable {
 								float lastWorked = 0;
 								float totalWorked = 0;
 								while ((awaitingJobsCount = awaitingJobsCount()) > 0) {
-									if (subProgress != null && subProgress.isCanceled() && this.processingThread == null)
+									if ((subProgress != null && subProgress.isCanceled())
+											|| this.processingThread == null)
 										throw new OperationCanceledException();
 									IJob currentJob = currentJob();
 									// currentJob can be null when jobs have been added to the queue but job manager is not enabled
