1c283dafd0108b9db87faab1d0d5663f66ada136
Olivier Thomann <oliviert>
2011-02-03 16:15:10 +0000

    BETA_JAVA7 - Add support for polymorphic method and MethodHandle invokeExact(..)/invokeGeneric(..) invocations

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/MessageSend.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/MessageSend.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -457,6 +457,9 @@ public TypeBinding resolveType(BlockScope scope) {
 		return (this.resolvedType != null && (this.resolvedType.tagBits & TagBits.HasMissingType) == 0)
 						? this.resolvedType
 						: null;
+	}
+	if ((this.binding.tagBits & TagBits.AnnotationPolymorphicSignature) != 0) {
+		this.binding.returnType = TypeBinding.VOID;
 	}
 	if ((this.binding.tagBits & TagBits.HasMissingType) != 0) {
 		scope.problemReporter().missingTypeInMethod(this, this.binding);
