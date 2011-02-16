ac8b2356773cc6e92e0d19b5268137d464946f23
Ayushman Jain <ajain>
2011-02-16 11:33:56 +0000

    HEAD- Fixed bug 337275: Incorrect/outdated javadoc for org.eclipse.jdt.core.dom.Expression

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Expression.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Expression.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,8 +18,7 @@ package org.eclipse.jdt.core.dom;
  * <pre>
  * Expression:
  *    Name
- *    IntegerLiteral (includes decimal, hex, and octal forms; and long)
- *    FloatingPointLiteral (includes both float and double)
+ *    NumberLiteral
  *    CharacterLiteral
  *    NullLiteral
  *    BooleanLiteral
