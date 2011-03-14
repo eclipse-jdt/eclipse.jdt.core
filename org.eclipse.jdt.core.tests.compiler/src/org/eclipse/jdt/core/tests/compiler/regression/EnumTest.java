63dba7712ab4bdfaaa0bbe623f904acf7a71574d
Olivier Thomann <oliviert>
2011-03-14 19:00:27 +0000

    BETA_JAVA7 - Fixed bug 339913: [compiler] Misleading error message for annotations inside a method body

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/EnumTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/EnumTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2524,7 +2524,7 @@ public void test081() {
 		"1. ERROR in X.java (at line 3)\n" +
 		"	enum E {}\n" +
 		"	     ^\n" +
-		"The member enum E cannot be local\n" +
+		"The member enum E can only be defined inside a top-level class or interface\n" +
 		"----------\n");
 }
 
@@ -2570,7 +2570,7 @@ public void test082() {
 		"1. ERROR in X.java (at line 4)\n" +
 		"	enum E {}\n" +
 		"	     ^\n" +
-		"The member enum E cannot be local\n" +
+		"The member enum E can only be defined inside a top-level class or interface\n" +
 		"----------\n");
 }
 
