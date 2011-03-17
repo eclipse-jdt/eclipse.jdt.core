ed89d20682cf3e428e7c3a63b036b09cef5eb576
Satyam Kandula <skandula>
2011-03-17 14:08:43 +0000

    HEAD -  Make it easy to run/debug one test

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaSearchBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaSearchBugsTests.java
@@ -102,6 +102,9 @@ public JavaSearchBugsTests(String name) {
 	this.endChar = "";
 }
 public static Test suite() {
+	if (TESTS_PREFIX != null || TESTS_NAMES != null || TESTS_NUMBERS!=null || TESTS_RANGE !=null) {
+		return buildModelTestSuite(JavaSearchBugsTests.class);
+	}
 	// hack to guarantee the test order
 	TestSuite suite = new Suite(JavaSearchBugsTests.class.getName());
 	suite.addTest(new JavaSearchBugsTests("testBug41018"));
@@ -13480,5 +13483,4 @@ public void testBug339891() throws CoreException {
 		deleteProject("P");
 	}
 }
-
 }
