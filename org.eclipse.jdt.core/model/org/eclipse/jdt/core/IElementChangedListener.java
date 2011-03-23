465b3db2978fedfce55ada7182e837dd356c3758
Jayaprakash Arthanareeswaran <jarthanaree>
2011-03-23 07:53:30 +0000

    BETA_JAVA7 - Fixed bug 200827: [spec] IElementChangedListener should mention where to register

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/IElementChangedListener.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/IElementChangedListener.java
@@ -16,6 +16,8 @@ package org.eclipse.jdt.core;
  * <p>
  * This interface may be implemented by clients.
  * </p>
+ * 
+ * @see JavaCore#addElementChangedListener(IElementChangedListener)
  */
 public interface IElementChangedListener {
 
