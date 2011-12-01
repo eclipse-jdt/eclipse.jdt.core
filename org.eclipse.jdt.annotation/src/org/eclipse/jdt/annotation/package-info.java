d204f81f44a990ae74280d415d69da53964a3837
Markus Keller <mkeller>
2011-12-01 18:29:44 +0100

    Bug 364820: User documentation of null annotations

---
+++ org.eclipse.jdt.annotation/src/org/eclipse/jdt/annotation/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains annotations that can trigger special behavior
+ * when annotated types are compiled by the Eclipse Compiler for Java.
+ * <p>
+ * Currently, the package contains annotations that specify nullness contracts
+ * for annotated elements.
+ * </p>
+ */
+package org.eclipse.jdt.annotation;
+
