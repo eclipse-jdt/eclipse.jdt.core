f9060155e3cfd199faeec23a0a107fc22b1b0c9f
Ayushman Jain <ajain>
2012-02-02 21:56:39 +0530

    Fixed bug 364254: Reduce console output during JDT/Core junits execution.

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTestsRequestor2.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTestsRequestor2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2011 IBM Corporation and others.
+ * Copyright (c) 2004, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -603,7 +603,6 @@ public class CompletionTestsRequestor2 extends CompletionRequestor {
 	
 	public boolean canUseDiamond(int proposalNo) {
 		if (proposalNo < this.proposals.length && this.proposals[proposalNo] != null) {
-			System.out.println(this.proposals[proposalNo]);
 			return this.proposals[proposalNo].canUseDiamond(this.context);
 		}
 		return false;
