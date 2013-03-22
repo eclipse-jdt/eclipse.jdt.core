2ea28b71481adeeaf9e7adba83fdf2894ac85ffd
Markus Keller <markus_keller@ch.ibm.com>
2013-03-22 17:51:16 +0100

    Bug 404169: deprecation warnings in test projects on junit.framework.Assert

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/GetKeyVisitor.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/GetKeyVisitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,8 @@
  *******************************************************************************/
 package org.eclipse.jdt.core.tests.dom;
 
-import junit.framework.Assert;
+import junit.framework.TestCase;
+
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.IMethodBinding;
@@ -22,8 +23,8 @@ class GetKeyVisitor extends ASTVisitor {
 	 */
 	public boolean visit(ClassInstanceCreation node) {
 		IMethodBinding constructorBinding = node.resolveConstructorBinding();
-		Assert.assertNotNull(constructorBinding);
-		Assert.assertNotNull(constructorBinding.getKey());
+		TestCase.assertNotNull(constructorBinding);
+		TestCase.assertNotNull(constructorBinding.getKey());
 		return super.visit(node);
 	}
 }
