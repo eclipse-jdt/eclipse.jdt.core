998718a6f3c4fdf42208417429777816ff07d23a
Stephan Herrmann <stephan.herrmann@berlin.de>
2013-10-08 14:52:54 +0200

    Bug 418235 - [compiler][null] Unreported nullness error when using generic - test & fix

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullAnnotationTest.java
@@ -6471,4 +6471,29 @@ public void testBug416267() {
 		"Missing cannot be resolved to a type\n" + 
 		"----------\n");
 }
+public void testBug418235() {
+    runNegativeTestWithLibs(
+            new String[] {
+                    "GenericInterface.java",
+                    "public interface GenericInterface<T> {\n" + 
+                    "       T doSomethingGeneric(T o);\n" + 
+                    "}",
+                    "Implementation.java",
+                    "import org.eclipse.jdt.annotation.NonNullByDefault;\n" + 
+                    "@NonNullByDefault\n" + 
+                    "public class Implementation implements GenericInterface<Object> {\n" + 
+                    "\n" + 
+                    (this.complianceLevel < ClassFileConstants.JDK1_6 ? "\n" : "      @Override\n" ) +
+                    "       public Object doSomethingGeneric(Object o) {\n" + 
+                    "               return o;\n" + 
+                    "       }\n" + 
+                    "}\n"
+            },
+            "----------\n" + 
+            "1. ERROR in Implementation.java (at line 6)\n" + 
+    		"	public Object doSomethingGeneric(Object o) {\n" + 
+    		"	                                 ^^^^^^\n" + 
+            "Illegal redefinition of parameter o, inherited method from GenericInterface<Object> does not constrain this parameter\n" + 
+            "----------\n");
+}
 }
--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ImplicitNullAnnotationVerifier.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ImplicitNullAnnotationVerifier.java
@@ -163,7 +163,7 @@ public class ImplicitNullAnnotationVerifier {
 			MethodBinding currentMethod = ifcMethods[i];
 			if (currentMethod.isStatic())
 				continue;
-			if (areParametersEqual(original, currentMethod.original())) {
+			if (doesMethodOverride(original, currentMethod)) {
 				result.add(currentMethod);
 				return; // at most one method is overridden from any supertype
 			}
