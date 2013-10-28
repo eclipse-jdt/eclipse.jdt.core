91f3edc5382142e13f8351418b65563536c6670f
Markus Keller <markus_keller@ch.ibm.com>
2013-10-28 12:16:44 +0100

    identify the invalid identifier in the IllegalArgumentException

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/SimpleName.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/SimpleName.java
@@ -191,14 +191,16 @@ public class SimpleName extends Name {
 			try {
 				int tokenType = scanner.scanIdentifier();
 				if (tokenType != TerminalTokens.TokenNameIdentifier) {
-					throw new IllegalArgumentException();
+					throw new IllegalArgumentException("Invalid identifier : >" + identifier + "<");  //$NON-NLS-1$//$NON-NLS-2$
 				}
 				if (scanner.currentPosition != length) {
 					// this is the case when there is only one identifier see 87849
-					throw new IllegalArgumentException();
+					throw new IllegalArgumentException("Invalid identifier : >" + identifier + "<");  //$NON-NLS-1$//$NON-NLS-2$
 				}
-			} catch(InvalidInputException e) {
-				throw new IllegalArgumentException();
+			} catch (InvalidInputException e) {
+				IllegalArgumentException iae = new IllegalArgumentException("Invalid identifier : >" + identifier + "<"); //$NON-NLS-1$//$NON-NLS-2$
+				iae.initCause(e);
+				throw iae; 
 			}
 		} finally {
 			this.ast.scanner.sourceLevel = sourceLevel;
