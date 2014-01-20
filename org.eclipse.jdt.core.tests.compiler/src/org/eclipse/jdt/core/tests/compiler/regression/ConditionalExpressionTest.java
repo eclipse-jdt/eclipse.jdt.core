ed45f69d40b17b59915e726fa4a306f40a296886
ssankaran <srikanth_sankaran@in.ibm.com>
2014-01-20 18:49:21 +0530

    [1.8] (Disabled) test for Bug 426078 - [1.8] VerifyError when conditional expression passed as an argument

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ConditionalExpressionTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ConditionalExpressionTest.java
@@ -1,14 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.core.tests.compiler.regression;
+
+import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
 
 import junit.framework.Test;
 
@@ -20,11 +26,11 @@ public class ConditionalExpressionTest extends AbstractRegressionTest {
 
 	// Static initializer to specify tests subset using TESTS_* static variables
 	// All specified tests which does not belong to the class are skipped...
-//	static {
-//		TESTS_NAMES = new String[] { "test000" };
+	static {
+//		TESTS_NAMES = new String[] { "test003" };
 //		TESTS_NUMBERS = new int[] { 65 };
 //		TESTS_RANGE = new int[] { 11, -1 };
-//	}
+	}
 	public static Test suite() {
 		return buildAllCompliancesTestSuite(testClass());
 	}
@@ -69,4 +75,28 @@ public class ConditionalExpressionTest extends AbstractRegressionTest {
 			""
 		);
 	}
+	
+	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=426078, Bug 426078 - [1.8] VerifyError when conditional expression passed as an argument
+	public void _test003() {
+		if (this.complianceLevel < ClassFileConstants.JDK1_7)
+			return;
+		this.runConformTest(
+			new String[] {
+				"X.java",
+				"public class X {\n" +
+				"	boolean isOdd(boolean what) {\n" +
+				"		return square(what ? new Integer(1) : new Integer(2)) % 2 == 1; // trouble here\n" +
+				"	}\n" +
+				"	<T> int square(int i) {\n" +
+				"		return i * i;\n" +
+				"	}\n" +
+				"	public static void main(String argv[]) {\n" +
+				"		System.out.println(new X().isOdd(true));\n" +
+				"	}\n" +
+				"}\n",
+			},
+			"true"
+		);
+	}
+	
 }
