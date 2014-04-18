83931270dafcffd4fac0c86baa5fcbf9d052b698
Jayaprakash Arthanareeswaran <jarthana@in.ibm.com>
2014-04-18 21:52:19 +0530

    Bug 433084 - NPE in AnnotationMirrorImpl.equals in Java 8 Patch for Kepler

--- org.eclipse.jdt.compiler.apt/src/org/eclipse/jdt/internal/compiler/apt/model/AnnotationMirrorImpl.java
+++ org.eclipse.jdt.compiler.apt/src/org/eclipse/jdt/internal/compiler/apt/model/AnnotationMirrorImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -59,7 +59,7 @@ public class AnnotationMirrorImpl implements AnnotationMirror, InvocationHandler
 			}
 			return equals(this._binding, ((AnnotationMirrorImpl) obj)._binding);
 		}
-		return obj.equals(this); // obj could be wrapped by a proxy.
+		return obj == null ? false : obj.equals(this); // obj could be wrapped by a proxy.
 	}
 
 	private static boolean equals(AnnotationBinding annotationBinding, AnnotationBinding annotationBinding2) {
