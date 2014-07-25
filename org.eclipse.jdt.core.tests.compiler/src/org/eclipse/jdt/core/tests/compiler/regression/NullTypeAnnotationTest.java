e279a962293bc2f4331f962a40342ff32ae925b7
Stephan Herrmann <stephan.herrmann@berlin.de>
2014-07-25 22:41:36 +0200

    Bug 440462 - [null][compiler]NPE in EJC for erroneous null annotations

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
@@ -5767,6 +5767,23 @@ public void testBug435962() {
 		getCompilerOptions(),
 		"");
 }
+public void testBug440462() {
+	runConformTestWithLibs(
+		new String[]{
+			"CompilerError.java",
+			"import org.eclipse.jdt.annotation.NonNullByDefault;\n" + 
+			"import org.eclipse.jdt.annotation.Nullable;\n" + 
+			"import java.util.*;\n" + 
+			"@NonNullByDefault\n" + 
+			"public class CompilerError {\n" + 
+			"\n" + 
+			"    List<@Nullable ? extends Integer> list = new ArrayList<@Nullable Integer>();\n" + // FIXME: should be able to use diamond!
+			"\n" + 
+			"}\n"
+		},
+		getCompilerOptions(),
+		"");
+}
 public void testBug439298_comment2() {
 	runConformTestWithLibs(
 		new String[] {
