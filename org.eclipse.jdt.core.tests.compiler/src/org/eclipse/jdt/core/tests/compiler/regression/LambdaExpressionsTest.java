26caa6d7ff233490ba9f271eca4eb626713ed350
Sasikanth Bharadwaj <saammana@in.ibm.com>
2014-08-04 10:34:24 +0530

    Fixed Bug 440152 - [codegen]"Missing code implementation in the compiler" on cascaded inner class references
    Signed-off-by: Sasikanth Bharadwaj <saammana@in.ibm.com>

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
@@ -4505,6 +4505,51 @@ public void test436542() throws Exception {
 	IClassFileAttribute signature = org.eclipse.jdt.internal.core.util.Util.getAttribute(lambdaMethod, IAttributeNamesConstants.SIGNATURE);
 	assertNull("Found generic signature for lambda method", signature);
 }
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=440152 [codegen]"Missing code implementation in the compiler" on cascaded inner class references
+public void test440152() {
+	this.runConformTest(
+		new String[] {
+			"Foo.java",
+			"import java.util.function.Function;\n" + 
+			"interface Foo {void alpha(Bar pBar);}\n" + 
+			"class Bar {Object bravo() {return null;}}\n" + 
+			"class Test {\n" + 
+			"  Test(Function pFunction) {\n" + 
+			"    class Baz {public Baz(Object pObj) {pFunction.apply(pObj);}}\n" + 
+			"    delta(pBar -> charlie(new Baz(pBar.bravo())));\n" + 
+			"  }\n" + 
+			"  void charlie(Object pRemovals) {}\n" + 
+			"  void delta(Foo pListener) {}\n" + 
+			"}"
+	});
+}
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=440152 [codegen]"Missing code implementation in the compiler" on cascaded inner class references
+public void test440152a() {
+	this.runConformTest(
+		new String[] {
+			"Foo.java",
+			"import java.util.function.Function;\n" + 
+			"interface Foo {void alpha(Bar pBar);}\n" + 
+			"class Bar {Object bravo() {return null;}}\n" + 
+			"class Test {\n" + 
+			"	Test(Function pFunction) {\n" + 
+			"	    class Baz {\n" + 
+			"	    	public Baz(Object pObj) {\n" + 
+			"	    	}\n" + 
+			"	    	class NestedBaz extends Baz {\n" + 
+			"	    		NestedBaz(Object pObj) {\n" + 
+			"	    			super(pObj);\n" + 
+			"	    			pFunction.apply(pObj);\n" + 
+			"	    		}\n" + 
+			"	    	}\n" + 
+			"	    	}\n" + 
+			"	    delta(pBar -> charlie(new Baz(pBar).new NestedBaz(pBar.bravo())));\n" + 
+			"	  }\n" + 
+			"	  void charlie(Object pRemovals) {}\n" + 
+			"	  void delta(Foo pListener) {}\n" + 
+			"}\n"
+	});
+}
 public static Class testClass() {
 	return LambdaExpressionsTest.class;
 }
