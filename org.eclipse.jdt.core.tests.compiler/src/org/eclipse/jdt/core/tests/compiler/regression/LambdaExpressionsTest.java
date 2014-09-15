e53b9824e6fa472a59a835086ec90d9f07bd0e41
ssankaran <srikanth_sankaran@in.ibm.com>
2014-09-15 23:35:11 +0530

    Fixed Bug 441907 - [1.8][compiler] Eclipse 4.4.x compiler generics bugs with streams and lambdas

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
@@ -4550,6 +4550,36 @@ public void test440152a() {
 			"}\n"
 	});
 }
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=441907, [1.8][compiler] Eclipse 4.4.x compiler generics bugs with streams and lambdas 
+public void test441907() {
+	this.runConformTest(
+		new String[] {
+			"X.java",
+			"import java.util.*;\n" +
+			"import java.util.function.Predicate;\n" +
+			"import java.util.stream.Stream;\n" +
+			"public class X {\n" +
+			"  public static class FooBar<V> {\n" +
+			"  }\n" +
+			"  public interface FooBarred {\n" +
+			"    public <V> boolean hasFooBar(final FooBar<V> fooBar);\n" +
+			"  }\n" +
+			"  public interface Widget extends FooBarred {\n" +
+			"  }\n" +
+			"  public static void test() {\n" +
+			"    Set<FooBar<?>> foobars = new HashSet<>();\n" +
+			"    Set<Widget> widgets = new HashSet<>();\n" +
+			"    Stream<X.FooBar<?>> s = null;\n" +
+			"    FooBarred fb = null;\n" +
+			"    fb.hasFooBar((FooBar<?>) null);\n" +
+			"    boolean anyWidgetHasFooBar = widgets.stream().anyMatch(\n" +
+			"        widget -> foobars.stream().anyMatch(widget::hasFooBar)\n" +
+			"        );\n" +
+			"  }\n" +
+			"}\n"
+		},
+		"");
+}
 public static Class testClass() {
 	return LambdaExpressionsTest.class;
 }
