f316b069838038f8089cd2fbc2dd1992644b54d4
ssankaran <srikanth_sankaran@in.ibm.com>
2014-10-13 13:03:34 +0530

    Fixed Bug 445669 - java.lang.IllegalStateException at UnresolvedReferenceBinding.clone

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
@@ -6821,4 +6821,48 @@ public void test446715() {
 		"Null type mismatch: required \'Z.ZI @NonNull[]\' but the provided value is null\n" + 
 		"----------\n");
 }
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=445669, java.lang.IllegalStateException at org.eclipse.jdt.internal.compiler.lookup.UnresolvedReferenceBinding.clone
+public void test445669() {
+	Map options = getCompilerOptions();
+	runConformTestWithLibs(
+		new String[] {
+			"Y.java",
+			"import org.eclipse.jdt.annotation.*;\n" +
+			"@NonNullByDefault(DefaultLocation.FIELD)\n" +
+			"public class Y {\n" +
+			"	public Z.ZI zzi = new Z().new ZI();\n" +
+			"	public Z z = new Z();\n" +
+			"}\n",
+			"Z.java",
+			"public class Z {\n" +
+			"	public class ZI {\n" +
+			"	}\n" +
+			"}\n"
+		},
+		options,
+		"");
+	runNegativeTestWithLibs(
+		new String[] {
+			"X.java",
+			"public class X {\n" +
+			"	public static void main(String[] args) {\n" +
+			"		Y y = new Y();\n" +
+			"		y.zzi = null;\n" +
+			"       y.z = null;\n" +
+			"	}\n" +
+			"}\n"
+		},
+		options,
+		"----------\n" + 
+		"1. ERROR in X.java (at line 4)\n" + 
+		"	y.zzi = null;\n" + 
+		"	        ^^^^\n" + 
+		"Null type mismatch: required \'Z.@NonNull ZI\' but the provided value is null\n" + 
+		"----------\n" + 
+		"2. ERROR in X.java (at line 5)\n" + 
+		"	y.z = null;\n" + 
+		"	      ^^^^\n" + 
+		"Null type mismatch: required \'@NonNull Z\' but the provided value is null\n" + 
+		"----------\n");
+}
 }
