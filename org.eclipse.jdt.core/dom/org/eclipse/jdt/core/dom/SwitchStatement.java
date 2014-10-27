f9e1e520523019e969eacf451222621b96e96623
Manoj Palat <manpalat@in.ibm.com>
2014-10-27 11:02:47 +0530

    Fix for [Bug 446953] Mismatched braces in SwitchStatement JavaDoc

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/SwitchStatement.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/SwitchStatement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,7 @@ import java.util.List;
  * <pre>
  * SwitchStatement:
  *		<b>switch</b> <b>(</b> Expression <b>)</b>
- * 			<b>{</b> { SwitchCase | Statement } } <b>}</b>
+ * 			<b>{</b> { SwitchCase | Statement } <b>}</b>
  * SwitchCase:
  *		<b>case</b> Expression  <b>:</b>
  *		<b>default</b> <b>:</b>
