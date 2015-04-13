b89cad8de7807e4ff0375dd01e42e7a766053716
Manoj Palat <manpalat@in.ibm.com>
2015-04-13 09:14:35 +0530

    Enabling the test GDRT for Bug 383608 [1.8][compiler] Test failures need to be investigated

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/parser/GenericDietRecoveryTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/parser/GenericDietRecoveryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -923,7 +923,7 @@ public void test0018() {
 		expectedFullUnitToString,
 		expectedCompletionDietUnitToString, testName);
 }
-public void _test0019() {
+public void test0019() {
 
 	String s =
 		"package a;											\n"
