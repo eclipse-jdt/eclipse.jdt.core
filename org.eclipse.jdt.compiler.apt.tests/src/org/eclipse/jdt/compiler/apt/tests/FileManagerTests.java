07b9b07aa2fb6f110f79c1acddd03f17baac3f11
Jay Arthanareeswaran <jarthana@in.ibm.com>
2015-05-15 10:36:50 -0400

    Bug 466878 - Fix failing test
    Change-Id: If2d100050d8169d5c22f14fbbd5b8bba6f9fe238

--- org.eclipse.jdt.compiler.apt.tests/src/org/eclipse/jdt/compiler/apt/tests/FileManagerTests.java
+++ org.eclipse.jdt.compiler.apt.tests/src/org/eclipse/jdt/compiler/apt/tests/FileManagerTests.java
@@ -190,8 +190,8 @@ public class FileManagerTests extends TestCase {
 	public void testBug466878_getResource_defaultPackage() throws Exception {
 		EclipseFileManager fileManager = new EclipseFileManager(Locale.getDefault(), Charset.defaultCharset());
 		List<File> classpath = new ArrayList<>();
-		classpath.add(new File("resources/targets/filemanager/classes"));
-		classpath.add(new File("resources/targets/filemanager/dependency.zip"));
+		classpath.add(new File(BatchTestUtils.getPluginDirectoryPath(), "resources/targets/filemanager/classes"));
+		classpath.add(new File(BatchTestUtils.getPluginDirectoryPath(), "resources/targets/filemanager/dependency.zip"));
 		fileManager.setLocation(javax.tools.StandardLocation.CLASS_PATH, classpath);
 		assertNotNull(fileManager.getFileForInput(javax.tools.StandardLocation.CLASS_PATH, "", "dirresource.txt"));
 		assertNotNull(fileManager.getFileForInput(javax.tools.StandardLocation.CLASS_PATH, "", "jarresource.txt"));
