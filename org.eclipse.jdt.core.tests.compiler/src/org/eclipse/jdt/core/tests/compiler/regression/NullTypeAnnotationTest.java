4e652bbe11383e976d60d20e3975e9453afa72ca
Stephan Herrmann <stephan.herrmann@berlin.de>
2015-07-14 20:09:36 +0200

    Bug 436091 - [compiler][null] Null analysis can't seem to decide if a null warning should be emitted
    Change-Id: Ia4d15a9da59cf6fe7d29c279fe6b2ba37632f9d8
    Signed-off-by: Stephan Herrmann <stephan.herrmann@berlin.de>

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
@@ -5199,11 +5199,16 @@ public void testDefault07_bin() {
 			"}\n"
 		},
 		getCompilerOptions(),
-		"----------\n" +  // FIXME: this should not be a warning, a case of unrecognized boxing
+		"----------\n" +
 		"1. WARNING in Y.java (at line 5)\n" + 
 		"	@NonNull Number nnn = inner.process(Integer.valueOf(3), new ArrayList<@Nullable Integer>()); // WARN on 1. arg; ERR on 2. arg\n" + 
 		"	                                    ^^^^^^^^^^^^^^^^^^\n" + 
 		"Null type safety (type annotations): The expression of type \'Integer\' needs unchecked conversion to conform to \'@NonNull Integer\'\n" + 
+		"----------\n" + 
+		"2. ERROR in Y.java (at line 5)\n" + 
+		"	@NonNull Number nnn = inner.process(Integer.valueOf(3), new ArrayList<@Nullable Integer>()); // WARN on 1. arg; ERR on 2. arg\n" + 
+		"	                                                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+		"Null type mismatch (type annotations): required \'List<? extends @NonNull Number>\' but this expression has type \'ArrayList<@Nullable Integer>\', corresponding supertype is \'List<@Nullable Integer>\'\n" + 
 		"----------\n");
 }
 public void testBug431269() {
@@ -8188,6 +8193,88 @@ public void testBug447661() {
 			"\n" + 
 			"	public void test() {\n" + 
 			"		Set<String> set = Two.getSet();\n" + 
+			"	}\n" + 
+			"}\n"
+		},
+		getCompilerOptions(),
+		"");
+}
+public void testBug436091() {
+	runConformTestWithLibs(
+		new String[] {
+			"p/package-info.java",
+			"@org.eclipse.jdt.annotation.NonNullByDefault\n" + 
+			"package p;\n",
+
+			"p/Program.java",
+			"package p;\n" +
+			"public class Program {\n" + 
+			"	private final ProgramNode program;\n" + 
+			"	" + 
+			"	public Program(final ProgramNode astRoot) {\n" + 
+			"		program = astRoot;\n" + 
+			"	}\n" + 
+			"	" + 
+			"	public Integer execute() {\n" + 
+			"		return program.accept(ExecutionEvaluationVisitor.VISITOR);\n" + 
+			"	}\n" + 
+			"	" + 
+			"	class ProgramNode {\n" + 
+			"		public <R> R accept(final ConcreteNodeVisitor<R> visitor) {\n" + 
+			"			return visitor.visit(this);\n" + 
+			"		}\n" + 
+			"	}\n" + 
+			"}\n",
+			
+			"p/ConcreteNodeVisitor.java",
+			"package p;\n" +
+			"import p.Program.ProgramNode;\n" + 
+			"public interface ConcreteNodeVisitor<R> {\n" + 
+			"	R visit(ProgramNode node);\n" + 
+			"}\n",
+			
+			"p/ExecutionEvaluationVisitor.java",
+			"package p;\n" + 
+			"" + 
+			"import org.eclipse.jdt.annotation.NonNull;\n" + 
+			"" + 
+			"import p.Program.ProgramNode;\n" + 
+			"" + 
+			"public enum ExecutionEvaluationVisitor implements ConcreteNodeVisitor<Integer> {\n" + 
+			"	" + 
+			"	VISITOR;\n" + 
+			"	" + 
+			"	@Override" + 
+			"	public Integer visit(final ProgramNode node) {\n" + 
+			"		@SuppressWarnings(\"null\")\n" + 
+			"		@NonNull\n" + 
+			"		final Integer i = Integer.valueOf(0);\n" + 
+			"		return i;\n" + 
+			"	}\n" + 
+			"}\n"
+		},
+		getCompilerOptions(),
+		"");
+	// re-compile only one of the above:
+	runConformTestWithLibs(
+		new String[] {
+			"p/Program.java",
+			"package p;\n" +
+			"public class Program {\n" + 
+			"	private final ProgramNode program;\n" + 
+			"	" + 
+			"	public Program(final ProgramNode astRoot) {\n" + 
+			"		program = astRoot;\n" + 
+			"	}\n" + 
+			"	" + 
+			"	public Integer execute() {\n" + 
+			"		return program.accept(ExecutionEvaluationVisitor.VISITOR);\n" + 
+			"	}\n" + 
+			"	" + 
+			"	class ProgramNode {\n" + 
+			"		public <R> R accept(final ConcreteNodeVisitor<R> visitor) {\n" + 
+			"			return visitor.visit(this);\n" + 
+			"		}\n" + 
 			"	}\n" + 
 			"}\n"
 		},
