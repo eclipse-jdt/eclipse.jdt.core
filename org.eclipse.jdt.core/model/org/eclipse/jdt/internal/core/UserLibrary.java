8db4906e6220f222d10f8f23475147fcad83b2f9
Alex Panchenko <alex.panchenko@gmail.com>
2015-07-30 17:36:32 +0530

    Bug 470535 - Bug in UserLibrary.hashCode()
    Change-Id: I3ac5df30d1c6163e518805766a7b4aa15704b420
    Signed-off-by: Alex Panchenko <alex.panchenko@gmail.com>

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/UserLibrary.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/UserLibrary.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     alex.panchenko@gmail.com - Bug 470535 - Bug in UserLibrary.hashCode()
  *******************************************************************************/
 package org.eclipse.jdt.internal.core;
 
@@ -95,7 +96,7 @@ public class UserLibrary {
 			hashCode++;
 		}
 		for (int i= 0; i < this.entries.length; i++) {
-			hashCode= hashCode * 17 + this.entries.hashCode();
+			hashCode= hashCode * 17 + this.entries[i].hashCode();
 		}
 		return hashCode;
 	}
