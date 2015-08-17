a14c60327114f1fa86ec6ec530683d2342768018
Mateusz Matela <mateusz.matela@gmail.com>
2015-08-17 21:37:46 +0200

    fixed bug 472009: Formatter does not respect "keep else if on one line"
    Change-Id: I4f37aba1621984170a8d856d3238cf456f7557c6
    Signed-off-by: Mateusz Matela <mateusz.matela@gmail.com>

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
@@ -11142,4 +11142,23 @@ public void testBug471090() throws JavaModelException {
 		"}\n"
 	);
 }
+/**
+ * https://bugs.eclipse.org/472009 - Formatter does not respect "keep else if on one line"
+ */
+public void testBug472009() {
+	this.formatterPrefs.alignment_for_compact_if |= Alignment.M_FORCE;
+	String source = 
+		"public class A {\r\n" + 
+		"	void a() {\r\n" + 
+		"		if (a == b) {\r\n" + 
+		"\r\n" + 
+		"		} else if (c == d) {\r\n" + 
+		"\r\n" + 
+		"		} else if (e == f) {\r\n" + 
+		"\r\n" + 
+		"		}\r\n" + 
+		"	}\r\n" + 
+		"}";
+	formatSource(source);
+}
 }
