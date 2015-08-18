85033a9a0b06a585c00635377ce19983086c281d
Mateusz Matela <mateusz.matela@gmail.com>
2015-08-18 20:15:33 +0200

    fixed bug 474629: [save actions][clean up] Exceptions thrown

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
@@ -11199,4 +11199,12 @@ public void testBug471780() {
 		"}";
 	formatSource(source);
 }
+/**
+ * https://bugs.eclipse.org/474629 - [save actions][clean up] Exceptions thrown
+ */
+public void testBug474629() {
+	this.formatterPrefs.alignment_for_binary_expression |= Alignment.M_INDENT_ON_COLUMN;
+	String source = "aaaaa + bbbb";
+	formatSource(source, source, CodeFormatter.K_EXPRESSION, 0, true);
+}
 }
