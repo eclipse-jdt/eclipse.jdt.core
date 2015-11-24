0ed94a38898e0f462619c149733be66ddffc82f6
Stephan Herrmann <stephan.herrmann@berlin.de>
2015-11-24 18:27:07 +0100

    Bug 482791: [null] NullPointerException when saving empty external annotation file (.eea)
    Change-Id: I225b93a50a3d7135dfad40fc43376b2370218cec

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/ExternalAnnotations17Test.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/ExternalAnnotations17Test.java
@@ -420,4 +420,79 @@ public class ExternalAnnotations17Test extends ExternalAnnotations18Test {
 		assertNoProblems(reconciled.getProblems());
 	}
 
+	// ==== error scenarii: ====
+	
+	// annotation file is empty
+	public void testBogusAnnotationFile1() throws Exception {
+		myCreateJavaProject("TestLibs");
+		String lib1Content = 
+				"package libs;\n" + 
+				"\n" +
+				"public interface Lib1<T> {\n" +
+				"	public Lib1<T> getLib();\n" +
+				"}\n";
+		addLibraryWithExternalAnnotations(this.project, "lib1.jar", "annots", new String[] {
+				"/UnannotatedLib/libs/Lib1.java",
+				lib1Content
+			}, null);
+		createFileInProject("annots/libs", "Lib1.eea", 
+				"");
+
+		// type check sources:
+		IPackageFragment fragment = this.project.getPackageFragmentRoots()[0].createPackageFragment("tests", true, null);
+		ICompilationUnit cu = fragment.createCompilationUnit("Test1.java",
+				"package tests;\n" + 
+				"import org.eclipse.jdt.annotation.*;\n" + 
+				"\n" + 
+				"import libs.Lib1;\n" + 
+				"\n" + 
+				"public class Test1 {\n" + 
+				"	@NonNull Lib1<String> test0(Lib1<String> stringLib) {\n" + 
+				"		return stringLib.getLib();\n" + 
+				"	}\n" +
+				"}\n",
+				true, new NullProgressMonitor()).getWorkingCopy(new NullProgressMonitor());
+		CompilationUnit reconciled = cu.reconcile(AST.JLS8, true, null, new NullProgressMonitor());
+		IProblem[] problems = reconciled.getProblems();
+		assertProblems(problems, new String[] {
+				"Pb(912) Null type safety: The expression of type 'Lib1<String>' needs unchecked conversion to conform to '@NonNull Lib1<String>'",
+		}, new int[] { 8 });
+	}
+
+	// wrong class header
+	public void testBogusAnnotationFile2() throws Exception {
+		myCreateJavaProject("TestLibs");
+		String lib1Content = 
+				"package libs;\n" + 
+				"\n" +
+				"public interface Lib1<T> {\n" +
+				"	public Lib1<T> getLib();\n" +
+				"}\n";
+		addLibraryWithExternalAnnotations(this.project, "lib1.jar", "annots", new String[] {
+				"/UnannotatedLib/libs/Lib1.java",
+				lib1Content
+			}, null);
+		createFileInProject("annots/libs", "Lib1.eea", 
+				"type Lib1\n");
+
+		// type check sources:
+		IPackageFragment fragment = this.project.getPackageFragmentRoots()[0].createPackageFragment("tests", true, null);
+		ICompilationUnit cu = fragment.createCompilationUnit("Test1.java",
+				"package tests;\n" + 
+				"import org.eclipse.jdt.annotation.*;\n" + 
+				"\n" + 
+				"import libs.Lib1;\n" + 
+				"\n" + 
+				"public class Test1 {\n" + 
+				"	@NonNull Lib1<String> test0(Lib1<String> stringLib) {\n" + 
+				"		return stringLib.getLib();\n" + 
+				"	}\n" +
+				"}\n",
+				true, new NullProgressMonitor()).getWorkingCopy(new NullProgressMonitor());
+		CompilationUnit reconciled = cu.reconcile(AST.JLS8, true, null, new NullProgressMonitor());
+		IProblem[] problems = reconciled.getProblems();
+		assertProblems(problems, new String[] {
+				"Pb(912) Null type safety: The expression of type 'Lib1<String>' needs unchecked conversion to conform to '@NonNull Lib1<String>'",
+		}, new int[] { 8 });
+	}
 }
