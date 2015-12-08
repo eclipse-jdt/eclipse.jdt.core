d79de0229c608563894d10437737b39e54570982
Till Brychcy <register.eclipse@brychcy.de>
2015-12-08 23:09:07 +0100

    Testcase and fix for bug 483952: NPE with raw type in generics context
    Change-Id: Ib65c495b3ade89135bb05d719371b721d6da9942
    Signed-off-by: Till Brychcy <register.eclipse@brychcy.de>

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
@@ -8930,4 +8930,30 @@ public void testMultipleAnnotations1() {
 		"Null type mismatch (type annotations): required \'@NonNull String\' but this expression has type \'@Nullable String\'\n" + 
  		"----------\n");
 }
+public void test483952 () {
+	runConformTestWithLibs(
+		new String[] {
+			"test/Test.java",
+			"package test;\n" +
+			"import java.util.function.Function;\n" +
+			"import org.eclipse.jdt.annotation.Nullable;\n" +
+			"public class Test {\n" +
+			"	void test1() {\n" +
+			"		Function function = x -> x;\n" +
+			"		String @Nullable [] z = test2(function, \"\");\n" +
+			"	}\n" +
+			"	<T> T @Nullable [] test2(Function<T, T> function, T t) {\n" +
+			"		return null;\n" +
+			"	}\n" +
+			"}"
+
+		},
+		getCompilerOptions(),
+		"----------\n" + 
+		"1. WARNING in test\\Test.java (at line 6)\n" + 
+		"	Function function = x -> x;\n" + 
+		"	^^^^^^^^\n" + 
+		"Function is a raw type. References to generic type Function<T,R> should be parameterized\n" + 
+		"----------\n");
+}
 }
