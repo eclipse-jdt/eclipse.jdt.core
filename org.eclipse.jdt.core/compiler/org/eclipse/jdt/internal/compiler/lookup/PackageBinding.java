073a93807b55e0b3b23cbd6bea6ed9df11a64245
Markus Keller <markus_keller@ch.ibm.com>
2016-03-02 17:17:46 +0100

    removed merge artifacts

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/PackageBinding.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/PackageBinding.java
@@ -1,9 +1,5 @@
 /*******************************************************************************
-<<<<<<< HEAD
- * Copyright (c) 2000, 2015 IBM Corporation and others.
-=======
  * Copyright (c) 2000, 2016 IBM Corporation and others.
->>>>>>> Modularization -WIP
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
