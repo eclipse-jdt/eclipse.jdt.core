3f07cee03bf21eb29e60cb096a0f462d3c1d5779
Jay Arthanareeswaran <jarthana@in.ibm.com>
2016-04-21 16:07:30 +0530

    Bug 466252 - [templates] new 'finally' template does not appear when there's no catch block
    Change-Id: I5ac5ddd0edbf9f529fb25c8d8ef3a332665d88f2

--- org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/CompletionEngine.java
+++ org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/CompletionEngine.java
@@ -1633,6 +1633,8 @@ public final class CompletionEngine
 			if (completionOnQualifiedTypeReference.isConstructorType){
 						context.setTokenLocation(CompletionContext.TL_CONSTRUCTOR_START);
 			}
+		} else if (astNode instanceof CompletionOnKeyword3 && ((CompletionOnKeyword3) astNode).afterTryOrCatch()) {
+				context.setTokenLocation(CompletionContext.TL_STATEMENT_START);
 		} else {
 			ReferenceContext referenceContext = scope.referenceContext();
 			if (referenceContext instanceof AbstractMethodDeclaration) {
