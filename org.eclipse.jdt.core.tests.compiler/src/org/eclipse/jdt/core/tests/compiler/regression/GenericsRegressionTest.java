206aa5802880dae5da4b2ca47664cf599e26194f
Till Brychcy <register.eclipse@brychcy.de>
2016-07-31 16:02:14 +0200

    Test for Bug 498486 - Erroneous Compiler Error on generics code
    Change-Id: I3c00a26bc9722024840b2d976aa41eaab4308d60

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest.java
@@ -5863,5 +5863,49 @@ public void testBug460491_comment23() {
 			"}\n"
 		});
 }
+public void testBug498486() {
+	runConformTest(
+			new String[] {
+				"t/Ab.java",
+				"package t;\n" +
+				"public interface Ab<T, O extends Ob<? extends Ob.Id, ?>> {}\n" +
+				"",
+				"t/At.java",
+				"package t;\n" +
+				"public interface At<I extends Ob.Id & Comparable<I>, O extends Ob<I, O>> {\n" +
+				"}\n" +
+				"",
+				"t/Ob.java",
+				"package t;\n" +
+				"public interface Ob<I extends Ob.Id & Comparable<I>, O extends Ob<I, O>> extends At<I, O> {\n" +
+				"  interface Id {}\n" +
+				"}\n" +
+				"",
+			}
+		);
+	runConformTest(
+			false,
+			new String[] {
+				"i/Test.java",
+				"package i;\n" +
+				"\n" +
+				"import t.Ab;\n" +
+				"import t.Ob;\n" +
+				"\n" +
+				"\n" +
+				"public class Test {\n" +
+				"	<T, I extends Ob.Id & Comparable<I>, O extends Ob<I, O>, A extends Ab<T, O>> A // Erroneous compiler error here on the last O\n" +
+				"			m() {\n" +
+				"		return null;\n" +
+				"	}\n" +
+				"}\n" +
+				"",
+			},
+			null,
+			null,
+			null,
+			null
+		);
+}
 }
 
