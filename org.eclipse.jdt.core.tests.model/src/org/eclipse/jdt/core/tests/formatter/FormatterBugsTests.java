d9dbfa43df171bba670ba75d156712621f085803
Mateusz Matela <mateusz.matela@gmail.com>
2016-09-01 01:53:17 +0200

    Bug 500093 - [formatter] AssertionError with 'Next line on wrap' for array initializers

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
@@ -12573,4 +12573,38 @@ public void testBug500096b() {
 		"}";
 	formatSource(source);
 }
+/**
+ * https://bugs.eclipse.org/500093 - [formatter] AssertionError with 'Next line on wrap' for array initializers
+ */
+public void testBug500093() {
+	this.formatterPrefs.brace_position_for_array_initializer = DefaultCodeFormatterConstants.NEXT_LINE_ON_WRAP;
+	this.formatterPrefs.page_width = 60;
+	this.formatterPrefs.use_tabs_only_for_leading_indentations = true;
+	String source =
+		"public class SomeClass {\n" + 
+		"	void foo() {\n" + 
+		"		Arrays.asList(new String[] { \"ddd\", \"eee\", \"fff\" });\n" + 
+		"		Arrays.asList(new String[] { \"a\", \"b\", \"c\" },\n" + 
+		"		        new String[]\n" + 
+		"		        { \"a\", \"b\", \"c\", });\n" + 
+		"		Arrays.asList(//\n" + 
+		"		        new String[]\n" + 
+		"		        { \"ddd\", \"eee\", \"fff\" });\n" + 
+		"		Arrays.asList(\n" + 
+		"		        new String[]\n" + 
+		"		        { \"eedd\", \"eee\", \"fff\" });\n" + 
+		"		Arrays.asList(\n" + 
+		"		        new String[]\n" + 
+		"		        { \"aa\", \"bb\", \"cc\", \"dd\", \"ee\", \"ff\", \"gg\",\n" + 
+		"		                \"hh\", \"ii\" });\n" + 
+		"		String[][] test = { { \"aaaaaa\", \"bbbbb\", \"ccccc\" },\n" + 
+		"		        { \"aaaa\", \"bb\", \"ccc\" } };\n" + 
+		"		test[123456 //\n" + 
+		"		        * (234567 + 345678 + 456789 - 567890\n" + 
+		"		                - 678901)] = new String[]\n" + 
+		"		                { \"a\", \"b\", \"c\" };\n" + 
+		"	}\n" + 
+		"}";
+	formatSource(source);
+}
 }
--- org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/linewrap/WrapPreparator.java
+++ org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/linewrap/WrapPreparator.java
@@ -582,13 +582,18 @@ public class WrapPreparator extends ASTVisitor {
 			prepareElementsList(expressions, TokenNameCOMMA, TokenNameLBRACE);
 			handleWrap(this.options.alignment_for_expressions_in_array_initializer, node);
 		}
+		int openingBraceIndex = this.tm.firstIndexIn(node, TokenNameLBRACE);
+		Token openingBrace = this.tm.get(openingBraceIndex);
+		if (openingBrace.isNextLineOnWrap() && openingBrace.getWrapPolicy() == null && openingBraceIndex > 0) {
+			// add fake wrap policy to make sure the brace indentation is right
+			openingBrace.setWrapPolicy(new WrapPolicy(WrapMode.DISABLED, openingBraceIndex - 1, 0));
+		}
 		if (!this.options.join_wrapped_lines
 				&& !this.options.insert_new_line_before_closing_brace_in_array_initializer) {
 			// if there is a line break before the closing brace, formatter should treat it as a valid wrap to preserve
 			int closingBraceIndex = this.tm.lastIndexIn(node, TokenNameRBRACE);
 			Token closingBrace = this.tm.get(closingBraceIndex);
 			if (this.tm.countLineBreaksBetween(this.tm.get(closingBraceIndex - 1), closingBrace) == 1) {
-				int openingBraceIndex = this.tm.firstIndexIn(node, TokenNameLBRACE);
 				closingBrace.setWrapPolicy(new WrapPolicy(WrapMode.WHERE_NECESSARY, openingBraceIndex,
 						closingBraceIndex, 0, this.currentDepth, 1, true, false));
 			}
