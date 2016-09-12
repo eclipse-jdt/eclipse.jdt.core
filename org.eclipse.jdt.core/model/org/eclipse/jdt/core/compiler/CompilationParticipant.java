3b161059bdaa1ef0a8e5cf5b92188506d0b9dd48
Jay Arthanareeswaran <jarthana@in.ibm.com>
2016-09-12 12:51:58 +0530

    Bug 501069 - Fix incorrect reference and usage of IJavaProject#isOpen()
    Change-Id: I45a2eb8826f04c86fcc5594fddf4428edd180c41

--- org.eclipse.jdt.core/model/org/eclipse/jdt/core/compiler/CompilationParticipant.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/core/compiler/CompilationParticipant.java
@@ -101,8 +101,11 @@ public void cleanStarting(IJavaProject project) {
  * given project should return <code>false</code> for that project.
  * </p><p>
  * Note: In {@link org.eclipse.jdt.core.WorkingCopyOwner#newWorkingCopy(String, org.eclipse.jdt.core.IClasspathEntry[], org.eclipse.core.runtime.IProgressMonitor)
- * special cases}, the project may be closed and not exist. Participants typically return false for projects that are
- * !{@link IJavaProject#isOpen()}.
+ * special cases}, the project may be closed and not exist. Participants typically return false when the
+ * underlying project is closed. I.e. when the following check returns false:
+ *  <pre>
+ * 	javaProject.getProject().isOpen();
+ * </pre>
  * </p>
  * @param project the project to participate in
  * @return whether this participant is active for a given project
