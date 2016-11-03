8ccc1b5da989e2155e439ba0a0ef4643ecfd3b92
Manoj Palat <manpalat@in.ibm.com>
2016-11-03 19:21:00 +0530

    Test cases for bug 486988  [1.9][code completion] Basic framework for code completion in module-info.java
    Change-Id: I1cacb263dffd29a4f89f214ef23fa0baacd87ddd

---
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests9.java
@@ -0,0 +1,348 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core.tests.model;
+
+import java.util.Hashtable;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+
+import junit.framework.Test;
+
+public class CompletionTests9 extends AbstractJavaModelCompletionTests {
+
+	static {
+//		TESTS_NUMBERS = new int[] { 19 };
+//		TESTS_NAMES = new String[] {"test486988_001"};
+//		TESTS_NAMES = new String[] {"test0001"};
+}
+
+public CompletionTests9(String name) {
+	super(name);
+}
+
+public void setUpSuite() throws Exception {
+	if (COMPLETION_PROJECT == null)  {
+		COMPLETION_PROJECT = setUpJavaProject("Completion", "1.8", true);
+	} else {
+		setUpProjectCompliance(COMPLETION_PROJECT, "1.8", true);
+	}
+	super.setUpSuite();
+}
+
+public static Test suite() {
+	return buildModelTestSuite(CompletionTests9.class);
+}
+
+public void test486988_0001() throws Exception {
+	IJavaProject project = createJavaProject("Completion9_1", new String[] {"src"}, new String[] {"JCL18_LIB"}, "bin", "9");
+	try {
+		project.open(null);
+		String projName = "/" + project.getElementName();
+		String packageName = "/src/";
+		String fullFilePath = projName + packageName + "module-info.java";
+		String fileContent =  "module my.mod { }\n";
+		createFile(fullFilePath, fileContent);
+		String completeBehind = "{";
+		int cursorLocation = fileContent.lastIndexOf(completeBehind) + completeBehind.length();
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2();
+
+		ICompilationUnit unit = getCompilationUnit(fullFilePath);
+		unit.codeComplete(cursorLocation, requestor);
+
+		String expected = "exports[KEYWORD]{exports, null, null, exports, 49}\n"
+				+ "provides[KEYWORD]{provides, null, null, provides, 49}\n"
+				+ "requires[KEYWORD]{requires, null, null, requires, 49}\n"
+				+ "uses[KEYWORD]{uses, null, null, uses, 49}";
+		assertResults(expected,	requestor.getResults());
+	} finally {
+		assertNotNull("Project Null", project);
+		deleteProject(project);
+	}
+}
+
+public void test486988_0002() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	String fileContent =  "module my.mod { }\n";
+
+	this.workingCopies[0] = getWorkingCopy("/Completion/src/module-info.java", fileContent);
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "{";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	String expected = "exports[KEYWORD]{exports, null, null, exports, null, 49}\n"
+			+ "provides[KEYWORD]{provides, null, null, provides, null, 49}\n"
+			+ "requires[KEYWORD]{requires, null, null, requires, null, 49}\n"
+			+ "uses[KEYWORD]{uses, null, null, uses, null, 49}";
+	assertResults(expected,	requestor.getResults());
+}
+
+public void test486988_0003() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	String fileContent =  "module my.mod {e }\n";
+
+	this.workingCopies[0] = getWorkingCopy("/Completion/src/module-info.java", fileContent);
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "{e";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	String expected = "exports[KEYWORD]{exports, null, null, exports, null, 49}";
+	assertResults(expected,	requestor.getResults());
+}
+
+public void test486988_0004() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	String fileContent =  "module my.mod { p }\n";
+
+
+	this.workingCopies[0] = getWorkingCopy("/Completion/src/module-info.java", fileContent);
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "p";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+	Hashtable<String, String> tmpOld = JavaCore.getOptions();
+	Hashtable<String, String> options = new Hashtable<>(tmpOld);
+	options.put(JavaCore.CODEASSIST_SUBSTRING_MATCH, JavaCore.DISABLED);
+	JavaCore.setOptions(options);
+
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+
+	JavaCore.setOptions(tmpOld);
+	String expected = "provides[KEYWORD]{provides, null, null, provides, null, 49}";
+	assertResults(expected,	requestor.getResults());
+}
+
+// run locally until th
+public void _test486988_0005() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	String fileContent =  "module my.mod { p }\n";
+
+	this.workingCopies[0] = getWorkingCopy("/Completion/src/module-info.java", fileContent);
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "p";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+	Hashtable<String, String> tmpOld = JavaCore.getOptions();
+	Hashtable<String, String> options = new Hashtable<>(tmpOld);
+	options.put(JavaCore.CODEASSIST_SUBSTRING_MATCH, JavaCore.ENABLED);
+	JavaCore.setOptions(options);
+
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+
+	JavaCore.setOptions(tmpOld);
+	String expected = "exports[KEYWORD]{exports, null, null, exports, null, 19}\n"
+			+ "provides[KEYWORD]{provides, null, null, provides, null, 49}";
+	assertResults(expected,	requestor.getResults());
+}
+
+public void _test486988_0006() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	String fileContent =  "module my.mod { u }\n";
+
+	this.workingCopies[0] = getWorkingCopy("/Completion/src/module-info.java", fileContent);
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "u";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+
+	Hashtable<String, String> tmpOld = JavaCore.getOptions();
+	Hashtable<String, String> options = new Hashtable<>(tmpOld);
+	options.put(JavaCore.CODEASSIST_SUBSTRING_MATCH, JavaCore.ENABLED);
+	JavaCore.setOptions(options);
+
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+
+	JavaCore.setOptions(tmpOld);
+
+	String expected = "requires[KEYWORD]{requires, null, null, requires, null, 19}\n"
+			+ "uses[KEYWORD]{uses, null, null, uses, null, 49}";
+	assertResults(expected, requestor.getResults());
+}
+
+public void test486988_0007() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[4];
+	String fileContent =  "module my.mod {"
+			+ "exports mypa"
+			+ "}\n";
+
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion/src/module-info.java", fileContent);
+	
+	this.workingCopies[1] = getWorkingCopy(
+			"/Completion/src/mypack1/Y.java",
+			"package pack1;\n" +
+			"public class Y {\n" +
+			"}");
+
+	this.workingCopies[2] = getWorkingCopy(
+			"/Completion/src/mypack2/Z.java",
+			"package pack2;\n" +
+			"public class Z {\n" +
+			"}");
+
+	this.workingCopies[3] = getWorkingCopy(
+			"/Completion/src/mypack1.mypack2/Z.java",
+			"package mypack1.mypack2;\n" +
+			"public class Z {\n" +
+			"}");
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "mypa";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+
+	Hashtable<String, String> tmpOld = JavaCore.getOptions();
+	Hashtable<String, String> options = new Hashtable<>(tmpOld);
+	options.put(JavaCore.CODEASSIST_SUBSTRING_MATCH, JavaCore.DISABLED);
+	JavaCore.setOptions(options);
+
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	String expected = "mypack1[PACKAGE_REF]{mypack1, mypack1, null, null, null, 49}\n"
+			+ "mypack2[PACKAGE_REF]{mypack2, mypack2, null, null, null, 49}\n" 
+			+ "mypackage[PACKAGE_REF]{mypackage, mypackage, null, null, null, 49}";
+	assertResults(expected, requestor.getResults());
+	JavaCore.setOptions(tmpOld);
+}
+
+public void test486988_0008() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[4];
+	String fileContent =  "module my.mod {"
+			+ "exports mypack1 t"
+			+ "}\n";
+
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion/src/module-info.java", fileContent);
+
+	this.workingCopies[1] = getWorkingCopy(
+			"/Completion/src/mypack1/Y.java",
+			"package pack1;\n" +
+			"public class Y {\n" +
+			"}");
+
+	this.workingCopies[2] = getWorkingCopy(
+			"/Completion/src/mypack2/Z.java",
+			"package pack2;\n" +
+			"public class Z {\n" +
+			"}");
+
+	this.workingCopies[3] = getWorkingCopy(
+			"/Completion/src/mypack1.mypack2/Z.java",
+			"package mypack1.mypack2;\n" +
+			"public class Z {\n" +
+			"}");
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "mypack1 t";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+
+	Hashtable<String, String> tmpOld = JavaCore.getOptions();
+	Hashtable<String, String> options = new Hashtable<>(tmpOld);
+	options.put(JavaCore.CODEASSIST_SUBSTRING_MATCH, JavaCore.DISABLED);
+	JavaCore.setOptions(options);
+
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	String expected = "to[KEYWORD]{to, null, null, to, null, 49}";
+	assertResults(expected, requestor.getResults());
+	JavaCore.setOptions(tmpOld);
+
+}
+public void test486988_0009() throws Exception {
+	IJavaProject project1 = createJavaProject("Completion9_1", new String[] {"src"}, new String[] {"JCL18_LIB"}, "bin", "9");
+	IJavaProject project2 = createJavaProject("Completion9_2", new String[] {"src"}, new String[] {"JCL18_LIB"}, "bin", "9");
+	try {
+		project2.open(null);
+		String fileContent =  "module org.eclipse.foo {}\n";
+		String filePath = "/Completion9_2/src/module-info.java";
+		createFile(filePath, fileContent);
+
+		project1.open(null);
+		filePath = "/Completion9_1/src/module-info.java";
+		fileContent =  "module com.greetings {requires o }\n";
+		createFile(filePath, fileContent);
+		String completeBehind = "requires o";
+		int cursorLocation = fileContent.lastIndexOf(completeBehind) + completeBehind.length();
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2();
+
+		ICompilationUnit unit = getCompilationUnit(filePath);
+		unit.codeComplete(cursorLocation, requestor);
+
+		String expected = "[PROPOSAL]{org.eclipse.foo, org.eclipse.foo, null, null, 49}";
+		assertResults(expected,	requestor.getResults());
+	} finally {
+		deleteProject(project1);
+		deleteProject(project2);
+	}
+}
+
+public void test486988_0010() throws Exception {
+	IJavaProject project1 = createJavaProject("Completion9_1", new String[] {"src"}, new String[] {"JCL18_LIB"}, "bin", "9");
+	IJavaProject project2 = createJavaProject("Completion9_2", new String[] {"src"}, new String[] {"JCL18_LIB"}, "bin", "9");
+	try {
+		project1.open(null);
+		String filePath = "/Completion9_1/src/module-info.java";
+		String fileContent =  "module com.greetings {"
+				+ "requires org.eclipse.foo;\n"
+				+ "}\n";
+		createFile(filePath, fileContent);
+		project1.close(); //sync
+		project1.open(null);
+
+		project2.open(null);
+		String pack = "/Completion9_2/src/mypack1";
+		createFolder(pack);
+		filePath = pack + "/Y.java";
+		fileContent = "package pack1;\n" +
+		"public class Y {}\n";
+		createFile(filePath, fileContent);
+
+		fileContent =  "module org.eclipse.foo { "
+				+ "exports mypack1 to com"
+				+ "}\n";
+		filePath = "/Completion9_2/src/module-info.java";
+		createFile(filePath, fileContent);
+
+		String completeBehind = "com";
+		int cursorLocation = fileContent.lastIndexOf(completeBehind) + completeBehind.length();
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2();
+
+		ICompilationUnit unit = getCompilationUnit(filePath);
+		unit.codeComplete(cursorLocation, requestor);
+
+		String expected = "[PROPOSAL]{com.greetings, com.greetings, null, null, 49}";
+		assertResults(expected,	requestor.getResults());
+	} finally {
+		deleteProject(project1);
+		deleteProject(project2);
+	}
+}
+
+}
