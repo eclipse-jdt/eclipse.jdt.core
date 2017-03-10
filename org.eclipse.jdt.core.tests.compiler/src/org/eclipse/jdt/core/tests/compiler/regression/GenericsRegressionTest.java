90d7a482ab40b7c03970e70766cdc7419fd23c2e
Till Brychcy <register.eclipse@brychcy.de>
2017-03-10 21:55:07 +0100

    Bug 508799 - Incremental compile produces invalid errors
    Change-Id: Ie221cb0e7040b3b6d693273821fe32103e16a05e

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest.java
@@ -6013,5 +6013,53 @@ public void testBug469297() {
 			"----------\n");
 	}
 }
+public void testBug508799() {
+	Map customOptions = getCompilerOptions();
+	customOptions.put(JavaCore.COMPILER_PB_RAW_TYPE_REFERENCE, JavaCore.IGNORE);
+	runConformTest(
+		new String[] {
+			"test/A.java",
+			"package test;\n" +
+			"\n" +
+			"public interface A<T extends Iterable> {\n" +
+			"    void a();\n" +
+			"}\n" +
+			"",
+			"test/B1.java",
+			"package test;\n" +
+			"\n" +
+			"public interface B1 extends A<Iterable> {\n" +
+			"    void b1();\n" +
+			"}\n" +
+			"",
+			"test/B2.java",
+			"package test;\n" +
+			"\n" +
+			"public interface B2 extends A<Iterable> {\n" +
+			"    void b2();\n" +
+			"}\n" +
+			"",
+		},
+		customOptions
+	);
+	runConformTest(false,
+		new String[] {
+			"test/C.java",
+			"package test;\n" +
+			"\n" +
+			"public class C implements B1, B2 {\n" +
+			"    public void a() {\n" +
+			"    }\n" +
+			"\n" +
+			"    public void b1() {\n" +
+			"    }\n" +
+			"\n" +
+			"    public void b2() {\n" +
+			"    }\n" +
+			"}\n" +
+			"",
+		}, null, customOptions, "", "", "", null
+	);
+}
 }
 
