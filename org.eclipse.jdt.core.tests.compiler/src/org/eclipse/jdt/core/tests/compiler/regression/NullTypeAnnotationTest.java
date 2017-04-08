0da3c2bfd41bdb1134fb297a803d154ee2ea7998
Till Brychcy <register.eclipse@brychcy.de>
2017-04-08 23:23:32 +0200

    Bug 514977 - [1.8][null] Arguments of varargs invocation are not always checked
    Change-Id: I98de9bbfafd64c67d3f7e42be00700b120c7e066

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
@@ -15262,4 +15262,81 @@ public void testBug514570() {
 		""
 	);
 }
+public void testBug514977() {
+	runNegativeTestWithLibs(
+		new String[] {
+			"test/Test.java",
+			"package test;\n" +
+			"\n" +
+			"import org.eclipse.jdt.annotation.DefaultLocation;\n" +
+			"import org.eclipse.jdt.annotation.NonNull;\n" +
+			"import org.eclipse.jdt.annotation.NonNullByDefault;\n" +
+			"\n" +
+			"public class Test {\n" +
+			"	static void nn(@NonNull Object i) {\n" +
+			"		i.hashCode();\n" +
+			"	}\n" +
+			"\n" +
+			"	static void f(@NonNull Integer @NonNull... args) {\n" +
+			"		nn(args);\n" +
+			"		for (Integer s : args) {\n" +
+			"			nn(s);\n" +
+			"		}\n" +
+			"	}\n" +
+			"\n" +
+			"	@NonNullByDefault({ DefaultLocation.ARRAY_CONTENTS, DefaultLocation.PARAMETER })\n" +
+			"	static void g(Integer... args) {\n" +
+			"		nn(args);\n" +
+			"		for (Integer s : args) {\n" +
+			"			nn(s);\n" +
+			"		}\n" +
+			"	}\n" +
+			"\n" +
+			"	public static void main(String[] args) {\n" +
+			"		Integer i = args.length == 0 ? null : 1;\n" +
+			"		Integer[] array = i == null ? null : new Integer[] {i};\n" +
+			"		f(array);\n" +
+			"		f(i);\n" +
+			"		f(1, i);\n" +
+			"		g(array);\n" +
+			"		g(i);\n" +
+			"		g(1, i);\n" +
+			"	}\n" +
+			"}\n" +
+			"",
+		}, 
+		getCompilerOptions(),
+		"----------\n" + 
+		"1. ERROR in test\\Test.java (at line 30)\n" + 
+		"	f(array);\n" + 
+		"	  ^^^^^\n" + 
+		"Null type mismatch: required \'@NonNull Integer @NonNull[]\' but the provided value is inferred as @Nullable\n" + 
+		"----------\n" + 
+		"2. ERROR in test\\Test.java (at line 31)\n" + 
+		"	f(i);\n" + 
+		"	  ^\n" + 
+		"Null type mismatch: required \'@NonNull Integer\' but the provided value is inferred as @Nullable\n" + 
+		"----------\n" + 
+		"3. ERROR in test\\Test.java (at line 32)\n" + 
+		"	f(1, i);\n" + 
+		"	     ^\n" + 
+		"Null type mismatch: required \'@NonNull Integer\' but the provided value is inferred as @Nullable\n" + 
+		"----------\n" + 
+		"4. ERROR in test\\Test.java (at line 33)\n" + 
+		"	g(array);\n" + 
+		"	  ^^^^^\n" + 
+		"Null type mismatch: required \'@NonNull Integer @NonNull[]\' but the provided value is inferred as @Nullable\n" + 
+		"----------\n" + 
+		"5. ERROR in test\\Test.java (at line 34)\n" + 
+		"	g(i);\n" + 
+		"	  ^\n" + 
+		"Null type mismatch: required \'@NonNull Integer\' but the provided value is inferred as @Nullable\n" + 
+		"----------\n" + 
+		"6. ERROR in test\\Test.java (at line 35)\n" + 
+		"	g(1, i);\n" + 
+		"	     ^\n" + 
+		"Null type mismatch: required \'@NonNull Integer\' but the provided value is inferred as @Nullable\n" + 
+		"----------\n"
+	);
+}
 }
