1da45304dd42749d33eb1efcdf3d18208c383e6a
Sasikanth Bharadwaj <sasikanth.bharadwaj@in.ibm.com>
2017-11-07 12:07:15 +0530

    Fixed Bug 517951: Internal compiler error: null pointer exception on method reference to varargs method in a finally block
    Change-Id: Ie3060c53f05896e3e48e4d100df21c2579d038f7

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
@@ -6539,6 +6539,115 @@ public void testBug522469() {
 		}
 	);
 }
+public void testBug517951() {
+	runConformTest(
+		new String[] {
+			"Minimal.java",
+			"public class Minimal {\n" + 
+			"    public void iCrash() {\n" + 
+			"        try {\n" + 
+			"            System.out.println(\"Body can't be empty\");\n" + 
+			"        } finally {\n" + 
+			"            consumes(sneaky()::withVarargs);\n" + 
+			"        }\n" + 
+			"    }\n" + 
+			"    public static void main(String[] args) {\n" + 
+			"		new Minimal().iCrash();\n" + 
+			"	}\n" + 
+			"    private Minimal sneaky() { return this; }\n" + 
+			"    private void withVarargs(String... test) {}\n" + 
+			"    private void consumes(Runnable r) {}\n" + 
+			"}"
+		},
+		"Body can't be empty"
+	);
+}
+public void testBug517951a() {
+	runConformTest(
+		new String[] {
+			"Snippet.java",
+			"import java.nio.file.Files;\n" + 
+			"import java.nio.file.Paths;\n" + 
+			"import java.util.function.Consumer;\n" + 
+			"public class Snippet {\n" + 
+			"	void postError( String fmt, Object ... args ) {\n" + 
+			"	}\n" + 
+			"	public boolean test(Consumer<String> postError ) {\n" + 
+			"		return false;\n" + 
+			"	}\n" + 
+			"	void func() {\n" + 
+			"		try( java.io.InputStream istr = Files.newInputStream( Paths.get( \"\" ) )){\n" + 
+			"		} catch( Exception e ) {\n" + 
+			"		} finally {\n" + 
+			"			test( this::postError);\n" + 
+			"		}\n" + 
+			"	}\n" + 
+			"}"
+		}
+	);
+}
+public void testBug517951b() {
+	runConformTest(
+		new String[] {
+			"Element.java",
+			"public class Element\n" + 
+			"{\n" + 
+			"    Operation operation = new Operation(Element::new);\n" + 
+			"    public Element(Integer matrix)\n" + 
+			"    {\n" + 
+			"    		//...\n" + 
+			"    }\n" + 
+			"    public Element(Operation... operation)\n" + 
+			"    {\n" + 
+			"    		//...\n" + 
+			"    }\n" + 
+			"}\n" + 
+			"class Operation\n" + 
+			"{\n" + 
+			"    public Operation(Factory factory)\n" + 
+			"    {\n" + 
+			"        //...\n" + 
+			"    }\n" + 
+			"}\n" + 
+			"interface Factory\n" + 
+			"{\n" + 
+			"    Element create(Operation... operations);\n" + 
+			"}"
+		}
+	);
+}
+public void testBug517951c() {
+	runConformTest(
+		new String[] {
+			"npetest/NpeTest.java",
+			"package npetest;\n" + 
+			"public class NpeTest {\n" + 
+			"    public NpeTestScheduler scheduler;\n" + 
+			"    private void doIt(Object... params) {\n" + 
+			"        try {\n" + 
+			"            System.out.println(\"Done\");\n" + 
+			"        }\n" + 
+			"        finally {\n" + 
+			"            scheduler.schedule(this::doIt);\n" + 
+			"        }\n" + 
+			"    }\n" + 
+			"}",
+			"npetest/NpeTestIf.java",
+			"package npetest;\n" + 
+			"@FunctionalInterface\n" + 
+			"public interface NpeTestIf {\n" + 
+			"    void doSomething(Object... params);\n" + 
+			"}",
+			"npetest/NpeTestScheduler.java",
+			"package npetest;\n" + 
+			"public class NpeTestScheduler {\n" + 
+			"    public void schedule(NpeTestIf what) {\n" + 
+			"        what.doSomething();\n" + 
+			"    }\n" + 
+			"}"
+		}
+	);
+}
 public static Class testClass() {
 	return LambdaExpressionsTest.class;
 }
