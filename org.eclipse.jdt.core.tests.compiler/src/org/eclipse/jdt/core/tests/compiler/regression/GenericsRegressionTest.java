f8ebd9dcd7fd27b8feb51794363d70a22ae2741f
Sasikanth Bharadwaj <sasikanth.bharadwaj@in.ibm.com>
2017-11-29 14:13:34 +0530

    Fixed Bug 526132: Copy and paste the same function in a class gave NPE
    Change-Id: Ia85e4691dac2a4aa4d38844120c7b162be6b0237

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -6200,6 +6200,64 @@ public void testBug526423() {
 			"}"
 		}
 	);
+}
+public void testBug526132() {
+	Map customOptions = getCompilerOptions();
+	customOptions.put(CompilerOptions.OPTION_ReportUnavoidableGenericTypeProblems, CompilerOptions.DISABLED);
+	runNegativeTest(
+		new String[] {
+			"Test.java",
+			"import java.util.HashMap;\n" + 
+			"import java.util.Map;\n" + 
+			"public class Test {\n" + 
+			"	private Map field = new HashMap();\n" + 
+			"	private void method() {\n" + 
+			"		field.put(\"key\", \"value\");\n" + 
+			"	}\n" +
+			"	private void method() {\n" + 
+			"		field.put(\"key\", \"value\");\n" + 
+			"	}\n" +
+			"}\n"
+		},
+		"----------\n" + 
+		"1. WARNING in Test.java (at line 4)\n" + 
+		"	private Map field = new HashMap();\n" + 
+		"	        ^^^\n" + 
+		"Map is a raw type. References to generic type Map<K,V> should be parameterized\n" + 
+		"----------\n" + 
+		"2. WARNING in Test.java (at line 4)\n" + 
+		"	private Map field = new HashMap();\n" + 
+		"	                        ^^^^^^^\n" + 
+		"HashMap is a raw type. References to generic type HashMap<K,V> should be parameterized\n" + 
+		"----------\n" + 
+		"3. ERROR in Test.java (at line 5)\n" + 
+		"	private void method() {\n" + 
+		"	             ^^^^^^^^\n" + 
+		"Duplicate method method() in type Test\n" + 
+		"----------\n" + 
+		"4. WARNING in Test.java (at line 5)\n" + 
+		"	private void method() {\n" + 
+		"	             ^^^^^^^^\n" + 
+		"The method method() from the type Test is never used locally\n" + 
+		"----------\n" + 
+		"5. WARNING in Test.java (at line 6)\n" + 
+		"	field.put(\"key\", \"value\");\n" + 
+		"	^^^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+		"Type safety: The method put(Object, Object) belongs to the raw type Map. References to generic type Map<K,V> should be parameterized\n" + 
+		"----------\n" + 
+		"6. ERROR in Test.java (at line 8)\n" + 
+		"	private void method() {\n" + 
+		"	             ^^^^^^^^\n" + 
+		"Duplicate method method() in type Test\n" + 
+		"----------\n" + 
+		"7. WARNING in Test.java (at line 9)\n" + 
+		"	field.put(\"key\", \"value\");\n" + 
+		"	^^^^^^^^^^^^^^^^^^^^^^^^^\n" + 
+		"Type safety: The method put(Object, Object) belongs to the raw type Map. References to generic type Map<K,V> should be parameterized\n" + 
+		"----------\n",
+	null,
+	true,
+	customOptions);
 }
 }
 
