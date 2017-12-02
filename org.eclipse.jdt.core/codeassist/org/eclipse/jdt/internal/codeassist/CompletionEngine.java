2c6f0bc37140ce246758664ec505690d14eb4b09
Manoj Palat <manpalat@in.ibm.com>
2017-12-02 12:21:56 +0530

    Fix for Bug 527873 - [9][content assist] proposes default package

--- org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/CompletionEngine.java
+++ org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/CompletionEngine.java
@@ -712,7 +712,7 @@ public final class CompletionEngine
 	private INameEnvironment noCacheNameEnvironment;
 	char[] source;
 	ModuleDeclaration moduleDeclaration;
-	boolean isPackageVisibilityCompletion = false;
+	boolean skipDefaultPackage = false;
 	char[] completionToken;
 
 	char[] qualifiedCompletionToken;
@@ -1332,7 +1332,7 @@ public final class CompletionEngine
 
 		if (!isValidPackageName(packageName)) return;
 		
-		if (this.isPackageVisibilityCompletion &&
+		if (this.skipDefaultPackage &&
 			CharOperation.equals(packageName, CharOperation.NO_CHAR))
 			return;
 
@@ -2111,6 +2111,7 @@ public final class CompletionEngine
 								TypeReference implementation = implementations[j];
 								if (implementation instanceof CompletionOnProvidesImplementationsSingleTypeReference ||
 										implementation instanceof CompletionOnProvidesImplementationsQualifiedTypeReference) {
+									this.skipDefaultPackage = true;
 									contextAccepted = checkForCNF(implementation, parsedUnit, false);
 									return;
 								} else if (implementation instanceof CompletionOnKeyword) {
@@ -2142,6 +2143,8 @@ public final class CompletionEngine
 											e.scope,
 											e.insideTypeAnnotation);
 						}
+					} finally {
+						this.skipDefaultPackage = false;
 					}
 				}
 				// scan the package & import statements first
@@ -2343,7 +2346,7 @@ public final class CompletionEngine
 	private boolean completeOnPackageVisibilityStatements(boolean contextAccepted,
 			CompilationUnitDeclaration parsedUnit, PackageVisibilityStatement[] pvsStmts) {
 		try {
-			this.isPackageVisibilityCompletion = true;
+			this.skipDefaultPackage = true;
 			for (int i = 0, l = pvsStmts.length; i < l; ++i) {
 				PackageVisibilityStatement pvs = pvsStmts[i];
 				if (pvs instanceof CompletionOnKeywordModuleInfo) { // dummy pvs statement
@@ -2384,7 +2387,7 @@ public final class CompletionEngine
 				}
 			}
 		} finally {
-			this.isPackageVisibilityCompletion = false;
+			this.skipDefaultPackage = false;
 		}
 		return contextAccepted;
 	}
