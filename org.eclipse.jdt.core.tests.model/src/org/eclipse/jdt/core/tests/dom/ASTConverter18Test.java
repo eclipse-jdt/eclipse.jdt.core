eb56c0ff84220d1e3d1f2fcde8e841e499ae26e6
Manoj Palat <manpalat@in.ibm.com>
2017-12-11 20:20:22 +0530

    Fix for Bug 526449: ASTNode.setSourceRange() throws IllegalArgumentException
    Change-Id: I36d65584cdf122cfef6c7572061e6249b538b947

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverter18Test.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverter18Test.java
@@ -5355,5 +5355,25 @@ public void testBug490698_001() throws JavaModelException {
 	ASTNode node = buildAST(contents, this.workingCopy, false);
 	assertEquals("Not a compilation unit", ASTNode.COMPILATION_UNIT, node.getNodeType());
 }
+public void testBug526449_001() throws JavaModelException {
+	String contents =
+			"class com.google.android.gms.common.X {\n" +
+			"    class zzc {\n" +
+			"        void getBytes() {\n" +
+			"            this;\n" +
+			"        }\n" +
+			"    }\n" +
+			"\n" +
+			"    class zze {\n" +
+			"        zza zzLC[] = {\n" +
+			"            new zzc(){}, \n" +
+			"            new zzc(){}\n" +
+			"        };\n" +
+			"    }\n" +
+			"}\n";
+	this.workingCopy = getWorkingCopy("/Converter18/src/jsr308/myex/X.java", true/*resolve*/);
+	ASTNode node = buildAST(contents, this.workingCopy, false);
+	assertEquals("Not a compilation unit", ASTNode.COMPILATION_UNIT, node.getNodeType());
+}
 
 }
