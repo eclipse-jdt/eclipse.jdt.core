eb21f442c8cee0fdeff4a227bd4bb4fef58f9a52
Sasikanth Bharadwaj <sasikanth.bharadwaj@in.ibm.com>
2018-01-03 11:23:59 +0530

    Fixed bug 526310 - Making changes to 'Is modular' node does not trigger build
    Change-Id: I764f28d91589f73b2fb76f69772b495474280007

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/AllJavaModelTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/AllJavaModelTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,6 +47,7 @@ private static Class[] getAllTestClasses() {
 		JavaProjectTests.class,
 		// Modularity tests (Java 9)
 		ModuleBuilderTests.class,
+		ModuleOptionsTests.class,
 
 		// Compilation unit tests
 		CompilationUnitTests.class,
--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaProjectTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaProjectTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3039,8 +3039,5 @@ public void testBug522554_3() throws CoreException {
 		if (prj != null)
 			deleteProject(prj);
 	}
-}
-private IPath getJRE9Path() {
-	return new Path(System.getProperty("java.home") + "/lib/jrt-fs.jar");
 }
 }
