77243bbcfe3ea935bddfbf047e794a3d6a6483b2
Stephan Herrmann <stephan.herrmann@berlin.de>
2018-01-25 12:56:56 +0100

    Bug 526335 - [9][hovering] Deprecation warning should show the new 'since' deprecation value
    - test isolation
    
    Change-Id: I1374156faf78e0b28cbb1d560fb7032f512bb8a1

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AbstractRegressionTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AbstractRegressionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3425,6 +3425,10 @@ protected void runNegativeTest(
 		File outputDir = new File(OUTPUT_DIR);
 		if (outputDir.exists()) {
 			Util.flushDirectoryContent(outputDir);
+		}
+		File libDir = new File(LIB_DIR);
+		if (libDir.exists()) {
+			Util.flushDirectoryContent(libDir);
 		}
 		super.tearDown();
 		if (RUN_JAVAC) {
