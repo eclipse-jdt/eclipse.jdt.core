078507813cb8fb0834f7721498d7ae004e43090e
Stephan Herrmann <stephan.herrmann@berlin.de>
2018-02-08 10:15:08 +0530

    fix for bug 519417 [9][completion] should propose types & packages exported from required modules
    Also-by: Manoj Palat <manpalat@in.ibm.com>

--- org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/CompletionEngine.java
+++ org.eclipse.jdt.core/codeassist/org/eclipse/jdt/internal/codeassist/CompletionEngine.java
@@ -8164,7 +8164,11 @@ public final class CompletionEngine
 			setSourceRange(
 				importReference.sourceStart,
 				importReference.declarationSourceEnd);
-			this.nameEnvironment.findPackages(importName, this);
+			try {
+				this.nameEnvironment.findPackages(importName, this, this.javaProject.getAllPackageFragmentRoots(), true);
+			} catch (JavaModelException e) {
+				// silent
+			}
 			setSourceRange(
 				oldStart,
 				oldEnd - 1,
@@ -10732,7 +10736,7 @@ public final class CompletionEngine
 	private void findPackagesInCurrentModule() {
 		try {
 			IPackageFragmentRoot[] moduleRoots = SearchableEnvironment.getOwnedPackageFragmentRoots(this.javaProject);
-			this.nameEnvironment.findPackages(CharOperation.toLowerCase(this.completionToken), this, moduleRoots);
+			this.nameEnvironment.findPackages(CharOperation.toLowerCase(this.completionToken), this, moduleRoots, false);
 		} catch (JavaModelException e) {
 			// silent
 		}
@@ -10744,7 +10748,11 @@ public final class CompletionEngine
 		setSourceRange(packageStatement.sourceStart, packageStatement.sourceEnd);
 		long completionPosition = packageStatement.sourcePositions[packageStatement.sourcePositions.length - 1];
 		setTokenRange((int) (completionPosition >>> 32), (int) completionPosition);
-		this.nameEnvironment.findPackages(CharOperation.toLowerCase(this.completionToken), this);
+		try {
+			this.nameEnvironment.findPackages(CharOperation.toLowerCase(this.completionToken), this, this.javaProject.getAllPackageFragmentRoots(), true);
+		} catch (JavaModelException e) {
+			// silent
+		}
 	}
 
 	private void findParameterizedType(TypeReference ref, Scope scope) {
