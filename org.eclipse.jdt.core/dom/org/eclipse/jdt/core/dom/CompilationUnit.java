25a43a25fad53f3631d60581dd868405c3df418a
kalyan prasad <kalyan_prasad@in.ibm.com>
2018-03-23 12:54:25 +0530

    Bug 532800: [9] The Documentation for isModule() API available on CompilationUnit does not specify that it throws an Exception
    Change-Id: I1d50f5173520bf909afa0005550f81b3a67c6187
    Signed-off-by: kalyan prasad <kalyan_prasad@in.ibm.com>

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/CompilationUnit.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/CompilationUnit.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -583,6 +583,7 @@ public class CompilationUnit extends ASTNode {
 	 * unit, or <code>null</code> if this compilation unit is not a module info.
 	 *
 	 * @return the module declaration node, or <code>null</code> if none
+	 * @exception UnsupportedOperationException if this operation is used below JLS9
 	 * @since 3.14
 	 */
 	public ModuleDeclaration getModule() {
