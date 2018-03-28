21ae9ab8a7f405549e86525e56831bf4bcc24efe
Manoj Palat <manpalat@in.ibm.com>
2018-03-28 17:55:38 +0530

    Bug 532535 - [10][18.3][dom] ITypeBinding expected to be returned by default
    Change-Id: I06e529cfedd5aa90bbc0e3a5b19b713eb8794562

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
@@ -3648,7 +3648,8 @@ class ASTConverter {
 		if (this.resolveBindings) {
 			recordNodes(variableDeclarationFragment, localDeclaration);
 			if (this.ast.apiLevel() >= AST.JLS10_INTERNAL && type.isVar()) {
-				Name varName = ((SimpleType) type).getName();
+				SimpleName varName = (SimpleName) ((SimpleType) type).getName();
+				varName.setVar(true);
 				recordNodes(varName, localDeclaration);				
 			}
 		}
