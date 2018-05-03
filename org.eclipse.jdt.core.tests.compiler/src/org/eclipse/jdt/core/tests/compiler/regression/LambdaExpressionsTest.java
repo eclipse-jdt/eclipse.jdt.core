f37dfa3a63bfb2d01f5426d3d6e72205d17c7ceb
Till Brychcy <register.eclipse@brychcy.de>
2018-05-03 08:07:37 +0200

    Test for Bug 531093
    Change-Id: Ia1c44fda480363f823336c06677dc90660fd1920

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/LambdaExpressionsTest.java
@@ -6962,6 +6962,38 @@ public void testBug531093comment1() {
 		""
 	);
 }
+public void testBug531093() {
+	runConformTest(
+		new String[] {
+			"bug/Bug.java",
+			"package bug;\n" +
+			"import java.io.Serializable;\n" +
+			"import java.lang.reflect.Method;\n" +
+			"import java.util.Arrays;\n" +
+			"import java.util.Optional;\n" +
+			"import java.util.function.Function;\n" +
+			"\n" +
+			"public class Bug {\n" +
+			"    public <E extends Number & Serializable> Function<E, Optional<String>> useMethodRef() {\n" +
+			"        return Bug::getMapper;\n" +
+			"    }\n" +
+			"\n" +
+			"    private static Optional<String> getMapper(Number event) {\n" +
+			"        return null;\n" +
+			"    }\n" +
+			"\n" +
+			"    public static void main(String[] args) {\n" +
+			"        Method[] methods = Bug.class.getDeclaredMethods();\n" +
+			"        for (Method method : methods) {\n" +
+			"            String.valueOf(Arrays.asList(method.getParameters()));\n" +
+			"        }\n" +
+			"    }\n" +
+			"}\n" +
+			"",
+		}, 
+		""
+	);
+}
 public static Class testClass() {
 	return LambdaExpressionsTest.class;
 }
