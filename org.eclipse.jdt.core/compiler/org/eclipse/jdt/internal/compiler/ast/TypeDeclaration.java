0160cea0969f1896a04c71dc66a96ec118a2ae93
Manoj Palat <manpalat@in.ibm.com>
2018-07-10 09:21:46 +0530

    Revert "Bug 535847 - [11][compiler]JEP 181 - Nestmate - generation of the nest related classfile attribute"
    This reverts commit f2c0e1e1a5788a95bc2f7d0614bbc8f50330d4bd.

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
@@ -18,11 +18,6 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.compiler.ast;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-
 import org.eclipse.jdt.core.compiler.*;
 import org.eclipse.jdt.internal.compiler.*;
 import org.eclipse.jdt.internal.compiler.impl.*;
@@ -73,8 +68,6 @@ public class TypeDeclaration extends Statement implements ProblemSeverities, Ref
 
 	// 1.5 support
 	public TypeParameter[] typeParameters;
-
-	private HashSet<String> nestedMembers;
 
 public TypeDeclaration(CompilationResult compilationResult){
 	this.compilationResult = compilationResult;
@@ -201,21 +194,6 @@ public MethodDeclaration addMissingAbstractMethodFor(MethodBinding methodBinding
 	return methodDeclaration;
 }
 
-public void addNestedMembers(char[] nestedMember) {
-	if (nestedMember == null || nestedMember.length == 0)
-		return;
-	if (this.nestedMembers == null) {
-		this.nestedMembers = new HashSet<>();
-	}
-	this.nestedMembers.add(new String(nestedMember));
-}
-public List<String> getNestedMembers() {
-	if (this.nestedMembers == null)
-		return null;
-	ArrayList<String> list = new ArrayList<>(this.nestedMembers);
-	Collections.sort(list);
-	return list;
-}
 /**
  *	Flow analysis for a local innertype
  *
