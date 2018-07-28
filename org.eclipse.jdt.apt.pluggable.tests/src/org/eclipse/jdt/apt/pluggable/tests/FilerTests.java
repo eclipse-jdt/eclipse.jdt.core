63684dd8b01031f0dc2d269a59c59d3b22725f4a
Andrey Loskutov <loskutov@gmx.de>
2018-07-28 23:15:43 +0200

    Bug 537459 - Failures in org.eclipse.jdt.apt.pluggable.tests.FilerTests.testCreateClass1
    Change-Id: I2967f7dedae1aec9f11edc14bd0b510df1da5d5d
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/FilerTests.java
+++ org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/FilerTests.java
@@ -44,7 +44,7 @@ public class FilerTests extends TestBase
 	public static Test suite() {
 		return new TestSuite(FilerTests.class);
 	}
-	
+
 	/**
 	 * Test generation of a source file, using the GenClass6 annotation
 	 * @see javax.annotation.processing.Filer#createClassFile(CharSequence, javax.lang.model.element.Element...)
@@ -64,25 +64,25 @@ public class FilerTests extends TestBase
 		AptConfig.setEnabled(jproj, true);
 		fullBuild();
 		expectingNoProblems();
-		
+
 		// Check whether generated sources were generated and compiled
 		expectingFile(proj, ".apt_generated/gen6/Generated01.java");
 		final String[] expectedClasses = { "targets.filer.Parent01", "gen6.Generated01" };
 		expectingUniqueCompiledClasses(expectedClasses);
-		
+
 		// Check whether non-source resource was generated in final round
 		expectingFile( proj, ".apt_generated/summary.txt" );
-		
+
 		//TODO: if a parent file is modified,
 		// the generated file should be regenerated and recompiled.
 		// The generated resource file should be regenerated (but not compiled).
-	
-		// Modify target file to remove annotation and incrementally rebuild; 
+
+		// Modify target file to remove annotation and incrementally rebuild;
 		// generated file should be deleted.
 		IdeTestUtils.copyResources(proj, "targets/filer01b", "src/targets/filer");
 		incrementalBuild();
 		expectingNoProblems();
-		
+
 		final String[] expectedClasses2 = { "targets.filer.Parent01" };
 		expectingUniqueCompiledClasses(expectedClasses2);
 
@@ -106,25 +106,25 @@ public class FilerTests extends TestBase
 		IJavaProject jproj = createJavaProject(_projectName);
 		IProject proj = jproj.getProject();
 		IdeTestUtils.copyResources(proj, "targets/filer02a", "src/targets/filer");
-		
+
 		disableJava5Factories(jproj);
 		AptConfig.setEnabled(jproj, true);
 		fullBuild();
 		expectingNoProblems();
-		
+
 		// Check whether generated sources were generated and compiled
 		expectingFile(proj, ".apt_generated/gen6/Generated02.java");
 		final String[] expectedClasses = { "targets.filer.Parent02", "gen6.Generated02" };
 		expectingUniqueCompiledClasses(expectedClasses);
-		
-		// Modify target file to change name of generated file and incrementally rebuild; 
+
+		// Modify target file to change name of generated file and incrementally rebuild;
 		// generated file should be deleted.
 		IdeTestUtils.copyResources(proj, "targets/filer02b", "src/targets/filer");
 		incrementalBuild();
-		
+
 		IPath parentPath = proj.getFullPath().append("src/targets/filer/Parent02.java");
 		expectingOnlySpecificProblemFor(parentPath, new Problem("Parent02", "gen6.Generated02 cannot be resolved to a type", parentPath, 842, 858, CategorizedProblem.CAT_TYPE, IMarker.SEVERITY_ERROR));
-		
+
 		// This test only works if there are no Java 5 processors (e.g., apt.tests plugin) in the factory path.
 		// If Java 5 processors are present, then gen6.Generated02 will also be recompiled, before it's deleted.
 		final String[] expectedClasses2 = { "gen6.XxxGenerated02", "targets.filer.Parent02" };
@@ -132,7 +132,7 @@ public class FilerTests extends TestBase
 
 		expectingNoFile(proj, ".apt_generated/gen6/Generated02.java");
 	}
-	
+
 	/**
 	 * Test generation of a source file that is referenced by the parent, using the GenClass6 annotation.
 	 * Processor calls getEnclosedElements(), causing the to-be-generated type to be resolved before it exists.
@@ -149,7 +149,7 @@ public class FilerTests extends TestBase
 		IJavaProject jproj = createJavaProject(_projectName);
 		IProject proj = jproj.getProject();
 		IdeTestUtils.copyResources(proj, "targets/filer03a", "src/targets/filer");
-		
+
 		disableJava5Factories(jproj);
 		AptConfig.setEnabled(jproj, true);
 		fullBuild();
@@ -159,12 +159,12 @@ public class FilerTests extends TestBase
 			// expectingOnlySpecificProblemFor(parentPath, problem);
 		// With the fix, the following line works:
 		expectingNoProblems();
-		
+
 		// Check whether generated sources were generated and compiled
 		expectingFile(proj, ".apt_generated/gen6/Generated03.java");
 		final String[] expectedClasses = { "targets.filer.Parent03", "gen6.Generated03" };
 		expectingUniqueCompiledClasses(expectedClasses);
-		
+
 		// Modify target file to and incrementally rebuild; file should be regenerated.
 		IdeTestUtils.copyResources(proj, "targets/filer03b", "src/targets/filer");
 		incrementalBuild();
@@ -172,7 +172,7 @@ public class FilerTests extends TestBase
 		expectingNoProblems();
 		expectingUniqueCompiledClasses(expectedClasses);
 	}
-	
+
 	/**
 	 * Call FilerTesterProc.testGetResource01(), which checks getResource() in SOURCE_OUTPUT location
 	 */
@@ -182,17 +182,17 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testGetResource01\", arg0 = \"g\", arg1 = \"Test.java\")" +
 				"public class Trigger {\n" +
 				"}"
-			); 
-		
+			);
+
 		AptConfig.setEnabled(jproj, true);
-		
+
 		// FilerTesterProc looks for the existence and contents of this class:
 		env.addClass(projPath.append(".apt_generated"), "g", "Test",
 				FilerTesterProc.resource01FileContents);
@@ -212,17 +212,17 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testGetResource02\", arg0 = \"t\", arg1 = \"Test.txt\")" +
 				"public class Trigger {\n" +
 				"}"
-			); 
-		
+			);
+
 		AptConfig.setEnabled(jproj, true);
-		
+
 		// FilerTesterProc looks for the existence and contents of this file after it is copied to the output folder:
 		IFolder textFileFolder = proj.getFolder("src/t");
 		textFileFolder.create(false, true, null);
@@ -245,23 +245,23 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testCreateNonSourceFile\", arg0 = \"t\", arg1 = \"Test.txt\")" +
 				"public class Trigger {\n" +
 				"}"
-			); 
-		
+			);
+
 		AptConfig.setEnabled(jproj, true);
-		
+
 		fullBuild();
 		expectingNoProblems();
 		assertTrue("Processor did not run", ProcessorTestStatus.processorRan());
 		assertEquals("Processor reported errors", ProcessorTestStatus.NO_ERRORS, ProcessorTestStatus.getErrors());
 	}
-	
+
 	/**
 	 * Call FilerTesterProc.testNullParents(), which checks handling of null originatingElements
 	 * in the Filer.createXxx() methods.
@@ -272,17 +272,17 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testNullParents\", arg0 = \"t\", arg1 = \"Test\")" +
 				"public class Trigger {\n" +
 				"}"
-		); 
-		
+		);
+
 		AptConfig.setEnabled(jproj, true);
-		
+
 		fullBuild();
 		expectingNoProblems();
 		assertTrue("Processor did not run", ProcessorTestStatus.processorRan());
@@ -293,7 +293,7 @@ public class FilerTests extends TestBase
 		final String[] expectedClasses = { "p.Trigger", "t.Test" };
 		expectingUniqueCompiledClasses(expectedClasses);
 	}
-	
+
 	/**
 	 * Call FilerTesterProc.testURI, which tests the FileObject.toUri() method on
 	 * various different sorts of files
@@ -304,15 +304,15 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testURI\", arg0 = \"t\", arg1 = \"Test.txt\")" +
 				"public class Trigger {\n" +
 				"}"
-			); 
-		
+			);
+
 		IFolder textFileFolder = proj.getFolder("src/t");
 		textFileFolder.create(false, true, null);
 		IFile textFile = proj.getFile(textFileFolder.getProjectRelativePath().append("Test.txt"));
@@ -320,7 +320,7 @@ public class FilerTests extends TestBase
 		textFile.create(textSource, false, null);
 
 		AptConfig.setEnabled(jproj, true);
-		
+
 		fullBuild();
 		expectingNoProblems();
 		assertTrue("Processor did not run", ProcessorTestStatus.processorRan());
@@ -328,7 +328,7 @@ public class FilerTests extends TestBase
 	}
 
 	/**
-	 * Call FilerTesterProc.testGetCharContentLarge(), which checks FileObject.getCharContent() 
+	 * Call FilerTesterProc.testGetCharContentLarge(), which checks FileObject.getCharContent()
 	 * for a large (multiple buffers long) file
 	 */
 	public void testGetCharContentLarge() throws Throwable {
@@ -337,17 +337,17 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testGetCharContentLarge\", arg0 = \"g\", arg1 = \"Test.java\")" +
 				"public class Trigger {\n" +
 				"}"
-			); 
-		
+			);
+
 		AptConfig.setEnabled(jproj, true);
-		
+
 		// FilerTesterProc looks for the existence and contents of this class:
 		env.addClass(projPath.append(".apt_generated"), "g", "Test",
 				FilerTesterProc.largeJavaClass());
@@ -364,16 +364,16 @@ public class FilerTests extends TestBase
 		disableJava5Factories(jproj);
 		IProject proj = jproj.getProject();
 		IPath projPath = proj.getFullPath();
-		
+
 		env.addClass(projPath.append("src"), "p", "Trigger",
 				"package p;\n" +
 				"import org.eclipse.jdt.apt.pluggable.tests.annotations.FilerTestTrigger;\n" +
 				"@FilerTestTrigger(test = \"testCreateClass1\", arg0 = \"p\", arg1 = \"Test.java\")" +
 				"public class Trigger {\n" +
 				"}"
-			); 
+			);
 		AptConfig.setEnabled(jproj, true);
-		
+
 		fullBuild();
 		final String[] expectedClasses = {"p.Trigger" };
 		expectingUniqueCompiledClasses(expectedClasses);
@@ -381,7 +381,7 @@ public class FilerTests extends TestBase
 		File file = new File(path.toOSString());
 		assertTrue("File should exist", file.exists());
 		long lastModified = file.lastModified();
-		Thread.sleep(500);
+		Thread.sleep(1000);
 		ClassFile[] classFiles = this.debugRequestor.getClassFiles();
 		FilerTesterProc.classContent = classFiles[0].getBytes();
 		env.addClass(projPath.append(".apt_generated"), "g", "Test",
@@ -393,7 +393,6 @@ public class FilerTests extends TestBase
 
 		incrementalBuild();
 		assertEquals("should have triggered 5 rounds", 5, FilerTesterProc.roundNo);
-		file = new File(path.toOSString());
 		assertTrue("File should exist", file.exists());
 		long lastModified2 = file.lastModified();
 		assertTrue("file should have been overwritten", (lastModified2 > lastModified));
