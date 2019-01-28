ee7324ba0a51032ce221abeebeb246b857bfaee7
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2019-01-28 11:38:59 +0530

    Bug 542564 - [12] [tests][regression] completion tests failing
    Change-Id: Ie2b7c0e4fc7ba3dab4cffa05175aff076f68cc60
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/JavadocTagConstants.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/JavadocTagConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -157,6 +161,8 @@ public interface JavadocTagConstants {
 		// since 10
 		{},
 		// since 11
+		{},
+		//since 12
 		{}
 	};
 	public static final char[][][] INLINE_TAGS = {
@@ -183,6 +189,8 @@ public interface JavadocTagConstants {
 		// since 10
 		{},
 		// since 11
+		{},
+		//since 12
 		{}
 	};
 	public final static int INLINE_TAGS_LENGTH = INLINE_TAGS.length;
