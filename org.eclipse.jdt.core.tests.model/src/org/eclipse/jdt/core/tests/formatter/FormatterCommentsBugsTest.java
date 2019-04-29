0af93e735d1c71697728854a488fb34b4c2d280b
Mateusz Matela <mateusz.matela@gmail.com>
2019-04-29 20:08:06 +0200

    Bug 545898 - [formatter] IOOBE with empty javadoc @param

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterCommentsBugsTest.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterCommentsBugsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7657,5 +7657,18 @@ public void testBug512095() {
 		"	}\n" + 
 		"}"
 	);
+}
+/**
+ * https://bugs.eclipse.org/545898 - [formatter] IOOBE with empty javadoc @param
+ */
+public void testBug545898() {
+	useOldCommentWidthCounting();
+	String source = 
+		"/**\n" + 
+		" * @param\n" + 
+		" */\n" + 
+		"void foo() {\n" + 
+		"}";
+	formatSource(source);
 }
 }
