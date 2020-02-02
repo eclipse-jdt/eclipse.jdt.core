15851c5303d40e521271c9b4816a4db511e67aa0
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2020-02-02 12:48:13 +0530

    Bug 559730 - [14] Records - search - Go to definition not working for records
    Change-Id: Ic55577956d498a049359c560086830c5b1244876
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/SelectionRequestor.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/SelectionRequestor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -199,7 +199,7 @@ public void acceptType(char[] packageName, char[] typeName, int modifiers, boole
 			acceptFlags = NameLookup.ACCEPT_INTERFACES;
 			break;
 		default:
-			acceptFlags = NameLookup.ACCEPT_CLASSES;
+			acceptFlags = NameLookup.ACCEPT_CLASSES | NameLookup.ACCEPT_RECORDS ;
 			break;
 	}
 	IType type = null;
