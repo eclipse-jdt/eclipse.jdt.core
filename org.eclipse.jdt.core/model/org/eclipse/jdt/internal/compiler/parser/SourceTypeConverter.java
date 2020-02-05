8a595fac9a42b11d666cf7886cfaf44e3e80eef4
Jay Arthanareeswaran <jarthana@in.ibm.com>
2020-02-05 13:46:53 +0530

    Bug 559774 - [14] [model] Records - Incorrect error message "The method a() is undefined for the type X
    Change-Id: I22fa410fe4ccc49d060a17a4d81413d5fa7e0d7e
    Signed-off-by: Jay Arthanareeswaran <jarthana@in.ibm.com>

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/compiler/parser/SourceTypeConverter.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/compiler/parser/SourceTypeConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -475,8 +479,13 @@ public class SourceTypeConverter extends TypeConverter {
 		SourceTypeElementInfo typeInfo = (SourceTypeElementInfo) typeHandle.getElementInfo();
 		if (typeInfo.isAnonymousMember())
 			throw new AnonymousMemberFound();
-		/* create type declaration - can be member type */
+		/* create type/record declaration - can be member type */
 		TypeDeclaration type = new TypeDeclaration(compilationResult);
+		if ((TypeDeclaration.kind(typeInfo.getModifiers()) == TypeDeclaration.RECORD_DECL)) {
+			// The first choice constructor that takes CompilationResult as arg is not setting all the fields
+			// Hence, use the one that does
+			type = new RecordDeclaration(type);
+		}
 		if (typeInfo.getEnclosingType() == null) {
 			if (typeHandle.isAnonymous()) {
 				type.name = CharOperation.NO_CHAR;
