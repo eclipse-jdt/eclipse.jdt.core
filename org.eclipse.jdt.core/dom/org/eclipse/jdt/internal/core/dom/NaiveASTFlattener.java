6aa4f7c2c85be51b177fd47246b5e4fb03a84039
Sarika Sinha <sarika.sinha@in.ibm.com>
2020-02-06 17:09:28 +0530

    Bug 558008 - [14] Record - DOM AST rewrite support
    Change-Id: Ibd3696352b985058d08f523091c3989e9f1e665f

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/internal/core/dom/NaiveASTFlattener.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/internal/core/dom/NaiveASTFlattener.java
@@ -1315,17 +1315,7 @@ public class NaiveASTFlattener extends ASTVisitor {
 		this.buffer.append("record ");//$NON-NLS-1$
 		node.getName().accept(this);
 		this.buffer.append(" ");//$NON-NLS-1$
-		if (!node.superInterfaceTypes().isEmpty()) {
-			this.buffer.append("implements ");//$NON-NLS-1$
-			for (Iterator it = node.superInterfaceTypes().iterator(); it.hasNext(); ) {
-				Type t = (Type) it.next();
-				t.accept(this);
-				if (it.hasNext()) {
-					this.buffer.append(", ");//$NON-NLS-1$
-				}
-			}
-			this.buffer.append(" ");//$NON-NLS-1$
-		}
+		
 		if (!node.typeParameters().isEmpty()) {
 			this.buffer.append("<");//$NON-NLS-1$
 			for (Iterator it = node.typeParameters().iterator(); it.hasNext(); ) {
@@ -1337,7 +1327,7 @@ public class NaiveASTFlattener extends ASTVisitor {
 			}
 			this.buffer.append(">");//$NON-NLS-1$
 		}
-		
+		this.buffer.append(" ");//$NON-NLS-1$
 		this.buffer.append("(");//$NON-NLS-1$
 		for (Iterator it = node.recordComponents().iterator(); it.hasNext(); ) {
 			SingleVariableDeclaration v = (SingleVariableDeclaration) it.next();
@@ -1347,7 +1337,17 @@ public class NaiveASTFlattener extends ASTVisitor {
 			}
 		}
 		this.buffer.append(")");//$NON-NLS-1$
-		
+		if (!node.superInterfaceTypes().isEmpty()) {
+			this.buffer.append(" implements ");//$NON-NLS-1$
+			for (Iterator it = node.superInterfaceTypes().iterator(); it.hasNext(); ) {
+				Type t = (Type) it.next();
+				t.accept(this);
+				if (it.hasNext()) {
+					this.buffer.append(", ");//$NON-NLS-1$
+				}
+			}
+			this.buffer.append(" ");//$NON-NLS-1$
+		}
 		this.buffer.append("{");//$NON-NLS-1$
 		if (!node.bodyDeclarations().isEmpty()) {
 			this.buffer.append("\n");//$NON-NLS-1$
