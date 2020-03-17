3f9675b9ac3cc321bd33f36f64b997cd2743f750
Jay Arthanareeswaran <jarthana@in.ibm.com>
2020-03-17 09:31:10 +0530

    Bug 551496 - [14] Remove JCP disclaimers from code, UI and marketplace
    Change-Id: I83d9bb822140edf0e641eb61f759fb1ee7e502f2
    Signed-off-by: Jay Arthanareeswaran <jarthana@in.ibm.com>

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/RepeatableAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/RepeatableAnnotationTest.java
@@ -8,10 +8,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * This is an implementation of an early-draft specification developed under the Java
- * Community Process (JCP) and is made available for testing and evaluation purposes
- * only. The code is not compatible with any specification of the JCP.
- *
  * Contributors:
  *     Jesper S Moller - initial API and implementation
  *     					Bug 412151 - [1.8][compiler] Check repeating annotation's collection type
