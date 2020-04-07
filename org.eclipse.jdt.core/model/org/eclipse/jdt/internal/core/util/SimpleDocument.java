56c5d112530094c1f1d5b271a150441609fdc1cd
Eric Milles <eric.milles@thomsonreuters.com>
2020-04-07 12:37:43 -0500

    Bug 560470 - Implement getChar(int)
    Change-Id: Ic61743e0d5d7587d49c1795c18f19af8417ea161
    Signed-off-by: Eric Milles <eric.milles@thomsonreuters.com>

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/SimpleDocument.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/util/SimpleDocument.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -36,7 +36,7 @@ public class SimpleDocument implements IDocument {
 
 	@Override
 	public char getChar(int offset) {
-		return 0;
+		return this.buffer.charAt(offset);
 	}
 
 	@Override
