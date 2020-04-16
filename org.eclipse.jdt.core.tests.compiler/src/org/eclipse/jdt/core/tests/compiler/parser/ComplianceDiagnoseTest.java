2879704212fa36dd959c9c467a2ea65bd9f6ed3c
Manoj Palat <manpalat@in.ibm.com>
2020-04-16 10:47:49 +0530

    Bug 552769 - [14] ComplianceDiagnoseTest.test0042 and Compliance_1_{4, 5}.test079 failures
    Change-Id: I283d1e0a180bbd66c6678c96bc8d9f311f811169

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/parser/ComplianceDiagnoseTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/parser/ComplianceDiagnoseTest.java
@@ -1938,7 +1938,7 @@ public void test0041() {
 	);
 }
 //TODO:  Enable after Bug 552769  is fixed
-public void _test0042() {
+public void test0042() {
 	String[] testFiles = new String[] {
 		"X.java",
 		"void ___eval() {\n" +
@@ -2035,28 +2035,23 @@ public void _test0042() {
 			"Syntax error, insert \"EnumBody\" to complete CompilationUnit\n" +
 			"----------\n";
 
-	String expected_Java14_ProblemLog =
+	String expectedJ14ProblemLog =
 			"----------\n" +
 			"1. ERROR in X.java (at line 1)\n" +
 			"	void ___eval() {\n" +
 			"	^^^^\n" +
 			"Syntax error on token \"void\", record expected\n" +
 			"----------\n" +
-			"2. ERROR in X.java (at line 1)\n" +
-			"	void ___eval() {\n" +
-			"	             ^\n" +
-			"Syntax error on token \")\", { expected after this token\n" +
-			"----------\n" +
-			"3. ERROR in X.java (at line 23)\n" +
-			"	}\n" +
-			"	^\n" +
-			"Syntax error, insert \"}\" to complete RecordBody\n" +
+			"2. ERROR in X.java (at line 2)\n" +
+			"	new Runnable() {\n" +
+			"	^^^\n" +
+			"Syntax error on token \"new\", record expected\n" +
 			"----------\n";
 	runComplianceParserTest(
 		testFiles,
-		expected_Java14_ProblemLog,
-		expected_Java14_ProblemLog,
-		expected_Java14_ProblemLog
+		expected13ProblemLog,
+		expected14ProblemLog,
+		(this.complianceLevel < ClassFileConstants.JDK14 ? expected15ProblemLog : expectedJ14ProblemLog)
 	);
 }
 /*
