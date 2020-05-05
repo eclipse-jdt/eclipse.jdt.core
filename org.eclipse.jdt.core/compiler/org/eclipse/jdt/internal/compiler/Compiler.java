261283cb409f0bedd24b6e6d1b920375f2d487b0
Andrey Loskutov <loskutov@gmx.de>
2020-05-05 23:48:31 +0200

    Bug 562853 - Compiler build stats are wrong
    Collect overall time to avoid none sense data printed with
    debug/builder/stats flag if number of compiled units exceeds 2000
    (default in AbstractImageBuilder.MAX_AT_ONCE).
    
    Change-Id: I3296826fe6a65cde773c2579aea50fb4a31c30eb
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/Compiler.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/Compiler.java
@@ -648,6 +648,7 @@ public class Compiler implements ITypeRequestor, ProblemSeverities {
 			reset();
 			this.annotationProcessorStartIndex  = 0;
 			this.stats.endTime = System.currentTimeMillis();
+			this.stats.overallTime += this.stats.endTime - this.stats.startTime;
 		}
 	}
 
