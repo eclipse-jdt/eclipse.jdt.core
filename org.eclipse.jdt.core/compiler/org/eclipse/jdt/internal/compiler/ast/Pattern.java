5e7c7366fe1020b79db12887f9c1893a1f058aae
Manoj Palat <manpalat@in.ibm.com>
2021-04-22 18:03:58 +0530

    Bug 572992 - [compiler][grammar] Transform Instanceof Grammar to incorporate "Pattern"
    Change-Id: I261c973f2de1d711419026151a2b22b993b00ec1
    Signed-off-by: Manoj Palat <manpalat@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/jdt/eclipse.jdt.core/+/179672

---
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/ast/Pattern.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.compiler.ast;
+
+public abstract class Pattern extends ASTNode {
+
+	public enum PatternKind {
+		TYPE_PATTERN,
+		NOT_A_PATTERN
+	}
+
+	public PatternKind kind() {
+		return PatternKind.NOT_A_PATTERN;
+	}
+}
--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/ParserBasicInformation.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/parser/ParserBasicInformation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -25,17 +29,17 @@ public interface ParserBasicInformation {
 					NUM_STATES = 1205,
 
 					NT_OFFSET = 135,
-					SCOPE_UBOUND = 313,
-					SCOPE_SIZE = 314,
-					LA_STATE_OFFSET = 17578,
+					SCOPE_UBOUND = 315,
+					SCOPE_SIZE = 316,
+					LA_STATE_OFFSET = 17572,
 					MAX_LA = 1,
-					NUM_RULES = 918,
+					NUM_RULES = 920,
 					NUM_TERMINALS = 135,
-					NUM_NON_TERMINALS = 423,
-					NUM_SYMBOLS = 558,
-					START_STATE = 991,
+					NUM_NON_TERMINALS = 425,
+					NUM_SYMBOLS = 560,
+					START_STATE = 1066,
 					EOFT_SYMBOL = 64,
 					EOLT_SYMBOL = 64,
-					ACCEPT_ACTION = 17577,
-					ERROR_ACTION = 17578;
+					ACCEPT_ACTION = 17571,
+					ERROR_ACTION = 17572;
 }
