77abb30e8b321c127f4e33834841c8290d7225fd
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2021-06-09 18:26:22 +0530

    Bug 574088 - Comparator errors in I20210608-2130
    Change-Id: I0295592b7d2c5654c3414cc992abe8527a5873c8
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/jdt/eclipse.jdt.core/+/181697
    Tested-by: Jay Arthanareeswaran <jarthana@in.ibm.com>
    Reviewed-by: Jay Arthanareeswaran <jarthana@in.ibm.com>

--- org.eclipse.jdt.core.tests.builder/src/org/eclipse/jdt/core/tests/builder/OutputFolderTests.java
+++ org.eclipse.jdt.core.tests.builder/src/org/eclipse/jdt/core/tests/builder/OutputFolderTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -119,7 +119,7 @@ public class OutputFolderTests extends BuilderTests {
 
 		// simulate exit/restart
 		JavaModelManager manager = JavaModelManager.getJavaModelManager();
-		JavaProject project = (JavaProject)manager.getJavaModel().getJavaProject("P"); //$NON-NLS-1$
+		JavaProject project = manager.getJavaModel().getJavaProject("P"); //$NON-NLS-1$
 		manager.removePerProjectInfo(project, true /* remove external jar files indexes and timestamps*/);
 
 		// change cu and build
