77d057a9a4148c530545ffcdb36c44e0f21b39f8
Gayan Perera <gayanper@gmail.com>
2022-06-05 20:34:51 +0200

    Fix completions for lambda variables under nested if blocks. Fixes #109
    The fix adds a additional guard when trying to build the enclosing
    context by collecting if conditions which supposed to have instanceof
    previously. But not having the guard condition to only add the if
    statement in presence of instanceof or completion node been part of if
    statement cause the current issue. The fix add those guard conditions.

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests18.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests18.java
@@ -6524,4 +6524,81 @@ public void testBug578817() throws JavaModelException {
 				+ "getClass[METHOD_REF]{getClass(), Ljava.lang.Object;, ()Ljava.lang.Class<*>;, null, null, getClass, null, [229, 232], "+(R_DEFAULT+R_PACKAGE_EXPECTED_TYPE+30)+"}\n"
 				+ "getLastName[METHOD_REF]{getLastName(), LPerson;, ()Ljava.lang.String;, null, null, getLastName, null, [229, 232], "+(R_DEFAULT+R_EXACT_EXPECTED_TYPE+30)+"}", requestor.getResults());
 	}
+public void testGH109_expectCompletions_insideLambdaNestedBlocks() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"Completion/src/GH109.java",
+			"import java.util.stream.Stream;\n"
+			+ "\n"
+			+ "public class GH109\n"
+			+ "{\n"
+			+ "  public static void main(String[] args)\n"
+			+ "  {\n"
+			+ "    if(args.length > 0) {\n"
+			+ "      Stream.of(args).forEach(name -> {\n"
+			+ "        try\n"
+			+ "        {\n"
+			+ "          if (name.startsWith(\"A\"))\n"
+			+ "          {\n"
+			+ "            name.\n"
+			+ "          }\n"
+			+ "          \n"
+			+ "        }\n"
+			+ "        catch (Exception e)\n"
+			+ "        {\n"
+			+ "        }\n"
+			+ "      });\n"
+			+ "    }\n"
+			+ "  }\n"
+			+ "}");
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "  name.";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	String result = requestor.getResults();
+	assertTrue(result.contains("startsWith[METHOD_REF]{startsWith(), Ljava.lang.String;, (Ljava.lang.String;)Z, startsWith, (arg0), 60}\n"
+			+ "startsWith[METHOD_REF]{startsWith(), Ljava.lang.String;, (Ljava.lang.String;I)Z, startsWith, (arg0, arg1), 60}\n"));
+}
+
+public void testGH109_expectCompletionsWithCast_insideLambdaNestedBlocksWithInstanceOf() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"Completion/src/GH109.java",
+			"import java.util.stream.Stream;\n"
+			+ "\n"
+			+ "public class GH109\n"
+			+ "{\n"
+			+ "  public static void main(String[] args)\n"
+			+ "  {\n"
+			+ "    if(args.length > 0) {\n"
+			+ "      Stream.of(args).map(Object.class::cast).forEach(name -> {\n"
+			+ "        try\n"
+			+ "        {\n"
+			+ "          if (name instanceof String)\n"
+			+ "          {\n"
+			+ "            name.sta\n"
+			+ "          }\n"
+			+ "          \n"
+			+ "        }\n"
+			+ "        catch (Exception e)\n"
+			+ "        {\n"
+			+ "        }\n"
+			+ "      });\n"
+			+ "    }\n"
+			+ "  }\n"
+			+ "}");
+
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "  name.sta";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	String result = requestor.getResults();
+	assertTrue(result.contains("startsWith[METHOD_REF_WITH_CASTED_RECEIVER]{((String)name).startsWith(), Ljava.lang.String;, (Ljava.lang.String;)Z, Ljava.lang.String;, startsWith, (arg0), 60}\n"
+			+ "startsWith[METHOD_REF_WITH_CASTED_RECEIVER]{((String)name).startsWith(), Ljava.lang.String;, (Ljava.lang.String;I)Z, Ljava.lang.String;, startsWith, (arg0, arg1), 60}"));
+}
 }
