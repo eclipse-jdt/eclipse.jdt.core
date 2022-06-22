0ff093885bc368fb34bdc246bb41f2962e96c8b6
Jay Arthanareeswaran <jarthana@in.ibm.com>
2022-06-22 17:04:11 +0530

    [19] Class Cast exception with Record pattern example in editor #157 (#159)
    AreRecordComponentsComplete bit should be saved in extendedTagBits for records and not tagBits.

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/BinaryTypeBinding.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/BinaryTypeBinding.java
@@ -8,6 +8,10 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Stephan Herrmann - Contributions for
@@ -1290,7 +1294,7 @@ public RecordComponentBinding[] components() {
 	for (int i = this.components.length; --i >= 0;) {
 		resolveTypeFor(this.components[i]);
 	}
-	this.tagBits |= ExtendedTagBits.AreRecordComponentsComplete;
+	this.extendedTagBits |= ExtendedTagBits.AreRecordComponentsComplete;
 	return this.components;
 }
 // NOTE: the type of each field of a binary type is resolved when needed
