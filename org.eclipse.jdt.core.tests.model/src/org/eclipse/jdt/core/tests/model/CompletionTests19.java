43b7673386e062989bb9227fe64b10374ef1ac57
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2022-07-08 12:13:16 +0530

    Add more test cases
    Change-Id: Ic5a109c712ae0825e953a785c085d08913034e3a
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests19.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests19.java
@@ -47,7 +47,7 @@ public class CompletionTests19 extends AbstractJavaModelCompletionTests {
 	public static Test suite() {
 		return buildModelTestSuite(CompletionTests19.class);
 	}
-	//content assist of a java lang class in case statement in switch pattern
+	//content assist just record pattern usage
 	public void test001() throws JavaModelException {
 		this.workingCopies = new ICompilationUnit[1];
 		this.workingCopies[0] = getWorkingCopy(
@@ -84,4 +84,251 @@ public class CompletionTests19 extends AbstractJavaModelCompletionTests {
 				requestor.getResults());
 
 	}
+	//content assist in record pattern switch case just after case
+	public void test002() throws JavaModelException {
+		this.workingCopies = new ICompilationUnit[1];
+		this.workingCopies[0] = getWorkingCopy(
+				"/Completion/src/X.java",
+				"@SuppressWarnings(\"preview\")"
+				+ "public class X {\n"
+				+ "  public static void printLowerRight(Rectangle r) {\n"
+				+ "    int res = switch(r) {\n"
+				+ "       case /*here*/ Rectangl (ColoredPoint(Point(int x, int y), Color c),\n"
+				+ "                               ColoredPoint lr) r1  -> {\n"
+				+ "        		yield 1;  \n"
+				+ "        } \n"
+				+ "        default -> 0;\n"
+				+ "    }; \n"
+				+ "  }\n"
+				+ "  public static void main(String[] args) {\n"
+				+ "    printLowerRight(new Rectangle(new ColoredPoint(new Point(15, 5), Color.BLUE), \n"
+				+ "        new ColoredPoint(new Point(30, 10), Color.RED)));\n"
+				+ "  }\n"
+				+ "}\n"
+				+ "record Point(int x, int y) {}\n"
+				+ "enum Color { RED, GREEN, BLUE }\n"
+				+ "record ColoredPoint(Point p, Color c) {}\n"
+				+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+				);
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+		requestor.allowAllRequiredProposals();
+		String str = this.workingCopies[0].getSource();
+		String completeBehind = "/*here*/ Rectangl";
+		int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+		this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+		assertResults("Rectangle[TYPE_REF]{Rectangle, , LRectangle;, null, null, 52}",
+				requestor.getResults());
+
+	}
+	//content assist in record pattern switch case - 1st level nested
+	public void test003() throws JavaModelException {
+		this.workingCopies = new ICompilationUnit[1];
+		this.workingCopies[0] = getWorkingCopy(
+				"/Completion/src/X.java",
+				"@SuppressWarnings(\"preview\")"
+				+ "public class X {\n"
+				+ "  public static void printLowerRight(Rectangle r) {\n"
+				+ "    int res = switch(r) {\n"
+				+ "       case  Rectangle(/*here*/ ColoredPoin(Point(int x, int y), Color c),\n"
+				+ "                               ColoredPoint lr) r1  -> {\n"
+				+ "        		yield 1;  \n"
+				+ "        } \n"
+				+ "        default -> 0;\n"
+				+ "    }; \n"
+				+ "  }\n"
+				+ "  public static void main(String[] args) {\n"
+				+ "    printLowerRight(new Rectangle(new ColoredPoint(new Point(15, 5), Color.BLUE), \n"
+				+ "        new ColoredPoint(new Point(30, 10), Color.RED)));\n"
+				+ "  }\n"
+				+ "}\n"
+				+ "record Point(int x, int y) {}\n"
+				+ "enum Color { RED, GREEN, BLUE }\n"
+				+ "record ColoredPoint(Point p, Color c) {}\n"
+				+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+				);
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+		requestor.allowAllRequiredProposals();
+		String str = this.workingCopies[0].getSource();
+		String completeBehind = "/*here*/ ColoredPoin";
+		int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+		this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+		assertResults("ColoredPoint[TYPE_REF]{ColoredPoint, , LColoredPoint;, null, null, 52}",
+				requestor.getResults());
+
+	}
+	//content assist in record pattern switch case - 2nd level nested
+	public void test004() throws JavaModelException {
+		this.workingCopies = new ICompilationUnit[1];
+		this.workingCopies[0] = getWorkingCopy(
+				"/Completion/src/X.java",
+				"@SuppressWarnings(\"preview\")"
+				+ "public class X {\n"
+				+ "  public static void printLowerRight(Rectangle r) {\n"
+				+ "    int res = switch(r) {\n"
+				+ "       case  Rectangle(ColoredPoint(/*here*/ Poin(int x, int y), Color c),\n"
+				+ "                               ColoredPoint lr) r1  -> {\n"
+				+ "        		yield 1;  \n"
+				+ "        } \n"
+				+ "        default -> 0;\n"
+				+ "    }; \n"
+				+ "  }\n"
+				+ "  public static void main(String[] args) {\n"
+				+ "    printLowerRight(new Rectangle(new ColoredPoint(new Point(15, 5), Color.BLUE), \n"
+				+ "        new ColoredPoint(new Point(30, 10), Color.RED)));\n"
+				+ "  }\n"
+				+ "}\n"
+				+ "record Point(int x, int y) {}\n"
+				+ "enum Color { RED, GREEN, BLUE }\n"
+				+ "record ColoredPoint(Point p, Color c) {}\n"
+				+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+				);
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+		requestor.allowAllRequiredProposals();
+		String str = this.workingCopies[0].getSource();
+		String completeBehind = "/*here*/ Poin";
+		int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+		this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+		assertResults("Point[TYPE_REF]{Point, , LPoint;, null, null, 52}",
+				requestor.getResults());
+
+	}
+	//content assist in record pattern switch case - 2nd param in nested
+	public void test005() throws JavaModelException {
+		this.workingCopies = new ICompilationUnit[1];
+		this.workingCopies[0] = getWorkingCopy(
+				"/Completion/src/X.java",
+				"@SuppressWarnings(\"preview\")"
+				+ "public class X {\n"
+				+ "  public static void printLowerRight(Rectangle r) {\n"
+				+ "    int res = switch(r) {\n"
+				+ "       case  Rectangle(ColoredPoint( Point(int x, int y), /*here*/ Colo c),\n"
+				+ "                               ColoredPoint lr) r1  -> {\n"
+				+ "        		yield 1;  \n"
+				+ "        } \n"
+				+ "        default -> 0;\n"
+				+ "    }; \n"
+				+ "  }\n"
+				+ "  public static void main(String[] args) {\n"
+				+ "    printLowerRight(new Rectangle(new ColoredPoint(new Point(15, 5), Color.BLUE), \n"
+				+ "        new ColoredPoint(new Point(30, 10), Color.RED)));\n"
+				+ "  }\n"
+				+ "}\n"
+				+ "record Point(int x, int y) {}\n"
+				+ "enum Color { RED, GREEN, BLUE }\n"
+				+ "record ColoredPoint(Point p, Color c) {}\n"
+				+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+				);
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+		requestor.allowAllRequiredProposals();
+		String str = this.workingCopies[0].getSource();
+		String completeBehind = "/*here*/ Colo";
+		int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+		this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+		assertResults("Color[TYPE_REF]{Color, , LColor;, null, null, 52}\n"
+				+ "ColoredPoint[TYPE_REF]{ColoredPoint, , LColoredPoint;, null, null, 52}",
+				requestor.getResults());
+
+	}
+	//content assist in record pattern switch case - use the variable in case statement
+	public void test006() throws JavaModelException {
+		this.workingCopies = new ICompilationUnit[1];
+		this.workingCopies[0] = getWorkingCopy(
+				"/Completion/src/X.java",
+				"@SuppressWarnings(\"preview\")"
+				+ "public class X {\n"
+				+ "  public static void printLowerRight(Rectangle r) {\n"
+				+ "    int res = switch(r) {\n"
+				+ "       case  Rectangle(ColoredPoint( Point(int x, int y),  Color c),\n"
+				+ "                               ColoredPoint lr) r1  -> {\n"
+				+ "        		r1.toStrin ;yield 1;  \n"
+				+ "        } \n"
+				+ "        default -> 0;\n"
+				+ "    }; \n"
+				+ "  }\n"
+				+ "  public static void main(String[] args) {\n"
+				+ "    printLowerRight(new Rectangle(new ColoredPoint(new Point(15, 5), Color.BLUE), \n"
+				+ "        new ColoredPoint(new Point(30, 10), Color.RED)));\n"
+				+ "  }\n"
+				+ "}\n"
+				+ "record Point(int x, int y) {}\n"
+				+ "enum Color { RED, GREEN, BLUE }\n"
+				+ "record ColoredPoint(Point p, Color c) {}\n"
+				+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+				);
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+		requestor.allowAllRequiredProposals();
+		String str = this.workingCopies[0].getSource();
+		String completeBehind = "r1.toStrin";
+		int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+		this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+		assertResults("toString[METHOD_REF]{toString(), LRectangle;, ()Ljava.lang.String;, toString, null, 60}",
+				requestor.getResults());
+
+	}
+	//content assist in record pattern -instanceof record pattern - 2nd param in nested
+	public void test007() throws JavaModelException {
+		this.workingCopies = new ICompilationUnit[1];
+		this.workingCopies[0] = getWorkingCopy(
+				"/Completion/src/X.java",
+				"@SuppressWarnings(\"preview\")"
+				+ "public class X {\n"
+				+ "  static void print(Rectangle r) {\n"
+				+ "    if (r instanceof (Rectangle(ColoredPoint(Point(int x, int y), Color c),\n"
+				+ "                              /*here*/ ColoredPoin lr) r1)) {\n"
+				+ "        System.out.println(\"Upper-left corner: \" + r1);\n"
+				+ "    }\n"
+				+ "  }\n"
+				+ "  public static void main(String[] obj) {\n"
+				+ "    print(new Rectangle(new ColoredPoint(new Point(0, 0), Color.BLUE), \n"
+				+ "                               new ColoredPoint(new Point(10, 15), Color.RED)));\n"
+				+ "  }\n"
+				+ "}\n"
+				+ "record Point(int x, int y) {}\n"
+				+ "enum Color { RED, GREEN, BLUE }\n"
+				+ "record ColoredPoint(Point p, Color c) {}\n"
+				+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+				);
+		CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+		requestor.allowAllRequiredProposals();
+		String str = this.workingCopies[0].getSource();
+		String completeBehind = "/*here*/ ColoredPoin";
+		int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+		this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+		assertResults("ColoredPoint[TYPE_REF]{ColoredPoint, , LColoredPoint;, null, null, 52}",
+				requestor.getResults());
+
+	}
+	//content assist in record pattern -instanceof record pattern - record creation
+		public void test008() throws JavaModelException {
+			this.workingCopies = new ICompilationUnit[1];
+			this.workingCopies[0] = getWorkingCopy(
+					"/Completion/src/X.java",
+					"@SuppressWarnings(\"preview\")"
+					+ "public class X {\n"
+					+ "  static void print(Rectangle r) {\n"
+					+ "    if (r instanceof (Rectangle(ColoredPoint(Point(int x, int y), Color c),\n"
+					+ "                              /*here*/ ColoredPoin lr) r1)) {\n"
+					+ "        System.out.println(\"Upper-left corner: \" + r1);\n"
+					+ "    }\n"
+					+ "  }\n"
+					+ "  public static void main(String[] obj) {\n"
+					+ "    print(new Rectangle(new /*here*/ ColoredPoin(new Point(0, 0), Color.BLUE), \n"
+					+ "                               new ColoredPoint(new Point(10, 15), Color.RED)));\n"
+					+ "  }\n"
+					+ "}\n"
+					+ "record Point(int x, int y) {}\n"
+					+ "enum Color { RED, GREEN, BLUE }\n"
+					+ "record ColoredPoint(Point p, Color c) {}\n"
+					+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+					);
+			CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+			requestor.allowAllRequiredProposals();
+			String str = this.workingCopies[0].getSource();
+			String completeBehind = "/*here*/ ColoredPoin";
+			int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+			this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+			assertResults("ColoredPoint[TYPE_REF]{ColoredPoint, , LColoredPoint;, null, null, 52}",
+					requestor.getResults());
+
+		}
 }
