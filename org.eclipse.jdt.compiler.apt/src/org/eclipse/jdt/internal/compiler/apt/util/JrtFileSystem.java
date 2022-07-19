4352fac61f3e0805ef7666e00c28f348cac0b62a
Andrey Loskutov <loskutov@gmx.de>
2022-07-19 09:18:24 +0200

    Remove unused import (#216)
    Fixes compilation warning
    
    See https://github.com/eclipse-jdt/eclipse.jdt.core/issues/216

--- org.eclipse.jdt.compiler.apt/src/org/eclipse/jdt/internal/compiler/apt/util/JrtFileSystem.java
+++ org.eclipse.jdt.compiler.apt/src/org/eclipse/jdt/internal/compiler/apt/util/JrtFileSystem.java
@@ -29,7 +29,6 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
