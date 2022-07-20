62385260f2e64583b3d38b0512d0417c11453ab9
Jay Arthanareeswaran <jarthana@in.ibm.com>
2022-07-20 13:38:59 +0530

    Fix API errors post version increment in master

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Modifier.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Modifier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -112,7 +112,8 @@ public final class Modifier extends ASTNode implements IExtendedModifier {
 		 */
 		public static final ModifierKeyword SEALED_KEYWORD = new ModifierKeyword("sealed", SEALED);//$NON-NLS-1$
 		/**
-		 * @since 3.30
+		 * @since 3.31
+		 * @noreference preview feature
 		 */
 		public static final ModifierKeyword WHEN_KEYWORD = new ModifierKeyword("when", WHEN);//$NON-NLS-1$
 		/**
@@ -344,7 +345,8 @@ public final class Modifier extends ASTNode implements IExtendedModifier {
 	/**
 	 * "when" modifier constant (bit mask).
 	 * Applicable only to types.
-	 * @since 3.30
+	 * @since 3.31
+	 * @noreference preview feature
 	 */
 	public static final int WHEN = 0x2000;
 
