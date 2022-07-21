7008c98b8b39d3e5f385c8c2f617725f72632a52
Christopher Gerking <christopher.gerking@kit.edu>
2022-07-21 22:42:47 +0200

    Add test class (#190)
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Christopher Gerking <christopher.gerking@kit.edu>

---
+++ org.eclipse.jdt.core.tests.model/workspace/JavaSearchIssue190/src/org/eclipse/jdt/core/tests/model/issue190/TestClass.java
@@ -0,0 +1,10 @@
+package org.eclipse.jdt.core.tests.model.issue190;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+public class TestClass {
+	
+	public Nullable getNullable() {
+		return null;
+	}
+}
