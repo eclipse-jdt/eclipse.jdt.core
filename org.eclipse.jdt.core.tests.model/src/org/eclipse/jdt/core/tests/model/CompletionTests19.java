8183d06e509e3a8e1179f4917721a04f58f347b5
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2022-09-10 19:04:09 +0530

    adding test case
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests19.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests19.java
@@ -427,4 +427,43 @@ public class CompletionTests19 extends AbstractJavaModelCompletionTests {
 					requestor.getResults());
 
 		}
+		//content assist ArrayStoreException fix
+		//https://github.com/eclipse-jdt/eclipse.jdt.core/issues/345
+		public void test011() throws JavaModelException {
+			this.workingCopies = new ICompilationUnit[1];
+			this.workingCopies[0] = getWorkingCopy(
+					"/Completion/src/X.java",
+					"@SuppressWarnings(\"preview\")"
+					+ "public class X {\n"
+					+ "  public static void printLowerRight(Rectangle r) {\n"
+					+ "    int res = switch(r) {\n"
+					+ "       case Rectangle(ColoredPoint(Point(int x, int y), Color c),\n"
+					+ "                               ColoredPoint lr) r1  -> {\n"
+					+ "                               fals "
+					+ "        		yield 1;  \n"
+					+ "        } \n"
+					+ "        default -> 0;\n"
+					+ "    }; \n"
+					+ "     \n"
+					+ "  }\n"
+					+ "  public static void main(String[] args) {\n"
+					+ "    printLowerRight(new Rectangle(new ColoredPoint(new Point(15, 5), Color.BLUE), \n"
+					+ "        new ColoredPoint(new Point(30, 10), Color.RED)));\n"
+					+ "  }\n"
+					+ "}\n"
+					+ "record Point(int x, int y) {}\n"
+					+ "enum Color { RED, GREEN, BLUE }\n"
+					+ "record ColoredPoint(Point p, Color c) {}\n"
+					+ "record Rectangle(ColoredPoint upperLeft, ColoredPoint lowerRight) {}"
+					);
+			CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+			requestor.allowAllRequiredProposals();
+			String str = this.workingCopies[0].getSource();
+			String completeBehind = "fals";
+			int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+			this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+			assertResults("false[KEYWORD]{false, null, null, false, null, 52}",
+					requestor.getResults());
+
+		}
 }
