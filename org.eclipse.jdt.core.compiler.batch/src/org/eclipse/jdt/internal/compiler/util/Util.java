3c432e3aef19431ed1892a7855b246a9e2262f28
Jay Arthanareeswaran <jarthana@in.ibm.com>
2023-02-07 09:10:56 +0530

    Bug 578073 - Ensure FileSystem is closed

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/Util.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/Util.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -642,7 +642,7 @@ public class Util implements SuffixConstants {
 		java.nio.file.FileSystem fs = FileSystems.newFileSystem(zipfile, null);
 		Path path = fs.getPath(fileName);
 		InputStream stream = null;
-		try {
+		try(fs) {
 			if (!Files.exists(path)) {
 				throw new IOException("Invalid zip entry name : " + zipName); //$NON-NLS-1$
 			}
