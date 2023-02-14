18dd5b41947d77d1f8540ac14d056a19245e28b5
Jay Arthanareeswaran <jarthana@in.ibm.com>
2023-02-14 10:40:40 +0530

    [20] Implement type inference for Record patterns #737
    Do not reject raw types in record patterns.

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/InstanceOfExpression.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/InstanceOfExpression.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2022 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -56,6 +60,7 @@ public InstanceOfExpression(Expression expression, Pattern pattern) {
 	this.pattern = pattern;
 	this.elementVariable = pattern.getPatternVariable();
 	this.type = pattern.getType();
+	this.type.bits |= IgnoreRawTypeCheck;
 	this.bits |= INSTANCEOF << OperatorSHIFT;
 	this.sourceStart = expression.sourceStart;
 	this.sourceEnd = this.pattern.sourceEnd;
--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/RecordPatternTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/RecordPatternTest.java
@@ -1624,7 +1624,7 @@ public class RecordPatternTest extends AbstractRegressionTest9 {
 				"----------\n");
 	}
 	public void test48() {
-		runNegativeTest(new String[] {
+		runConformTest(new String[] {
 			"X.java",
 				"  @SuppressWarnings(\"preview\")\n"
 				+ "public class X {\n"
@@ -1634,44 +1634,20 @@ public class RecordPatternTest extends AbstractRegressionTest9 {
 				+ "	}\n"
 				+ "	static void erroneousTest1(Box<Object> bo) {\n"
 				+ "		if (bo instanceof Box(var s)) {\n"
-				+ "			System.out.println(\"I'm a box\");\n"
+				+ "			System.out.println(\"I'm a box of \" + s.getClass().getName());\n"
 				+ "		}\n"
 				+ "	}\n"
 				+ "	static void erroneousTest2(Box b) {\n"
 				+ "		if (b instanceof Box(var t)) {\n"
-				+ "			System.out.println(\"I'm a box\");\n"
+				+ "			System.out.println(\"I'm a box of \" + t.getClass().getName());\n"
 				+ "		}\n"
 				+ "	}\n"
 				+ "	record Box<T> (T t) {\n"
 				+ "	}\n"
 				+ "}"
 			},
-				"----------\n" +
-				"1. WARNING in X.java (at line 8)\n" +
-				"	if (bo instanceof Box(var s)) {\n" +
-				"	                  ^^^\n" +
-				"X.Box is a raw type. References to generic type X.Box<T> should be parameterized\n" +
-				"----------\n" +
-				"2. ERROR in X.java (at line 8)\n" +
-				"	if (bo instanceof Box(var s)) {\n" +
-				"	                  ^^^\n" +
-				"Raw types are not allowed in record patterns\n" +
-				"----------\n" +
-				"3. WARNING in X.java (at line 12)\n" +
-				"	static void erroneousTest2(Box b) {\n" +
-				"	                           ^^^\n" +
-				"X.Box is a raw type. References to generic type X.Box<T> should be parameterized\n" +
-				"----------\n" +
-				"4. WARNING in X.java (at line 13)\n" +
-				"	if (b instanceof Box(var t)) {\n" +
-				"	                 ^^^\n" +
-				"X.Box is a raw type. References to generic type X.Box<T> should be parameterized\n" +
-				"----------\n" +
-				"5. ERROR in X.java (at line 13)\n" +
-				"	if (b instanceof Box(var t)) {\n" +
-				"	                 ^^^\n" +
-				"Raw types are not allowed in record patterns\n" +
-				"----------\n");
+				"I\'m a box of java.lang.String\n" +
+				"I\'m a box of java.lang.String");
 	}
 	public void testIssue690_1() {
 		runNegativeTest(new String[] {
