a593218cc2ad33cda13831cb1f8790dde13feb10
Manoj N Palat <manoj.palat@in.ibm.com>
2023-02-24 12:53:21 +0530

    Disabling and creating issues for change in failed tests

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/rewrite/describing/ASTRewritingRecordPatternTest.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/rewrite/describing/ASTRewritingRecordPatternTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -157,7 +161,9 @@ public class ASTRewritingRecordPatternTest extends ASTRewritingTest {
 		assertEqualString(preview, buf.toString());
 	}
 
-	public void testModifyRecordSwitchPattern() throws Exception {
+
+	// TRACKED via https://github.com/eclipse-jdt/eclipse.jdt.core/issues/784
+	public void _testModifyRecordSwitchPattern() throws Exception {
 		if (checkAPILevel()) {
 			return;
 		}
