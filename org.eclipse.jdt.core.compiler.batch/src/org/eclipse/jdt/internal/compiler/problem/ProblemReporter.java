eb00e8bbca64e65297bdee74007699b7065e7fe4
Srikanth Sankaran <srikanth.sankaran@advantest.com>
2023-05-12 17:13:20 +0530

    continue without label is incorrectly handled in a switch expression
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.core/issues/53

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/problem/ProblemReporter.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/problem/ProblemReporter.java
@@ -11666,6 +11666,7 @@ public void switchExpressionMixedCase(ASTNode statement) {
 		statement.sourceStart,
 		statement.sourceEnd);
 }
+// Is this redundant ?? See switchExpressionsBreakOutOfSwitchExpression
 public void switchExpressionBreakNotAllowed(ASTNode statement) {
 	this.handle(
 		IProblem.SwitchExpressionsYieldBreakNotAllowed,
