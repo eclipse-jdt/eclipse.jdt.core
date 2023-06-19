3390b998e35471e9e70c209f952f75b33bfcd14c
Andrey Loskutov <loskutov@gmx.de>
2023-06-19 17:51:31 +0200

    Second attempt to disable automated JVM discovery & install on startup
    See https://github.com/eclipse-jdt/eclipse.jdt.core/issues/1150

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/APIDocumentationTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/APIDocumentationTests.java
@@ -21,18 +21,21 @@ import java.io.StringReader;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
-
-import junit.framework.Test;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
@@ -42,7 +45,10 @@ import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.Javadoc;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.internal.compiler.util.Util;
+import org.eclipse.jdt.launching.JavaRuntime;
 import org.osgi.framework.Bundle;
+
+import junit.framework.Test;
 
 /**
  * The intent of this tests series is to check the consistency of parts of our
@@ -139,9 +145,158 @@ static class JavaCoreJavadocAnalyzer {
 // the javadoc
 public void testJavaCoreAPI() throws CoreException, IllegalArgumentException, IllegalAccessException, IOException {
 
+	// Makes sure the
+	JavaRuntime.getVMInstallTypes();
+
 	// fetch JavaCore class
 	Class javaCoreClass = JavaCore.class;
 
+	char[] sourceChars = getJavaCoreClassSource();
+	assertNotNull("JavaCore.java not found, skipping APIDocumentationTests#test001", sourceChars);
+
+	// load field values in a map
+	LinkedHashMap<String, String> realOptionIDs = new LinkedHashMap();
+	Field[] fields = javaCoreClass.getDeclaredFields();
+	for (int i = 0, l = fields.length; i < l; i++) {
+		Field field = fields[i];
+		int modifiers = field.getModifiers();
+		if (Modifier.isPublic(modifiers) &&
+				Modifier.isStatic(modifiers) &&
+				field.getType() == String.class) {
+			String constantValue = (String) field.get(null);
+			if (constantValue.startsWith(JavaCore.PLUGIN_ID)) {
+				realOptionIDs.put(field.getName(), constantValue);
+			}
+		}
+	}
+	// exempt a few values
+	realOptionIDs.remove("PLUGIN_ID");
+	realOptionIDs.remove("BUILDER_ID");
+	realOptionIDs.remove("JAVA_SOURCE_CONTENT_TYPE");
+	realOptionIDs.remove("MODEL_ID");
+	realOptionIDs.remove("NATURE_ID");
+	realOptionIDs.remove("DEFAULT_JAVA_FORMATTER");
+
+	// build cross-index
+	LinkedHashMap<String, String> realOptionNames = new LinkedHashMap();
+	Iterator<Map.Entry<String, String>> optionIDs = realOptionIDs.entrySet().iterator();
+	while (optionIDs.hasNext()) {
+		Map.Entry<String, String> optionID = optionIDs.next();
+		realOptionNames.put(optionID.getValue(), optionID.getKey());
+	}
+	// tests have specific defaults for these:
+	realOptionNames.remove(JavaCore.COMPILER_SOURCE);
+	realOptionNames.remove(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM);
+	realOptionNames.remove(JavaCore.COMPILER_COMPLIANCE);
+
+	// This option is set in LaunchingPreferenceInitializer and always
+	// differs from empty default value
+	realOptionNames.remove(JavaCore.CORE_JAVA_BUILD_RESOURCE_COPY_FILTER);
+
+	// fetch default option values
+	Hashtable<String, String> realDefaultValues = JavaCore.getDefaultOptions();
+	// load documented values in a map
+	ASTParser parser = ASTParser.newParser(JLS3_INTERNAL);
+	parser.setSource(sourceChars);
+	ASTNode rootNode = parser.createAST(null);
+	final JavaCoreJavadocAnalyzer analyzer = new JavaCoreJavadocAnalyzer();
+	final Hashtable javadocOptionIDs = new Hashtable();
+	final Hashtable javadocDefaultValues = new Hashtable();
+	final Hashtable deprecatedFields = new Hashtable();
+	rootNode.accept(new ASTVisitor() {
+		public boolean visit(FieldDeclaration node) {
+			String key = ((VariableDeclarationFragment) node.fragments().get(0)).getName().getIdentifier();
+			Javadoc javadoc = node.getJavadoc();
+			if (javadoc != null) {
+				analyzer.reset(javadoc.toString());
+				String id, value;
+				if ((id = analyzer.getOptionID()) != null) {
+					javadocOptionIDs.put(key, id);
+				}
+				if ((value = analyzer.getDefaultValue()) != null) {
+					javadocDefaultValues.put(id, value);
+				}
+				if (analyzer.isDeprecated()) {
+					deprecatedFields.put(key, key /* not null */);
+				}
+			}
+			return super.visit(node);
+		}
+	});
+	// checking ids
+	Iterator check = realOptionIDs.entrySet().iterator();
+	String key, value;
+	String expected = "", actual = "";
+	List<String> errors = new ArrayList<>();
+	while (check.hasNext()) {
+		Map.Entry entry = (Map.Entry) check.next();
+		key = (String) entry.getKey();
+		value = (String) entry.getValue();
+		if (deprecatedFields.get(key) == null) {
+			if (!value.equals(javadocOptionIDs.get(key))) {
+				expected = value;
+				actual = (String) javadocOptionIDs.get(key);
+				String errMessage = "option ID mismatch for " + key + ", real: " + expected + ", javadoc: " + actual;
+				System.out.println(errMessage);
+				errors.add(errMessage);
+			}
+		}
+	}
+	check = javadocOptionIDs.entrySet().iterator();
+	while (check.hasNext()) {
+		Map.Entry entry = (Map.Entry) check.next();
+		key = (String) entry.getKey();
+		value = (String) entry.getValue();
+		if (!value.equals(realOptionIDs.get(key))) {
+			expected = value;
+			actual = realOptionIDs.get(key);
+			String errMessage = "option ID mismatch, javadoc " + expected + ", real " + actual;
+			System.out.println(errMessage);
+			errors.add(errMessage);
+		}
+	}
+	// checking default values
+	check = realDefaultValues.entrySet().iterator();
+	while (check.hasNext()) {
+		Map.Entry entry = (Map.Entry) check.next();
+		key = (String) entry.getKey();
+		value = (String) entry.getValue();
+		String name = realOptionNames.get(key);
+		if (name != null && deprecatedFields.get(name) == null) {
+			if (!value.equals(javadocDefaultValues.get(key)) &&
+					!"org.eclipse.jdt.core.encoding".equals(key)) {
+				expected = value;
+				actual = (String) javadocDefaultValues.get(key);
+				String errMessage = "default value mismatch for " + key + ", real: " + expected + ", javadoc: " + actual;
+				System.out.println(errMessage);
+				errors.add(errMessage);
+			}
+		}
+	}
+	check = javadocDefaultValues.entrySet().iterator();
+	while (check.hasNext()) {
+		Map.Entry entry = (Map.Entry) check.next();
+		key = (String) entry.getKey();
+		value = (String) entry.getValue();
+		if (realOptionNames.containsKey(key) && !value.equals(realDefaultValues.get(key)) &&
+				!"org.eclipse.jdt.core.compiler.problem.booleanMethodThrowingException".equals(key)) { // will remove once bug 216571 is fixed
+			expected = value;
+			actual = realDefaultValues.get(key);
+			String errMessage = "default value mismatch for " + key + ", javadoc " + expected +	", real " + actual;
+			System.out.println(errMessage);
+			errors.add(errMessage);
+		}
+	}
+	assertEquals("JavaCore options discrepancies found", List.of().toString(), errors.toString());
+}
+
+@Override
+protected void tearDown() throws Exception {
+	JavaCore.setOptions(JavaCore.getDefaultOptions());
+	super.tearDown();
+}
+
+private char[] getJavaCoreClassSource() throws IOException, ZipException {
 	// fetch JavaCore source file
 	// 1. attempt: workspace relative location in project org.eclipse.jdt.core:
 	Bundle bundle = org.eclipse.jdt.core.tests.Activator.getInstance().getBundle();
@@ -155,154 +310,22 @@ public void testJavaCoreAPI() throws CoreException, IllegalArgumentException, Il
 		sourceChars = Util.getFileCharContent(javaCoreSourceFile, null);
 	} else {
 		// 2. attempt: locate org.eclipse.jdt.core.source jar next to org.eclipse.jdt.core jar:
-		@SuppressWarnings("deprecation")Bundle[] sourceBundles =
-				org.eclipse.jdt.core.tests.Activator.getPackageAdmin().getBundles(ORG_ECLIPSE_JDT_CORE, null);
-		if (sourceBundles != null && sourceBundles.length > 0) {
-			bundle = sourceBundles[0];
-			stringPath = bundle.getLocation();
-			if (stringPath.startsWith(REFERENCE_FILE_SCHEMA))
-				stringPath = stringPath.substring(REFERENCE_FILE_SCHEMA.length());
-			stringPath = stringPath.replace(ORG_ECLIPSE_JDT_CORE, ORG_ECLIPSE_JDT_CORE_SOURCE);
-			if (stringPath.endsWith(".jar")) {
-				File jarFile = new File(stringPath);
-				try (ZipFile zipFile = new ZipFile(jarFile)) {
-					ZipEntry entry = zipFile.getEntry(PATH_JAVA_CORE_JAVA);
-					try (InputStream inputStream = zipFile.getInputStream(entry)) {
-						sourceChars = Util.getInputStreamAsCharArray(inputStream, null);
-					}
+		String location = Platform.getBundle(ORG_ECLIPSE_JDT_CORE).getLocation();
+		if (location.startsWith(REFERENCE_FILE_SCHEMA)) {
+			location = location.substring(REFERENCE_FILE_SCHEMA.length());
+		}
+		location = location.replace(ORG_ECLIPSE_JDT_CORE, ORG_ECLIPSE_JDT_CORE_SOURCE);
+		if (location.endsWith(".jar")) {
+			File jarFile = new File(location);
+			try (ZipFile zipFile = new ZipFile(jarFile)) {
+				ZipEntry entry = zipFile.getEntry(PATH_JAVA_CORE_JAVA);
+				try (InputStream inputStream = zipFile.getInputStream(entry)) {
+					sourceChars = Util.getInputStreamAsCharArray(inputStream, null);
 				}
 			}
 		}
 	}
-
-	if (sourceChars != null) {
-		// load field values in a map
-		Hashtable realOptionIDs = new Hashtable();
-		Field[] fields = javaCoreClass.getDeclaredFields();
-		for (int i = 0, l = fields.length; i < l; i++) {
-			Field field = fields[i];
-			int modifiers = field.getModifiers();
-			if (Modifier.isPublic(modifiers) &&
-					Modifier.isStatic(modifiers) &&
-					field.getType() == String.class) {
-				String constantValue = (String) field.get(null);
-				if (constantValue.startsWith(JavaCore.PLUGIN_ID)) {
-					realOptionIDs.put(field.getName(), constantValue);
-				}
-			}
-		}
-		// exempt a few values
-		realOptionIDs.remove("PLUGIN_ID");
-		realOptionIDs.remove("BUILDER_ID");
-		realOptionIDs.remove("JAVA_SOURCE_CONTENT_TYPE");
-		realOptionIDs.remove("MODEL_ID");
-		realOptionIDs.remove("NATURE_ID");
-		realOptionIDs.remove("DEFAULT_JAVA_FORMATTER");
-		// build cross-index
-		Hashtable realOptionNames = new Hashtable();
-		Iterator optionIDs = realOptionIDs.entrySet().iterator();
-		while (optionIDs.hasNext()) {
-			Map.Entry optionID = (Map.Entry) optionIDs.next();
-			realOptionNames.put(optionID.getValue(), optionID.getKey());
-		}
-		// tests have specific defaults for these:
-		realOptionNames.remove(JavaCore.COMPILER_SOURCE);
-		realOptionNames.remove(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM);
-		realOptionNames.remove(JavaCore.COMPILER_COMPLIANCE);
-
-		// fetch default option values
-		Hashtable realDefaultValues = JavaCore.getDefaultOptions();
-		// load documented values in a map
-		ASTParser parser = ASTParser.newParser(JLS3_INTERNAL);
-		parser.setSource(sourceChars);
-		ASTNode rootNode = parser.createAST(null);
-		final JavaCoreJavadocAnalyzer analyzer = new JavaCoreJavadocAnalyzer();
-		final Hashtable javadocOptionIDs = new Hashtable();
-		final Hashtable javadocDefaultValues = new Hashtable();
-		final Hashtable deprecatedFields = new Hashtable();
-		rootNode.accept(new ASTVisitor() {
-			public boolean visit(FieldDeclaration node) {
-				String key = ((VariableDeclarationFragment) node.fragments().get(0)).getName().getIdentifier();
-				Javadoc javadoc = node.getJavadoc();
-				if (javadoc != null) {
-					analyzer.reset(javadoc.toString());
-					String id, value;
-					if ((id = analyzer.getOptionID()) != null) {
-						javadocOptionIDs.put(key, id);
-					}
-					if ((value = analyzer.getDefaultValue()) != null) {
-						javadocDefaultValues.put(id, value);
-					}
-					if (analyzer.isDeprecated()) {
-						deprecatedFields.put(key, key /* not null */);
-					}
-				}
-				return super.visit(node);
-			}
-		});
-		// checking ids
-		Iterator check = realOptionIDs.entrySet().iterator();
-		String key, value;
-		String expected = "", actual = "";
-		while (check.hasNext()) {
-			Map.Entry entry = (Map.Entry) check.next();
-			key = (String) entry.getKey();
-			value = (String) entry.getValue();
-			if (deprecatedFields.get(key) == null) {
-				if (!value.equals(javadocOptionIDs.get(key))) {
-					expected = value;
-					actual = (String) javadocOptionIDs.get(key);
-					System.out.println("option ID mismatch for " + key + ", real: " + expected +
-						", javadoc: " + actual);
-				}
-			}
-		}
-		check = javadocOptionIDs.entrySet().iterator();
-		while (check.hasNext()) {
-			Map.Entry entry = (Map.Entry) check.next();
-			key = (String) entry.getKey();
-			value = (String) entry.getValue();
-			if (!value.equals(realOptionIDs.get(key))) {
-				expected = value;
-				actual = (String) realOptionIDs.get(key);
-				System.out.println("option ID mismatch, javadoc " + expected +
-					", real " + actual);
-			}
-		}
-		// checking default values
-		check = realDefaultValues.entrySet().iterator();
-		while (check.hasNext()) {
-			Map.Entry entry = (Map.Entry) check.next();
-			key = (String) entry.getKey();
-			value = (String) entry.getValue();
-			String name = (String) realOptionNames.get(key);
-			if (name != null && deprecatedFields.get(name) == null) {
-				if (!value.equals(javadocDefaultValues.get(key)) &&
-						!"org.eclipse.jdt.core.encoding".equals(key)) {
-					expected = value;
-					actual = (String) javadocDefaultValues.get(key);
-					System.out.println("default value mismatch for " + key + ", real: " + expected +
-						", javadoc: " + actual);
-				}
-			}
-		}
-		check = javadocDefaultValues.entrySet().iterator();
-		while (check.hasNext()) {
-			Map.Entry entry = (Map.Entry) check.next();
-			key = (String) entry.getKey();
-			value = (String) entry.getValue();
-			if (realOptionNames.containsKey(key) && !value.equals(realDefaultValues.get(key)) &&
-					!"org.eclipse.jdt.core.compiler.problem.booleanMethodThrowingException".equals(key)) { // will remove once bug 216571 is fixed
-				expected = value;
-				actual = (String) realDefaultValues.get(key);
-				System.out.println("default value mismatch for " + key + ", javadoc " + expected +
-					", real " + actual);
-			}
-		}
-		assertEquals("One or many discrepancies, including: ", expected, actual);
-	} else {
-		System.err.println("JavaCore.java not found, skipping APIDocumentationTests#test001");
-	}
+	return sourceChars;
 }
 // see https://bugs.eclipse.org/482991
 // check that tests are run with -Djdt.default.test.compliance=1.8 in effect
