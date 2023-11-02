5b87e7b78f338c4e4b3a0e3b8662b63bf8d3668b
Jeff Johnston <jjohnstn@redhat.com>
2023-11-02 15:07:39 -0400

    Fix token assert in WrapPreparator

--- org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/linewrap/WrapPreparator.java
+++ org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/linewrap/WrapPreparator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2022 Mateusz Matela and others.
+ * Copyright (c) 2014, 2023 Mateusz Matela and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -40,6 +40,7 @@ import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamei
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamenew;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameRestrictedIdentifierpermits;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamesuper;
+import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameTextBlock;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamethis;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNamethrows;
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.TokenNameto;
@@ -1566,7 +1567,7 @@ public class WrapPreparator extends ASTVisitor {
 			if (token.getLineBreaksBefore() > 0 || token.getLineBreaksAfter() > 0)
 				isNLSTagInLine = false;
 			if (token.hasNLSTag()) {
-				assert token.tokenType == TokenNameStringLiteral;
+				assert token.tokenType == TokenNameStringLiteral || token.tokenType == TokenNameTextBlock;
 				isNLSTagInLine = true;
 			}
 			List<Token> structure = token.getInternalStructure();
