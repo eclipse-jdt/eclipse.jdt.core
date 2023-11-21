19979811dd5d59626933e0f5c9c80d0f7ee4fae4
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2023-11-21 16:12:03 +0100

    [test] fix AbstractJavaModelTests #1333
    avoid asynchronous refresh. Implementation taken from
    org.eclipse.core.tests.resources.refresh.RefreshProviderTest.joinAutoRefreshJobs()
    
    https://github.com/eclipse-jdt/eclipse.jdt.core/issues/1333

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/AbstractJavaModelTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/AbstractJavaModelTests.java
@@ -354,7 +354,7 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 		 * <code>#startDeltas</code> and
 		 * <code>#stopDeltas</code>.
 		 */
-		private IJavaElementDelta[] deltas;
+		private ArrayList<IJavaElementDelta> deltas = new ArrayList<>();
 
 		private final int eventType;
 
@@ -373,10 +373,7 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 
 		public synchronized void elementChanged(ElementChangedEvent event) {
 			if (this.eventType == -1 || event.getType() == this.eventType) {
-				IJavaElementDelta[] copy= new IJavaElementDelta[this.deltas.length + 1];
-				System.arraycopy(this.deltas, 0, copy, 0, this.deltas.length);
-				copy[this.deltas.length]= event.getDelta();
-				this.deltas= copy;
+				this.deltas.add(event.getDelta());
 				StringBuilder message = new StringBuilder();
 				Job currentJob = Job.getJobManager().currentJob();
 				if (currentJob != null) {
@@ -389,8 +386,8 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 			}
 		}
 		public synchronized CompilationUnit getCompilationUnitAST(ICompilationUnit workingCopy) {
-			for (int i=0, length= this.deltas.length; i<length; i++) {
-				CompilationUnit result = getCompilationUnitAST(workingCopy, this.deltas[i]);
+			for (IJavaElementDelta delta: this.deltas) {
+				CompilationUnit result = getCompilationUnitAST(workingCopy,delta);
 				if (result != null)
 					return result;
 			}
@@ -418,8 +415,8 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 			if (this.deltas == null) waitForResourceDelta();
 			if (this.deltas == null) return null;
 			IJavaElementDelta result = null;
-			for (int i = 0; i < this.deltas.length; i++) {
-				IJavaElementDelta delta = searchForDelta(element, this.deltas[i]);
+			for (IJavaElementDelta delta1: this.deltas) {
+				IJavaElementDelta delta = searchForDelta(element, delta1);
 				if (delta != null) {
 					if (returnFirst) {
 						return delta;
@@ -431,15 +428,15 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 		}
 
 		public synchronized IJavaElementDelta getLastDelta() {
-			return this.deltas[this.deltas.length - 1];
+			return this.deltas.get(this.deltas.size()-1);
 		}
 
 		public synchronized List<IJavaElementDelta> getAllDeltas() {
-			return List.of(this.deltas);
+			return this.deltas;
 		}
 
 		public synchronized void flush() {
-			this.deltas = new IJavaElementDelta[0];
+			this.deltas.clear();
 			this.stackTraces = new ByteArrayOutputStream();
 			this.gotResourceDelta = false;
 		}
@@ -512,8 +509,7 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 		@Override
 		public synchronized String toString() {
 			StringBuilder buffer = new StringBuilder();
-			for (int i = 0, length= this.deltas.length; i < length; i++) {
-				IJavaElementDelta delta = this.deltas[i];
+			for (IJavaElementDelta delta: this.deltas) {
 				if (((JavaElementDelta) delta).ignoreFromTests) {
 					continue;
 				}
@@ -1607,6 +1603,14 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 		listener.flush();
 	}
 	public void clearDeltas() {
+		// We must join on the auto-refresh family because the workspace changes done in the
+		// tests may be batched and broadcasted by the RefreshJob, not the main thread.
+		Job.getJobManager().wakeUp(ResourcesPlugin.FAMILY_AUTO_REFRESH);
+		try {
+			Job.getJobManager().join(ResourcesPlugin.FAMILY_AUTO_REFRESH, null);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 		this.deltaListener.flush();
 	}
 	protected IJavaElement[] codeSelect(ISourceReference sourceReference, String selectAt, String selection) throws JavaModelException {
