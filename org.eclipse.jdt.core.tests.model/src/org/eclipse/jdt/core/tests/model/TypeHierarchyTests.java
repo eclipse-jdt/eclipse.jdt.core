35ce2ac0561d09d6c5b774372cd94747dfd100fa
David Thompson <davthomp@redhat.com>
2023-12-13 11:58:41 -0500

    [WIP] fix type hierarchy tests
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/TypeHierarchyTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/TypeHierarchyTests.java
@@ -23,8 +23,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import junit.framework.Test;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
@@ -49,6 +47,8 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.tests.model.SearchTests.WaitingJob;
 import org.eclipse.jdt.core.tests.model.Semaphore.TimeOutException;
 import org.eclipse.jdt.core.tests.util.Util;
+
+import junit.framework.Test;
 
 @SuppressWarnings("rawtypes")
 public class TypeHierarchyTests extends ModifyingResourceTests {
@@ -446,7 +446,7 @@ public void testBinaryInWrongPackage() throws CoreException {
 		);
 		getProject("P").build(IncrementalProjectBuilder.FULL_BUILD, null);
 		waitForAutoBuild();
-		getFile("/P/bin/p/X.class").copy(new Path("/P/lib/X.class"), false, null);
+		getFile("/P/bin/p/<unnamed_class$X>.class").copy(new Path("/P/lib/X.class"), false, null);
 		ITypeHierarchy hierarchy = getClassFile("P", "/P/lib", "", "X.class").getType().newSupertypeHierarchy(null);
 		assertHierarchyEquals(
 			"Focus: X [in X.class [in <default> [in lib [in P]]]]\n" +
@@ -2032,24 +2032,27 @@ public void testResilienceToMissingBinaries() throws CoreException {
 		createFolder("/P/src/tools/");
 		createFile(
 			"/P/src/tools/DisplayTestResult2.java",
-			"pakage tools;\n" +
+			"package tools;\n" +
 			"import servlet.*;\n" +
 			"public class DisplayTestResult2 extends TmrServlet2 {\n" +
-			"}"
+			"}\n" +
+			"obscruction"
 		);
 		createFolder("/P/src/servlet/");
 		createFile(
 				"/P/src/servlet/TmrServlet2.java",
-				"pakage servlet;\n" +
+				"package servlet;\n" +
 				"public class TmrServlet2 extends TmrServlet {\n" +
-				"}"
+				"}\n" +
+				"obstruction"
 			);
 		createFile(
 				"/P/src/servlet/TmrServlet.java",
-				"pakage servlet;\n" +
+				"package servlet;\n" +
 				"import gk.*;\n" +
 				"public class TmrServlet extends GKServlet {\n" +
-				"}"
+				"}\n" +
+				"obstruction"
 			);
 		IType type = getCompilationUnit("P", "src", "tools", "DisplayTestResult2.java").getType("DisplayTestResult2");
 		ITypeHierarchy hierarchy = type.newSupertypeHierarchy(null);
@@ -3381,12 +3384,13 @@ public void testBug457813() throws CoreException {
 				"public class X extends aspose.b.a.a {\n" +
 				"}"
 			);
-		IType type = getCompilationUnit("P", "src", "hierarchy", "X.java").getType("X");
+		IType type = getCompilationUnit("P", "src", "hierarchy", "X.java").getType("<unnamed_class$X>");
 		assertTrue("Type should exist!", type.exists());
 		ITypeHierarchy hierarchy = type.newTypeHierarchy(null); // when bug occurred a stack overflow happened here...
 		assertHierarchyEquals(
-				"Focus: X [in X.java [in hierarchy [in src [in P]]]]\n" +
+				"Focus: <unnamed_class$X> [in X.java [in hierarchy [in src [in P]]]]\n" +
 				"Super types:\n" +
+				"  Object [in Object.class [in java.lang [in "+ getExternalJCLPathString() + "]]]\n" +
 				"Sub types:\n",
 				hierarchy);
 	} finally {
