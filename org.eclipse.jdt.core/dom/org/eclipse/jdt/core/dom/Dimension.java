8ca38990efaca3ccd371ad2a288d0746c566923f
Patrick Ziegler <ziegler.patrick@hotmail.de>
2023-12-23 14:48:59 +0100

    Start position and length of Dimension node lost when cloned
    When a Dimension node is cloned, the start position and length of the
    node needs to be explicitly set. Otherwise this information is lost.
    
    Fixes #1786

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Dimension.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/Dimension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -114,6 +114,7 @@ public class Dimension extends ASTNode {
 	@Override
 	ASTNode clone0(AST target) {
 		Dimension result = new Dimension(target);
+		result.setSourceRange(getStartPosition(), getLength());
 		result.annotations().addAll(
 				ASTNode.copySubtrees(target, annotations()));
 		return result;
