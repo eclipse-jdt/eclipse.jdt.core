6170f1e56b470f80e0596983ea3cc812a530da7a
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2024-01-10 11:19:38 +0100

    [performance] less calls to isInModuleDeclaration() #1747
    Reorder costly conditions
    
    https://github.com/eclipse-jdt/eclipse.jdt.core/issues/1747

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/parser/Scanner.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/parser/Scanner.java
@@ -4902,8 +4902,9 @@ private VanguardScanner getNewVanguardScanner() {
 	return vs;
 }
 protected final boolean mayBeAtCasePattern(int token) {
-	return (!isInModuleDeclaration() && JavaFeature.PATTERN_MATCHING_IN_SWITCH.isSupported(this.complianceLevel, this.previewEnabled))
-			&& (token == TokenNamecase || this.multiCaseLabelComma);
+	return ((token == TokenNamecase || this.multiCaseLabelComma)
+			&& JavaFeature.PATTERN_MATCHING_IN_SWITCH.isSupported(this.complianceLevel, this.previewEnabled))
+			&& !isInModuleDeclaration();
 }
 protected final boolean maybeAtLambdaOrCast() { // Could the '(' we saw just now herald a lambda parameter list or a cast expression ? (the possible locations for both are identical.)
 
