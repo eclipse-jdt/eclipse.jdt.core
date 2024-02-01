e6209242688fde5504a190a4fcbbf00163e498f7
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-02-01 08:55:43 +0100

    Capture all information of a source file into one SourceInfo record
    To get a consistent compilation order that is independent of the order
    of arguments one needs to sort the files to process by name. As the name
    might be related to to other information all of these are first captured
    inside a SourceInfo record and are the sort this array by name, in the
    following code always this sorted array is used and possibly updated
    with additional information.

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/tool/EclipseCompilerImpl.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/tool/EclipseCompilerImpl.java
@@ -61,6 +61,7 @@ import org.eclipse.jdt.internal.compiler.batch.CompilationUnit;
 import org.eclipse.jdt.internal.compiler.batch.FileSystem;
 import org.eclipse.jdt.internal.compiler.batch.FileSystem.Classpath;
 import org.eclipse.jdt.internal.compiler.batch.Main;
+import org.eclipse.jdt.internal.compiler.batch.SourceInfo;
 import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
 import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 import org.eclipse.jdt.internal.compiler.lookup.TypeConstants;
@@ -174,11 +175,12 @@ public class EclipseCompilerImpl extends Main {
 							throw new IllegalArgumentException(this.bind("unit.missing", name)); //$NON-NLS-1$
 					}
 
+					SourceInfo sourceInfo = getSourceInfo(i);
 					CompilationUnit cu = new CompilationUnit(null,
 							name,
 							null,
-							this.destinationPaths[i],
-							shouldIgnoreOptionalProblems(this.ignoreOptionalProblemsFromFolders, name.toCharArray()), this.modNames[i]) {
+							sourceInfo.destinationPath(),
+							shouldIgnoreOptionalProblems(this.ignoreOptionalProblemsFromFolders, name.toCharArray()), sourceInfo.moduleName()) {
 
 							@Override
 							public char[] getContents() {
@@ -200,6 +202,7 @@ public class EclipseCompilerImpl extends Main {
 		units.toArray(result);
 		return result;
 	}
+
 	/*
 	 *  Low-level API performing the actual compilation
 	 */
