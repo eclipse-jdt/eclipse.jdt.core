fef6717bc8e121316ac8510d52917bd7d851ea3a
David Thompson <davthomp@redhat.com>
2024-04-25 12:02:53 -0400

    Contribute new test bundle to test javac features
    Signed-off-by: David Thompson <davthomp@redhat.com>

---
+++ org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunConverterTestsJavac.java
@@ -0,0 +1,9 @@
+package org.eclipse.jdt.core.tests.javac;
+
+public class RunConverterTestsJavac extends org.eclipse.jdt.core.tests.dom.RunConverterTests {
+
+	public RunConverterTestsJavac(String name) {
+		super(name);
+	}
+
+}
