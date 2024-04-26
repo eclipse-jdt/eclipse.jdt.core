74fe4ace525b80a1914f2e1ca93fed67023fe3e3
Mickael Istria <mistria@redhat.com>
2024-04-26 10:59:52 +0200

    Fix opening class files
    + Add test

--- org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunConverterTestsJavac.java
+++ org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunConverterTestsJavac.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2024, Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package org.eclipse.jdt.core.tests.javac;
 
 public class RunConverterTestsJavac extends org.eclipse.jdt.core.tests.dom.RunConverterTests {
