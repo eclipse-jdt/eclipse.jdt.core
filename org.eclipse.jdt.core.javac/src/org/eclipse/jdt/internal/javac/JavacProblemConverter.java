eaadb349f7ee61f71f6fb2577ce52aaf380b184b
Snjezana Peco <snjezana.peco@redhat.com>
2024-04-30 20:36:09 +0200

    [javac] Diagnostic ranges at the class level are too aggressive
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

--- org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/javac/JavacProblemConverter.java
+++ org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/javac/JavacProblemConverter.java
@@ -13,36 +13,94 @@
 
 package org.eclipse.jdt.internal.javac;
 
+import java.io.IOException;
 import java.util.Locale;
 
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 
+import org.eclipse.core.runtime.ILog;
 import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jdt.internal.compiler.problem.ProblemSeverities;
 
 import com.sun.tools.javac.code.Kinds;
 import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.tree.JCTree.JCClassDecl;
+import com.sun.tools.javac.util.DiagnosticSource;
 import com.sun.tools.javac.util.JCDiagnostic;
+import com.sun.tools.javac.util.Position;
 
 public class JavacProblemConverter {
 	public static JavacProblem createJavacProblem(Diagnostic<? extends JavaFileObject> diagnostic) {
+		int problemId = toProblemId(diagnostic);
+		return switch (problemId) {
+			case IProblem.MissingSerialVersion -> classLevelProblem(diagnostic, problemId);
+			default -> defaultProblem(diagnostic, problemId);
+		};
+	}
+
+	private static JavacProblem defaultProblem(Diagnostic<? extends JavaFileObject> diagnostic, int problemId) {
 		return new JavacProblem(
-			diagnostic.getSource().getName().toCharArray(),
-			diagnostic.getMessage(Locale.getDefault()),
-			diagnostic.getCode(),
-			toProblemId(diagnostic),
-			new String[0],
-			switch (diagnostic.getKind()) {
-				case ERROR -> ProblemSeverities.Error;
-				case WARNING, MANDATORY_WARNING -> ProblemSeverities.Warning;
-				case NOTE -> ProblemSeverities.Info;
-				default -> ProblemSeverities.Error;
-			},
-			(int) Math.min(diagnostic.getPosition(), diagnostic.getStartPosition()),
-			(int) (diagnostic.getEndPosition() - 1),
-			(int) diagnostic.getLineNumber(),
-			(int) diagnostic.getColumnNumber());
+				diagnostic.getSource().getName().toCharArray(),
+				diagnostic.getMessage(Locale.getDefault()),
+				diagnostic.getCode(),
+				problemId,
+				new String[0],
+				toSeverity(diagnostic),
+				(int) Math.min(diagnostic.getPosition(), diagnostic.getStartPosition()),
+				(int) (diagnostic.getEndPosition() - 1),
+				(int) diagnostic.getLineNumber(),
+				(int) diagnostic.getColumnNumber());
+	}
+
+	private static int toSeverity(Diagnostic<? extends JavaFileObject> diagnostic) {
+		return switch (diagnostic.getKind()) {
+			case ERROR -> ProblemSeverities.Error;
+			case WARNING, MANDATORY_WARNING -> ProblemSeverities.Warning;
+			case NOTE -> ProblemSeverities.Info;
+			default -> ProblemSeverities.Error;
+		};
+	}
+
+	private static JavacProblem classLevelProblem(Diagnostic<? extends JavaFileObject> diagnostic, int problemId) {
+		int startPosition = - 1;
+		int endPosition = - 1;
+		if (diagnostic instanceof JCDiagnostic jcDiagnostic
+				&& jcDiagnostic.getDiagnosticPosition() instanceof JCClassDecl jcClassDecl) {
+			startPosition = (int) diagnostic.getPosition();
+			if (startPosition != Position.NOPOS) {
+				DiagnosticSource source = jcDiagnostic.getDiagnosticSource();
+				JavaFileObject fileObject = source.getFile();
+				try {
+					CharSequence charContent = fileObject.getCharContent(true);
+					String content = charContent.toString();
+					String name = jcClassDecl.getSimpleName().toString();
+					if (content != null && name != null && content.length() > startPosition) {
+						String temp = content.substring(startPosition);
+						int ind = temp.indexOf(name);
+						startPosition += ind;
+						endPosition = startPosition + name.length() - 1;
+					}
+				} catch (IOException ex) {
+					ILog.get().error(ex.getMessage(), ex);
+				}
+			}
+		}
+		if (startPosition == -1 ) {
+			startPosition = (int) Math.min(diagnostic.getPosition(), diagnostic.getStartPosition());
+			endPosition = (int) (diagnostic.getEndPosition() - 1);
+		}
+		return new JavacProblem(
+				diagnostic.getSource().getName().toCharArray(),
+				diagnostic.getMessage(Locale.getDefault()),
+				diagnostic.getCode(),
+				problemId,
+				new String[0],
+				toSeverity(diagnostic),
+				startPosition,
+				endPosition,
+				(int) diagnostic.getLineNumber(),
+				(int) diagnostic.getColumnNumber());
 	}
 
 	/**
@@ -64,6 +122,7 @@ public class JavacProblemConverter {
 			case "compiler.err.cant.apply.symbols" -> IProblem.UndefinedConstructor;
 			case "compiler.err.premature.eof" -> IProblem.ParsingErrorUnexpectedEOF; // syntax error
 			case "compiler.err.report.access" -> convertNotVisibleAccess(javacDiagnostic);
+			case "compiler.warn.missing.SVUID" -> IProblem.MissingSerialVersion;
 			// TODO complete mapping list; dig in https://github.com/openjdk/jdk/blob/master/src/jdk.compiler/share/classes/com/sun/tools/javac/resources/compiler.properties
 			// for an exhaustive (but polluted) list, unless a better source can be found (spec?)
 			default -> 0;
