8ae09a12e8832a6353dbce57522b8f1260a43b2f
David Thompson <davthomp@redhat.com>
2024-05-03 15:35:37 -0400

    Avoid SIOOBE when hovering method/type with blank Javadoc
    Fixes #321
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- org.eclipse.jdt.core.javac/src/org/eclipse/jdt/core/dom/JavacConverter.java
+++ org.eclipse.jdt.core.javac/src/org/eclipse/jdt/core/dom/JavacConverter.java
@@ -196,7 +196,7 @@ class JavacConverter {
 	private ModuleDeclaration convert(JCModuleDecl javac) {
 		ModuleDeclaration res = this.ast.newModuleDeclaration();
 		res.setName(toName(javac.getName()));
-		boolean isOpen = javac.getModuleType() == ModuleKind.OPEN; 
+		boolean isOpen = javac.getModuleType() == ModuleKind.OPEN;
 		res.setOpen(isOpen);
 		if (javac.getDirectives() != null) {
 			List<JCDirective> directives = javac.getDirectives();
@@ -210,7 +210,7 @@ class JavacConverter {
 			int start = res.getStartPosition();
 			if( !this.rawText.substring(start).trim().startsWith("open")) {
 				// we are open but we don't start with open... so... gotta look backwards
-				String prefix =  this.rawText.substring(0,start); 
+				String prefix =  this.rawText.substring(0,start);
 				if( prefix.trim().endsWith("open")) {
 					// previous token is open
 					int ind = new StringBuffer().append(prefix).reverse().toString().indexOf("nepo");
@@ -901,7 +901,12 @@ class JavacConverter {
 		Comment c = this.javacCompilationUnit.docComments.getComment(javac);
 		if( c != null && c.getStyle() == Comment.CommentStyle.JAVADOC) {
 			String textVal = c.getText(); // initialize
-			int start = c.getSourcePos(0);
+			int start = -1;
+			if (textVal.isEmpty()) {
+				start = new StringBuilder(this.rawText.substring(0, javac.getStartPosition())).lastIndexOf("/**") + 3;
+			} else {
+				start = c.getSourcePos(0);
+			}
 			String prefix = new StringBuilder(this.rawText.substring(0, start)).reverse().toString();
 			int ind = prefix.indexOf("**/");
 			if( ind != -1 ) {
@@ -1063,7 +1068,7 @@ class JavacConverter {
 					return res2;
 				}
 			}
-			
+
 			MethodInvocation res = this.ast.newMethodInvocation();
 			commonSettings(res, methodInvocation);
 			if (nameExpr instanceof JCIdent ident) {
@@ -1346,8 +1351,8 @@ class JavacConverter {
 				// we have no type, we should return an initializer directly
 				ArrayInitializer ret = createArrayInitializerFromJCNewArray(jcNewArray);
 				return ret;
-			}	
-			
+			}
+
 			if (jcNewArray.getType() != null) {
 				Type type = convertToType(jcNewArray.getType());
 				ArrayType arrayType;
@@ -1393,7 +1398,7 @@ class JavacConverter {
 		jcNewArray.getInitializers().stream().map(this::convertExpression).forEach(initializer.expressions()::add);
 		return initializer;
 	}
-	
+
 	private AnonymousClassDeclaration createAnonymousClassDeclaration(JCClassDecl javacAnon, ASTNode parent) {
 		AnonymousClassDeclaration anon = this.ast.newAnonymousClassDeclaration();
 		commonSettings(anon, javacAnon);
@@ -1475,12 +1480,12 @@ class JavacConverter {
 			} else {
 				PrefixExpression res = this.ast.newPrefixExpression();
 				commonSettings(res, literal);
-				
+
 				String fromSrc = this.rawText.substring(res.getStartPosition()+1, res.getStartPosition() + res.getLength());
 				NumberLiteral operand = this.ast.newNumberLiteral();
 				commonSettings(operand, literal);
 				operand.setToken(fromSrc);
-				
+
 				res.setOperand(operand);
 				res.setOperator(Operator.MINUS);
 				return res;
@@ -1818,7 +1823,7 @@ class JavacConverter {
 		}
 		return vartype;
 	}
-	
+
 	private Block convertBlock(JCBlock javac) {
 		Block res = this.ast.newBlock();
 		commonSettings(res, javac);
@@ -1898,7 +1903,7 @@ class JavacConverter {
 					SimpleType res = this.ast.newSimpleType(qn);
 					commonSettings(res, qualified);
 					return res;
-				} 
+				}
 			} catch (Exception ex) {
 			}
 			// case of not translatable name, eg because of generics
@@ -2240,7 +2245,7 @@ class JavacConverter {
 		return 0;
 	}
 
-	
+
 	private Modifier convert(javax.lang.model.element.Modifier javac, int startPos, int endPos) {
 		Modifier res = modifierToDom(javac);
 		if (startPos >= 0) {
@@ -2407,7 +2412,7 @@ class JavacConverter {
 							}
 						}
 					}
-				} 
+				}
 			}
 		}
 		return enumConstantDeclaration;
