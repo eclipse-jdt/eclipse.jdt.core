1daf51a6ff6c7af19dc1f60897912a1699338960
Mickael Istria <mistria@redhat.com>
2024-05-17 16:08:36 +0200

    DOMToIndex: restore statements

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaSearchBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/JavaSearchBugsTests.java
@@ -12931,7 +12931,7 @@ public void testBug341462() throws CoreException {
 				"        }\n" +
 				"        public static void main(String[] args) {\n" +
 				"            X.testFunction(new X<>(\"hello\").getField());\n" +
-				"            X.testFunction(new X<>(new Object()).getField());\n" +
+				"...         X.testFunction(new X<>(new Object()).getField());\n" +
 				"        }\n" +
 				"}\n");
 		waitUntilIndexesReady();
