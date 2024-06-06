72d8fe4023598209579e9355467cb1f90c96d9e9
Stephan Herrmann <stephan.herrmann@berlin.de>
2024-06-06 11:07:19 +0200

    Capture test cases from hohwille (#2460)
    + See https://github.com/m-m-m/util/issues/166
    + Prepare tests for run.javac capturing the current differences

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AbstractRegressionTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/AbstractRegressionTest.java
@@ -508,17 +508,23 @@ static class JavacCompiler {
 			}
 		}
 		if (version == JavaCore.VERSION_17) {
-			if ("17-ea".equals(rawVersion)) {
-				return 0000;
-			}
-			if ("17".equals(rawVersion)) {
-				return 0000;
-			}
-			if ("17.0.1".equals(rawVersion)) {
-				return 0100;
-			}
-			if ("17.0.2".equals(rawVersion)) {
-				return 0200;
+			switch (rawVersion) {
+				case "17-ea":
+					return 0000;
+				case "17":
+					return 0000;
+				case "17.0.1":
+					return 0100;
+				case "17.0.2":
+					return 0200;
+				case "17.0.3":
+					return 0300;
+				case "17.0.4":
+					return 0400;
+				case "17.0.5":
+					return 0500;
+				case "17.0.6":
+					return 0600;
 			}
 		}
 		if (version == JavaCore.VERSION_18) {
@@ -561,6 +567,11 @@ static class JavacCompiler {
 			}
 			if ("20.0.2".equals(rawVersion)) {
 				return 0200;
+			}
+		}
+		if (version == JavaCore.VERSION_21) {
+			switch (rawVersion) {
+				case "21": return 0;
 			}
 		}
 		if (version == JavaCore.VERSION_22) {
@@ -825,6 +836,19 @@ protected static class JavacTestOptions {
 	}
 	Excuse excuseFor(JavacCompiler compiler) {
 		return null;
+	}
+	/** Difference were we're not sure which is correct, but want to be informed if either compiler changes. */
+	public static class DubiousOutcome extends Excuse {
+		DubiousOutcome(int mismatchType) {
+			super(mismatchType);
+		}
+		public static DubiousOutcome
+		EclipseErrorsJavacNone = RUN_JAVAC ?
+				new DubiousOutcome(MismatchType.EclipseErrorsJavacNone) : null,
+		JavacErrorsEclipseNone = RUN_JAVAC ?
+				new DubiousOutcome(MismatchType.JavacErrorsEclipseNone) : null,
+		JDK8319461 = RUN_JAVAC ? // https://bugs.openjdk.org/browse/JDK-8319461
+				new DubiousOutcome(MismatchType.JavacErrorsEclipseNone) : null;
 	}
 	public static class EclipseHasABug extends Excuse {
 		EclipseHasABug(int mismatchType) {
