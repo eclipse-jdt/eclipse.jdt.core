94698d05aca7f775a795ae4fb0eccda02cacc241
Srikanth Sankaran <srikanth.sankaran@advantest.com>
2024-06-14 06:57:03 +0530

    [Sealed classes] Inconsistent type hierarchy on sealed interface when used with other from different level in class hierarchy
    * Regression test for
    https://github.com/eclipse-jdt/eclipse.jdt.core/issues/2108

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/AbstractJavaModelTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/AbstractJavaModelTests.java
@@ -1757,6 +1757,9 @@ public abstract class AbstractJavaModelTests extends SuiteOfTestCases {
 	protected IJavaProject createJava16Project(String name) throws CoreException {
 		return createJava9ProjectWithJREAttributes(name, new String[]{"src"}, null, "16");
 	}
+	protected IJavaProject createJava21Project(String name) throws CoreException {
+		return createJava9ProjectWithJREAttributes(name, new String[]{"src"}, null, "21");
+	}
 	protected IJavaProject createJava9ProjectWithJREAttributes(String name, String[] srcFolders, IClasspathAttribute[] attributes) throws CoreException {
 		return createJava9ProjectWithJREAttributes(name, srcFolders, attributes, "9");
 	}
