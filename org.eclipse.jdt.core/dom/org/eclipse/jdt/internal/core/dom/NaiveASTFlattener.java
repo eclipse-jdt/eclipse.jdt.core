c4e27a85e63da81e1cf3cfa7428d89292d1e7131
twasik <twasik@parasoft.com>
2024-07-01 15:32:14 +0530

    Add the test case along with some changes
    Also-by: Jay Arthanareeswaran <jarthana@in.ibm.com>

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/internal/core/dom/NaiveASTFlattener.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/internal/core/dom/NaiveASTFlattener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2023 IBM Corporation and others.
+ * Copyright (c) 2000, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -92,7 +92,6 @@ public class NaiveASTFlattener extends ASTVisitor {
 	 * Internal synonym for {@link AST#JLS21}. Use to alleviate
 	 * deprecation warnings.
 	 * @deprecated
-	 * @since 3.38
 	 */
 	private static final int JLS21 = AST.JLS21;
 
