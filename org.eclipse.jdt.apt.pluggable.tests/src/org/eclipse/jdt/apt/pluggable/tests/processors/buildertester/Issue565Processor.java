90eafb058297a8e84af50a9bcdb684ff92bd19b7
Kamil Krzywanski <kamilkrzywanski01@gmail.com>
2024-07-13 17:07:59 +0200

    #Issue #565 - initial creation of intersection type

--- org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/processors/buildertester/Issue565Processor.java
+++ org.eclipse.jdt.apt.pluggable.tests/src/org/eclipse/jdt/apt/pluggable/tests/processors/buildertester/Issue565Processor.java
@@ -21,10 +21,8 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.*;
-import javax.lang.model.util.SimpleTypeVisitor6;
+import javax.lang.model.util.SimpleTypeVisitor8;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -57,83 +55,52 @@ public class Issue565Processor extends AbstractProcessor {
 	}
 
 
-	private final TypeVisitor<List<String>, Boolean> keyBuilder = new SimpleTypeVisitor6<List<String>, Boolean>() {
+	private final TypeVisitor<List<String>, Boolean> keyBuilder = new SimpleTypeVisitor8<>() {
+        private final List<String> defaultValue = Collections.singletonList("Object");
 
-		private final List<String> defaultValue = Collections.singletonList("Object");
+        private List<String> visitBase(TypeMirror t) {
+            List<String> rv = new ArrayList<>();
+            String name = t.toString();
+            if (name.contains("<")) {
+                name = name.substring(0, name.indexOf('<'));
+            }
+            rv.add(name);
+            return rv;
+        }
 
+        @Override
+        public List<String> visitDeclared(DeclaredType t, Boolean p) {
+            List<String> rv = visitBase(t);
+            for (TypeMirror arg : t.getTypeArguments()) {
+                if (p) {
+                    rv.addAll(visit(arg, false));
+                } else {
+                    rv.add(arg.toString());
+                }
+            }
+            return rv;
+        }
 
-		private static final Class<?> intersectionTypeClass;
+        @Override
+        public List<String> visitTypeVariable(TypeVariable t, Boolean p) {
+            List<String> rv = visitBase(t);
+            if (t.getUpperBound() != null) {
+                rv.addAll(visit(t.getUpperBound(), p));
+            }
+            if (t.getLowerBound() != null) {
+                rv.addAll(visit(t.getLowerBound(), p));
+            }
+            return rv;
+        }
 
-		private static final Method getBoundsMethod;
+        @Override
+        public List<String> visitIntersection(IntersectionType t, Boolean p) {
+            return t.getBounds().get(0).accept(this, p);
+        }
 
-		static {
-			Class<?> availableClass;
-			Method availableMethod;
-			try {
-				availableClass = Class.forName("javax.lang.model.type.IntersectionType");
-				availableMethod = availableClass.getMethod("getBounds");
-			} catch (Exception e) {
-				// Not using Java 8
-				availableClass = null;
-				availableMethod = null;
-			}
-			intersectionTypeClass = availableClass;
-			getBoundsMethod = availableMethod;
-		}
-
-
-		private List<String> visitBase(TypeMirror t) {
-			List<String> rv = new ArrayList<String>();
-			String name = t.toString();
-			if (name.contains("<")) {
-				name = name.substring(0, name.indexOf('<'));
-			}
-			rv.add(name);
-			return rv;
-		}
-
-		@Override
-		public List<String> visitDeclared(DeclaredType t, Boolean p) {
-			List<String> rv = visitBase(t);
-			for (TypeMirror arg : t.getTypeArguments()) {
-				if (p) {
-					rv.addAll(visit(arg, false));
-				} else {
-					rv.add(arg.toString());
-				}
-			}
-			return rv;
-		}
-
-		@Override
-		public List<String> visitTypeVariable(TypeVariable t, Boolean p) {
-			List<String> rv = visitBase(t);
-			if (t.getUpperBound() != null) {
-				rv.addAll(visit(t.getUpperBound(), p));
-			}
-			if (t.getLowerBound() != null) {
-				rv.addAll(visit(t.getLowerBound(), p));
-			}
-			return rv;
-		}
-
-		@Override
-		public List<String> visitUnknown(TypeMirror t, Boolean p) {
-			if (intersectionTypeClass != null && intersectionTypeClass.isInstance(t)) {
-				try {
-					List<TypeMirror> bounds = (List<TypeMirror>) getBoundsMethod.invoke(t);
-					return bounds.get(0).accept(this, p);
-				} catch (IllegalAccessException | InvocationTargetException e) {
-					throw new RuntimeException(e.getMessage(), e);
-				}
-			} else {
-				return super.visitUnknown(t, p);
-			}
-		}
-
-		@Override
-		public List<String> visitNull(NullType t, Boolean p) {
-			return defaultValue;
-		}
-	};
+        @Override
+        public List<String> visitNull(NullType t, Boolean p) {
+            return defaultValue;
+        }
+    };
 }
