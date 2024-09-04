184e5213c75cda541eca16f72fab11c0e5e91beb
Suby S Surendran <suby.surendran@ibm.com>
2024-09-04 12:07:06 +0530

    Search support for the implicitly declared class

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/RunJavaSearchTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/RunJavaSearchTests.java
@@ -84,6 +84,7 @@ public class RunJavaSearchTests extends junit.framework.TestCase {
 		allClasses.add(JavaSearchNameEnvironmentTest.class);
 		allClasses.add(JavaSearchSuperAfterStatementTests.class);
 		allClasses.add(JavaSearchIssue190Test.class);
+		allClasses.add(JavaSearchImplicitTypeDeclaration.class);
 
 		// Reset forgotten subsets of tests
 		TestCase.TESTS_PREFIX = null;
