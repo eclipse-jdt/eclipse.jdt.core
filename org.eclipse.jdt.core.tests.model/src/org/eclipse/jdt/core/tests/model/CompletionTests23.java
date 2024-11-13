74ec8f9c7953ec048a83b29e9ff7e5bb547375d6
Srikanth Sankaran <131454720+srikanth-sankaran@users.noreply.github.com>
2024-11-13 16:09:52 +0530

    Suboptimal/No code completion suggestions for switching on sealed interfaces(#3302)
    * Fixes https://github.com/eclipse-jdt/eclipse.jdt.core/issues/3300

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests23.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/model/CompletionTests23.java
@@ -30,6 +30,7 @@ static {
 }
 
 private static int DEFAULT_RELEVANCE = R_DEFAULT + R_RESOLVED + R_INTERESTING + R_CASE + R_NON_RESTRICTED;
+private static int keyword_Rel= R_DEFAULT + R_RESOLVED + R_INTERESTING + R_CASE + R_NON_RESTRICTED;
 
 private IJavaProject completion23Project;
 
@@ -408,5 +409,297 @@ public void testMarkdownMethodLink2_qualified2() throws CoreException {
 	assertResults(
 			"length[METHOD_REF]{length(), Ljava.lang.String;, ()I, length, null, "+(DEFAULT_RELEVANCE + R_NON_STATIC)+"}",
 			requestor.getResults());
+}
+
+public void test011() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case     : System.out.println(\"Integer:\" + i);break;\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case ";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("default[KEYWORD]{default, null, null, default, null, "+keyword_Rel+"}\n"
+			+ "null[KEYWORD]{null, null, null, null, null, "+keyword_Rel+"}\n" +
+			"X[TYPE_REF]{X, , LX;, null, null, 72}\n" +
+			"Object[TYPE_REF]{Object, java.lang, Ljava.lang.Object;, null, null, 84}",
+			requestor.getResults());
+
+
+
+}
+public void test012() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case nu    : System.out.println(\"Integer:\" + i);break;\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case nu";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("null[KEYWORD]{null, null, null, null, null, "+keyword_Rel+"}\n" +
+			"NullPointerException[TYPE_REF]{NullPointerException, java.lang, Ljava.lang.NullPointerException;, null, null, 64}\n" +
+			"Number[TYPE_REF]{Number, java.lang, Ljava.lang.Number;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test013() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case de    : System.out.println(\"Integer:\" + i);break;\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case de";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("default[KEYWORD]{default, null, null, default, null, "+keyword_Rel+"}\n" +
+			"Deprecated[TYPE_REF]{Deprecated, java.lang, Ljava.lang.Deprecated;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test014() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case null    : System.out.println(\"Integer:\" + i);break;\n" +
+					"	/*here*/case nu    : System.out.println(\"Integer:\" + i);break;\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "/*here*/case nu";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("NullPointerException[TYPE_REF]{NullPointerException, java.lang, Ljava.lang.NullPointerException;, null, null, 64}\n" +
+			"Number[TYPE_REF]{Number, java.lang, Ljava.lang.Number;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test015() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case default    : System.out.println(\"Integer:\" + i);break;\n" +
+					"	/*here*/case de    : System.out.println(\"Integer:\" + i);break;\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "/*here*/case de";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("Deprecated[TYPE_REF]{Deprecated, java.lang, Ljava.lang.Deprecated;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test016() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case nu    -> System.out.println(\"Integer:\" + i);\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case nu";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("null[KEYWORD]{null, null, null, null, null, "+keyword_Rel+"}\n" +
+			"NullPointerException[TYPE_REF]{NullPointerException, java.lang, Ljava.lang.NullPointerException;, null, null, 64}\n" +
+			"Number[TYPE_REF]{Number, java.lang, Ljava.lang.Number;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test017() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case de    -> System.out.println(\"Integer:\" + i);\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case de";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("default[KEYWORD]{default, null, null, default, null, "+keyword_Rel+"}\n" +
+				"Deprecated[TYPE_REF]{Deprecated, java.lang, Ljava.lang.Deprecated;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test018() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case Integer i    -> System.out.println(\"Integer:\"  +i);\n" +
+					"	case de    -> System.out.println(\"Integer:\" + i);\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case de";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("default[KEYWORD]{default, null, null, default, null, "+keyword_Rel+"}\n" +
+				  "Deprecated[TYPE_REF]{Deprecated, java.lang, Ljava.lang.Deprecated;, null, null, 64}",
+			requestor.getResults());
+
+}
+public void test019() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy(
+			"/Completion23/src/X.java",
+			"public class X {\n" +
+					"public static void main(String[] args) {\n" +
+					"foo(Integer.valueOf(5));\n" +
+					"foo(new Object());\n" +
+					"}\n" +
+					"private static void foo(Object o) {\n" +
+					" switch (o) {\n" +
+					"	case Integer i    -> System.out.println(\"Integer:\"  +i);\n" +
+					"	case n    -> System.out.println(\"Integer:\" + i);\n" +
+					" 	}\n" +
+					"}\n" +
+					"}\n"
+			);
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true);
+	requestor.allowAllRequiredProposals();
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case n";
+	int cursorLocation = str.indexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+	assertResults("null[KEYWORD]{null, null, null, null, null, "+keyword_Rel+"}\n" +
+			"NoClassDefFoundError[TYPE_REF]{NoClassDefFoundError, java.lang, Ljava.lang.NoClassDefFoundError;, null, null, 64}\n" +
+			"NoSuchFieldError[TYPE_REF]{NoSuchFieldError, java.lang, Ljava.lang.NoSuchFieldError;, null, null, 64}\n" +
+			"NoSuchMethodException[TYPE_REF]{NoSuchMethodException, java.lang, Ljava.lang.NoSuchMethodException;, null, null, 64}\n" +
+			"NullPointerException[TYPE_REF]{NullPointerException, java.lang, Ljava.lang.NullPointerException;, null, null, 64}\n" +
+			"Number[TYPE_REF]{Number, java.lang, Ljava.lang.Number;, null, null, 64}",
+			requestor.getResults());
+
+}
+// https://github.com/eclipse-jdt/eclipse.jdt.core/issues/3300
+// Suboptimal/No code completion suggestions for switching on sealed interfaces
+public void testIssue3300() throws JavaModelException {
+	this.workingCopies = new ICompilationUnit[1];
+	this.workingCopies[0] = getWorkingCopy("/Completion23/src/Foo.java",
+			"""
+			public sealed interface Foo {
+				record FooImpl_a(String x) implements Foo {
+				}
+
+				record FooImpl_b(String y, String z) implements Foo {
+				}
+
+				private static Foo getFoo() {
+					return new Foo.FooImpl_b("a", "b");
+				}
+
+				public static void main(String[] args) {
+					Foo foo = getFoo();
+					switch (foo) {
+						case Foo  // offers no completion here.
+					}
+				}
+			}
+				""");
+	CompletionTestsRequestor2 requestor = new CompletionTestsRequestor2(true, true, true, false);
+	String str = this.workingCopies[0].getSource();
+	String completeBehind = "case Foo";
+	int cursorLocation = str.lastIndexOf(completeBehind) + completeBehind.length();
+	this.workingCopies[0].codeComplete(cursorLocation, requestor, this.wcOwner);
+
+	assertResults("Foo.FooImpl_a[TYPE_REF]{FooImpl_a, , LFoo$FooImpl_a;, null, null, null, null, [300, 303], 72}\n" +
+	              "Foo.FooImpl_b[TYPE_REF]{FooImpl_b, , LFoo$FooImpl_b;, null, null, null, null, [300, 303], 72}\n" +
+			      "Foo[TYPE_REF]{Foo, , LFoo;, null, null, null, null, [300, 303], 86}", requestor.getResults());
 }
 }
