9dee5f4e3f8f63661aaca792f44295992a45abc6
Александър Куртаков <akurtakov@gmail.com>
2025-01-09 18:04:13 +0200

    Stop creating jls2 AST in runJLS8Conversion
    This only verifies that "some" AST is being created and is being
    discarded without any further assertions in order to create the JLS8
    one.

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ConverterTestSetup.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ConverterTestSetup.java
@@ -665,26 +665,13 @@ public abstract class ConverterTestSetup extends AbstractASTTests {
 		return result;
 	}
 
-	public ASTNode runJLS8Conversion(ICompilationUnit unit, boolean resolveBindings, boolean checkJLS2) {
-		return runJLS8Conversion(unit, resolveBindings, checkJLS2, false);
+	public ASTNode runJLS8Conversion(ICompilationUnit unit, boolean resolveBindings) {
+		return runJLS8Conversion(unit, resolveBindings, false);
 	}
 
-	/**
-	 * @deprecated references deprecated old AST level
-	 */
-	public ASTNode runJLS8Conversion(ICompilationUnit unit, boolean resolveBindings, boolean checkJLS2, boolean bindingRecovery) {
-
+	public ASTNode runJLS8Conversion(ICompilationUnit unit, boolean resolveBindings, boolean bindingRecovery) {
 		// Create parser
-		ASTParser parser;
-		if (checkJLS2) {
-			parser = ASTParser.newParser(astInternalJLS2());
-			parser.setSource(unit);
-			parser.setResolveBindings(resolveBindings);
-			parser.setBindingsRecovery(bindingRecovery);
-			parser.createAST(null);
-		}
-
-		parser = ASTParser.newParser(AST.JLS8);
+        ASTParser parser = ASTParser.newParser(JLS8_INTERNAL);
 		parser.setSource(unit);
 		parser.setResolveBindings(resolveBindings);
 		parser.setBindingsRecovery(bindingRecovery);
