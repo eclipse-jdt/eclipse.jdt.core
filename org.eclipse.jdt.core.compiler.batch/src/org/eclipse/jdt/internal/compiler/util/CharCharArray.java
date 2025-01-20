dff5823641391fe22ead46f2708bc1d96e9804cd
Jörg Kubitz <joerg.kubitz@ssi-schaefer.com>
2025-01-20 12:30:51 +0100

    simplify2

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/CharCharArray.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/CharCharArray.java
@@ -1,43 +1,11 @@
-/*******************************************************************************
- * Copyright (c) 2025 jkubitz and others.
- *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     jkubitz - initial API and implementation
- *******************************************************************************/
 package org.eclipse.jdt.internal.compiler.util;
 
 import java.util.Arrays;
 
-/**
- * Wrapper around char[][] that can be used as a key in a Map or Set.
- */
-public final record CharCharArray(char[][] key) implements Comparable<CharCharArray> {
+public final record CharCharArray(char[][] key) {
 
 	public CharCharArray(char[][] key) {
 		this.key = key;
-	}
-
-	@Override
-	public int compareTo(CharCharArray other) {
-		int length = Math.min(this.key.length, other.key.length);
-		for (int i = 0; i < length; i++) {
-			int c = Arrays.compare(this.key[i], other.key[i]);
-			if (c != 0) {
-				return c;
-			}
-		}
-		return this.key.length - other.key.length;
-	}
-
-	public char[][] getKey() {
-		return this.key;
 	}
 
 	@Override
@@ -48,16 +16,4 @@ public final record CharCharArray(char[][] key) implements Comparable<CharCharAr
 		return false;
 	}
 
-	@Override
-	public int hashCode() {
-		return Arrays.deepHashCode(this.key);
-	}
-
-	/**
-	 * @return <code>Arrays.toString</code> of the underlying array
-	 */
-	@Override
-	public String toString() {
-		return Arrays.deepToString(this.key);
-	}
 }
