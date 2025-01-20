aba60b29c6d5048793d47faebf3e7f39f6238e73
Jörg Kubitz <joerg.kubitz@ssi-schaefer.com>
2025-01-20 12:29:01 +0100

    Revert "simplify reproducer"
    This reverts commit 960a089d5fe8bf6ddab5cd3e05bd30e4539f5e47.

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/CharCharArray.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/CharCharArray.java
@@ -1,8 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2025 jkubitz and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     jkubitz - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.jdt.internal.compiler.util;
 
 import java.util.Arrays;
 
-public final record CharCharArray(char[][] key){
+/**
+ * Wrapper around char[][] that can be used as a key in a Map or Set.
+ */
+public final record CharCharArray(char[][] key) implements Comparable<CharCharArray> {
+
+	public CharCharArray(char[][] key) {
+		this.key = key;
+	}
+
+	@Override
+	public int compareTo(CharCharArray other) {
+		int length = Math.min(this.key.length, other.key.length);
+		for (int i = 0; i < length; i++) {
+			int c = Arrays.compare(this.key[i], other.key[i]);
+			if (c != 0) {
+				return c;
+			}
+		}
+		return this.key.length - other.key.length;
+	}
+
+	public char[][] getKey() {
+		return this.key;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -12,4 +48,16 @@ public final record CharCharArray(char[][] key){
 		return false;
 	}
 
+	@Override
+	public int hashCode() {
+		return Arrays.deepHashCode(this.key);
+	}
+
+	/**
+	 * @return <code>Arrays.toString</code> of the underlying array
+	 */
+	@Override
+	public String toString() {
+		return Arrays.deepToString(this.key);
+	}
 }
