57ae0e908f9aa809ddd40442d36c8a22b34fb4b8
Jörg Kubitz <joerg.kubitz@ssi-schaefer.com>
2025-01-20 09:08:22 +0100

    Fix builder.State.write internalization
    Using generic Map interface instead of SimpleLookupTable showed up that
    State.write(DataOutputStream) did use Arrays (char[] / char[][]) as key
    in internedRootNames, internedQualifiedNames, internedSimpleNames.
    
    Arrays however do have equals() contract based on identity. Therefore
    the code did not deduplicate anything as intended.
    It normally only did not matter as most char arrays in jdt are already
    weakly deduplicated.

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/CharCharArray.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/util/CharCharArray.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 jkubitz and others.
+ * Copyright (c) 2025 jkubitz and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -9,46 +9,53 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Gunnar Wagenknecht, jkubitz - initial API and implementation
+ *     jkubitz - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.internal.compiler.util;
 
 import java.util.Arrays;
 
 /**
- * Wrapper around char arrays that can be used as a key in a Map or Set.
- * <p>
- * The {@link #hashCode()} and {@link #equals(Object)} method will work with the underlying array using
- * <code>Arrays.hashCode</code> and <code>Arrays.equals</code>.
- * </p>
+ * Wrapper around char[][] that can be used as a key in a Map or Set.
  */
-public final record CharArray(char[] key) implements Comparable<CharArray> {
+public final record CharCharArray(char[][] key) implements Comparable<CharCharArray> {
 
 	@Override
-	public int compareTo(CharArray o) {
+	public int compareTo(CharCharArray other) {
 		// just any technical sort order for Comparable interface used in HashMap https://openjdk.org/jeps/180
-		return Arrays.compare(this.key, o.key);
+		int d = this.key.length - other.key.length;
+		if (d != 0) {
+			return d;
+		}
+		int length = this.key.length;
+		for (int i = 0; i < length; i++) {
+			int c = Arrays.compare(this.key[i], other.key[i]);
+			if (c != 0) {
+				return c;
+			}
+		}
+		return 0;
 	}
 
-	public char[] getKey() {
+	public char[][] getKey() {
 		return this.key;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof CharArray other) {
-			return Arrays.equals(this.key, other.key);
+		if (obj instanceof CharCharArray other) {
+			return Arrays.deepEquals(this.key, other.key);
 		}
 		return false;
 	}
 
 	@Override
 	public int hashCode() {
-		return Arrays.hashCode(this.key);
+		return Arrays.deepHashCode(this.key);
 	}
 
 	@Override
 	public String toString() {
-		return Arrays.toString(this.key);
+		return Arrays.deepToString(this.key);
 	}
 }
