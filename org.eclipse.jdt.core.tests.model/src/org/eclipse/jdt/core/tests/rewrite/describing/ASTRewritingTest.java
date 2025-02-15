9db61282eeb40433caa703fb24afa42236e81a16
Stephan Herrmann <stephan.herrmann@berlin.de>
2025-02-15 19:01:12 +0100

    Fix failures of ASTRewritingRecordDeclarationTest at 24 - test runs at this level only since https://github.com/eclipse-jdt/eclipse.jdt.core/pull/3533

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/rewrite/describing/ASTRewritingTest.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/rewrite/describing/ASTRewritingTest.java
@@ -273,6 +273,14 @@ public class ASTRewritingTest extends AbstractJavaModelTests {
 			this.project1.setOption(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_23);
 			this.project1.setOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_23);
 		}
+		setUpProjectAbove24();
+	}
+	protected void setUpProjectAbove24() throws Exception {
+		if (this.apiLevel == AST_INTERNAL_JLS24) {
+			this.project1.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_24);
+			this.project1.setOption(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_24);
+			this.project1.setOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_24);
+		}
 	}
 
 	protected IJavaProject createProject(String projectName, String complianceVersion) throws CoreException {
