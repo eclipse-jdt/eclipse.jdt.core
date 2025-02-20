25e251d1275fbe4649d244c02f900c958de0efbd
David Thompson <davthomp@redhat.com>
2025-02-20 15:17:10 -0500

    Run search tests using javac
    Uses a similar method to the existing borrowed tests:
    add a new class to the javac test bundle that extends the upstream test suite.
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

---
+++ org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunSearchJavacTests.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2025, Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.jdt.core.tests.javac;
+
+import org.eclipse.jdt.core.tests.model.RunJavaSearchTests;
+
+public class RunSearchJavacTests extends RunJavaSearchTests {
+
+	public RunSearchJavacTests(String name) {
+		super(name);
+	}
+
+}
+
