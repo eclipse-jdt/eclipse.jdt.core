edbb094882cc2fd5069175417b28f6674381f686
Jay Arthanareeswaran <jarthana@in.ibm.com>
2025-04-16 11:33:23 +0530

    Update annotation processors to report errors without throwing exceptions. Due to the recent changes in the RoundDispatcher, these exceptions are trapped and the test framework never gets to see them to be able to report the failures.

--- org.eclipse.jdt.compiler.apt.tests/processors8/org/eclipse/jdt/compiler/apt/tests/processors/elements/RecordElementProcessor.java
+++ org.eclipse.jdt.compiler.apt.tests/processors8/org/eclipse/jdt/compiler/apt/tests/processors/elements/RecordElementProcessor.java
@@ -17,8 +17,6 @@ package org.eclipse.jdt.compiler.apt.tests.processors.elements;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -80,34 +78,12 @@ public class RecordElementProcessor extends BaseElementProcessor {
 					super.reportSuccess();
 				}
 			} catch (AssertionFailedError e) {
-				super.reportError(getExceptionStackTrace(e));
+				super.reportError(e);
 			} catch (Throwable e) {
 				e.printStackTrace();
 			}
 		}
 		return false;
-	}
-
-	private boolean invokeTestMethods(Map<String, String> options) throws Throwable {
-		Method testMethod = null;
-		Set<String> keys = options.keySet();
-		boolean testsFound = false;
-		for (String option : keys) {
-			if (option.startsWith("test")) {
-				try {
-					testMethod = this.getClass().getDeclaredMethod(option, new Class[0]);
-					if (testMethod != null) {
-						testsFound = true;
-						testMethod.invoke(this,  new Object[0]);
-					}
-				} catch (InvocationTargetException e) {
-					throw e.getCause();
-				} catch (Exception e) {
-					super.reportError(getExceptionStackTrace(e));
-				}
-			}
-		}
-		return testsFound;
 	}
 
 	public void testAll() throws AssertionFailedError, IOException {
