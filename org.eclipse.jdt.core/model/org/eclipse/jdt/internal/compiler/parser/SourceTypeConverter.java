a209f0f39ba389b1b99f31f61964d61c292c3005
Srikanth Sankaran <srikanth.sankaran@advantest.com>
2025-05-06 09:26:59 +0530

    additional changes

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/compiler/parser/SourceTypeConverter.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/compiler/parser/SourceTypeConverter.java
@@ -602,21 +602,10 @@ public class SourceTypeConverter extends TypeConverter {
 			initializers = typeInfo.getInitializers();
 			initializerCount = initializers.length;
 		}
-		SourceField[] sourceFields = null;
+		IField[] sourceFields = null;
 		int sourceFieldCount = 0;
 		if ((this.flags & FIELD) != 0) {
-			SourceField[] allFields = typeInfo.getFieldHandles();
-			if (type.isRecord()) {
-				int staticFieldCount = 0;
-				for (int i = 0, length = allFields.length; i < length; i++) {
-					SourceFieldElementInfo elementInfo = (SourceFieldElementInfo) allFields[i].getElementInfo();
-					if ((elementInfo.getModifiers() & ClassFileConstants.AccStatic) != 0)
-						allFields[staticFieldCount++] = allFields[i];
-				}
-				System.arraycopy(allFields, 0, sourceFields = new SourceField[staticFieldCount], 0, staticFieldCount);
-			} else {
-				sourceFields = allFields;
-			}
+			sourceFields = typeHandle.getFields();
 			sourceFieldCount = sourceFields.length;
 		}
 		int length = initializerCount + sourceFieldCount;
@@ -627,7 +616,7 @@ public class SourceTypeConverter extends TypeConverter {
 			}
 			int index = 0;
 			for (int i = initializerCount; i < length; i++) {
-				type.fields[i] = convert(sourceFields[index++], type, compilationResult);
+				type.fields[i] = convert((SourceField) sourceFields[index++], type, compilationResult);
 			}
 		}
 
