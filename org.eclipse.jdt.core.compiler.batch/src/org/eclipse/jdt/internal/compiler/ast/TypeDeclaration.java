6a50cddb1000a403a7e59652bde0d5dfa76b5ecf
Srikanth Sankaran <131454720+srikanth-sankaran@users.noreply.github.com>
2025-07-17 10:50:57 +0530

    ECJ should not warn about missing serialVersionUID for anonymous inner classes(#4210)
    * Fixes https://github.com/eclipse-jdt/eclipse.jdt.core/issues/4209

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
@@ -1262,6 +1262,7 @@ public void resolve() {
 		boolean needSerialVersion =
 						this.scope.compilerOptions().getSeverity(CompilerOptions.MissingSerialVersion) != ProblemSeverities.Ignore
 						&& sourceType.isClass()
+						&& !sourceType.isAnonymousType()
 						&& !sourceType.isRecord()
 						&& sourceType.findSuperTypeOriginatingFrom(TypeIds.T_JavaIoExternalizable, false /*Externalizable is not a class*/) == null
 						&& sourceType.findSuperTypeOriginatingFrom(TypeIds.T_JavaIoSerializable, false /*Serializable is not a class*/) != null;
