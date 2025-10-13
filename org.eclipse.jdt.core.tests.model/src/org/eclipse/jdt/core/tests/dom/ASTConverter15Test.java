4c90daea3ea56e1f79c7b1836d9727921bf5faa3
Srikanth Sankaran <srikanth.sankaran@advantest.com>
2025-10-13 12:49:42 +0530

    Reenable disabled test `org.eclipse.jdt.core.tests.dom.ASTConverter15Test.test0151()`

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverter15Test.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/dom/ASTConverter15Test.java
@@ -4749,7 +4749,7 @@ public class ASTConverter15Test extends ConverterTestSetup {
 
 	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=88548
     @SuppressWarnings("deprecation")
-	public void _2551_test0151() throws CoreException {
+	public void test0151() throws CoreException {
     	this.workingCopy = getWorkingCopy("/Converter15/src/X.java", true/*resolve*/);
     	String contents =
 	   		"public enum X {\n" +
