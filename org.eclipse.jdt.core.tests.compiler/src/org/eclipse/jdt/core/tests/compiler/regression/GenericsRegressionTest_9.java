52e6257bff530c14a3d2d9b30c483e2eedd38e0f
Stephan Herrmann <stephan.herrmann@berlin.de>
2025-10-19 23:02:39 +0200

    Inconsistent inference of nested generic types
    Test for https://github.com/eclipse-jdt/eclipse.jdt.core/issues/1501

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest_9.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/GenericsRegressionTest_9.java
@@ -1718,6 +1718,69 @@ public void testGH4281() {
 		"""
 	});
 }
+public void testGH1501() {
+	runNegativeTest(new String[] {
+			"Test.java",
+			"""
+			import java.util.Map;
+			import java.util.function.Function;
+
+			public class Test {
+			  public Outer<Data> errorInLambda() {
+			    Outer<Data> x = new Outer<>(new InnerImpl<>(Map.of()));
+			    // the above one works but on mouse over InnerImpl produces:
+			    // InnerImpl.InnerImpl<Object>(Map<String, Function<Object, Object>> map)
+			    // should be:
+			    // InnerImpl.InnerImpl<Data>(Map<String, Function<Data, Object>> map)
+
+			    Outer<Data> y = new Outer<>(new InnerImpl<Object>(Map.of()));
+			    // but this one correctly errors with:
+			    // Cannot infer type arguments for Outer<>
+
+			    return new Outer<>(new InnerImpl<>(Map.of("x",
+			        data -> data.getValue() // Eclipse error:
+			        // This lambda expression must return a result of type Object
+			        // javac correctly infers Data to both diamonds here and no errors produced
+			        )));
+			  }
+			}
+
+			class Outer<T> {
+			  final Inner<T> inner;
+
+			  Outer(Inner<T> inner) {
+			      this.inner = inner;
+			  }
+			}
+
+			interface Inner<T> {}
+
+			class InnerImpl<T> implements Inner<T> {
+			  InnerImpl(Map<String, Function<T, Object>> map) {}
+			}
+
+			class Data {
+			  String value;
+
+			  Data(String value) {
+			      this.value = value;
+			  }
+
+			  String getValue() {
+			      return value;
+			  }
+			}
+			"""
+		},
+		"""
+		----------
+		1. ERROR in Test.java (at line 12)
+			Outer<Data> y = new Outer<>(new InnerImpl<Object>(Map.of()));
+			                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		Cannot infer type arguments for Outer<>
+		----------
+		""");
+}
 public static Class<GenericsRegressionTest_9> testClass() {
 	return GenericsRegressionTest_9.class;
 }
