92f949b4458d41cfc5f5ead6032f05fb2eb2f236
Suby S Surendran <suby.surendran@ibm.com>
2025-12-04 22:03:47 +0530

    Compiler fails to generate tagElements for Markdown fences
    Markdown parser fails to generate TagElements after single-line triple
    backtick fence
    
    Fix: https://github.com/eclipse-jdt/eclipse.jdt.core/issues/4609

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/parser/IMarkdownCommentHelper.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/parser/IMarkdownCommentHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 GK Software SE and others.
+ * Copyright (c) 2024, 2025 GK Software SE and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -137,8 +137,14 @@ class MarkdownCommentHelper implements IMarkdownCommentHelper {
 			this.fenceCharCount = 1;
 			return;
 		}
-		if (next != this.fenceChar || previous != next)
+		if (next != this.fenceChar)
 			return;
+		if (previous != next) {
+			if (this.insideFencedCodeBlock) {
+				this.fenceCharCount = 1;
+			}
+			return;
+		}
 		int required = this.insideFencedCodeBlock ? this.fenceLength : 3;
 		if (++this.fenceCharCount == required) {
 			this.insideFencedCodeBlock^=true;
