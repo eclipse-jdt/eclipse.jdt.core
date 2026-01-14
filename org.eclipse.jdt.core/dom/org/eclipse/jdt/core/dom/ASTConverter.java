be6a48e9b848a0f8c5916df452c0a8772d11f02c
Suby S Surendran <suby.surendran@ibm.com>
2026-01-14 12:28:14 +0530

    @code and @literal tags behave differently in Markdown
    The inline code tag and inline literal tag behave differently in
    Markdown
    
    Fix: https://github.com/eclipse-jdt/eclipse.jdt.core/issues/4695

--- org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
+++ org.eclipse.jdt.core/dom/org/eclipse/jdt/core/dom/ASTConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2025 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -2425,7 +2425,11 @@ class ASTConverter {
 							ASTNode prev = fragments.get(0);
 							for (int i = 1; i < fragments.size(); i++) {
 								ASTNode cur = fragments.get(i);
-								if (cur.getStartPosition() <= prev.getStartPosition()) cur.setFlags(ASTNode.MALFORMED);
+								if (cur.getStartPosition() <= prev.getStartPosition())
+									cur.setFlags(ASTNode.MALFORMED);
+								else if ((cur.getStartPosition() + cur.getLength()) == (prev.getStartPosition()
+										+ prev.getLength()))
+									cur.setFlags(ASTNode.MALFORMED);
 								prev = cur;
 							}
 						}
