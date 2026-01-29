f5ed1c168f30b596c30ca5f6140c036a55ea60d9
Srikanth Sankaran <srikanth.sankaran@advantest.com>
2026-01-29 13:34:05 +0530

    Additional test to make sure that PR https://github.com/eclipse-jdt/eclipse.jdt.core/pull/4554 does not inadvertently mask problems (See https://github.com/eclipse-jdt/eclipse.jdt.core/pull/4740#issuecomment-3746960540)

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/NullTypeAnnotationTest.java
@@ -27,6 +27,7 @@ import java.util.Map;
 import junit.framework.Test;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.tests.compiler.regression.AbstractRegressionTest.JavacTestOptions.Excuse;
+import org.eclipse.jdt.internal.compiler.ast.TypeDeclaration;
 import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
 import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 
@@ -19365,6 +19366,14 @@ public void testGH2158() {
 	runner.customOptions = getCompilerOptions();
 	runner.classLibraries = this.LIBS;
 	runner.runConformTest();
+
+	// challenge other part of the fix:
+	TypeDeclaration.TESTING_GH_2158 = true;
+	try {
+		runner.runConformTest();
+	} finally {
+		TypeDeclaration.TESTING_GH_2158 = false;
+	}
 }
 public void testGH2325() {
 	Runner runner = new Runner();
