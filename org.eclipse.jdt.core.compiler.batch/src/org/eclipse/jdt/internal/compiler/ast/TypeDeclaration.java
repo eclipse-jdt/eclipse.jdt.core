78ef5fc5afbff5f37603bc38060908c8930d9989
coehlrich <coehlrich@users.noreply.github.com>
2026-02-13 04:59:25 +1300

    Fix compile error with final field assignment in constructor with prologue in local class in constructor (#4701)
    * drop flow info from enclosing class when analysing nested class
    
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.core/issues/4700
    ---------
    Co-authored-by: Stephan Herrmann <stephan.herrmann@berlin.de>

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/TypeDeclaration.java
@@ -264,7 +264,7 @@ public FlowInfo analyseCode(BlockScope currentScope, FlowContext flowContext, Fl
 		}
 		manageEnclosingInstanceAccessIfNecessary(currentScope, flowInfo);
 		updateMaxFieldCount(); // propagate down the max field count
-		internalAnalyseCode(flowContext, flowInfo);
+		internalAnalyseCode(flowContext, flowInfo.unconditionalFieldLessCopy()); // discards info about fields of enclosing classes
 	} catch (AbortType e) {
 		this.ignoreFurtherInvestigation = true;
 	}
@@ -300,7 +300,7 @@ public void analyseCode(ClassScope currentScope, FlowContext flowContext, FlowIn
 		}
 		manageEnclosingInstanceAccessIfNecessary(currentScope, flowInfo);
 		updateMaxFieldCount(); // propagate down the max field count
-		internalAnalyseCode(flowContext, flowInfo);
+		internalAnalyseCode(flowContext, flowInfo.unconditionalFieldLessCopy()); // discards info about fields of enclosing classes
 	} catch (AbortType e) {
 		this.ignoreFurtherInvestigation = true;
 	}
@@ -744,8 +744,8 @@ private void internalAnalyseCode(FlowContext flowContext, FlowInfo flowInfo) {
 	InitializationFlowContext initializerContext = new InitializationFlowContext(parentContext, this, flowInfo, flowContext, this.initializerScope);
 	// no static initializer in local classes, thus no need to set parent:
 	InitializationFlowContext staticInitializerContext = new InitializationFlowContext(null, this, flowInfo, flowContext, this.staticInitializerScope);
-	FlowInfo nonStaticFieldInfo = flowInfo.unconditionalFieldLessCopy();	// discards info about fields of inclosing classes
-	FlowInfo staticFieldInfo = flowInfo.unconditionalFieldLessCopy();
+	FlowInfo nonStaticFieldInfo = flowInfo.copy();
+	FlowInfo staticFieldInfo = flowInfo.copy();
 
 	if (JavaFeature.FLEXIBLE_CONSTRUCTOR_BODIES.isSupported(this.scope.compilerOptions())) {
 		if (this.methods != null) {
@@ -856,7 +856,7 @@ private void internalAnalyseCode(FlowContext flowContext, FlowInfo flowInfo) {
 		}
 	}
 	if (this.methods != null) {
-		UnconditionalFlowInfo outerInfo = flowInfo.unconditionalFieldLessCopy();
+		FlowInfo outerInfo = flowInfo.copy();
 		if (nonStaticFieldInfo instanceof UnconditionalDualFlowInfo udfi) {
 			nonStaticFieldInfo = udfi.getMainInits(); // drop info from prologues
 		} else if (nonStaticFieldInfo instanceof DualFlowInfo dfi) {
