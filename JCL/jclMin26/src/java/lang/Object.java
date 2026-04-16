e05fe595da4e75851aac269edc93912c0a13cd41
Suby S Surendran <suby.surendran@ibm.com>
2026-04-16 21:17:31 +0530

    [Markdown] Snippet tag failed to generate javadoc
    The @snippet tag content was not being rendered in the Javadoc view
    because plain snippet text was not captured during parsing. This change
    ensures snippet content is properly extracted and displayed, similar to
    standard Javadoc elements.
    
    Fix: https://github.com/eclipse-jdt/eclipse.jdt.core/issues/4968

---
+++ JCL/jclMin26/src/java/lang/Object.java
@@ -0,0 +1,34 @@
+package java.lang;
+
+public class Object {
+
+public Object() {
+}
+protected Object clone() throws CloneNotSupportedException {
+	return null;
+}
+public boolean equals (Object obj) {
+	return false;
+}
+protected void finalize () throws Throwable {
+}
+public final Class<? extends Object> getClass() {
+	return null;
+}
+public int hashCode() {
+	return -1;
+}
+public final void notify() throws IllegalMonitorStateException {
+}
+public final void notifyAll() throws IllegalMonitorStateException {
+}
+public String toString () {
+	return null;
+}
+public final void wait () throws IllegalMonitorStateException, InterruptedException {
+}
+public final void wait (long millis) throws IllegalMonitorStateException, InterruptedException {
+}
+public final void wait (long millis, int nanos) throws IllegalMonitorStateException, InterruptedException {
+}
+}
---
+++ JCL/jclMin26/src/java/lang/RuntimeException.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+public class RuntimeException extends Exception {
+
+	public RuntimeException(java.lang.String s) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RuntimeException() {
+		// TODO Auto-generated constructor stub
+	}
+}
