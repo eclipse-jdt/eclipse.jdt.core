3da30e453c95b2d3ffe825cc877eaff3d7e4309a
Manoj N Palat <manoj.palat@in.ibm.com>
2026-04-23 10:36:21 +0200

    fixes #5024 - Disassembler omitting to show primitive data types in BootStrap methods

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/PrimitiveInPatternsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/PrimitiveInPatternsTest.java
@@ -30,7 +30,7 @@ public class PrimitiveInPatternsTest extends AbstractRegressionTest9 {
 	static {
 //		TESTS_NUMBERS = new int [] { 1 };
 //		TESTS_RANGE = new int[] { 1, -1 };
-//		TESTS_NAMES = new String[] { "testSwitchPrimitiveboolean_01" };
+//		TESTS_NAMES = new String[] { "testSwitchPrimitiveboolean_04" };
 	}
 	private String extraLibPath;
 	public static Class<?> testClass() {
@@ -7536,6 +7536,81 @@ public class PrimitiveInPatternsTest extends AbstractRegressionTest9 {
 			"100\n"+
 			"200");
 		String expectedOutput = "invokedynamic 1 typeSwitch(boolean, int)";
+		verifyClassFile(expectedOutput, "X.class", ClassFileBytesDisassembler.SYSTEM);
+	}
+
+	public void testSwitchPrimitiveboolean_02() throws IOException, ClassFormatException {
+		runConformTest(new String[] { "X.java",
+				"""
+				public class X {
+					public static int primitiveSwitch(float f) {
+						return switch (f) {
+						case 1.0f -> 100;
+						//		case 0.999999999f -> 200;
+						default -> 300;
+						};
+					}
+
+					public static void main(String[] args) {
+						System.out.println(primitiveSwitch(1.0f));
+
+					}
+				}
+				"""
+			},
+			"100");
+		String expectedOutput =
+		"	Method arguments:\n" +
+		"		#50 1.0\n";
+		verifyClassFile(expectedOutput, "X.class", ClassFileBytesDisassembler.SYSTEM);
+	}
+	public void testSwitchPrimitiveboolean_03() throws IOException, ClassFormatException {
+		runConformTest(new String[] { "X.java",
+				"""
+				public class X {
+					public static int primitiveSwitch(long l) {
+						return switch (l) {
+						case 10L -> 100;
+						default -> 300;
+						};
+					}
+
+					public static void main(String[] args) {
+						System.out.println(primitiveSwitch(10L));
+
+					}
+				}
+				"""
+			},
+			"100");
+		String expectedOutput =
+		"	Method arguments:\n" +
+		"		#31 10\n";
+		verifyClassFile(expectedOutput, "X.class", ClassFileBytesDisassembler.SYSTEM);
+	}
+
+	public void testSwitchPrimitiveboolean_04() throws IOException, ClassFormatException {
+		runConformTest(new String[] { "X.java",
+				"""
+				public class X {
+					public static int primitiveSwitch(double d) {
+						return switch (d) {
+						case 10.0 -> 100;
+						default -> 300;
+						};
+					}
+
+					public static void main(String[] args) {
+						System.out.println(primitiveSwitch(10.0));
+
+					}
+				}
+				"""
+			},
+			"100");
+		String expectedOutput =
+		"	Method arguments:\n" +
+		"		#31 10.0\n";
 		verifyClassFile(expectedOutput, "X.class", ClassFileBytesDisassembler.SYSTEM);
 	}
 
