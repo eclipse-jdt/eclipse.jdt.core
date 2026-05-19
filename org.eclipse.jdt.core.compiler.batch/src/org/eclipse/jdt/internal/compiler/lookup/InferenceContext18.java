dab1ff61b5a350ed871bce026133ad2a64519bde
Stephan Herrmann <stephan.herrmann@berlin.de>
2026-05-19 18:18:56 +0200

    Revert part of #4735: ARGUMENT_CONSTRAINTS_ARE_SOFT is back to false as recommended

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/lookup/InferenceContext18.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/lookup/InferenceContext18.java
@@ -106,12 +106,11 @@ public class InferenceContext18 {
 
 	/**
 	 * NON-JLS: Detail flag to control the extent of {@link #SIMULATE_BUG_JDK_8026527}.
-	 * A setting of 'false' would implement the advice from http://mail.openjdk.java.net/pipermail/lambda-spec-experts/2013-December/000447.html
+	 * A setting of 'false' implements the advice from http://mail.openjdk.java.net/pipermail/lambda-spec-experts/2013-December/000447.html
 	 * i.e., raw types are not considered as compatible in constraints/bounds derived from invocation arguments,
 	 * but only for constraints derived from type variable bounds.
-	 * NON-JLS: Unfortunately, 'true' is required by GenericsRegressionTest.test434118()
 	 */
-	static final boolean ARGUMENT_CONSTRAINTS_ARE_SOFT = true;
+	static final boolean ARGUMENT_CONSTRAINTS_ARE_SOFT = false;
 
 	// --- Main State of the Inference: ---
 
