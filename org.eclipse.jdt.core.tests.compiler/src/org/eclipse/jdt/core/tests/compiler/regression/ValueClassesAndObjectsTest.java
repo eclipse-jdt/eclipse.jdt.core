9108e43d241365d5b329cb6ef1ecd6c3ddaccb63
Manoj Palat <manoj.palat@in.ibm.com>
2026-06-18 11:19:43 +0530

    Further Refactorings

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ValueClassesAndObjectsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ValueClassesAndObjectsTest.java
@@ -20,7 +20,6 @@ import org.eclipse.jdt.core.tests.util.PreviewTest;
 
 @PreviewTest
 public class ValueClassesAndObjectsTest extends AbstractRegressionTestCommon {
-	private static final JavacTestOptions JAVAC_OPTIONS = new JavacTestOptions("--enable-preview -source 26");
 	static {
 //		TESTS_NUMBERS = new int [] { 1 };
 //		TESTS_RANGE = new int[] { 1, -1 };
@@ -49,7 +48,7 @@ public class ValueClassesAndObjectsTest extends AbstractRegressionTestCommon {
 //
 //	@Override
 //	protected JavacTestOptions getJavacTestOptions() {
-//		return JAVAC_OPTIONS;
+//		return getPreviewJavacTestOptions();
 //	}
 	// =================================================
 	// https://cr.openjdk.org/~dlsmith/jep401/latest/
