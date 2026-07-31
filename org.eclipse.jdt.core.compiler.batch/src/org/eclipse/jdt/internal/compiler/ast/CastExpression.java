5568ae36755ff4f0d1f915c2deafdb61fbe1b6a1
Stephan Herrmann <stephan.herrmann@berlin.de>
2026-07-31 15:05:08 +0200

    Missing unchecked cast warning in polymorphic method call (#5260)
    + Suppress CheckCast after polymorphic method call
    
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.core/pull/5256

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/CastExpression.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/CastExpression.java
@@ -655,10 +655,12 @@ public TypeBinding resolveType(BlockScope scope) {
 	}
 	TypeBinding expressionType = this.expression.resolveType(scope);
 	TypeBinding originalExpressionType = expressionType;
+	boolean isPolymorphicMethod = false;
 	if (this.expression instanceof MessageSend) {
 		MessageSend messageSend = (MessageSend) this.expression;
 		MethodBinding methodBinding = messageSend.binding;
 		if (methodBinding != null && methodBinding.isPolymorphic()) {
+			isPolymorphicMethod = true;
 			messageSend.binding = scope.environment().updatePolymorphicMethodReturnType((PolymorphicMethodBinding) methodBinding, castType);
 			if (TypeBinding.notEquals(expressionType, castType)) {
 				expressionType = castType;
@@ -681,6 +683,8 @@ public TypeBinding resolveType(BlockScope scope) {
 			}
 			boolean isLegal = checkCastTypesCompatibility(scope, castType, originalExpressionType, this.expression, true);
 			if (isLegal) {
+				if (isPolymorphicMethod)
+					this.bits &= ~ASTNode.GenerateCheckcast;
 				this.expression.computeConversion(scope, castType, expressionType);
 				if ((this.bits & ASTNode.UnsafeCast) != 0) { // unsafe cast
 					if (scope.compilerOptions().reportUnavoidableGenericTypeProblems
