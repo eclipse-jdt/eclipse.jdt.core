8d8210115ccfa0639c3912711f9f3234eea9031b
Srikanth Sankaran <131454720+srikanth-sankaran@users.noreply.github.com>
2026-08-18 16:16:02 +0530

    * Hook up Value classes testing properly. (#5294)
    * Hook up Value classes testing properly
    * Various minor edits to copyrights and such.
    * Use a test style that will allow us to compare with javac mode better

--- org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ValueClassesAndObjectsTest.java
+++ org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/regression/ValueClassesAndObjectsTest.java
@@ -15,8 +15,10 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.core.tests.compiler.regression;
+import java.util.Map;
 import junit.framework.Test;
 import org.eclipse.jdt.core.tests.util.PreviewTest;
+import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 
 @PreviewTest
 public class ValueClassesAndObjectsTest extends AbstractRegressionTestCommon {
@@ -29,27 +31,58 @@ public class ValueClassesAndObjectsTest extends AbstractRegressionTestCommon {
 		return ValueClassesAndObjectsTest.class;
 	}
 	public static Test suite() {
-		return buildMinimalComplianceTestSuite(testClass(), F_26);
+		return buildMinimalComplianceTestSuite(testClass(), F_28);
 	}
 	public ValueClassesAndObjectsTest(String testName) {
 		super(testName);
 	}
+
+	private static final JavacTestOptions JAVAC_OPTIONS = new JavacTestOptions("--enable-preview -source 28");
+	private static final String[] VMARGS = new String[] {"--enable-preview"};
+
+	// Enables the tests to run individually
+	protected Map<String, String> getCompilerOptions(boolean preview) {
+		Map<String, String> defaultOptions = super.getCompilerOptions();
+		defaultOptions.put(CompilerOptions.OPTION_Compliance, CompilerOptions.VERSION_28);
+		defaultOptions.put(CompilerOptions.OPTION_Source, CompilerOptions.VERSION_28);
+		defaultOptions.put(CompilerOptions.OPTION_TargetPlatform, CompilerOptions.VERSION_28);
+		defaultOptions.put(CompilerOptions.OPTION_EnablePreviews, preview ? CompilerOptions.ENABLED : CompilerOptions.DISABLED);
+		defaultOptions.put(CompilerOptions.OPTION_ReportPreviewFeatures, CompilerOptions.WARNING);
+		return defaultOptions;
+	}
+
+	protected void runNegativeTest(String[] testFiles, String expectedCompilerLog) {
+		Map<String, String> customOptions = getCompilerOptions(true);
+		Runner runner = new Runner();
+		runner.testFiles = testFiles;
+		runner.expectedCompilerLog = expectedCompilerLog;
+		runner.javacTestOptions = JAVAC_OPTIONS;
+		runner.customOptions = customOptions;
+		runner.expectedJavacOutputString = null;
+		runner.runNegativeTest();
+	}
+
+	@Override
+	protected void runConformTest(String[] testFiles, String expectedOutput) {
+		runConformTest(testFiles, expectedOutput, getCompilerOptions(true), VMARGS, JAVAC_OPTIONS);
+	}
+
 	// ========= OPT-IN to run.javac mode: ===========
-//	@Override
-//	protected void setUp() throws Exception {
-//		this.runJavacOptIn = true;
-//		super.setUp();
-//	}
-//	@Override
-//	protected void tearDown() throws Exception {
-//		super.tearDown();
-//		this.runJavacOptIn = false; // do it last, so super can still clean up
-//	}
-//
-//	@Override
-//	protected JavacTestOptions getJavacTestOptions() {
-//		return JAVAC_OPTIONS;
-//	}
+	@Override
+	protected void setUp() throws Exception {
+		this.runJavacOptIn = true;
+		super.setUp();
+	}
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		this.runJavacOptIn = false; // do it last, so super can still clean up
+	}
+
+	@Override
+	protected JavacTestOptions getJavacTestOptions() {
+		return JAVAC_OPTIONS;
+	}
 	// =================================================
 	// https://cr.openjdk.org/~dlsmith/jep401/latest/
   public void testValueTypes_001() {
@@ -68,25 +101,20 @@ public class ValueClassesAndObjectsTest extends AbstractRegressionTestCommon {
 			"1. ERROR in X.java (at line 6)\n" +
 			"	class value {}\n" +
 			"	      ^^^^^\n" +
-			"\'value\' is not a valid type name; it is a restricted identifier and not allowed as a type identifier in Java 26\n" +
+			"\'value\' is not a valid type name; it is a restricted identifier and not allowed as a type identifier in Java 28\n" +
 			"----------\n");
 	}
   public void testValueTypes_002() {
-		runNegativeTest(new String[] {
+		runConformTest(new String[] {
 			"X.java",
 				"""
 				 public value class X {
 				    public static void main(String[] args) {
-				    	Zork();
+				    	System.out.println("Ok!");
 					}
 				}
 			"""
 			},
-			"----------\n" +
-			"1. ERROR in X.java (at line 3)\n" +
-			"	Zork();\n" +
-			"	^^^^\n" +
-			"The method Zork() is undefined for the type X\n" +
-			"----------\n");
+			"Ok!");
 	}
  }
