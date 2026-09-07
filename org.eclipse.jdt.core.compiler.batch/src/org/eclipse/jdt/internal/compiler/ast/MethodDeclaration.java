65f305a4d854ebdde939ef9c5df891ebaa45c6cd
srikanth.sankaran <srikanth.sankaran@advantest.com>
2026-09-07 12:31:48 +0530

    Incremental restructuring of constructor flow analysis
    * Declutter ConstructorDeclaration.analyzeCode() by moving unused
    constructor checks and unused type variables checks into
    separate methods
    
    * Fix broken nullity tracking inside compact constructors
    
    * Fixes https://github.com/eclipse-jdt/eclipse.jdt.core/issues/5369

--- org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/MethodDeclaration.java
+++ org.eclipse.jdt.core.compiler.batch/src/org/eclipse/jdt/internal/compiler/ast/MethodDeclaration.java
@@ -104,7 +104,7 @@ public class MethodDeclaration extends AbstractMethodDeclaration {
 					FlowInfo.DEAD_END);
 
 			// nullity, owning and mark as assigned
-			analyseArguments(classScope.environment(), flowInfo, flowContext, this.arguments, this.binding);
+			analyseArguments(this.scope, flowInfo, flowContext, this.arguments, this.binding);
 
 			BiPredicate<TypeBinding, ReferenceBinding> condition = (argType, declClass) -> {
 				ReferenceBinding enclosingType = argType.enclosingType();
