4e94b4c420a84932df58b2988343aa5f8569a833
Jay Arthanareeswaran <jarthana@in.ibm.com>
2026-09-08 11:58:21 +0530

    Workspace compliance level is not properly "inherited" by projects (#5372)
    The --release option should default to workspace setting if not set
    
    Co-authored-by: Stephan Herrmann <stephan.herrmann@berlin.de>

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/builder/NameEnvironment.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/builder/NameEnvironment.java
@@ -145,7 +145,7 @@ private void computeClasspathLocations(
 		isReleaseEnabled = true;
 	} else {
 		compliance = javaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true);
-		isReleaseEnabled = JavaCore.ENABLED.equals(javaProject.getOption(JavaCore.COMPILER_RELEASE, false));
+		isReleaseEnabled = JavaCore.ENABLED.equals(javaProject.getOption(JavaCore.COMPILER_RELEASE, true));
 	}
 	if (CompilerOptions.versionToJdkLevel(compliance) >= ClassFileConstants.JDK9) {
 		moduleEntries = new LinkedHashMap<>(classpathEntries.length);
