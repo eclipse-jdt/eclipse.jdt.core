6b777ed704d1a0e00eb87f63ec9042b01b0acd6f
Carsten Hammer <carsten.hammer@t-online.de>
2026-09-11 10:16:48 +0200

    Strengthen formatter region regression coverage

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterRegionTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterRegionTests.java
@@ -18,6 +18,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
 import org.eclipse.jdt.internal.formatter.DefaultCodeFormatter;
+import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Region;
 import org.eclipse.text.edits.TextEdit;
@@ -39,15 +40,44 @@ public class FormatterRegionTests extends TestCase {
 	 * region used to make the formatter pass {@code -1} to
 	 * {@code TokenManager.countLineBreaksBetween()}.
 	 */
-	public void testEmptyFirstRegionAtDocumentStart() {
+	public void testEmptyFirstRegionAtDocumentStart() throws Exception {
 		String source = "\nclass A {\n\tvoid foo() {\n\t}\n}\n";
-		IRegion[] regions = {
-				new Region(0, 0),
-				new Region(1, source.length() - 1)
-		};
+		assertEmptyLeadingRegionIsNeutral(source, 1, "\n");
+	}
 
-		TextEdit edit = new DefaultCodeFormatter().format(CodeFormatter.K_COMPILATION_UNIT, source, regions, 0, "\n");
+	public void testEmptyFirstRegionAtDocumentStartWithCrLf() throws Exception {
+		String source = "\r\nclass A {\r\n\tvoid foo() {\r\n\t}\r\n}\r\n";
+		assertEmptyLeadingRegionIsNeutral(source, 2, "\r\n");
+	}
 
+	public void testSingleEmptyRegionAtDocumentStartDoesNotChangeSource() throws Exception {
+		String source = "\nclass A {\n\tvoid foo() {\n\t}\n}\n";
+
+		assertEquals(source, formatAndApply(source, new IRegion[] { new Region(0, 0) }, "\n"));
+	}
+
+	public void testSingleEmptyRegionAfterDocumentStartDoesNotChangeSource() throws Exception {
+		String source = "class A {\n\tvoid foo() {\n\t}\n}\n";
+		int offset = source.indexOf("void");
+
+		assertTrue(offset > 0);
+		assertEquals(source, formatAndApply(source, new IRegion[] { new Region(offset, 0) }, "\n"));
+	}
+
+	private void assertEmptyLeadingRegionIsNeutral(String source, int nextRegionOffset, String lineSeparator) throws Exception {
+		IRegion followingRegion = new Region(nextRegionOffset, source.length() - nextRegionOffset);
+		String expected = formatAndApply(source, new IRegion[] { followingRegion }, lineSeparator);
+		String actual = formatAndApply(source, new IRegion[] { new Region(0, 0), followingRegion }, lineSeparator);
+
+		assertEquals(expected, actual);
+		assertTrue(actual.startsWith(lineSeparator));
+	}
+
+	private String formatAndApply(String source, IRegion[] regions, String lineSeparator) throws Exception {
+		TextEdit edit = new DefaultCodeFormatter().format(CodeFormatter.K_COMPILATION_UNIT, source, regions, 0, lineSeparator);
 		assertNotNull(edit);
+		Document document = new Document(source);
+		edit.apply(document);
+		return document.get();
 	}
 }
